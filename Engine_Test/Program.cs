4b51f5f61a47f5a034bfa3f9d32f0815c97b7294
adecler <arnaud.declercq@burohappold.com>
2016-06-16 15:47:03 +0100

    Link to Mongo Database

--- Engine_Test/Program.cs
+++ Engine_Test/Program.cs
@@ -4,14 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BHoM.Geometry;
+using BHoM.Global;
 using BHoM.Generic;
 using BHoM_Engine.Graph;
+using BHoM.Structural;
 
 namespace Engine_Test
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            TestMongo();
+
+            Console.Read();
+        }
+
+        static void TestMongo()
+        {
+            // Create a fake project
+            List<Node> nodes = new List<Node>();
+            for (int i = 0; i < 10; i++)
+                nodes.Add(new Node(i, 2, 3));
+
+            List<Bar> bars = new List<Bar>();
+            for (int i = 1; i < 10; i++)
+                bars.Add(new Bar(nodes[i - 1], nodes[i]));
+
+            var project = Project.ActiveProject;
+            foreach (Node node in nodes)
+                project.AddObject(node);
+            foreach (Bar bar in bars)
+                project.AddObject(bar);
+
+            // Create database
+            var mongo = new BHoM_Engine.Databases.Mongo.MongoLink();
+            Console.WriteLine("Database link created");
+
+            // Add Objects to the dtabase
+            //mongo.SaveObjects(project.Objects);
+            //Console.WriteLine("Objects added to the database");
+
+            // Get all object from database
+            IEnumerable<BHoM.Global.BHoMObject> objects = mongo.GetObjects("{}");
+            Console.WriteLine("Objects obtained from the database: {0}", objects.Count());
+        }
+
+        static void TestGraph()
         {
             List<Point> points = new List<Point>();
             points.Add(new Point(0, 0, 0));
@@ -23,9 +62,9 @@ namespace Engine_Test
             Graph<Point> graph = new Graph<Point>();
             List<GraphNode<Point>> nodes = new List<GraphNode<Point>>();
             foreach (Point pt in points)
-                nodes.Add(graph.AddNode(pt));  
+                nodes.Add(graph.AddNode(pt));
             for (int i = 1; i < points.Count; i++)
-                graph.AddUndirectedLink(points[i - 1], points[i], points[i-1].DistanceTo(points[i]));
+                graph.AddUndirectedLink(points[i - 1], points[i], points[i - 1].DistanceTo(points[i]));
             graph.AddUndirectedLink(points[1], points[3], points[1].DistanceTo(points[3]));
             graph.AddUndirectedLink(points[2], points[4], points[2].DistanceTo(points[4]));
 
