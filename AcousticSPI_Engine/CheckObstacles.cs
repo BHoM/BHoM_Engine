8ec58581d2c452cb3b58f9ce15c09849541ddc08
edupignatelli <edu.pignatelli@gmail.com>
2017-05-07 17:02:21 +0100

    Fixing access levels

---
+++ AcousticSPI_Engine/CheckObstacles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHoM.Base;
+using BHoM.Geometry;
+using BHA = BHoM.Acoustic;
+
+namespace AcousticSPI_Engine
+{
+    class CheckObstacles : BHoMObject
+    {
+        public List<BHA.Ray> CheckObstacles(List<BHA.Ray>, List<BHA.Panel>)
+        {
+
+        }
+    }
+}
---
+++ AcousticSPI_Engine/DirectSound.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHoM.Base;
+using BHoM.Geometry;
+using BHoM.Acoustic;
+
+namespace AcousticSPI_Engine
+{
+    class DirectSound
+    {
+        public List<Acoustic.Ray> DirectSound(List<Acoustic.Speaker> sources, List<Acoustic.Receiver> targets, List<Acoustic.Panel> surfaces)
+        {
+
+        }
+    }
+}
