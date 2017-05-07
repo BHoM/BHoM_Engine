cd63d81cfbd6b39c4d949e44a5db79dcf3d46c9b
epignatelli <eduardo.pignatelli@burohappold.com>
2017-05-07 19:56:58 +0100

    Added Obstacles check and DirectSound Solver

---
+++ AcousticSPI_Engine/CheckObstacles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHoM.Base;
+using BHoM.Geometry;
+using BHoM.Acoustic;
+
+namespace AcousticSPI_Engine
+{
+    public static class CheckObstacles
+    {
+
+        /// <summary>
+        /// Checks for obstacles in rays path. 
+        /// </summary>
+        /// <param name="rays">Acoustic rays list</param>
+        /// <param name="surfaces">Acoustic surface list</param>
+        /// <param name="ClearRays">Set true to output clear rays, false if blind rays.</param>
+        /// <param name="tol">Default tolerance to 0.00001. Beware of global unit setup.</param>
+        /// <returns>Returns a new list of filtered rays, either clear or blind ones.</returns>
+        public static List<Ray> Check(List<Ray> rays, List<Panel> surfaces, bool ClearRays = true, double tol = 0.00001)
+        {
+            List<Ray> filteredRays = new List<Ray>();
+            for (int i = 0; i<rays.Count; i++)                   //foreach ray
+            {
+                List<bool> checker = new List<bool>();
+                for (int j = 0; j < surfaces.Count; i++ )       //foreach surface
+                {
+                    // Write Mesh-Polyline (or Mesh-Curve) Intersection and replace when done, due to performance.
+                    if (Intersect.PlaneCurve(surfaces[j].mPlane(), rays[i].Path, tol).Count == 0)       // if ray hits a surface
+                        checker.Add(true);
+                }
+                if (ClearRays && checker.Any())     //if rays hits any surface and output is ClearRays
+                    filteredRays.Add(rays[i]);
+                else if (!ClearRays && !checker.Any())
+                    filteredRays.Add(rays[i]);
+            } return filteredRays;
+        }
+
+
+    }
+}
---
+++ AcousticSPI_Engine/DirectSound.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BHoM.Geometry;
+using BHoM.Acoustic;
+
+
+namespace AcousticSPI_Engine
+{
+    public static class DirectSound
+    {
+        #region Methods
+
+        /// <summary>
+        /// Performs a Direct Sound calculation with obstacles check.
+        /// </summary>
+        /// <param name="sources">BHoM acoustic Speaker</param>
+        /// <param name="targets">BHoM acoustic Receivers</param>
+        /// <param name="surfaces">BHoM acoustic Panel</param>
+        /// <returns>Returns a list of BHoM Acoustic Rays</returns>
+        public static List<Ray> Solve(List<Speaker> sources, List<Receiver> targets, List<Panel> surfaces)
+        {
+            List<Ray> rays = new List<Ray>();
+            for (int i = 0; i < sources.Count; i++)
+            {
+                for (int j = 0; j < targets.Count; i++)
+                {
+                    List<Point> rayPts = new List<Point>() { sources[i].Position, targets[j].Position };
+                    Polyline path = new Polyline(rayPts);
+                    Ray ray = new BHoM.Acoustic.Ray(path, i.ToString(), j.ToString());
+                    rays.Add(ray);
+                }
+            }
+            return CheckObstacles.Check(rays, surfaces);
+        }
+
+        #endregion
+    }
+}
