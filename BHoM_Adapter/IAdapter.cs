eb71fb545074984abf2014f5f64401bf973488b6
adecler <arnaud.declercq@burohappold.com>
2017-07-25 17:14:58 +0100

    Adding definition of IAdapter

--- BHoM_Adapter/IAdapter.cs
+++ BHoM_Adapter/IAdapter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BH.Adapter.Queries;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,16 @@ namespace BH.Adapter
 {
     public interface IAdapter
     {
+        bool Push(IEnumerable<object> objects, string key = "", Dictionary<string, string> config = null);
 
+        IList Pull(IEnumerable<IQuery> query, Dictionary<string, string> config = null);
+
+        int Update(FilterQuery filter, Dictionary<string, object> changes, Dictionary<string, string> config = null);
+
+        int Delete(FilterQuery filter, Dictionary<string, string> config = null);
+
+        bool Execute(string command, Dictionary<string, object> parameters = null, Dictionary<string, string> config = null);
+
+        List<string> ErrorLog { get; set; }
     }
 }
