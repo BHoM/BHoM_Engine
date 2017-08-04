efff0a2869bfaf473bc7a77872a204f4b89bd693
IsakNaslund <isak.naslund@burohappold.com>
2017-08-04 15:39:40 +0200

    Added comparers
    Added NameComaprer for BHoMObject
    Added NameOrToString comparer for bhomobject
    Added Distancecomparer for Node

---
+++ BHoM_Engine/Base/Comparers/BHoMObjectNameComparer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.Engine.Base
+{
+    public class BHoMObjectNameComparer : IEqualityComparer<BHoMObject>
+    {
+        public bool Equals(BHoMObject x, BHoMObject y)
+        {
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            //Check if the GUIDs are the same
+            if (x.BHoM_Guid == y.BHoM_Guid)
+                return true;
+
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode(BHoMObject obj)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(obj, null)) return 0;
+
+            return obj.Name == null ? 0 : obj.Name.GetHashCode();
+        }
+    }
+}
+
