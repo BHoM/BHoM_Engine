655b751eb12071c30466703a0286390ccb6fed29
epignatel <eduardo.pignatelli@burohappold.com>
2017-09-13 13:55:13 +0100

    Added MeshTriangulation and fixed Intersection

--- Geometry_Engine/Measure/MeshIntersection.cs
+++ Geometry_Engine/Measure/MeshIntersection.cs
@@ -10,20 +10,10 @@ namespace BH.Engine.Geometry
 {
     public static partial class Measure
     {
-        /***************************************************/
-        /**** Public Methods                            ****/
-        /***************************************************/
-
+        #region Public Methods
         public static List<Point> GetIntersections(this ICurve curve, Mesh mesh, double tolerance = Tolerance.Distance)
         {
-            throw new NotImplementedException();
-        }
-
-        /***************************************************/
-
-        public static Point GetIntersection(this Line line, Mesh mesh, bool useInfiniteLine = false, double tolerance = Tolerance.Distance)
-        {
-            throw new NotImplementedException();
+            return _GetIntersections(curve as dynamic, mesh);
         }
 
         /***************************************************/
@@ -32,102 +22,116 @@ namespace BH.Engine.Geometry
         {
             throw new NotImplementedException();
         }
+        #endregion
 
 
-        /***************************************************/
-        /**** Private Methods                           ****/
-        /***************************************************/
-
-        private static Point _GetIntersections(this Arc curve, Mesh mesh, double tolerance = Tolerance.Distance)
+        #region Private Methods
+        private static List<Point> _GetIntersections(this Arc curve, Mesh mesh, double tolerance = Tolerance.Distance)
         {
             throw new NotImplementedException();
         }
 
         /***************************************************/
 
-        private static Point _GetIntersections(this Circle curve, Mesh mesh, double tolerance = Tolerance.Distance)
+        private static List<Point> _GetIntersections(this Circle curve, Mesh mesh, double tolerance = Tolerance.Distance)
         {
             throw new NotImplementedException();
         }
 
         /***************************************************/
 
-        private static Point _GetIntersections(this Line curve, Mesh mesh, double tolerance = Tolerance.Distance)   // TODO Add Mesh.GetTriangulated
+        private static List<Point> _GetIntersections(this Line curve, Mesh mesh, double tolerance = Tolerance.Distance)
         {
-            // Mesh Points
-            List<Point> meshPts = mesh.Vertices;
-            Point p1 = meshPts[0];  /*Call function*/
-            Point p2 = meshPts[1];  /*Call function*/
-            Point p3 = meshPts[2];  /*Call function*/
+            // Outputs
+            List<Point> points = new List<Point>();
 
-            // Ray direction
-            Vector d = curve.GetPointAtParameter(1) - curve.GetPointAtParameter(0);                     /*Call function*/
-
-            // Vectors from p1 to p2/p3 (edges)
-            Vector e1, e2;
-
-            Vector p, q, t;
-            double det, invDet, u, v;
-
-            //Find vectors for two edges sharing vertex/point p1
-            e1 = p2 - p1;
-            e2 = p3 - p1;
-
-            // calculating determinant 
-            p = Measure.GetCrossProduct(d, e2);                                             /*Call function*/
-
-            //Calculate determinat
-            det = Measure.GetDotProduct(e1, p);                                             /*Call function*/
-
-            //if determinant is near zero, ray lies in plane of triangle otherwise not
-            if (det > -Double.Epsilon && det < Double.Epsilon) { return null; }
-            invDet = 1.0f / det;
-
-            //calculate distance from p1 to ray origin
-            t = curve.GetPointAtParameter(0) - p1;                                              /*Call function*/
-
-            //Calculate u parameter
-            u = Measure.GetDotProduct(t, p) * invDet;   // can this be replaced by   t*p*invDet   ? Is ti faster?
-
-            //Check for ray hit
-            if (u < 0 || u > 1) { return null; }
-
-            //Prepare to test v parameter
-            q = Measure.GetCrossProduct(t, e1);                                             /*Call function*/
-
-            //Calculate v parameter
-            v = Measure.GetDotProduct(d, q) * invDet;                                       /*Call function*/
-
-            //Check for ray hit
-            if (v < 0 || u + v > 1) { return null; }
-
-            if ((Measure.GetDotProduct(e2, q) * invDet) > Double.Epsilon)                   /*Call function*/
+            // Preprocessing Mesh
+            Mesh tMesh = mesh.GetTriangulated();                                                /*Call function*/
+            List<Face> faces = tMesh.Faces;                                                     /*Call function*/
+            List<Point> meshPts = tMesh.Vertices;                                               /*Call function*/
+            for (int i = 0; i < faces.Count; i++)
             {
-                //ray does intersect
-                return ((1 - u - v) * p1 + u * p2 + v * p3);
+                // Mesh Points
+                Point p1 = meshPts[faces[i].A];                                                 /*Call function*/
+                Point p2 = meshPts[faces[i].B];                                                 /*Call function*/
+                Point p3 = meshPts[faces[i].C];                                                 /*Call function*/
+
+                // Ray direction
+                Vector d = curve.GetPointAtParameter(1) - curve.GetPointAtParameter(0);         /*Call function*/
+
+                // Vectors from p1 to p2/p3 (edges)
+                Vector e1, e2;
+
+                Vector p, q, t;
+                double det, invDet, u, v;
+
+                //Find vectors for two edges sharing vertex/point p1
+                e1 = p2 - p1;
+                e2 = p3 - p1;
+
+                // calculating determinant 
+                p = Measure.GetCrossProduct(d, e2);                                             /*Call function*/
+
+                //Calculate determinat
+                det = e1 * p;                                                                   /*Call function*/
+
+                //if determinant is near zero, ray lies in plane of triangle otherwise not
+                if (det > -Double.Epsilon && det < Double.Epsilon) { continue; }
+                invDet = 1.0f / det;
+
+                //calculate distance from p1 to ray origin
+                t = curve.GetPointAtParameter(0) - p1;                                          /*Call function*/
+
+                //Calculate u parameter
+                u = t * p * invDet;                                                             /*Call function*/
+
+                //Check for ray hit
+                if (u < 0 || u > 1) { continue; }
+
+                //Prepare to test v parameter
+                q = Measure.GetCrossProduct(t, e1);                                             /*Call function*/
+
+                //Calculate v parameter
+                v = d * q * invDet;                                                             /*Call function*/
+
+                //Check for ray hit
+                if (v < 0 || u + v > 1) { continue; }
+
+                if ((e2 * q * invDet) > Double.Epsilon)                                         /*Call function*/
+                {
+                    //ray does intersect
+                    points.Add((1 - u - v) * p1 + u * p2 + v * p3);
+                }
             }
-            return null;
+            return points;
         }
 
         /***************************************************/
 
-        private static Point _GetIntersections(this NurbCurve c, Mesh mesh, double tolerance = Tolerance.Distance)
+        private static List<Point> _GetIntersections(this NurbCurve c, Mesh mesh, double tolerance = Tolerance.Distance)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException();    // TODO GetIntersections(NurbsCurve, Mesh)
         }
 
         /***************************************************/
 
-        private static Point _GetIntersections(this PolyCurve curve, Mesh mesh, double tolerance = Tolerance.Distance)
+        private static List<Point> _GetIntersections(this PolyCurve curve, Mesh mesh, double tolerance = Tolerance.Distance)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException();    // TODO GetIntersections(PolyCurve, Mesh)
         }
 
         /***************************************************/
 
-        private static Point _GetIntersections(this Polyline curve, Mesh mesh, double tolerance = Tolerance.Distance)
+        private static List<Point> _GetIntersections(this Polyline curve, Mesh mesh, double tolerance = Tolerance.Distance)
         {
-            throw new NotImplementedException();
+            List<Line> lines = curve.GetExploded() as List<Line>;
+            List<Point> points = new List<Point>();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                points.AddRange(_GetIntersections(lines[i], mesh));
+            }
+            return points;
         }
+        #endregion
     }
 }
---
+++ Geometry_Engine/Transform/Triangulate.cs
@@ -0,0 +1,62 @@
+﻿using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Transform
+    {
+        #region Public Methods
+        public static dynamic GetTriangulated(this IBHoMGeometry geo)   // TODO Verify that <dynamic> type as output type is correct
+        {
+            return _GetTriangulated(geo as dynamic);
+        }
+        #endregion
+
+        #region Private Methods
+        private static Mesh _GetTriangulated(this Mesh mesh)
+        {
+            Mesh tMesh = new Mesh();
+            List<Point> vertices = mesh.Vertices;
+            List<Face> faces = mesh.Faces;
+            tMesh.Vertices.AddRange(vertices);
+            for (int i = 0; i < faces.Count; i++)
+            {
+                if (!faces[i].IsQuad())
+                {
+                    tMesh.Faces.Add(faces[i]);
+                }
+
+                int i1 = faces[i].A;
+                int i2 = faces[i].B;
+                int i3 = faces[i].C;
+                int i4 = faces[i].D;
+                Point p1 = vertices[i1];
+                Point p2 = vertices[i2];
+                Point p3 = vertices[i3];
+                Point p4 = vertices[i4];
+                double d1 = new Line(p1, p3).GetLength();
+                double d2 = new Line(p2, p4).GetLength();
+                if (d1 > d2)    //Bracing based on shortest diagonal criteria
+                {
+                    Face fA = new Face(i1, i2, i4);
+                    Face fB = new Face(i2, i3, i4);
+                    tMesh.Faces.Add(fA);
+                    tMesh.Faces.Add(fB);
+                }
+                else
+                {
+                    Face fA = new Face(i1, i2, i3);
+                    Face fB = new Face(i1, i3, i4);
+                    tMesh.Faces.Add(fA);
+                    tMesh.Faces.Add(fB);
+                }
+            }
+            return tMesh;
+        }
+        #endregion
+    }
+}
