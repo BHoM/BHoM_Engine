323a803999180e8b96ad926557d66e3a7a0af8ac
adecler <arnaud.declercq@burohappold.com>
2017-09-13 10:26:55 +0100

    adding a couple of functions in the structure engine

---
+++ Structure_Engine/Measure/Centreline.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Geometry;
+using BH.oM.Structural.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Measure
+    {
+        public static Line GetCentreline(this Bar bar)
+        {
+            return new Line(bar.StartNode.Point, bar.EndNode.Point);
+        }
+    }
+}
