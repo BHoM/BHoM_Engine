5927b12b10bcbde05d87c7a21c2315e745cd74e2
epignatel <eduardo.pignatelli@burohappold.com>
2017-09-13 14:30:58 +0100

    Fixing namespace (again)

--- Geometry_Engine/Measure/Area.cs
+++ Geometry_Engine/Measure/Area.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BH.Engine.Geometry.Measure
+namespace BH.Engine.Geometry
 {
     public static partial class Measure
     {
