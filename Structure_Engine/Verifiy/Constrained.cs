87b91444c156c54c85091e4d8c1582484bd5433b
Puria Safari Hesari <puria.safarihesari@burohappold.com>
2017-09-15 17:53:50 +0100

    cleaning up

---
+++ Structure_Engine/Verifiy/Constrained.cs
@@ -0,0 +1,30 @@
+﻿using BH.oM.Geometry;
+using BH.oM.Structural.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Verify
+    {
+
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static bool IsConstrained(this Node node)
+        {
+            return node.Constraint != null;
+        }
+
+        /***************************************************/
+
+        public static Node Merge(List<Node> nodes, List<double> weights = null)
+        {
+
+        }
+    }
+}
