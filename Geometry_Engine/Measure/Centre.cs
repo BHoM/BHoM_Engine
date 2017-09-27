adc351de5b195e4dc80f19a38d8931e9f696caf0
epignatel <eduardo.pignatelli@burohappold.com>
2017-09-27 17:54:44 +0100

    Added Mesh Centre,Edges,Normal methods

--- Geometry_Engine/Measure/Centre.cs
+++ Geometry_Engine/Measure/Centre.cs
@@ -46,5 +46,30 @@ namespace BH.Engine.Geometry
 
             return mean /= count;
         }
+
+        /***************************************************/
+
+        public static List<Point> GetCentres(this Mesh mesh)
+        {
+            List<Face> faces = mesh.Faces;
+            List<Point> vertices = mesh.Vertices;
+            List<Point> centres = new List<Point>(faces.Count);
+            for (int i = 0; i < faces.Count; i++)
+            {
+                Point pA = vertices[(faces[i].A)];
+                Point pB = vertices[(faces[i].B)];
+                Point pC = vertices[(faces[i].C)];
+                if (!faces[i].IsQuad())
+                {
+                    centres.Add(new Point((pA.X + pB.X + pC.X) / 3, (pA.Y + pB.Y + pC.Y) / 3, (pA.Z + pB.Z + pC.Z) / 3));
+                }
+                else
+                {
+                    Point p4 = vertices[(faces[i].D)];
+                    centres.Add(new Point((pA.X + pB.X + pC.X + p4.X) / 4, (pA.X + pB.X + pC.X + p4.Y) / 4, (pA.X + pB.X + pC.X + p4.Z) / 4));  // Assumption that if the face is quad, it is a flat quad.
+                }
+            }
+            return centres;
+        }
     }
 }
