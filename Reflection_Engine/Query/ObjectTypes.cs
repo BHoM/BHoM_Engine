22e181340915713b48239a1efae07989f3a9fc28
Rob May <rob.may@burohappold.com>
2017-09-29 09:05:39 +0100

    Added GetObjectTypes to Reflection_Engine
    In order to construct dictionaries of assembly/namespace type names for
    processing (for example to generate drop down menus in UI).

---
+++ Reflection_Engine/Query/ObjectTypes.cs
@@ -0,0 +1,33 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Dictionary<string,Type> GetObjectTypes(Assembly assembly, string nameSpace)
+        {
+            return _GetObjectTypes(assembly, nameSpace);
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        public static Dictionary<string, Type> _GetObjectTypes(Assembly assembly, string nameSpace)
+        {   
+            Type[] typeList =  assembly.GetTypes().Where(x => String.Equals(x.Namespace, nameSpace, StringComparison.Ordinal)).ToArray();
+
+            return typeList.ToDictionary(x => x.ToString(), x => x);
+        }       
+    }
+}
