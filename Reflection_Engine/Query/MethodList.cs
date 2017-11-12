2ad1fa6b8845d21149fd518e3939d523e35d0876
adecler <arnaud.declercq@burohappold.com>
2017-11-12 17:37:48 +0800

    Extraction of Methods from the Engine

---
+++ Reflection_Engine/Query/MethodList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<MethodInfo> GetBHoMMethodList()
+        {
+            // If the dictionary exists already return it
+            if (m_BHoMMethodList != null && m_BHoMMethodList.Count > 0)
+                return m_BHoMMethodList;
+
+            // Otherwise, create it
+            ExtractAllMethods();
+
+            return m_BHoMMethodList;
+        }
+
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private static void ExtractAllMethods()
+        {
+            m_BHoMMethodList = new List<MethodInfo>();
+
+            foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                try
+                {
+                    // Save BHoM objects only
+                    string name = asm.GetName().Name;
+                    if (name.EndsWith("_Engine"))
+                    {
+                        foreach (Type type in asm.GetTypes())
+                        {
+                            if (!type.IsInterface && type.IsAbstract)
+                                m_BHoMMethodList.AddRange(type.GetMethods().Where(x => x.IsPublic));
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Cannot load types from assembly " + asm.GetName().Name);
+                }
+            }
+        }
+
+
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        private static List<MethodInfo> m_BHoMMethodList = new List<MethodInfo>();
+
+    }
+}
