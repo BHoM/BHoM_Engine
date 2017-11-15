dbf32e88e507bbce9660aebe3891bc43d31ee59d
epignatel <eduardo.pignatelli@burohappold.com>
2017-11-15 11:11:24 +0000

    Keep splitting Compute STI into smaller easy methods

--- Acoustic_Engine/Query/ComputeSTI.cs
+++ Acoustic_Engine/Query/ComputeSTI.cs
@@ -15,46 +15,32 @@ namespace BH.Engine.Acoustic
         /**** Public  Methods                           ****/
         /***************************************************/
 
-        public static List<double> ComputeSTI(this Room room, List<Speaker> m_speakers, List<double> speech = null, List<double> envNoise = null, List<double> revTimes = null)
+        public static List<RASTI> ComputeSTI(this Room room, List<Speaker> m_speakers, List<double> speech = null, List<double> envNoise = null, List<double> revTimes = null)
         {
             // Defaulting inputs
             List<Frequency> octaves = new List<Frequency> { Frequency.Hz500, Frequency.Hz2000 };
-            Dictionary<Frequency, double> gains = new Dictionary<Frequency, double> { { Frequency.Hz500, 1.6 }, { Frequency.Hz2000, 5.3 } }; // To be deleted in the future and moved into speakers input
+            Dictionary<Frequency, double> gains = new Dictionary<Frequency, double> { { Frequency.Hz500, 1.6 }, { Frequency.Hz2000, 5.3 } };
             if (speech == null) { speech = new List<double> { 85, 85 }; }
             if (envNoise == null) { envNoise = new List<double> { 53.5, 53.5 }; }
             if (revTimes == null) { revTimes = new List<double> { 0.001, 0.001 }; }
+
             List<Receiver> receivers = room.Samples;
-
-            List<double> timeConstants = new List<double>();
-            List<double> revDistances = new List<double>();
-            for (int i = 0; i < receivers.Count; i++)
-            {
-                timeConstants.Add(room.TimeConstant(revTimes[i]));
-                revDistances.Add(room.ReverbDistance(revTimes[i]));
-            }
-
-            List<Speaker> speakers = new List<Speaker>();
-            for (int j = 0; j < m_speakers.Count; j++)  // Setting new gains
-                speakers.Add(new Speaker(m_speakers[j].Geometry, m_speakers[j].Direction, m_speakers[j].Category, m_speakers[j].SpeakerID, gains));
+            List<double> timeConstants = revTimes.Select(x => Query.TimeConstant(x)).ToList();
+            List<double> revDistances = revTimes.Select(x => room.ReverbDistance(x)).ToList();
+            List<Speaker> speakers = m_speakers.Select(x=> new Speaker(x.Geometry, x.Direction, x.Category, x.SpeakerID, gains)).ToList();
             double closestDist = speakers.Select(x => x.Geometry).ToList().ClosestDist(receivers.Select(x => x.Geometry).ToList());
 
             // Outputs
-            List<RASTI> RASTI = new List<RASTI>();
+            List<RASTI> rasti = new List<RASTI>();
             List<SPL> spl = new List<SPL>();
             List<SNRatio> snRatio = new List<SNRatio>();
 
             for (int k = 0; k < octaves.Count; k++)
             {
                 spl.AddRange(ComputeSPL(room, speakers, revTimes, octaves[k]));
-                snRatio(AddRange(SoundNoiseRatio()))
                 for (int j = 0; j < speakers.Count; j++)
-                {
-
                     for (int i = 0; i < receivers.Count; i++)
-                    {
                         totalSN += CalcSoundToNoiseRatio(envNoise[i], closestDist, amb_pascals, levelSum, revDistances[i], timeConstants[i], octaves[k], gain);
-                    }
-                }
                 STI_oct = ((totalSN / octaves.Count) +15.0) / 30.0;
 
                 STI_OCT[octaves[k]].Add(STI_oct);
@@ -62,11 +48,9 @@ namespace BH.Engine.Acoustic
             }
 
             for (int i = 0; i < STI_OCT[Frequency.Hz500].Count; i++)
-            {
-                double rasti = ((4d / 9d) * STI_OCT[Frequency.Hz500][i]) + ((5d / 9d) * STI_OCT[Frequency.Hz500][i]);
-                RASTI.Add(rasti);
-            }
-            return RASTI;
+                rasti.Add(((4d / 9d) * STI_OCT[Frequency.Hz500][i]) + ((5d / 9d) * STI_OCT[Frequency.Hz500][i]));
+
+            return rasti;
         }
     }
 }
