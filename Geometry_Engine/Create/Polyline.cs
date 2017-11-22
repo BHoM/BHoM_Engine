2cb91be689eb0df17c0658ad2d6056ea3a4e1a43
Pawel Baran <pawel.baran@burohappold.com>
2017-11-22 01:31:57 +0100

    Polyline by lines.

---
+++ Geometry_Engine/Create/Polyline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Geometry;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Polyline Polyline(IEnumerable<Line> lines)
+        {
+            List<Point> ControlPoints = new List<Point>();
+            foreach (Line line in lines)
+            {
+                ControlPoints.Add(line.Start);
+            }
+            ControlPoints.Add(ControlPoints[0]);
+
+            return new Polyline(ControlPoints);
+        }
+    }
+}
