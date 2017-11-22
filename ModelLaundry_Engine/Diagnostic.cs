a297ce6dd8fb047716faf16c938ba4b06160e003
Pawel Baran <pawel.baran@burohappold.com>
2017-11-22 02:14:26 +0100

    catching up to date

--- ModelLaundry_Engine/Diagnostic.cs
+++ ModelLaundry_Engine/Diagnostic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,3 +39,4 @@ namespace ModelLaundry_Engine
         }
     }
 }
+*/
