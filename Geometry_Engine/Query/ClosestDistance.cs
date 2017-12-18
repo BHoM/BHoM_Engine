f95625dfd35ebac5dc50602b966ff2cfcedf98af
epignatel <eduardo.pignatelli@burohappold.com>
2017-12-18 12:12:25 +0000

    Adding ClosestDistance to Geometry

---
+++ Geometry_Engine/Query/ClosestDistance.cs
@@ -0,0 +1,29 @@
+﻿using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.Engine.Geometry;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public  Methods                           ****/
+        /***************************************************/
+
+        public static double ClosestDistance(this IEnumerable<Point> ptsA, IEnumerable<Point> ptsB)
+        {
+            double closestDist = Double.PositiveInfinity;
+            foreach (Point ptB in ptsB)
+            {
+                double dist = ptsA.GetClosestPoint(ptB).GetDistance(ptB);
+                if (dist <= Tolerance.Distance) { return dist; }
+                closestDist = dist < closestDist ? dist : closestDist;
+            }
+            return closestDist;
+        }
+    }
+}
