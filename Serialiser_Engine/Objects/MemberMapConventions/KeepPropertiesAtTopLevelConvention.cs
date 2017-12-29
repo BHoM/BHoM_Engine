e2e4adf89f876bc07789f9bf5a747c52ef2468e7
adecler <arnaud.declercq@burohappold.com>
2017-12-29 18:08:06 +0800

    Moving the serialisation converters from BHoM_Adapter to the engine

---
+++ Serialiser_Engine/Objects/MemberMapConventions/KeepPropertiesAtTopLevelConvention.cs
@@ -0,0 +1,31 @@
+﻿using BH.oM.Base;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using System;
+using MongoDB.Bson.Serialization.Conventions;
+
+
+namespace BH.Engine.Serialiser.MemberMapConventions
+{
+    public class KeepPropertiesAtTopLevelConvention : IMemberMapConvention
+    {
+        /*******************************************/
+        /**** Properties                        ****/
+        /*******************************************/
+
+        public string Name { get { return "KeepPropertiesAtTopLevel"; } }
+
+
+        /*******************************************/
+        /**** Public Methods                    ****/
+        /*******************************************/
+
+        public void Apply(BsonMemberMap memberMap)
+        {
+            Console.WriteLine("hey");
+        }
+
+        /*******************************************/
+    }
+
+}
