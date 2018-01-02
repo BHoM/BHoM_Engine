7e78f66b176ce84d7c9afc2d74aba87c6497aec1
epignatel <eduardo.pignatelli@burohappold.com>
2018-01-02 10:07:25 +0000

    Added constructor for Level
    Reflecting https://github.com/BuroHappoldEngineering/BHoM/pull/114/commits/13f9fbb33cde03c458e10def152502a7083475e1  to BHoM_Engine

---
+++ Architecture_Engine/Create/Level.cs
@@ -0,0 +1,21 @@
+﻿using BH.oM.Architecture.Elements;
+
+namespace BH.Engine.Architecture.Elements
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Level Level(double elevation)
+        {
+            return new Level
+            {
+                Elevation = elevation
+            };
+        }
+
+        /***************************************************/
+    }
+}
