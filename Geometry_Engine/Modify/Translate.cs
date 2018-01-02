b47c866634b3149818bf7a711ad6ca27e809ade4
epignatel <eduardo.pignatelli@burohappold.com>
2018-01-02 12:09:49 +0000

    Fixed Translate method to be extension method

--- Geometry_Engine/Modify/Translate.cs
+++ Geometry_Engine/Modify/Translate.cs
@@ -9,21 +9,21 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Vectors                  ****/
         /***************************************************/
 
-        public static Point Translate(Point pt, Vector transform)
+        public static Point Translate(this Point pt, Vector transform)
         {
             return pt + transform;
         }
 
         /***************************************************/
 
-        public static Vector Translate(Vector vector, Vector transform)
+        public static Vector Translate(this Vector vector, Vector transform)
         {
             return new Vector { X = vector.X, Y = vector.Y, Z = vector.Z };
         }
 
         /***************************************************/
 
-        public static Plane Translate(Plane plane, Vector transform)
+        public static Plane Translate(this Plane plane, Vector transform)
         {
             return new Plane { Origin = plane.Origin + transform, Normal = plane.Normal.Clone() as Vector };
         }
@@ -33,28 +33,28 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Curves                  ****/
         /***************************************************/
 
-        public static Arc Translate(Arc arc, Vector transform)
+        public static Arc Translate(this Arc arc, Vector transform)
         {
             return new Arc { Start = arc.Start + transform, Middle = arc.Middle + transform, End = arc.End + transform };
         }
 
         /***************************************************/
 
-        public static Circle Translate(Circle circle, Vector transform)
+        public static Circle Translate(this Circle circle, Vector transform)
         {
             return new Circle { Centre = circle.Centre + transform, Normal = circle.Normal.Clone() as Vector, Radius = circle.Radius };
         }
 
         /***************************************************/
 
-        public static Line Translate(Line line, Vector transform)
+        public static Line Translate(this Line line, Vector transform)
         {
             return new Line { Start = line.Start + transform, End = line.End + transform };
         }
 
         /***************************************************/
 
-        public static NurbCurve Translate(NurbCurve curve, Vector transform)
+        public static NurbCurve Translate(this NurbCurve curve, Vector transform)
         {
             return new NurbCurve { ControlPoints = curve.ControlPoints.Select(x => x + transform).ToList(), Weights = curve.Weights.ToList(), Knots = curve.Knots.ToList() };
         }
@@ -62,14 +62,14 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
-        public static PolyCurve Translate(PolyCurve curve, Vector transform)
+        public static PolyCurve Translate(this PolyCurve curve, Vector transform)
         {
             return new PolyCurve { Curves = curve.Curves.Select(x => x.ITranslate(transform)).ToList() };
         }
 
         /***************************************************/
 
-        public static Polyline Translate(Polyline curve, Vector transform)
+        public static Polyline Translate(this Polyline curve, Vector transform)
         {
             return new Polyline { ControlPoints = curve.ControlPoints.Select(x => x + transform).ToList() };
         }
@@ -79,35 +79,35 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Surfaces                ****/
         /***************************************************/
 
-        public static Extrusion Translate(Extrusion surface, Vector transform)
+        public static Extrusion Translate(this Extrusion surface, Vector transform)
         {
             return new Extrusion { Curve = surface.Curve.ITranslate(transform), Direction = surface.Direction.Clone() as Vector, Capped = surface.Capped };
         }
 
         /***************************************************/
 
-        public static Loft Translate(Loft surface, Vector transform)
+        public static Loft Translate(this Loft surface, Vector transform)
         {
             return new Loft { Curves = surface.Curves.Select(x => x.ITranslate(transform)).ToList() };
         }
 
         /***************************************************/
 
-        public static NurbSurface Translate(NurbSurface surface, Vector transform)
+        public static NurbSurface Translate(this NurbSurface surface, Vector transform)
         {
             return new NurbSurface { ControlPoints = surface.ControlPoints.Select(x => x + transform).ToList(), Weights = surface.Weights.ToList(), UKnots = surface.UKnots.ToList(), VKnots = surface.VKnots.ToList() };
         }
 
         /***************************************************/
 
-        public static Pipe Translate(Pipe surface, Vector transform)
+        public static Pipe Translate(this Pipe surface, Vector transform)
         {
             return new Pipe { Centreline = surface.Centreline.ITranslate(transform), Radius = surface.Radius, Capped = surface.Capped };
         }
 
         /***************************************************/
 
-        public static PolySurface Translate(PolySurface surface, Vector transform)
+        public static PolySurface Translate(this PolySurface surface, Vector transform)
         {
             return new PolySurface { Surfaces = surface.Surfaces.Select(x => x.ITranslate(transform)).ToList() };
         }
@@ -117,7 +117,7 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Others                  ****/
         /***************************************************/
 
-        public static Mesh Translate(Mesh mesh, Vector transform)
+        public static Mesh Translate(this Mesh mesh, Vector transform)
         {
             return new Mesh { Vertices = mesh.Vertices.Select(x => x + transform).ToList(), Faces = mesh.Faces.Select(x => x.Clone() as Face).ToList() };
         }
