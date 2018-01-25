93cbd8e69771d48e58c0ea06145f3c5faf29e68b
Pawel Baran <pawel.baran@burohappold.com>
2018-01-25 14:58:08 +0100

    Fix

--- Structure_Engine/Create/Opening.cs
+++ Structure_Engine/Create/Opening.cs
@@ -1,11 +1,12 @@
 ﻿using BH.oM.Structural.Elements;
 using BH.oM.Geometry;
+using BHE = BH.Engine.Geometry;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace BH.Engine.Structure
 {
-    public static partial class Create 
+    public static partial class Create
     {
         /***************************************************/
         /**** Public Methods                            ****/
@@ -13,9 +14,16 @@ namespace BH.Engine.Structure
 
         public static Opening Opening(IEnumerable<ICurve> edges)
         {
-            return new Opening { Edges = edges.ToList() };
+            // Todo:
+            // - check if the edges are closed
+            return new Opening { Edges = edges.Select(x => new Edge { Curve = x }).ToList() };
         }
 
         /***************************************************/
+
+        public static Opening Opening(ICurve outline)
+        {
+            return BHE.Query.IIsClosed(outline) ? Opening(BHE.Query.ISubParts(outline)) : null;
+        }
     }
 }
