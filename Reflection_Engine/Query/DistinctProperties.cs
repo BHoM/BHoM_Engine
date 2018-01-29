d14d7026ec0e7ebdcce37c0568f91bda3325172f
INaslund <isak.naslund@burohappold.com>
2018-01-29 15:41:57 +0100

    First implamentation to extract List properties

--- Reflection_Engine/Query/DistinctProperties.cs
+++ Reflection_Engine/Query/DistinctProperties.cs
@@ -30,41 +30,81 @@ namespace BH.Engine.Reflection
         {
             // Get the list of properties corresponding to type P
             Dictionary<Type, List<PropertyInfo>> propertyDictionary = typeof(T).GetProperties().GroupBy(x => x.PropertyType).ToDictionary(x => x.Key, x => x.ToList());
-            List<PropertyInfo> propertiesValue, propertiesCollection;
-            if (!propertyDictionary.TryGetValue(typeof(P), out propertiesValue) && !propertyDictionary.TryGetValue(typeof(IEnumerable<P>), out propertiesCollection))
-                return new List<P>();
+            List<PropertyInfo> propertiesSingle, propertiesList;
+            bool singleExist;
+            if (!(singleExist = propertyDictionary.TryGetValue(typeof(P), out propertiesSingle)))
+            {
+                singleExist = false;
+                propertiesSingle = new List<PropertyInfo>();
+            }
+            if (!propertyDictionary.TryGetValue(typeof(List<P>), out propertiesList))
+            {
+                if (!singleExist)
+                    return new List<P>();
+
+                propertiesList = new List<PropertyInfo>();
+            }
 
             // Collect the property objects
             List<P> propertyObjects = new List<P>();
-            Dictionary<PropertyInfo, Action<T, P>> setters = new Dictionary<PropertyInfo, Action<T, P>>();
-            Dictionary<PropertyInfo, Func<T, P>> getters = new Dictionary<PropertyInfo, Func<T, P>>();
+            Dictionary<PropertyInfo, Action<T, P>> settersSingle = new Dictionary<PropertyInfo, Action<T, P>>();
+            Dictionary<PropertyInfo, Func<T, P>> gettersSingle = new Dictionary<PropertyInfo, Func<T, P>>();
 
-            foreach (PropertyInfo property in propertiesValue)
+            //Get the distinct properties for the single value properties
+            foreach (PropertyInfo property in propertiesSingle)
             {
                 // Optimisation using this article: https://blogs.msmvps.com/jonskeet/2008/08/09/making-reflection-fly-and-exploring-delegates/
                 Func<T, P> getProp = (Func<T, P>)Delegate.CreateDelegate(typeof(Func<T, P>), property.GetGetMethod());
                 Action<T, P> setProp = (Action<T, P>)Delegate.CreateDelegate(typeof(Action<T, P>), property.GetSetMethod());
 
-
                 // Keep those for later
-                setters.Add(property, setProp);
-                getters.Add(property, getProp);
+                settersSingle.Add(property, setProp);
+                gettersSingle.Add(property, getProp);
 
                 // Collect the objects from this property
                 propertyObjects.AddRange(objects.Select(x => getProp(x)));
+            }
+
+            Dictionary<PropertyInfo, Action<T, List<P>>> settersList = new Dictionary<PropertyInfo, Action<T, List<P>>>();
+            Dictionary<PropertyInfo, Func<T, List<P>>> gettersList = new Dictionary<PropertyInfo, Func<T, List<P>>>();
+
+            //Get the distinct properties for the single value properties
+            foreach (PropertyInfo property in propertiesList)
+            {
+                // Optimisation using this article: https://blogs.msmvps.com/jonskeet/2008/08/09/making-reflection-fly-and-exploring-delegates/
+                Func<T, List<P>> getProp = (Func<T, List<P>>)Delegate.CreateDelegate(typeof(Func<T, List<P>>), property.GetGetMethod());
+                Action<T, List<P>> setProp = (Action<T, List<P>>)Delegate.CreateDelegate(typeof(Action<T, List<P>>), property.GetSetMethod());
+
+                // Keep those for later
+                settersList.Add(property, setProp);
+                gettersList.Add(property, getProp);
+
+                // Collect the objects from this property
+                propertyObjects.AddRange(objects.SelectMany(x => getProp(x)));
             }
 
             // Clone the distinct property objects
             Dictionary<Guid, P> cloneDictionary = CloneObjects<P>(propertyObjects.DistinctDictionary());
 
 
-            //Assign cloned distinct property objects back into input objects
-            foreach (PropertyInfo property in propertiesValue)
+            //Assign cloned distinct single property objects back into input objects
+            foreach (PropertyInfo property in propertiesSingle)
             {
-                Action<T, P> setProp = setters[property];
-                Func<T, P> getProp = getters[property];
+                Action<T, P> setProp = settersSingle[property];
+                Func<T, P> getProp = gettersSingle[property];
                 foreach (T x in objects)
                     setProp(x, cloneDictionary[getProp(x).BHoM_Guid]);
+            }
+
+            //Assign cloned distinct list property objects back into input objects
+            foreach (PropertyInfo property in propertiesList)
+            {
+                Action<T, List<P>> setProp = settersList[property];
+                Func<T, List<P>> getProp = gettersList[property];
+                
+                foreach (T x in objects)
+                    setProp(x, getProp(x).Select(y => cloneDictionary[y.BHoM_Guid]).ToList());
+
             }
 
             //Return the disticnt property objects
