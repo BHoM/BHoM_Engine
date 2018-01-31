344a49352c9f973a9fdff40e08a30e9b915ba62c
INaslund <isak.naslund@burohappold.com>
2018-01-31 15:31:49 +0100

    Material, meshface, loadingpanel and node creates + first extrude bar

--- Structure_Engine/Create/MeshFace.cs
+++ Structure_Engine/Create/MeshFace.cs
@@ -1,5 +1,9 @@
 ﻿using BH.oM.Structural.Elements;
+using BH.oM.Structural.Properties;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace BH.Engine.Structure
 {
@@ -9,14 +13,25 @@ namespace BH.Engine.Structure
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static MeshFace MeshFace(Node n0, Node n1, Node n2, Node n3 = null)
+        public static MeshFace MeshFace(Property2D property, Node n0, Node n1, Node n2, Node n3 = null)
         {
-            MeshFace mf = new MeshFace { Nodes = new List<Node> { n0, n1, n2 } };
+            MeshFace mf = new MeshFace { Nodes = new List<Node> { n0, n1, n2 }, Property = property };
 
             if (n3 != null)
                 mf.Nodes.Add(n3);
 
             return mf;
+        }
+
+        /***************************************************/
+
+        public static MeshFace MeshFace(Property2D property, IEnumerable<Node> nodes)
+        {
+            int nodeCount = nodes.Count();
+            if (nodeCount != 3 && nodeCount != 4)
+                throw new ArgumentException("Mesh faces only support 3 or 4 nodes");
+
+            return new MeshFace { Nodes = nodes.ToList(), Property = property };
         }
 
         /***************************************************/
