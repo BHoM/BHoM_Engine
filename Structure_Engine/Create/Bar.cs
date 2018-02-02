bc6d7e74eeeb81d31755e17d60d2e267d95659cd
INaslund <isak.naslund@burohappold.com>
2018-02-02 14:59:15 +0100

    Additional constraint creator and optional section property for bars

--- Structure_Engine/Create/Bar.cs
+++ Structure_Engine/Create/Bar.cs
@@ -10,14 +10,14 @@ namespace BH.Engine.Structure
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static Bar Bar(Line line, ISectionProperty property, BarRelease release = null, BarFEAType feaType = BarFEAType.Flexural, string name = "")
+        public static Bar Bar(Line line, ISectionProperty property = null, BarRelease release = null, BarFEAType feaType = BarFEAType.Flexural, string name = "")
         {          
             return Bar(new Node { Position = line.Start }, new Node { Position = line.End }, property, release, feaType, name);
         }
 
         /***************************************************/
 
-        public static Bar Bar(Node startNode, Node endNode, ISectionProperty property, BarRelease release = null, BarFEAType feaType = BarFEAType.Flexural, string name = "")
+        public static Bar Bar(Node startNode, Node endNode, ISectionProperty property = null, BarRelease release = null, BarFEAType feaType = BarFEAType.Flexural, string name = "")
         {
             release = release == null ? new BarRelease() { StartRelease = FixConstraint6DOF(), EndRelease = FixConstraint6DOF() } : release;
             return new Bar
