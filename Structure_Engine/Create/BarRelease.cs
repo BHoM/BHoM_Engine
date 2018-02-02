0a041659339ab242018de963926fc5c578d3c81d
INaslund <isak.naslund@burohappold.com>
2018-02-02 15:58:06 +0100

    Rigid link and BarRelease additions

--- Structure_Engine/Create/BarRelease.cs
+++ Structure_Engine/Create/BarRelease.cs
@@ -14,5 +14,36 @@ namespace BH.Engine.Structure
         }
 
         /***************************************************/
+
+        public static BarRelease BarReleaseFixFix( string name = "FixFix")
+        {
+            return new BarRelease { StartRelease = FullReleaseConstraint6DOF(), EndRelease = FullReleaseConstraint6DOF(), Name = name };
+        }
+
+        /***************************************************/
+
+        public static BarRelease BarReleasePinPin(string name = "PinPin")
+        {
+            return new BarRelease
+            {
+                StartRelease = Constraint6DOF(false, false, false, true,true,true, "Pin"),
+                EndRelease = Constraint6DOF(false, false, false, true, true, true, "Pin"),
+                Name = name
+            };
+        }
+
+        /***************************************************/
+
+        public static BarRelease BarReleasePinSlip(string name = "PinSlip")
+        {
+            return new BarRelease
+            {
+                StartRelease = Constraint6DOF(false, false, false, true, true, true, "Pin"),
+                EndRelease = Constraint6DOF(true, false, false, true, true, true, "Slip"),
+                Name = name
+            };
+        }
+
+        /***************************************************/
     }
 }
