3a365a1fac7feca3ca5fe24df79dabe78c41b659
INaslund <isak.naslund@burohappold.com>
2018-02-05 16:55:26 +0100

    First pass on Library_Engine with standard materials and SteelSections

---
+++ Library_Engine/Query/Object.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.Engine.Library
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static IObject Object(string libraryName, string objectName)
+        {
+            return Library(libraryName).Where(x => x.Name == objectName).FirstOrDefault();
+        }
+
+        /***************************************************/
+    }
+}
