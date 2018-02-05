ebede6736bc7b096d56ffabe6eb9f90cbaa72ff5
pawelbaran <pawel.baran@burohappold.com>
2018-02-05 01:53:56 +0100

    A few new methods & fixes
    - generic method for joining curves
    - line extend
    - flip edge
    - fixes for direction & plane/plane intersection

---
+++ Geometry_Engine/Modify/Extend.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Geometry;
+using System.Linq;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods - Curves                   ****/
+        /***************************************************/
+
+        public static Line Extend(this Line curve, double start, double end)
+        {
+            Vector dir = curve.Direction();
+            return new Line { Start = curve.Start - dir * start, End = curve.End + dir * end };
+        }
+    }
+}
