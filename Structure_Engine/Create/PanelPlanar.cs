0fa76867f4ba833973d7759dd2ed8c51afad7d3b
Pawel Baran <pawel.baran@burohappold.com>
2018-02-12 18:29:32 +0100

    PanelPlanars from a generic list of outlines

--- Structure_Engine/Create/PanelPlanar.cs
+++ Structure_Engine/Create/PanelPlanar.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using BHE = BH.Engine.Geometry;
 using BHS = BH.Engine.Structure;
+using ML = BH.Engine.ModelLaundry;
 
 namespace BH.Engine.Structure
 {
@@ -33,6 +34,27 @@ namespace BH.Engine.Structure
             panel.ExternalEdges = BHE.Query.ISubParts(outline).Select(x => new Edge { Curve = x }).ToList();
             panel.Openings = pOpenings;
             return panel;
+        }
+
+        /***************************************************/
+
+        public static List<PanelPlanar> PanelPlanar(List<ICurve> outlines)
+        {
+            List<PanelPlanar> result = new List<PanelPlanar>();
+            List<Polyline> pOutlines = outlines.Select(o => (Polyline)o).ToList();
+            List<List<Polyline>> sortedOutlines = ML.Compute.DistributeOutlines(pOutlines);
+            foreach (List<Polyline> panelOutlines in sortedOutlines)
+            {
+                List<Edge> externalEdges = BHE.Query.SubParts(panelOutlines[0]).Select(o => new Edge { Curve = o }).ToList();
+                List<Opening> openings = new List<Opening>();
+                foreach (Polyline p in panelOutlines.Skip(1))
+                {
+                    List<Edge> openingEdges = BHE.Query.SubParts(p).Select(o => new Edge { Curve = o }).ToList();
+                    openings.Add(new Opening { Edges = openingEdges });
+                }
+                result.Add(new PanelPlanar { ExternalEdges = externalEdges, Openings = openings });
+            }
+            return result;
         }
     }
 }
