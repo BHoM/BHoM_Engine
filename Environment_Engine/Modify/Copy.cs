e6db72dbe890a2d3fd803b5c3c929fb3ba737fbd
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-02-15 14:34:44 +0000

    Move methods added, Copy methods updated

--- Environment_Engine/Modify/Copy.cs
+++ Environment_Engine/Modify/Copy.cs
@@ -14,9 +14,48 @@ namespace BH.Engine.Environment
 {
     public static partial class Modify
     {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static BuildingElementCurve Copy(this BuildingElementCurve BuildingElementCurve)
+        {
+            BuildingElementCurve aBuildingElementCurve = BuildingElementCurve.GetShallowClone(true) as BuildingElementCurve;
+            aBuildingElementCurve.Curve = BuildingElementCurve.Curve.IClone();
+            return aBuildingElementCurve;
+        }
+
+        public static BuildingElementPanel Copy(this BuildingElementPanel BuildingElementPanel)
+        {
+            BuildingElementPanel aBuildingElementPanel = BuildingElementPanel.GetShallowClone(true) as BuildingElementPanel;
+            aBuildingElementPanel.PolyCurve = BuildingElementPanel.PolyCurve.IClone() as PolyCurve;
+            return aBuildingElementPanel;
+        }
+
+        public static IBuildingElementGeometry Copy(this IBuildingElementGeometry BuildingElementGeometry)
+        {
+            IBuildingElementGeometry aBuildingElementGeometry = Copy(BuildingElementGeometry as dynamic);
+            return aBuildingElementGeometry;
+        }
+
+        public static Storey Copy(this Storey Storey)
+        {
+            return Storey.GetShallowClone(true) as Storey;
+        }
+
+        public static BuildingElement Copy(this BuildingElement BuildingElement)
+        {
+            BuildingElement aBuildingElement = BuildingElement.GetShallowClone(true) as BuildingElement;
+            if (BuildingElement.BuildingElementGeometry != null)
+                aBuildingElement.BuildingElementGeometry = aBuildingElement.BuildingElementGeometry.Copy();
+            return aBuildingElement;
+        }
+
+        /***************************************************/
+
         public static Storey Copy(this Storey Storey, string Name, double Elevation)
         {
-            Storey aStorey = Storey.GetShallowClone(true) as Storey;
+            Storey aStorey = Storey.Copy();
             aStorey.Name = Name;
             aStorey.Elevation = Elevation;
 
@@ -25,19 +64,31 @@ namespace BH.Engine.Environment
 
         public static IBuildingElementGeometry Copy(this IBuildingElementGeometry BuildingElementGeometry, Vector Vector)
         {
-            IBuildingElementGeometry aBuildingElementGeometry = BuildingElementGeometry.GetShallowClone(true) as IBuildingElementGeometry;
-            aBuildingElementGeometry.Curve.ITranslate(Vector);
+            IBuildingElementGeometry aBuildingElementGeometry = Copy(BuildingElementGeometry);
+            aBuildingElementGeometry.Move(Vector);
             return aBuildingElementGeometry;
         }
 
         public static BuildingElement Copy(this BuildingElement BuildingElement, Vector Vector)
         {
-            BuildingElement aBuildingElement = BuildingElement.GetShallowClone(true) as BuildingElement;
-
-            if(BuildingElement.BuildingElementGeometry != null)
-                aBuildingElement.BuildingElementGeometry = BuildingElement.BuildingElementGeometry.Copy(Vector);
-
+            BuildingElement aBuildingElement = BuildingElement.Copy();
+            aBuildingElement.Move(Vector);
             return aBuildingElement;
         }
+
+        public static BuildingElement Copy(this BuildingElement BuildingElement, Storey Storey)
+        {
+            BuildingElement aBuildingElement = BuildingElement.Copy();
+            aBuildingElement.Move(Storey);
+            return aBuildingElement;
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+
+
+        /***************************************************/
     }
 }
