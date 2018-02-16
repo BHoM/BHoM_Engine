9db8b9cf39448b61a83cc2133828686fe44c22a8
edforsberg <31727750+edforsberg@users.noreply.github.com>
2018-02-16 10:56:08 +0000

    added simplify(polyline) method in modify

---
+++ Geometry_Engine/Modify/Simplify.cs
@@ -0,0 +1,17 @@
+﻿using BH.oM.Geometry;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Polyline Simplify(this Polyline polyline)
+        {
+            polyline.ControlPoints = polyline.DiscontinuityPoints();
+            return polyline;
+        }
+    }
+}
