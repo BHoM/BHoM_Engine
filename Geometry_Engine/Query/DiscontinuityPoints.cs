302c119cad22ea1683b080db2648e5f11ac45233
edforsberg <31727750+edforsberg@users.noreply.github.com>
2018-02-20 14:34:50 +0000

    changed discontinuity points and simplify
    -changed back to using angles to not miss 180 degree turns
    -added cull duplicates to the simplify method

--- Geometry_Engine/Query/DiscontinuityPoints.cs
+++ Geometry_Engine/Query/DiscontinuityPoints.cs
@@ -49,29 +49,25 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
-        public static List<Point> DiscontinuityPoints(this Polyline curve)
+        public static List<Point> DiscontinuityPoints(this Polyline curve, double angletolerance = Tolerance.Angle)
         {
             bool isClosed = curve.IsClosed();
-            List<Point> ctrlPts = curve.ControlPoints.CullDuplicates();
+            List<Point> ctrlPts = curve.ControlPoints;
             if (ctrlPts.Count < 3) return ctrlPts;
             for (int i = 2; i < ctrlPts.Count; i++)
             {
                 Vector v1 = ctrlPts[i - 1] - ctrlPts[i - 2];
                 Vector v2 = ctrlPts[i] - ctrlPts[i - 1];
-                double angle = v1.Angle(v2);
-                double tol = Tolerance.Angle;
-                if (angle <= tol || angle >= (2*Math.PI)-tol) //TODO: Dosn't work with IsColinear for small polylines 
+                double angle = v1.Angle(v2);             
+                if (angle <= angletolerance || angle >= (2 * Math.PI) - angletolerance) //TODO: Dosn't work with IsColinear for small polylines              
                 {
                     ctrlPts.RemoveAt(i - 1);
                     i--;
                 }
-            }           
-
-            if (isClosed) ctrlPts.Add(ctrlPts.First());
+            }
             return ctrlPts;
 
         }
-
 
         /***************************************************/
         /**** Public Methods - Interfaces               ****/
