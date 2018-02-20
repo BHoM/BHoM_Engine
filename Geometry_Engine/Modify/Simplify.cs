89d002498057fe9dc6ca3b60642c42f2da55d45e
edforsberg <31727750+edforsberg@users.noreply.github.com>
2018-02-20 14:52:03 +0000

    changed the simplify method according to previous commit

--- Geometry_Engine/Modify/Simplify.cs
+++ Geometry_Engine/Modify/Simplify.cs
@@ -9,9 +9,22 @@ namespace BH.Engine.Geometry
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static Polyline Simplify(this Polyline polyline)
+        public static Polyline Simplify(this Polyline polyline, double tolerance = Tolerance.Distance, double angletolerance = Tolerance.Angle)
         {
-            List<Point> ctrlPts = polyline.DiscontinuityPoints();
+            bool isClosed = polyline.IsClosed();
+            List<Point> ctrlPts = polyline.DiscontinuityPoints(angletolerance);
+            List<Point> newPts = new List<Point>(ctrlPts);
+            for (int i = 1; i < ctrlPts.Count - 1; i++)
+            {
+                List<Point> checkPoints = new List<Point>() { ctrlPts[i - 1], ctrlPts[i] }.CullDuplicates(tolerance);
+                if (checkPoints.Count == 1)
+                {
+                    ctrlPts.RemoveRange(i - 1, 2);
+                    ctrlPts.Insert(i - 1, checkPoints[0]);
+                    i--;
+                }
+            }
+            if (ctrlPts.Count < 2) return polyline;
             return new Polyline { ControlPoints = ctrlPts };
         }
     }
