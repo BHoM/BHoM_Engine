d7909522decb9ce689fc7723c44eee6b39a12259
Jakub Ziolkowski <Jakub.Ziolkowski@BuroHappold.com>
2018-02-21 10:35:57 +0000

    Issue for Material Type Query fixed

--- Environment_Engine/Query/MaterialType.cs
+++ Environment_Engine/Query/MaterialType.cs
@@ -14,26 +14,25 @@ namespace BH.Engine.Environment
         /***************************************************/
         /**** Public Methods                            ****/
         /***************************************************/
-#
+
         public static MaterialType MaterialType(this GasMaterial gasMaterial)
         {
-            return Curve(gasMaterial as dynamic);
+            return BH.oM.Environmental.Elements.MaterialType.Gas;
         }
 
         public static MaterialType MaterialType(this TransparentMaterial transparentMaterial)
         {
-            return Curve(transparentMaterial as dynamic);
+            return BH.oM.Environmental.Elements.MaterialType.Transparent;
         }
 
         public static MaterialType MaterialType(this OpaqueMaterial opaqueMaterial)
         {
-            return Curve(opaqueMaterial as dynamic);
+            return BH.oM.Environmental.Elements.MaterialType.Opaque;
         }
-
 
         public static MaterialType IMaterialType(this IMaterial material)
         {
-            return Curve(material as dynamic);
+            return MaterialType(material as dynamic);
         }
 
         /***************************************************/
