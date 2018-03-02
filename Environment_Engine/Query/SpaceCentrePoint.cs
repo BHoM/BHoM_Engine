866d36299ba5b329077eb8d38fcc88df516fd0d8
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-03-02 15:06:35 +0000

    Moved IsClockwise method to query
    + Added another method for IsClockwise

---
+++ Environment_Engine/Query/SpaceCentrePoint.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BHE = BH.oM.Environmental.Elements;
+using BH.oM.Environmental.Interface;
+using BHG = BH.oM.Geometry;
+using BH.Engine.Geometry;
+
+namespace BH.Engine.Environment.Query
+{
+    public static partial class Query
+    {
+
+        public static BHG.Point SpaceCentrePoint(List<BHE.BuildingElementPanel> bHoMPanels) //This does only work for convex spaces. we need to change this method later
+        {
+            List<BHG.Point> spacePts = new List<BHG.Point>();
+            foreach (BHE.BuildingElementPanel panel in bHoMPanels)
+            {
+                spacePts.AddRange(panel.PolyCurve.ControlPoints());
+            }
+            BHG.Point centrePt = BH.Engine.Geometry.Query.Bounds(spacePts).Centre();
+
+            return centrePt;
+        }
+    }
+}
