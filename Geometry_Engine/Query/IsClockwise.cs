cf08a0f1166385b56997e1ad8510dc745df1ea5a
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-03-07 11:14:06 +0000

    Changed to fitplane and normal vector from projected view point

--- Geometry_Engine/Query/IsClockwise.cs
+++ Geometry_Engine/Query/IsClockwise.cs
@@ -1,6 +1,7 @@
 ﻿using BH.oM.Geometry;
 using System.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace BH.Engine.Geometry
 {
@@ -12,7 +13,8 @@ namespace BH.Engine.Geometry
 
         public static bool IsClockwise(this Polyline curve, Vector normal)
         {
-            List<Point> cc = curve.ControlPoints;
+            if (!curve.IsClosed()) throw new Exception("The curve is not closed. IsClockwise method is relevant only to closed curves.");
+            List<Point> cc = curve.DiscontinuityPoints().CullDuplicates();
             Vector dir1 = (cc[0] - cc.Last()).Normalise();
             Vector dir2;
             double angleTot = 0;
@@ -27,35 +29,47 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
-        public static bool IsClockwise(this PolyCurve polyCurve, Point viewPoint)
+        public static bool IsClockwise(this PolyCurve curve, Vector normal)
         {
-            List<Point> pts = DiscontinuityPoints(polyCurve).CullDuplicates();
-            Vector centreVector = (pts[0] - viewPoint).Normalise();
 
-
-            /* Dot product of the normal and a vector from the center of the space. Positive dotproduct for clockwise
-               and negative for anticlockwise (but this depends on the handedness of the coordinate system)*/
-            List<double> dotProducts = new List<double>();
-            for (int i = 0; i < pts.Count - 2; i++)
+            if (!curve.IsClosed()) throw new Exception("The curve is not closed. IsClockwise method is relevant only to closed curves.");
+            List<Point> cc = curve.DiscontinuityPoints().CullDuplicates();
+            Vector dir1 = (cc[0] - cc.Last()).Normalise();
+            Vector dir2;
+            double angleTot = 0;
+            for (int i = 1; i < cc.Count; i++)
             {
-                Plane plane = Create.Plane(pts[i], pts[i + 1], pts[i + 2]);
-                dotProducts.Add(plane.Normal.DotProduct(centreVector));
+                dir2 = (cc[i] - cc[i - 1]).Normalise();
+                angleTot += dir1.SignedAngle(dir2, normal);
+                dir1 = dir2.Clone();
             }
-
-            List<double> pos = new List<double>();
-            List<double> neg = new List<double>();
-            pos.AddRange(dotProducts.Where(x => x > 0));
-            neg.AddRange(dotProducts.Where(x => x < 0));
-
-            //Since we can have local concavity in a global convex curve we have to check all the normal vectors.
-            //More positive than negative dotproducts => clockwise curve.
-            if (pos.Count > neg.Count)
-                return true;
-            return false;
-
-
+            return angleTot > 0;
         }
 
         /***************************************************/
+
+        public static bool IsClockwise(this Polyline curve, Point viewPoint)
+        {
+            Plane plane = curve.FitPlane();
+
+            Point projectedPoint = viewPoint.Project(plane);
+            Vector vector = (projectedPoint - viewPoint).Normalise();
+
+            return IsClockwise(curve, vector);
+        }
+
+        /***************************************************/
+
+        public static bool IsClockwise(this PolyCurve curve, Point viewPoint)
+        {
+            Plane plane = curve.FitPlane();
+
+            Point projectedPoint = viewPoint.Project(plane);
+            Vector vector = (projectedPoint - viewPoint).Normalise();
+
+            return IsClockwise(curve, vector);
+        }
+
+         /***************************************************/
     }
 }
