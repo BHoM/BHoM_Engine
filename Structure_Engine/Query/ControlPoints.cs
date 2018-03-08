0105dfe5a8c875e92553c2f4783360ef1d1238bf
Pawel Baran <pawel.baran@burohappold.com>
2018-03-08 14:45:59 +0100

    PanelPlanar from outlines + referenced methods added
    Tested on my side, ready for merging

---
+++ Structure_Engine/Query/ControlPoints.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Geometry;
+using BH.oM.Structural.Elements;
+using BH.Engine.Geometry;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Query
+    {
+        /******************************************/
+        /****      Element control points      ****/
+        /******************************************/
+
+        public static List<Point> ControlPoints(this PanelPlanar panel, bool externalOnly = false)
+        {
+            List<Point> pts = panel.ExternalEdges.Select(e => (e.Curve as Line).Start.Clone()).ToList();
+            if (!externalOnly)
+            {
+                foreach (Opening o in panel.Openings)
+                {
+                    pts.AddRange(o.Edges.Select(e => (e.Curve as Line).Start.Clone()).ToList());
+                }
+            }
+            return pts;
+        }
+
+        /******************************************/
+
+        public static List<Point> ControlPoints(this Opening opening)
+        {
+            List<Point> pts = opening.Edges.Select(e => (e.Curve as Line).Start.Clone()).ToList();
+            return pts;
+        }
+
+        /******************************************/
+
+        public static List<Point> ControlPoints(this Bar bar)
+        {
+            return new List<Point> { bar.StartNode.Position.Clone(), bar.EndNode.Position.Clone() };
+        }
+
+        /******************************************/
+    }
+}
---
+++ Structure_Engine/Query/Outline.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using BH.oM.Geometry;
+using BH.oM.Structural.Elements;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Query
+    {
+        /******************************************/
+        /****          Panel outline           ****/
+        /******************************************/
+
+        public static Polyline Outline(this PanelPlanar panel)
+        {
+            List<Point> pPts = panel.ControlPoints(true);
+            pPts.Add(pPts[0]);
+            return new Polyline { ControlPoints = pPts };
+        }
+
+
+        /******************************************/
+        /****         Opening outline          ****/
+        /******************************************/
+
+        public static Polyline Outline(this Opening opening)
+        {
+            List<Point> pPts = opening.ControlPoints();
+            pPts.Add(pPts[0]);
+            return new Polyline { ControlPoints = pPts };
+        }
+
+        /******************************************/
+    }
+}
