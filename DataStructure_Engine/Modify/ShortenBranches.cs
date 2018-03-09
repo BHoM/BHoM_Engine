8eecd1695e14394cf44cbc7740ff0c127ba69bb1
adecler <arnaud.declercq@burohappold.com>
2018-03-09 13:26:13 +0800

    Create Trees from list of paths + method to simplify the Tree

---
+++ DataStructure_Engine/Modify/ShortenBranches.cs
@@ -0,0 +1,27 @@
+﻿using BH.oM.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Engine.DataStructure
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Tree<T> ShortenBranches<T>(this Tree<T> tree)
+        {
+            foreach (Tree<T> child in tree.Children.Values)
+                child.ShortenBranches();
+
+            if (tree.Children.Count == 1 && tree.Children.Values.First().Children.Count > 0)
+                tree.Children = tree.Children.Values.First().Children;
+
+            return tree;
+        }
+
+        /***************************************************/
+    }
+}
