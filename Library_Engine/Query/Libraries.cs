6dae60b6d787e3ca26946f6e64fdb06b0ffe22a5
IsakNaslund <isak.naslund@burohappold.com>
2018-03-14 17:44:02 +0100

    Sub DB's and reading from BHoM/DataSets

--- Library_Engine/Query/Libraries.cs
+++ Library_Engine/Query/Libraries.cs
@@ -23,12 +23,14 @@ namespace BH.Engine.Library
             List<IBHoMObject> objects;
             if (!m_parsedLibrary.TryGetValue(name, out objects))
             {
-                string libraryString = LibraryString(name);
+                //string libraryString = LibraryString(name);
 
-                if (libraryString == null)
+                string[] entries = LibraryString(name);
+
+                if (entries == null)
                     return new List<IBHoMObject>();
 
-                string[] entries = libraryString.Split('\n');
+                //string[] entries = libraryString.Split('\n');
                 objects = new List<IBHoMObject>();
 
                 //Parse the library from json to IBHoMObjects
@@ -52,7 +54,7 @@ namespace BH.Engine.Library
         /**** Private Methods                           ****/
         /***************************************************/
 
-        private static Dictionary<string, string> LibraryStrings()
+        private static Dictionary<string, string[]> LibraryStrings()
         {
             //Check that libraries has been loaded
             if (m_libraryStrings.Count < 1)
@@ -63,9 +65,9 @@ namespace BH.Engine.Library
 
         /***************************************************/
 
-        private static string LibraryString(string name)
+        private static string[] LibraryString(string name)
         {
-            string db;
+            string[] db;
             if (!LibraryStrings().TryGetValue(name, out db))
             {
                 db = null;
@@ -76,20 +78,75 @@ namespace BH.Engine.Library
 
         private static void LoadAllLibraries()
         {
-            //Read in all text files in the resource section to memmory as strings
-            ResourceSet set = Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
-            foreach (DictionaryEntry entry in set)
+            //Check for any files straight in the DataSets folder
+            //foreach (string path in Directory.GetFiles(m_sourceFolder))
+            //{
+            //    m_libraryPaths[Path.GetFileNameWithoutExtension(path)] = new List<string>() { path };
+            //}
+
+            //foreach (string path in Directory.GetDirectories(sourceFolder))
+            //{
+            //    string dirName = Path.GetFileName(path);
+
+            //}
+
+            GetPaths();
+
+            ////Read in all text files in the resource section to memmory as strings
+            //ResourceSet set = Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+            //foreach (DictionaryEntry entry in set)
+            //{
+            //    m_libraryStrings[entry.Key.ToString()] = entry.Value.ToString();
+            //}
+        }
+
+
+        private static void GetPaths(string folderPath = "", string basePath = "")
+        {
+            string internalPath = Path.Combine(basePath, folderPath);
+            string folder = Path.Combine(m_sourceFolder, internalPath);
+
+            foreach (string path in Directory.GetFiles(folder))
             {
-                m_libraryStrings[entry.Key.ToString()] = entry.Value.ToString();
+                string filePathName = Path.Combine(internalPath, Path.GetFileNameWithoutExtension(path));
+                AddToPathDictionary(filePathName, filePathName);
+                m_libraryStrings[filePathName] = File.ReadAllLines(path);
             }
+
+            foreach (string dictPath in Directory.GetDirectories(folder))
+            {
+                GetPaths(Path.GetFileName(dictPath), internalPath);
+            }
+        }
+
+        /***************************************************/
+
+        private static void AddToPathDictionary(string path, string dictionaryPath)
+        {
+            if (m_libraryPaths.ContainsKey(path))
+                m_libraryPaths[path].Add(dictionaryPath);
+            else
+            {
+                m_libraryPaths[path] = new List<string>();
+                m_libraryPaths[path].Add(dictionaryPath);
+            }
+
+            string basePath = Path.GetDirectoryName(path);
+
+            if (!string.IsNullOrWhiteSpace(basePath))
+                AddToPathDictionary(basePath, dictionaryPath);
         }
 
         /***************************************************/
         /**** Private Fields                            ****/
         /***************************************************/
 
-        private static Dictionary<string, string> m_libraryStrings = new Dictionary<string, string>();
+        private static readonly string m_sourceFolder = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\BHoM\DataSets";
+
+        //private static Dictionary<string, string> m_libraryStrings = new Dictionary<string, string>();
+        private static Dictionary<string, string[]> m_libraryStrings = new Dictionary<string, string[]>();
         private static Dictionary<string, List<IBHoMObject>> m_parsedLibrary = new Dictionary<string, List<IBHoMObject>>();
+        private static Dictionary<string, List<string>> m_libraryPaths = new Dictionary<string, List<string>>();
 
         /***************************************************/
     }
