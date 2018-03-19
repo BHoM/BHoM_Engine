1de5f73923e86242b77f7a0de4112fd57445093a
Josef Taylor <josef.taylor@burohappold.com>
2018-03-19 10:47:26 -0400

    Fixed spelling of density
    Possible that line 21 "Density = density" will cause issues.

--- Common_Engine/Create/Material.cs
+++ Common_Engine/Create/Material.cs
@@ -8,7 +8,7 @@ namespace BH.Engine.Common
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static Material Material(string name, MaterialType type = MaterialType.Steel, double E = 210000000000, double v = 0.3, double tC = 0.000012, double denisty = 7850)
+        public static Material Material(string name, MaterialType type = MaterialType.Steel, double E = 210000000000, double v = 0.3, double tC = 0.000012, double density = 7850)
         {
             return new Material
             {
@@ -18,7 +18,7 @@ namespace BH.Engine.Common
                 PoissonsRatio = v,
                 CoeffThermalExpansion = tC,
                 ShearModulus = E/(2*(1+v)),
-                Density = denisty
+                Density = density
             };
         }
 
