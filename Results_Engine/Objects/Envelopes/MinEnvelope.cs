0a7d2dd76ae29a2de4fead840e14f878eaf64d87
INaslund <isak.naslund@burohappold.com>
2018-03-22 14:25:50 +0000

    Max, Min and MaxAbsolute envelopes for BarForces

---
+++ Results_Engine/Objects/Envelopes/MinEnvelope.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BH.Engine.Results.Objects.Envelopes
+//{
+//    public class MinEnvelope : IEnvelope
+//    {
+//        public double Apply<T>(IEnumerable<T> source, Func<T, double> selector)
+//        {
+//            return source.Min(selector);
+//        }
+//    }
+//}
---
+++ Results_Engine/Query/Totals.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structural.Results;
+
+namespace BH.Engine.Results
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static double FTotal(this GlobalReactions reaction)
+        {
+            return Math.Sqrt(reaction.FX * reaction.FX + reaction.FY * reaction.FY + reaction.FZ * reaction.FZ);
+        }
+
+        /***************************************************/
+
+        public static double MTotal(this GlobalReactions reaction)
+        {
+            return Math.Sqrt(reaction.MX * reaction.MX + reaction.MY * reaction.MY + reaction.MZ * reaction.MZ);
+        }
+
+        /***************************************************/
+
+        public static double TotalDisplacement(NodeDisplacement disp)
+        {
+            return Math.Sqrt(disp.UX * disp.UX + disp.UY * disp.UY + disp.UZ * disp.UZ);
+        }
+
+        /***************************************************/
+    }
+}
