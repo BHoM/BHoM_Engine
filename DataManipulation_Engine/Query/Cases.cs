bc2c7a56a7d993f674613ace04540d5b6e06f257
INaslund <isak.naslund@burohappold.com>
2018-03-27 17:56:19 +0200

    Help constructor for generation of filterqueries suited for pulling results

---
+++ DataManipulation_Engine/Query/Cases.cs
@@ -0,0 +1,30 @@
+﻿using BH.oM.DataManipulation.Queries;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+
+namespace BH.Engine.DataManipulation
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<object> Cases(this FilterQuery query)
+        {
+            object obj;
+
+            if (query.Equalities.TryGetValue("Cases", out obj))
+            {
+                if (obj is IEnumerable)
+                    return (obj as IEnumerable<object>).ToList();
+            }
+
+            return null;
+        }
+
+        /***************************************************/
+    }
+}
