4647aac85a59946b0e65a3f5d77fb7158fab9dde
INaslund <isak.naslund@burohappold.com>
2018-03-29 17:03:45 +0200

    Is vertical method implemented

---
+++ Structure_Engine/Query/IsVertical.cs
@@ -0,0 +1,41 @@
+﻿using BH.oM.Geometry;
+using BH.oM.Structural.Elements;
+using BH.Engine.Geometry;
+using System;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static bool IsVertical(this Bar bar)
+        {
+            return IsVertical(bar.StartNode.Position, bar.EndNode.Position);
+        }
+
+        /***************************************************/
+
+        public static bool IsVertical(this FramingElement element)
+        {
+            return IsVertical(element.LocationCurve.IStartPoint(), element.LocationCurve.IEndPoint()); //TODO: is this correct? what is the framing element is curved?
+        }
+
+        /***************************************************/
+        /**** Private Methods                            ****/
+        /***************************************************/
+
+        private static bool IsVertical(Point p1, Point p2)
+        {
+            double dx = p1.X - p2.X;
+            double dy = p1.Y - p2.Y;
+
+            return Math.Sqrt(dx * dx + dy * dy) < 0.0001;
+        }
+
+
+        /***************************************************/
+    }
+}
