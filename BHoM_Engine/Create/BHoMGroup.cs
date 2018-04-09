1611730ebe8df54855522f9e75696af9caaa593a
INaslund <isak.naslund@burohappold.com>
2018-04-09 19:14:59 +0200

    Constructor for generic types for BHoMGroups Issue268

--- BHoM_Engine/Create/BHoMGroup.cs
+++ BHoM_Engine/Create/BHoMGroup.cs
@@ -1,6 +1,8 @@
 ﻿using BH.oM.Base;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Reflection;
 
 namespace BH.Engine.Base
 {
@@ -26,6 +28,48 @@ namespace BH.Engine.Base
             {
                 Elements = elements.ToList()
             };
+        }
+
+        /***************************************************/
+
+        public static BHoMObject BHoMGroup(List<IBHoMObject> elements, Type typeHint = null, string name = "")
+        {
+            Type type;
+
+            if (typeHint == null)
+            {
+                if(elements.Count == 0)
+                    return new BHoMGroup<IBHoMObject>();
+
+                type = elements[0].GetType();
+
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    if (elements[i].GetType() == type)
+                        continue;
+                    else
+                    {
+                        type = typeof(IBHoMObject);
+                        break;
+                    }
+                }
+            }
+            else
+                type = typeHint;
+
+            var groupType = typeof(BHoMGroup<>).MakeGenericType(new Type[] { type });
+            var group = Activator.CreateInstance(groupType);
+
+            PropertyInfo info = groupType.GetProperty("Elements");
+            var list = info.GetValue(group);
+            var add = list.GetType().GetMethod("Add");
+
+            foreach (IBHoMObject obj in elements)
+            {
+                add.Invoke(list, new object[] { obj });
+            }
+
+            return group as BHoMObject;
         }
 
         /***************************************************/
