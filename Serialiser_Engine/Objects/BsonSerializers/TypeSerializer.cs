bc567694b6de3a577bbf3a7bead80e83047d6df9
INaslund <isak.naslund@burohappold.com>
2018-04-13 17:24:11 +0200

    null type serialised to empty string instead of null

--- Serialiser_Engine/Objects/BsonSerializers/TypeSerializer.cs
+++ Serialiser_Engine/Objects/BsonSerializers/TypeSerializer.cs
@@ -15,23 +15,19 @@ namespace BH.Engine.Serialiser.BsonSerializers
     {
         public override Type Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
         {
-            if (context.Reader.CurrentBsonType == BsonType.Null)
-            {
-                return null;
-            }
-            else
-            {
-                var fullName = context.Reader.ReadString();
+            var fullName = context.Reader.ReadString();
 
+            if (string.IsNullOrEmpty(fullName))
+                return null;
+            else
                 return Reflection.Create.Type(fullName);
-            }
         }
 
         public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, Type value)
         {
             if (value == null)
             {
-                context.Writer.WriteNull();
+                context.Writer.WriteString("");
             }
             else
             {
