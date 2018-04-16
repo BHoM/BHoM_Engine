d4267897a819862f0b5b8e993a3acdc71f8d3ea8
Sofia Malmsten <Sofia.Malmsten@burohappold.com>
2018-04-16 11:32:04 +0100

    Check IsClockwise

--- Environment_Engine/Query/Tilt.cs
+++ Environment_Engine/Query/Tilt.cs
@@ -28,7 +28,12 @@ namespace BH.Engine.Environment
             double tilt;
 
             List<BHG.Point> pts = BH.Engine.Geometry.Query.DiscontinuityPoints(pline);
+
             BHG.Plane plane = BH.Engine.Geometry.Create.Plane(pts[0], pts[1], pts[2]);
+
+            //The polyline can be locally concave. Check if the polyline is clockwise.
+            if (!BH.Engine.Geometry.Query.IsClockwise(pline, plane.Normal))
+                plane.Normal = -1 * Geometry.Create.Vector(plane.Normal.X, plane.Normal.Y, plane.Normal.Z);
 
             tilt = BH.Engine.Geometry.Query.Angle(plane.Normal, BHG.Plane.XY.Normal) * (180 / Math.PI);
 
