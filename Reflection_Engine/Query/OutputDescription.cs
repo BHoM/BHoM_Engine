cc38ea03bcf2853ef948cbb8659ac4e1d5415b8b
adecler <arnaud.declercq@burohappold.com>
2018-04-29 11:29:02 +0800

    Adding support for Input and Output custom attributes

---
+++ Reflection_Engine/Query/OutputDescription.cs
@@ -0,0 +1,28 @@
+﻿using BH.oM.Base.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Return the custom description of the output of a C# method")]
+        public static string OutputDescription(this MethodBase method)
+        {
+            Output attribute = method.GetCustomAttribute<Output>();
+            if (attribute != null)
+                return attribute.Description;
+            else
+                return "";
+        }
+
+        /***************************************************/
+    }
+}
