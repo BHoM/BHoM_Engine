bea96cbcc728119203570050fb6a2af7553fbd68
adecler <arnaud.declercq@burohappold.com>
2018-05-04 10:50:09 +0800

    Provide support for event logging

---
+++ Reflection_Engine/Compute/RecordEvent.cs
@@ -0,0 +1,65 @@
+﻿using BH.oM.Reflection.Debuging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static bool RecordEvent(string message, EventType type = EventType.Unknown)
+        {
+            return RecordEvent(new Event { Message = message, Type = type });
+        }
+
+        /***************************************************/
+
+        public static bool RecordError(string message)
+        {
+            return RecordEvent(new Event { Message = message, Type = EventType.Error });
+        }
+
+        /***************************************************/
+
+        public static bool RecordWarning(string message)
+        {
+            return RecordEvent(new Event { Message = message, Type = EventType.Warning });
+        }
+
+        /***************************************************/
+
+        public static bool RecordNote(string message)
+        {
+            return RecordEvent(new Event { Message = message, Type = EventType.Note });
+        }
+
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private static bool RecordEvent(Event newEvent)
+        {
+            string trace = Environment.StackTrace;
+            newEvent.StackTrace = string.Join("\n", trace.Split('\n').Skip(4).ToArray());
+
+            Log log = Query.DebugLog();
+            log.AllEvents.Add(newEvent);
+            log.CurrentEvents.Add(newEvent);
+
+            return true;
+        }
+
+
+        /***************************************************/
+    }
+}
---
+++ Reflection_Engine/Query/DebugLog.cs
@@ -0,0 +1,33 @@
+﻿using BH.oM.Reflection.Debuging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        internal static Log DebugLog()
+        {
+            if (m_DebugLog == null)
+                m_DebugLog = new Log();
+
+            return m_DebugLog;
+        }
+
+
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        [ThreadStatic]
+        private static Log m_DebugLog = new Log();
+
+
+        /***************************************************/
+    }
+}
