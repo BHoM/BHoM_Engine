4d7b99d8a01683bad4bd937c7a56676b262c5d51
adecler <arnaud.declercq@burohappold.com>
2018-05-04 10:50:09 +0800

    Provide support for event logging

---
+++ Reflection_Engine/Query/Events.cs
@@ -0,0 +1,32 @@
+﻿using BH.oM.Reflection.Debuging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<Event> AllEvents()
+        {
+            Log log = Query.DebugLog();
+            return log.AllEvents;
+        }
+
+
+        /***************************************************/
+
+        public static List<Event> CurrentEvents()
+        {
+            Log log = Query.DebugLog();
+            return log.CurrentEvents;       
+        }
+
+
+        /***************************************************/
+    }
+}
