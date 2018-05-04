e51e38aef7a72926b73d0b8fe59da9154afeacb0
adecler <arnaud.declercq@burohappold.com>
2018-05-04 10:50:09 +0800

    Provide support for event logging

---
+++ Reflection_Engine/Compute/ClearCurrentEvents.cs
@@ -0,0 +1,25 @@
+﻿using BH.oM.Base;
+using BH.oM.Reflection.Debuging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static bool ClearCurrentEvents()
+        {
+            Log log = Query.DebugLog();
+            log.CurrentEvents.Clear();
+            return true;
+        }
+
+        /***************************************************/
+    }
+}
