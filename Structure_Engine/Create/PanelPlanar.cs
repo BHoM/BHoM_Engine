c55fc4fd09468e0b75c2d5a37c20165713b0a08c
Puria Safari Hesari <puria.safarihesari@burohappold.com>
2018-05-25 15:45:17 +0100

    Adding & modifying Create methods for PanelPlanar

--- Structure_Engine/Create/PanelPlanar.cs
+++ Structure_Engine/Create/PanelPlanar.cs
@@ -1,4 +1,5 @@
 ﻿using BH.oM.Structural.Elements;
+using BH.oM.Structural.Properties;
 using BH.oM.Geometry;
 using System;
 using System.Collections.Generic;
@@ -13,30 +14,48 @@ namespace BH.Engine.Structure
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static PanelPlanar PanelPlanar(ICurve outline, List<Opening> openings = null)
+        public static PanelPlanar PanelPlanar(ICurve outline, List<Opening> openings = null, IProperty2D property = null, string name = "")
         {
             if (!outline.IIsClosed()) return null;
-            PanelPlanar panel = new PanelPlanar();
-            panel.ExternalEdges = outline.ISubParts().Select(x => new Edge { Curve = x }).ToList();
-            panel.Openings = openings;
-            return panel;
+            List<Edge> externalEdges = outline.ISubParts().Select(x => new Edge { Curve = x }).ToList();
+
+            return PanelPlanar(externalEdges, openings, property, name);
         }
 
         /***************************************************/
 
-        public static PanelPlanar PanelPlanar(ICurve outline, List<ICurve> openings = null)
+        public static PanelPlanar PanelPlanar(ICurve outline, List<ICurve> openings = null, IProperty2D property = null, string name = "")
         {
             if (!outline.IIsClosed()) return null;
             List<Opening> pOpenings = openings != null ? openings.Select(o => Create.Opening(o)).ToList() : new List<Opening>();
-            PanelPlanar panel = new PanelPlanar();
-            panel.ExternalEdges = outline.ISubParts().Select(x => new Edge { Curve = x }).ToList();
-            panel.Openings = pOpenings;
-            return panel;
+            List<Edge> externalEdges = outline.ISubParts().Select(x => new Edge { Curve = x }).ToList();
+            return PanelPlanar(externalEdges, pOpenings, property, name);
         }
 
         /***************************************************/
 
-        public static List<PanelPlanar> PanelPlanar(List<Polyline> outlines)
+        public static PanelPlanar PanelPlanar(List<Edge> externalEdges, List<ICurve> openings = null, IProperty2D property = null, string name = "")
+        {
+            List<Opening> pOpenings = openings != null ? openings.Select(o => Create.Opening(o)).ToList() : new List<Opening>();
+            return PanelPlanar(externalEdges, pOpenings, property, name);
+        }
+
+        /***************************************************/
+
+        public static PanelPlanar PanelPlanar(List<Edge> externalEdges, List<Opening> openings = null, IProperty2D property = null, string name = "")
+        {
+            return new PanelPlanar
+            {
+                ExternalEdges = externalEdges,
+                Openings = openings,
+                Property = property,
+                Name = name
+            };
+        }
+
+        /***************************************************/
+
+        public static List<PanelPlanar> PanelPlanar(List<Polyline> outlines, IProperty2D property = null)
         {
             List<PanelPlanar> result = new List<PanelPlanar>();
             List<List<Polyline>> sortedOutlines = outlines.DistributeOutlines();
@@ -49,14 +68,14 @@ namespace BH.Engine.Structure
                     List<Edge> openingEdges = p.SubParts().Select(o => new Edge { Curve = o }).ToList();
                     openings.Add(new Opening { Edges = openingEdges });
                 }
-                result.AddRange((new PanelPlanar { ExternalEdges = externalEdges, Openings = openings }).RecomputeEdges());
+                result.AddRange((new PanelPlanar { ExternalEdges = externalEdges, Openings = openings, Property = property }).RecomputeEdges());
             }
             return result;
         }
 
         /***************************************************/
 
-        public static List<PanelPlanar> PanelPlanar(List<ICurve> outlines)
+        public static List<PanelPlanar> PanelPlanar(List<ICurve> outlines, IProperty2D property = null)
         {
             List<PanelPlanar> result = new List<PanelPlanar>();
             List<List<ICurve>> sortedOutlines = outlines.DistributeOutlines();
@@ -69,7 +88,7 @@ namespace BH.Engine.Structure
                     List<Edge> openingEdges = p.ISubParts().Select(o => new Edge { Curve = o }).ToList();
                     openings.Add(new Opening { Edges = openingEdges });
                 }
-                result.Add(new PanelPlanar { ExternalEdges = externalEdges, Openings = openings });
+                result.Add(new PanelPlanar { ExternalEdges = externalEdges, Openings = openings, Property = property });
             }
             return result;
         }
@@ -77,3 +96,4 @@ namespace BH.Engine.Structure
         /***************************************************/
     }
 }
+
