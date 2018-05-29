2cb3484cdafa9330644ad9bc7338f3d75d18054c
adecler <arnaud.declercq@burohappold.com>
2018-05-29 15:08:58 +0800

    Adds Deep dependency analysis for types and methods

---
+++ Reflection_Engine/Query/InheritedTypes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Mono.Cecil;
+using Mono.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<Type> InheritedTypes(this Type type, bool onlyBHoM = false)
+        {
+            List<Type> types = type.GetInterfaces().ToList();
+
+            if (type.BaseType != typeof(object))
+                types.Add(type.BaseType);
+
+            if (onlyBHoM)
+                return types.Where(x => x.Namespace.StartsWith("BH.")).ToList();
+            else
+                return types;
+        }
+
+        /***************************************************/
+    }
+}
