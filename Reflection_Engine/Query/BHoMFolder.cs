7e5fc5ca01e798eac7b53c3622f9957f254c3bd6
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-06-04 11:39:28 +0800

    Adding Deep dependency analysis and a link to BHoMFolder

---
+++ Reflection_Engine/Query/BHoMFolder.cs
@@ -0,0 +1,22 @@
+﻿using BH.oM.Reflection.Debuging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static string BHoMFolder()
+        {
+            return @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\BHoM\";
+        }
+
+
+        /***************************************************/
+    }
+}
