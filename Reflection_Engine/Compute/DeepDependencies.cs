a3e1d32297c7bd6ab2685e215ff8728bf29e2d54
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-06-04 11:39:28 +0800

    Adding Deep dependency analysis and a link to BHoMFolder

---
+++ Reflection_Engine/Compute/DeepDependencies.cs
@@ -0,0 +1,44 @@
+﻿using BH.oM.Reflection.Debuging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<List<MethodBase>> DeepDependencies(List<MethodBase> methods, int maxDepth = 10)
+        {
+            List<MethodBase> current = methods;
+            IEnumerable<MethodBase> union = methods;
+            List<List<MethodBase>> dependencies = new List<List<MethodBase>>();
+
+            int depth = 0;
+            while (current.Count() > 0 && depth < maxDepth)
+            {
+                IEnumerable<MethodBase> used = current.SelectMany(x => x.UsedMethods(true)).Distinct();
+                IEnumerable<MethodBase> newUsed = used.Except(current);
+
+                current = newUsed.Except(union).ToList();
+                union = union.Union(used);
+                depth++;
+
+                if (current.Count > 0)
+                    dependencies.Add(current);
+            }
+
+            return dependencies;
+        }
+
+        /***************************************************/
+    }
+}
