735a6af2c79a9de661d4997b0cb4c66a37033732
Isak Näslund <isak.naslund@burohappold.com>
2018-06-11 12:52:28 +0200

    Updated create methods for arcs

--- Geometry_Engine/Create/Arc.cs
+++ Geometry_Engine/Create/Arc.cs
@@ -10,36 +10,80 @@ namespace BH.Engine.Geometry
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static Arc Arc(Point start, Point middle, Point end)
+        public static Arc Arc(CoordinateSystem coordinateSystem, double radius, double angle)
         {
             return new Arc
             {
-                Start = start,
-                End = end,
-                Middle = middle
-            }; 
+                CoordinateSystem = coordinateSystem,
+                Radius = radius,
+                Angle = angle
+            };
+        }
+
+        /***************************************************/
+
+        public static Arc Arc(Point start, Point middle, Point end, double tolerance = Tolerance.Distance)
+        {
+            Vector v1 = start - middle;
+            Vector v2 = end - middle;
+            Vector normal = v1.CrossProduct(v2);
+
+            Point centre =  Query.LineIntersection(
+                Create.Line(middle + v1 / 2, v1.CrossProduct(normal)),
+                Create.Line(middle + v2 / 2, v2.CrossProduct(normal)),
+                true,
+                tolerance
+            );
+
+            Vector stVec = start - centre;
+            Vector enVec = end - centre;
+
+
+            CoordinateSystem cs = CoordinateSystem(centre, stVec, enVec);
+
+            double angle = stVec.SignedAngle(enVec, cs.Z);
+
+            if (angle < 0)
+                angle = Math.PI * 2 - angle;
+
+            return new Arc
+            {
+                CoordinateSystem = cs,
+                Radius = stVec.Length(),
+                Angle = angle
+            };
+
         }
 
         /***************************************************/
 
         public static Arc ArcByCentre(Point centre, Point start, Point end, double tolerance = Tolerance.Distance)
         {
+            double radius = start.Distance(centre);
+
             //Check that start and end points are the same distance from the centre point
-            if (Math.Abs(start.Distance(centre) - end.Distance(centre)) > tolerance)
+            if (Math.Abs(radius - end.Distance(centre)) > tolerance)
+            {
+                Reflection.Compute.RecordError("Start and end points need to have the same distance from the centre point");
                 return null;
+            }
 
             Vector v1 = start - centre;
             Vector v2 = end - centre;
-            Vector normal = v1.CrossProduct(v2).Normalise();
 
-            if (double.IsNaN(normal.X) || normal.Length() < tolerance)
-                normal = oM.Geometry.Vector.ZAxis;
+            CoordinateSystem system = CoordinateSystem(centre, v1, v2);
 
-            double angle = v1.SignedAngle(v2, normal);
-            Vector midDir = ((Vector)v1.Rotate(angle / 2, normal)).Normalise();
-            double midRadius = (start.Distance(centre) + end.Distance(centre)) / 2;
+            double angle = v1.SignedAngle(v2, system.Z);
 
-            return new Arc { Start = start, Middle = centre + midRadius * midDir, End = end }; 
+            if (angle < 0)
+                angle = Math.PI * 2 - angle;
+
+            return new Arc
+            {
+                CoordinateSystem = system,
+                Angle = angle,
+                Radius = radius
+            };
         }
 
         /***************************************************/
