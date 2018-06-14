a73ae2260d4fa9b0ecd780f866bc5bee6480680a
al-fisher <al.fisher@burohappold.com>
2018-06-14 17:36:47 +0100

    Adding Create Human

---
+++ Humans_Engine/Create/Human.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Humans;
+
+namespace BH.Engine.Humans
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Human Human(string name)
+        {
+            return new Human { Name = name };
+        }
+
+        /***************************************************/
+    }
+}
