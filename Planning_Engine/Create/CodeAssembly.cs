307fb71ecc90434ad96a8667129d0fb39abbc225
al-fisher <al.fisher@burohappold.com>
2018-06-14 17:25:26 +0100

    Adding Create methods for basic planning objects

---
+++ Planning_Engine/Create/CodeAssembly.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Planning;
+
+namespace BH.Engine.Planning
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static CodeAssembly CodeAssembly(string name, string repo)
+        {
+            return new CodeAssembly { Repository = repo, Name = name };
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Planning_Engine/Create/Repository.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Planning;
+
+namespace BH.Engine.Planning
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Repository Repository(string name)
+        {
+            return new Repository { Name = name };
+        }
+
+        /***************************************************/
+    }
+}
