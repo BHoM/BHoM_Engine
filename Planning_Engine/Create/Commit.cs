0f3db214e7ed82a0b0f3fe7193354221ddaa9474
al-fisher <al.fisher@burohappold.com>
2018-06-14 17:25:26 +0100

    Adding Create methods for basic planning objects

---
+++ Planning_Engine/Create/Commit.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Planning;
+
+namespace BH.Engine.Planning
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Commit Commit(string name)
+        {
+            return new Commit { Name = name };
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Planning_Engine/Create/Label.cs
@@ -0,0 +1,22 @@
+﻿using BH.oM.Planning;
+
+namespace BH.Engine.Planning
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Label Label(string name, string colour)
+        {
+            return new Label
+            {
+                Name = name,
+                Colour = colour,
+            };
+        }
+
+        /***************************************************/
+    }
+}
