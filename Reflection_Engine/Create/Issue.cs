ef79c0d117cbf235a4f5846394be50a4be9c64a0
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-07-09 10:15:49 +0800

    Adding Issue Constructor

---
+++ Reflection_Engine/Create/Issue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+using BH.oM.Reflection.Testing;
+using BH.oM.Planning;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Issue Issue(string repoName, int number, string title = "", List<string> labels = null)
+        {
+            return new Issue
+            {
+                RepoName = repoName,
+                Number = number,
+                Title = title,
+                Labels = (labels == null) ? new List<string>() : labels,
+                Url = @"https://github.com/BuroHappoldEngineering/" + repoName + "/issues/" + number
+            };
+        }
+
+        /***************************************************/
+
+        public static Issue Issue(string repoName, int number, string title = "", List<string> labels = null, string milestone = "", string creator = "", List<string> assignees = null)
+        {
+            return new Issue
+            {
+                RepoName = repoName,
+                Number = number,
+                Title = title,
+                Labels = (labels == null) ? new List<string>() : labels,
+                MilestoneName = milestone,
+                Creator = creator,
+                Assignees = (assignees == null) ? new List<string>() : assignees,
+                Url = @"https://github.com/BuroHappoldEngineering/" + repoName + "/issues/" + number
+            };
+        }
+
+        /***************************************************/
+    }
+}
