514882eeceacc63497bc50755b7df9c9415a6e8d
Pawel Baran <pawel.baran@burohappold.com>
2018-07-17 14:11:03 +0200

    TangentAt methods added and cleaned up

---
+++ Geometry_Engine/Query/TangentAtLength.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BH.oM.Geometry;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods - Curves                   ****/
+        /***************************************************/
+
+        public static Vector TangentAtLength(this Arc curve, double length, double tolerance = Tolerance.Distance)
+        {
+            double parameter = length / curve.Length();
+            return curve.TangentAtParameter(parameter, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtLength(this Circle curve, double length, double tolerance = Tolerance.Distance)
+        {
+            double parameter = length / curve.Length();
+            return curve.TangentAtParameter(parameter, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtLength(this Line curve, double length, double tolerance = Tolerance.Distance)
+        {
+            double parameter = length / curve.Length();
+            return curve.TangentAtParameter(parameter, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtLength(this NurbCurve curve, double length, double tolerance = Tolerance.Distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtLength(this PolyCurve curve, double length, double tolerance = Tolerance.Distance)
+        {
+            double parameter = length / curve.Length();
+            return curve.TangentAtParameter(parameter, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtLength(this Polyline curve, double length, double tolerance = Tolerance.Distance)
+        {
+            double parameter = length / curve.Length();
+            return curve.TangentAtParameter(parameter, tolerance);
+        }
+
+
+        /***************************************************/
+        /**** Public Methods - Interfaces               ****/
+        /***************************************************/
+
+        public static Vector ITangentAtLength(this ICurve curve, double length, double tolerance = Tolerance.Distance)
+        {
+            return TangentAtLength(curve as dynamic, length, tolerance);
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Geometry_Engine/Query/TangentAtPoint.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BH.oM.Geometry;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods - Curves                   ****/
+        /***************************************************/
+
+        public static Vector TangentAtPoint(this Arc curve, Point point, double tolerance = Tolerance.Distance)
+        {
+            double parameterAtPoint = curve.ParameterAtPoint(point, tolerance);
+            return parameterAtPoint == -1 ? null : curve.TangentAtParameter(parameterAtPoint, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtPoint(this Circle curve, Point point, double tolerance = Tolerance.Distance)
+        {
+            double parameterAtPoint = curve.ParameterAtPoint(point, tolerance);
+            return parameterAtPoint == -1 ? null : curve.TangentAtParameter(parameterAtPoint, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtPoint(this Line curve, Point point, double tolerance = Tolerance.Distance)
+        {
+            double parameterAtPoint = curve.ParameterAtPoint(point, tolerance);
+            return parameterAtPoint == -1 ? null : curve.TangentAtParameter(parameterAtPoint, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtPoint(this NurbCurve curve, Point point, double tolerance = Tolerance.Distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtPoint(this PolyCurve curve, Point point, double tolerance = Tolerance.Distance)
+        {
+            double parameterAtPoint = curve.ParameterAtPoint(point, tolerance);
+            return parameterAtPoint == -1 ? null : curve.TangentAtParameter(parameterAtPoint, tolerance);
+        }
+
+        /***************************************************/
+
+        public static Vector TangentAtPoint(this Polyline curve, Point point, double tolerance = Tolerance.Distance)
+        {
+            double parameterAtPoint = curve.ParameterAtPoint(point, tolerance);
+            return parameterAtPoint == -1 ? null : curve.TangentAtParameter(parameterAtPoint, tolerance);
+        }
+
+
+        /***************************************************/
+        /**** Public Methods - Interfaces               ****/
+        /***************************************************/
+
+        public static Vector ITangentAtPoint(this ICurve curve, Point point, double tolerance = Tolerance.Distance)
+        {
+            return Query.TangentAtPoint(curve as dynamic, point, tolerance);
+        }
+
+        /***************************************************/
+    }
+}
