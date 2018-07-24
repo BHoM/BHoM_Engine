81abedb24dd20c7430f8ef48edcb0efc67e35026
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-07-24 16:33:13 +0800

    Adding staus check on methods and types

---
+++ Reflection_Engine/Query/IsNotImplemented.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static bool IsNotImplemented(this MethodBase method)
+        {
+            NotImplemented attribute = method.GetCustomAttribute<NotImplemented>();
+            return (attribute != null);
+        }
+
+
+        /***************************************************/
+
+        public static bool IsNotImplemented(this Type type) 
+        {
+            NotImplemented attribute = type.GetCustomAttribute<NotImplemented>();
+            return (attribute != null);
+        }
+
+
+        /***************************************************/
+
+
+
+    }
+}
---
+++ Reflection_Engine/Query/IsReleased.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static bool IsReleased(this MethodBase method)
+        {
+            Released attribute = method.GetCustomAttribute<Released>();
+            if (attribute != null)
+            {
+                try
+                {
+                    Version version = new Version(attribute.FromVersion);
+                    return (version.CompareTo(method.DeclaringType.Assembly.GetName().Version) > 0);
+                }
+                catch
+                {
+                    return true;
+                }
+            }
+            else
+                return false;
+        }
+
+
+        /***************************************************/
+
+        public static bool IsReleased(this Type type) 
+        {
+            Released attribute = type.GetCustomAttribute<Released>();
+            if (attribute != null)
+            {
+                try
+                {
+                    Version version = new Version(attribute.FromVersion);
+                    return (version.CompareTo(type.Assembly.GetName().Version) > 0);
+                }
+                catch
+                {
+                    return true;
+                }
+            }
+            else
+                return false;
+        }
+
+
+        /***************************************************/
+
+
+
+    }
+}
