7da9cc8ea0f341f525637e17c78c241b859c179d
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-07-31 16:44:10 +0800

    Casting support within SetProperty

---
+++ Reflection_Engine/Modify/Cast.cs
@@ -0,0 +1,23 @@
+﻿using BH.oM.Base;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static object CastGeneric<T>(this IEnumerable<T> source, Type genericType)
+        {
+            return typeof(System.Linq.Enumerable).GetMethod("Cast").MakeGenericMethod(genericType).Invoke(null, new object[] { source });
+        }
+
+        /***************************************************/
+    }
+}
