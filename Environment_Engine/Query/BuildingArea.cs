21b2ca5c7350ee60cb47ba0d92a1a81b6f54fbeb
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-08-30 12:45:41 +0100

    Adding BuildingArea to Environvment Engine Query

---
+++ Environment_Engine/Query/BuildingArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BHE = BH.oM.Environment.Elements;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static double BuildingArea(BHE.Building building)
+        {
+            List<double> area = new List<double>();
+            foreach (BHE.Space s in building.Spaces)
+                area.Add(Environment.Query.FloorArea(s));
+
+            return area.Sum();
+        }
+    }
+}
