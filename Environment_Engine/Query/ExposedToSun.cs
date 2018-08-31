8489256822ac3b8eeb27739e7eb8f7d977982824
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-08-31 10:00:24 +0100

    Moving ExposedToSun method from XML Toolkit

---
+++ Environment_Engine/Query/ExposedToSun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        public static bool ExposedToSun(string surfaceType)
+        {
+            if (String.IsNullOrEmpty(surfaceType)) return false;
+
+            surfaceType = surfaceType.Replace(" ", String.Empty).ToLower();
+
+            return surfaceType == "raisedFloor" || surfaceType == "exteriorwall" || surfaceType == "roof";
+        }
+    }
+}
