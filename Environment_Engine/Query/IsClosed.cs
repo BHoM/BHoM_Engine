45c300620fe4c3137633fc27f110ba8143890ec3
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-08-31 11:46:17 +0100

    Moving IsClosed from XML Toolkit

---
+++ Environment_Engine/Query/IsClosed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BHE = BH.oM.Environment.Elements;
+using BHG = BH.oM.Geometry;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static bool IsClosed(BHE.Space space, double tolerance = BHG.Tolerance.MacroDistance)
+        {
+            return (BH.Engine.Environment.Query.UnmatchedElementPoints(space, tolerance).Count == 0);
+        }
+    }
+}
---
+++ Environment_Engine/Query/UnmatchedPoints.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BHG = BH.oM.Geometry;
+using BHE = BH.oM.Environment.Elements;
+using BH.Engine.Geometry;
+using BH.Engine.Environment;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<BHG.Point> UnmatchedElementPoints(BHE.Space space, double tolerance = BHG.Tolerance.MacroDistance)
+        {
+            List<BHG.Point> nonMatchingPoints = new List<oM.Geometry.Point>();
+
+            foreach(BHE.BuildingElement be in space.BuildingElements)
+            {
+                if (be == null) continue;
+
+                foreach(BHG.Point pt in be.BuildingElementGeometry.ICurve().ICollapseToPolyline(BHG.Tolerance.Angle).IDiscontinuityPoints())
+                {
+                    List<BHE.BuildingElement> elementCompare = space.BuildingElements.Where(x => x != null).ToList().FindAll(x => x.BHoM_Guid != be.BHoM_Guid);
+
+                    BHE.BuildingElement matchingBE = elementCompare.Find(x => x.BuildingElementGeometry.ICurve().ICollapseToPolyline(BHG.Tolerance.Angle).DiscontinuityPoints().ClosestDistance(new List<BHG.Point>() { pt }) < tolerance && (x.BHoM_Guid != be.BHoM_Guid));
+
+                    if (matchingBE == null)
+                        nonMatchingPoints.Add(pt);
+                }
+            }
+
+            return nonMatchingPoints;
+        }
+    }
+}
