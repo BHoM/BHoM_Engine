ebb2283686e36e12c6093c27d3150afb6b4b9010
Isak Naslund <Isak.Naslund@burohappold.com>
2018-09-04 15:21:01 +0200

    Reshifting some mass functions and addition of MassPerArea for simple Property2d types

--- Structure_Engine/Query/MassPerArea.cs
+++ Structure_Engine/Query/MassPerArea.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BH.oM.Structure.Properties;
+using BH.oM.Reflection.Attributes;
+using System;
 
-namespace BH.Engine.Structure.Query
+namespace BH.Engine.Structure
 {
-    class MassPerArea
+    public static partial class Query
     {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static double MassPerArea(this ConstantThickness constantThickness)
+        {
+            return constantThickness.Thickness * constantThickness.Material.Density;
+        }
+
+        /***************************************************/
+
+        [NotImplemented]
+        public static double MassPerArea(this Ribbed ribbedProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************************/
+
+        [NotImplemented]
+        public static double MassPerArea(this Waffle ribbedProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************************/
+
+        [NotImplemented]
+        public static double MassPerArea(this LoadingPanelProperty loadingPanelProperty)
+        {
+            return 0;
+        }
+
+        /***************************************************/
+        /**** Public Methods - Interfaces               ****/
+        /***************************************************/
+
+        public static double IMassPerArea(this IProperty2D property)
+        {
+            return MassPerArea(property as dynamic);
+        }
+
+        /***************************************************/
     }
 }
