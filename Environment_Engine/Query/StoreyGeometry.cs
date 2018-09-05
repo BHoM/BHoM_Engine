14ac228fdb4bc2312fcf189621d2c6f1fe782676
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-05 17:21:46 +0100

    Moving a host of methods over from the XML Toolkit

---
+++ Environment_Engine/Query/StoreyGeometry.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BHG = BH.oM.Geometry;
+using BH.Engine.Geometry;
+
+using BHE = BH.oM.Environment.Elements;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static BHG.Polyline StoreyGeometry(this BH.oM.Architecture.Elements.Level level, List<BHE.Space> spaces)
+        {
+            List<BHE.Space> spacesAtLevel = spaces.FindAll(x => x.Level.Elevation == level.Elevation).ToList();
+
+            if (spacesAtLevel.Count == 0)
+                return null;
+
+            List<BHE.BuildingElement> bHoMBuildingElement = spacesAtLevel.SelectMany(x => x.BuildingElements).ToList();
+            List<BHG.Point> ctrlPoints = new List<BHG.Point>();
+
+            foreach (BHE.BuildingElement element in bHoMBuildingElement)
+            {
+                foreach (BHG.Point pt in element.BuildingElementGeometry.ICurve().IControlPoints())
+                {
+                    if (pt.Z > (level.Elevation - BH.oM.Geometry.Tolerance.Distance) && pt.Z < (level.Elevation + BH.oM.Geometry.Tolerance.Distance))
+                        ctrlPoints.Add(pt);
+
+                }
+            }
+
+            return BH.Engine.Geometry.Create.ConvexHull(ctrlPoints.CullDuplicates());
+        }
+    }
+}
---
+++ Geometry_Engine/Create/ConvexHull.cs
@@ -0,0 +1,62 @@
+﻿using BH.oM.Geometry;
+using System;
+using System.Linq;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Create
+    {
+        //TODO: Only works for points in the XY plane - add plane as input?
+
+        [Description("Creates a Convex Hull from a list of points. Currently only works for points in the XY plane")]
+        public static Polyline ConvexHull(List<Point> points)
+        {
+            List<Point> hull = new List<Point>();
+            hull.Add(points.First());
+            for(int x = 1; x < points.Count; x++)
+            {
+                if (hull[0].X > points[x].X)
+                    hull[0] = points[x];
+                else if (hull[0].X == points[x].X)
+                {
+                    if (hull[0].Y > points[x].Y)
+                        hull[0] = points[x];
+                }
+            }
+
+            Point nextPt = new Point();
+            int counter = 0;
+            while (counter < hull.Count)
+            {
+                nextPt = NextHullPoint(points, hull[counter]);
+                if (nextPt != hull[0])
+                    hull.Add(nextPt);
+                counter++;
+            }
+
+            hull.Add(hull[0]);
+
+            Polyline hullBoundary = new Polyline() { ControlPoints = hull };
+            return hullBoundary;
+        }
+
+        public static Point NextHullPoint(List<Point> points, Point currentPt)
+        {
+            int right = -1;
+            int none = 0;
+
+            Point nextPt = currentPt;
+            int t;
+            foreach (Point pt in points)
+            {
+                t = ((nextPt.X - currentPt.X) * (pt.Y - currentPt.Y) - (pt.X - currentPt.X) * (nextPt.Y - currentPt.Y)).CompareTo(0);
+                if (t == right || t == none && Geometry.Query.Distance(currentPt, pt) > Geometry.Query.Distance(currentPt, nextPt))
+                    nextPt = pt;
+            }
+
+            return nextPt;
+        }
+    }
+}
