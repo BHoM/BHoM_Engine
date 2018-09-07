9faa8c6bfaab92d8c49c46eb85e7bb2b64dc4836
Arnaud Declercq <Arnaud.Declercq@burohappold.com>
2018-09-07 14:24:09 +0800

    Adding support for UnderlyingType

---
+++ Reflection_Engine/Query/UnderlyingType.cs
@@ -0,0 +1,31 @@
+﻿using BH.oM.Reflection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static UnderlyingType UnderlyingType(this Type type)
+        {
+            int depth = 0;
+            while (type.GetGenericArguments().Count() == 1 && typeof(IEnumerable).IsAssignableFrom(type))
+            {
+                depth++;
+                type = type.GetGenericArguments().First();
+            }
+
+            return new UnderlyingType { Type = type, Depth = depth };
+        }
+
+        /***************************************************/
+    }
+}
