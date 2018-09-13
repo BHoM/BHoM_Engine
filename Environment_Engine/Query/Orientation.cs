57f377582df4cacc283a3a1fa46a221cdd977eeb
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-13 16:52:23 +0100

    Fixing issue 579 - moving query methods over from TAS Toolkit

---
+++ Environment_Engine/Query/Orientation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BHEE = BH.oM.Environment.Elements;
+using BH.Engine.Geometry;
+using BHEI = BH.oM.Environment.Interface;
+using BHG = BH.oM.Geometry;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static double Orientation(BHEI.IBuildingElementGeometry buildingPanel)
+        {
+            BHEE.BuildingElementPanel panel = buildingPanel as BHEE.BuildingElementPanel;
+            BHG.Polyline pLine = new BHG.Polyline { ControlPoints = panel.PolyCurve.IControlPoints() };
+
+            List<BHG.Point> pts = pLine.DiscontinuityPoints();
+            BHG.Plane plane = BH.Engine.Geometry.Create.Plane(pts[0], pts[1], pts[2]); //Some protection on this needed maybe?
+
+            BHG.Vector xyNormal = BH.Engine.Geometry.Create.Vector(0, 1, 0);
+
+            return BH.Engine.Geometry.Query.Angle(plane.Normal, xyNormal) * (180 / Math.PI);
+        }
+    }
+}
