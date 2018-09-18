4f21abdaf4a3d10d7636aeb9c0f95c848ed86c65
Rob May <rob.may@burohappold.com>
2018-09-18 07:34:09 +0100

    Add creating filter query for mesh result

---
+++ Structure_Engine/Create/QueryMeshResult.cs
@@ -0,0 +1,43 @@
+﻿using BH.oM.DataManipulation.Queries;
+using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+using BH.oM.Structure.Results;
+
+
+namespace BH.Engine.Structure.Results
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+         
+        [Description("Specific filter query to retrieve structural mesh results")]
+
+        public static FilterQuery MeshResult(   MeshResultSmoothingType smoothing, 
+                                                MeshResultLayer layer,
+                                                double layerPosition,
+                                                MeshResultType resultType,                                                
+                                                IEnumerable<object> cases = null, 
+                                                IEnumerable<object> objectIds = null)
+        {
+            FilterQuery query = new FilterQuery();
+            query.Type = typeof(MeshResults);
+
+            query.Equalities["Smoothing"] = smoothing;
+            query.Equalities["Layer"] = layer;
+            query.Equalities["LayerPosition"] = layerPosition;
+            query.Equalities["ResultType"] = resultType;
+            if (cases != null)
+                query.Equalities["Cases"] = cases.ToList();
+            if (objectIds != null)
+                query.Equalities["ObjectIds"] = objectIds.ToList();
+
+            return query;
+        }
+
+        /***************************************************/
+    }
+}
