230d30be465098c259f1131ad28982a1a16a1b23
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-19 15:27:09 +0100

    Updating modify files to new object model - Issue #576

--- Environment_Engine/Modify/Move.cs
+++ Environment_Engine/Modify/Move.cs
@@ -12,35 +12,5 @@ namespace BH.Engine.Environment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static BuildingElement Move(this BuildingElement buildingElement, Level level)
-        {
-            Vector aVector = Geometry.Create.Vector();
-            if (buildingElement.Level != null)
-                aVector = Geometry.Create.Vector(0, 0, level.Elevation - buildingElement.Level.Elevation);
-
-            BuildingElement aBuildingElement = buildingElement.GetShallowClone() as BuildingElement;
-            aBuildingElement = aBuildingElement.Move(aVector);
-            aBuildingElement.Level = level;
-
-            return aBuildingElement;
-        }
-
-        /***************************************************/
-
-        public static BuildingElement Move(this BuildingElement buildingElement, Vector vector)
-        {
-            BuildingElement aBuildingElement = buildingElement.GetShallowClone() as BuildingElement;
-            aBuildingElement.BuildingElementGeometry = Move(buildingElement.BuildingElementGeometry, vector);
-            return aBuildingElement;
-        }
-
-        /***************************************************/
-
-        public static IBuildingElementGeometry Move(this IBuildingElementGeometry buildingElementGeometry, Vector vector)
-        {
-           return buildingElementGeometry.ISetGeometry(buildingElementGeometry.ICurve().ITranslate(vector));
-        }
-
-        /***************************************************/
     }
 }
