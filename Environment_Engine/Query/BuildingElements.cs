fdcac3d7351d1ce4c8c869466ec08339edcd17c5
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-19 15:59:13 +0100

    Gutting the BuildingElements Query file - Issue #576

--- Environment_Engine/Query/BuildingElements.cs
+++ Environment_Engine/Query/BuildingElements.cs
@@ -10,30 +10,6 @@ namespace BH.Engine.Environment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static List<BuildingElement> BuildingElements(this Building building, Space space)
-        {
-            if (building == null || space == null)
-                return null;
-
-            List<BuildingElement> aResult = new List<BuildingElement>();
-            foreach(BuildingElement aBuildingElement in building.BuildingElements)
-            {
-                if(aBuildingElement.AdjacentSpaces != null && aBuildingElement.AdjacentSpaces.Count > 0)
-                {
-                    Guid aGuid = aBuildingElement.AdjacentSpaces.Find(x => x == space.BHoM_Guid);
-                    if(aGuid != null && aGuid != Guid.Empty)
-                    {
-                        Space aSpace = building.Spaces.Find(x => x.BHoM_Guid == aGuid);
-                        if (aSpace != null)
-                            aResult.Add(aBuildingElement);
-                    }
-                }
-            }
-
-            return aResult;
-        }
-
-        /***************************************************/
     }
 }
 
