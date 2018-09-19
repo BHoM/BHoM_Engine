ceb5873edc86b4fb265775b230333070a1d0ed9c
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-19 16:30:41 +0100

    Gutting the Level file - Issue #576

--- Environment_Engine/Query/Level.cs
+++ Environment_Engine/Query/Level.cs
@@ -12,29 +12,6 @@ namespace BH.Engine.Environment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static Level Level(this Building building, Point point)
-        {
-            if (building.Levels == null || point == null || building.Levels.Count < 1)
-                return null;
-
-            if (building.Levels.Count == 1)
-                return building.Levels.First();
-
-            if (point.Z >= building.Levels.Last().Elevation)
-                return building.Levels.Last();
-
-            if (point.Z <= building.Levels.First().Elevation)
-                return building.Levels.First();
-
-            for(int i = building.Levels.Count - 1; i <= 1; i--)
-            {
-                if (building.Levels[i - 1].Elevation < point.Z)
-                    return building.Levels[i];
-            }
-
-            return building.Levels.First();
-        }
-
-        /***************************************************/
+        
     }
 }
