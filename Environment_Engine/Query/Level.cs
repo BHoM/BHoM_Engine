4182631b75423932e33c9f09f8b0d5888d246b14
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-24 14:08:04 +0100

    Issue #593 - adding query for levels of building elements

--- Environment_Engine/Query/Level.cs
+++ Environment_Engine/Query/Level.cs
@@ -1,8 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 using BH.oM.Environment.Elements;
 using BH.oM.Geometry;
 using BH.oM.Architecture.Elements;
+
+using System.Collections.Generic;
+
+using BH.Engine.Geometry;
 
 namespace BH.Engine.Environment
 {
@@ -12,6 +17,34 @@ namespace BH.Engine.Environment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        
+        public static double MinimumLevel(this BuildingElement bElement)
+        {
+            List<Point> crvPts = bElement.PanelCurve.IControlPoints();
+
+            double min = 1e10;
+            foreach (Point p in crvPts)
+                min = Math.Min(min, p.Z);
+
+            return min;
+        }
+
+        public static double MaximumLevel(this BuildingElement bElement)
+        {
+            List<Point> crvPts = bElement.PanelCurve.IControlPoints();
+
+            double max = -1e10;
+            foreach (Point p in crvPts)
+                max = Math.Max(max, p.Z);
+
+            return max;
+        }
+
+        public static Level Level(this BuildingElement bElement, IEnumerable<Level> levels)
+        {
+            double min = bElement.MinimumLevel();
+            double max = bElement.MaximumLevel();
+
+            return levels.Where(x => x.Elevation >= min && x.Elevation <= max).FirstOrDefault();
+        }
     }
 }
