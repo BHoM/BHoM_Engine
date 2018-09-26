e7a28de8811c246f0a2310afeaacc687f6b5d7d8
Andrew Wakeman <andrew.wakeman@burohappold.com>
2018-09-26 16:42:05 +0100

    Create nuget package build specifications
    Additionally depend on BHoM through nuget
    
    Issue #600

--- Library_Engine/Properties/AssemblyInfo.cs
+++ Library_Engine/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Library_Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BHoM Library Engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Buro Happold")]
+[assembly: AssemblyCompany("BuroHappold")]
 [assembly: AssemblyProduct("Library_Engine")]
 [assembly: AssemblyCopyright("Copyright © Buro Happold 2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
