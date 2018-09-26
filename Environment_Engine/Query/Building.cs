3f88f9081b1f2c47034b890d6cfba6720dd90bea
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-09-26 09:13:55 +0100

    Adding building query object for UIs

---
+++ Environment_Engine/Query/Building.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using BH.oM.Environment.Elements;
+using BH.oM.Geometry;
+using BH.oM.Architecture.Elements;
+
+using BH.oM.Base;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<Building> Buildings(this List<IBHoMObject> bhomObjects)
+        {
+            List<Building> spaces = new List<Building>();
+
+            foreach (IBHoMObject obj in bhomObjects)
+            {
+                if (obj is Building)
+                    spaces.Add(obj as Building);
+            }
+
+            return spaces;
+        }
+    }
+}
