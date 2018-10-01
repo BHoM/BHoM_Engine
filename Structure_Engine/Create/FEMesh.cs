cb6f128a1c5d7b78bb9fb96ae2065cfb705fcb2b
Isak Naslund <Isak.Naslund@burohappold.com>
2018-10-01 17:44:45 +0200

    adding constructor for FEMesh

---
+++ Structure_Engine/Create/FEMesh.cs
@@ -0,0 +1,48 @@
+﻿using BH.oM.Structure.Elements;
+using BH.oM.Structure.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+
+
+namespace BH.Engine.Structure
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static FEMesh FEMesh(BH.oM.Geometry.Mesh mesh, IProperty2D property = null, string name = null)
+        {
+            FEMesh feMesh = new FEMesh();
+            feMesh.Nodes = mesh.Vertices.Select(x => Node(x)).ToList();
+
+            foreach (Face face in mesh.Faces)
+            {
+                FEMeshFace feFace = new FEMeshFace();
+                feFace.NodeListIndices.Add(face.A);
+                feFace.NodeListIndices.Add(face.B);
+                feFace.NodeListIndices.Add(face.C);
+
+                if (face.IsQuad())
+                    feFace.NodeListIndices.Add(face.D);
+
+                feMesh.MeshFaces.Add(feFace);
+
+            }
+
+            if (property != null)
+                feMesh.Property = property;
+
+            if (name != null)
+                feMesh.Name = name;
+
+            return feMesh;
+        }
+
+        /***************************************************/
+    }
+}
