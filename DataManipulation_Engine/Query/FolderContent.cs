720d16178b1c687002585a9d90d99326736686be
epignatel <eduardo.pignatelli@burohappold.com>
2018-10-19 11:01:46 +0100

    Fixes #626
    By adding a method to explore folder content

---
+++ DataManipulation_Engine/Query/FolderContent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BH.oM.DataStructure;
+
+namespace BH.Engine.DataManipulation
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Dictionary<int, List<string>> FolderContent(string directory, string searchKey = "*", int depth = 1)
+        {
+            Dictionary<int, List<string>> tree = new Dictionary<int, List<string>>();
+
+            List<string> toVisit = new List<string> { directory };
+            for (int i = 0; i < depth; i++)
+            {
+                tree.Add(i, new List<string>());
+                List<string> nextStop = new List<string>();
+                foreach (string dir in toVisit)
+                {
+                    tree[i].AddRange(System.IO.Directory.GetFiles(dir, searchKey).ToList());
+                    nextStop.AddRange(System.IO.Directory.GetDirectories(dir).ToList());
+                }
+                toVisit = nextStop;
+            }
+            return tree;
+        }
+
+        /***************************************************/
+    }
+}
