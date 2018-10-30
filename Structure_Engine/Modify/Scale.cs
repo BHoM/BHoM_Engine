71538fdfb9a748e0e1d6e3681250e3008e27aa22
alexradne <alex.radne@burohappold.com>
2018-10-30 15:25:31 +0000

    Scale methods for Bars and Nodes
    Fixes #641

---
+++ Structure_Engine/Modify/Scale.cs
@@ -0,0 +1,63 @@
+﻿using BH.oM.Geometry;
+using BH.Engine.Geometry;
+using System;
+using System.Linq;
+using BH.oM.Structure.Elements;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Node Scale(this Node node, Point origin, Vector scaleVector)
+        {
+            Node scaled = node.GetShallowClone() as Node;
+
+            scaled.Position = node.Position.Scale(origin, scaleVector);
+
+            return scaled;
+        }
+
+        /***************************************************/
+
+        public static Node Scale(this Node node, Point origin, double scaleFactor)
+        {
+            Vector scaleVector = new Vector() { X = scaleFactor, Y = scaleFactor, Z = scaleFactor };
+
+            return node.Scale(origin, scaleVector);
+        }
+        
+        /***************************************************/
+
+        public static Bar Scale(this Bar bar, Point origin, Vector scaleVector)
+        {
+            Bar scaled = bar.GetShallowClone() as Bar;
+
+            scaled.StartNode = bar.StartNode.Scale(origin, scaleVector);
+            scaled.EndNode = bar.EndNode.Scale(origin, scaleVector);
+
+            if (bar.Offset != null)
+            {
+                scaled.Offset.Start = bar.Offset.Start.Scale(origin, scaleVector);
+                scaled.Offset.End = bar.Offset.End.Scale(origin, scaleVector);
+            }
+
+            return scaled;
+        }
+
+        /***************************************************/
+
+        public static Bar Scale(this Bar bar, Point origin, double scaleFactor)
+        {
+            Vector scaleVector = new Vector() { X = scaleFactor, Y = scaleFactor, Z = scaleFactor };
+            
+            return bar.Scale(origin, scaleVector);
+        }
+
+        /***************************************************/
+
+    }
+}
