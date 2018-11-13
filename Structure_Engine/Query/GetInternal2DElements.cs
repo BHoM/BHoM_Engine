55356cc02d07d02ff4698ddf0ef9dda1e409e9f9
Pawel Baran <pawel.baran@burohappold.com>
2018-11-13 22:16:59 +0100

    GetGeometry & SetGeometry, GetOutlines & SetOutlines etc.

---
+++ Structure_Engine/Query/GetInternal2DElements.cs
@@ -0,0 +1,29 @@
+﻿using BH.oM.Geometry;
+using BH.oM.Structure.Elements;
+using System.Collections.Generic;
+using BH.oM.Base;
+using System.Linq;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        public static List<IElement2D> GetInternal2DElements(this Opening opening)
+        {
+            return new List<IElement2D>();
+        }
+
+        /***************************************************/
+
+        public static List<IElement2D> GetInternal2DElements(this PanelPlanar panelPlanar)
+        {
+            return panelPlanar.Openings.Select(o => o as IElement2D).ToList();
+        }
+
+        /***************************************************/
+    }
+}
