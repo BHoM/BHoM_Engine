74c561e952132498cf9585ba23ff47ca79e566cc
Isak Naslund <Isak.Naslund@burohappold.com>
2018-11-13 15:03:22 +0100

    Fixes #648 Adding inversion booleans to cross sections

--- Structure_Engine/Create/SectionProfile.cs
+++ Structure_Engine/Create/SectionProfile.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Collections.ObjectModel;
 using BH.oM.Structure.Properties;
 using BH.oM.Geometry;
 using BH.oM.Common.Materials;
 using System;
 using BH.oM.Reflection.Attributes;
+using BH.Engine.Geometry;
 
 namespace BH.Engine.Structure
 {
@@ -30,18 +32,28 @@ namespace BH.Engine.Structure
 
         /***************************************************/
 
-        public static AngleProfile AngleProfile(double height, double width, double webthickness, double flangeThickness, double rootRadius, double toeRadius)
+        public static AngleProfile AngleProfile(double height, double width, double webthickness, double flangeThickness, double rootRadius, double toeRadius, bool mirrorAboutLocalZ = false, bool mirrorAboutLocalY = false)
         {
             List<ICurve> curves = AngleSectionCurves(width, height, flangeThickness, webthickness, rootRadius, toeRadius);
-            return new AngleProfile(height, width, webthickness, flangeThickness, rootRadius, toeRadius, curves);
+
+            if (mirrorAboutLocalZ)
+                curves = curves.MirrorAboutLocalZ();
+            if (mirrorAboutLocalY)
+                curves = curves.MirrorAboutLocalY();
+
+            return new AngleProfile(height, width, webthickness, flangeThickness, rootRadius, toeRadius, mirrorAboutLocalZ, mirrorAboutLocalY, curves);
         }
 
         /***************************************************/
 
-        public static ChannelProfile ChannelProfile(double height, double width, double webthickness, double flangeThickness, double rootRadius, double toeRadius)
+        public static ChannelProfile ChannelProfile(double height, double width, double webthickness, double flangeThickness, double rootRadius, double toeRadius, bool mirrorAboutLocalZ = false)
         {
             List<ICurve> curves = ChannelSectionCurves(height, width, webthickness, flangeThickness, rootRadius, toeRadius);
-            return new ChannelProfile(height, width, webthickness, flangeThickness, rootRadius, toeRadius, curves);
+
+            if (mirrorAboutLocalZ)
+                curves = curves.MirrorAboutLocalZ();
+
+            return new ChannelProfile(height, width, webthickness, flangeThickness, rootRadius, toeRadius, mirrorAboutLocalZ, curves);
         }
 
         /***************************************************/
@@ -101,10 +113,26 @@ namespace BH.Engine.Structure
 
         /***************************************************/
 
-        public static TSectionProfile TSectionProfile(double height, double width, double webthickness, double flangeThickness, double rootRadius, double toeRadius)
+        public static TSectionProfile TSectionProfile(double height, double width, double webthickness, double flangeThickness, double rootRadius, double toeRadius, bool mirrorAboutLocalY = false)
         {
             List<ICurve> curves = TeeSectionCurves(flangeThickness, width, webthickness, height - flangeThickness, rootRadius, toeRadius);
-            return new TSectionProfile(height, width, webthickness, flangeThickness, rootRadius, toeRadius, curves);
+
+            if (mirrorAboutLocalY)
+                curves = curves.MirrorAboutLocalY();
+
+            return new TSectionProfile(height, width, webthickness, flangeThickness, rootRadius, toeRadius, mirrorAboutLocalY, curves);
+        }
+
+        /***************************************************/
+
+        public static GeneralisedTSectionProfile GeneralisedTSectionProfile(double height, double webThickness, double leftOutstandWidth, double leftOutstandThickness, double rightOutstandWidth, double rightOutstandThickness, bool mirrorAboutLocalY)
+        {
+            List<ICurve> curves = GeneralisedTeeSectionCurves(height, webThickness, leftOutstandWidth, leftOutstandThickness, rightOutstandWidth, rightOutstandThickness);
+
+            if (mirrorAboutLocalY)
+                curves = curves.MirrorAboutLocalY();
+
+            return new GeneralisedTSectionProfile(height, webThickness, leftOutstandWidth, leftOutstandThickness, rightOutstandWidth, rightOutstandThickness,mirrorAboutLocalY, curves);
         }
 
         /***************************************************/
@@ -124,6 +152,24 @@ namespace BH.Engine.Structure
             //TODO: Section curves for z-profile
             //List<ICurve> curves = ZSectionCurves(flangeThickness, width, webthickness, height - flangeThickness, rootRadius, toeRadius);
             //return new ZSectionProfile(height, width, webthickness, flangeThickness, rootRadius, toeRadius, curves);
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private static List<ICurve> MirrorAboutLocalY(this List<ICurve> curves)
+        {
+            Plane plane = Plane.XZ;
+            return curves.Select(x => x.IMirror(plane)).ToList();
+        }
+
+        /***************************************************/
+
+        private static List<ICurve> MirrorAboutLocalZ(this List<ICurve> curves)
+        {
+            Plane plane = Plane.YZ;
+            return curves.Select(x => x.IMirror(plane)).ToList();
         }
 
         /***************************************************/
