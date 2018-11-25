c303e2230ca48b822bfce9d946d531cf6a73937b
Pawel Baran <pawel.baran@burohappold.com>
2018-11-26 00:28:04 +0100

    Architecture namespace related methods added

---
+++ Architecture_Engine/Create/NewInstance.cs
@@ -0,0 +1,18 @@
+﻿using BH.oM.Architecture.Elements;
+
+namespace BH.Engine.Architecture
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Grid NewInstance(this Grid grid)
+        {
+            return new Grid();
+        }
+
+        /***************************************************/
+    }
+}
