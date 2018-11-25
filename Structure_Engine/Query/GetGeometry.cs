b25e63529a42829a238ab927598f812cd116e1bb
Pawel Baran <pawel.baran@burohappold.com>
2018-11-26 00:28:17 +0100

    Structure namespace related methods added

--- Structure_Engine/Query/GetGeometry.cs
+++ Structure_Engine/Query/GetGeometry.cs
@@ -29,28 +29,7 @@ namespace BH.Engine.Structure
         {
             return edge.Curve;
         }
-
-        /***************************************************/
-
-        //public static List<PolyCurve> GetGeometry(this Opening opening)
-        //{
-        //    return new List<PolyCurve> { new PolyCurve { Curves = opening.Edges.Select(e => e.Curve).ToList() } };
-        //}
-
-        ///***************************************************/
-
-        //public static List<PolyCurve> GetGeometry(this PanelPlanar panelPlanar)
-        //{
-        //    List<PolyCurve> result = new List<PolyCurve> { new PolyCurve { Curves = panelPlanar.ExternalEdges.Select(e => e.Curve).ToList() } };
-
-        //    foreach (Opening opening in panelPlanar.Openings)
-        //    {
-        //        result.AddRange(opening.GetGeometry());
-        //    }
-
-        //    return result;
-        //}
-
+        
         /***************************************************/
     }
 }
