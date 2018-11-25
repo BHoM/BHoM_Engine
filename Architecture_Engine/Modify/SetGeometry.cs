6721af68499d61bf16d50098bcec0d645db01307
Pawel Baran <pawel.baran@burohappold.com>
2018-11-26 00:28:04 +0100

    Architecture namespace related methods added

---
+++ Architecture_Engine/Modify/SetGeometry.cs
@@ -0,0 +1,21 @@
+﻿using BH.oM.Geometry;
+using BH.oM.Architecture.Elements;
+
+namespace BH.Engine.Architecture
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static Grid SetGeometry(this Grid grid, ICurve curve)
+        {
+            Grid clone = grid.GetShallowClone() as Grid;
+            clone.Curve = curve;
+            return clone;
+        }
+
+        /***************************************************/
+    }
+}
