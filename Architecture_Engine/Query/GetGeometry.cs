783a4b5c05e6dc29e556b818179c0b877bd06e08
Pawel Baran <pawel.baran@burohappold.com>
2018-11-26 00:28:04 +0100

    Architecture namespace related methods added

---
+++ Architecture_Engine/Query/GetGeometry.cs
@@ -0,0 +1,19 @@
+﻿using BH.oM.Architecture.Elements;
+using BH.oM.Geometry;
+
+namespace BH.Engine.Architecture
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static ICurve GetGeometry(this Grid grid)
+        {
+            return grid.Curve;
+        }
+
+        /***************************************************/
+    }
+}
