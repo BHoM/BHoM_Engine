99353d4b0d6c6bb96468a344dcfd36a39fb91391
Pawel Baran <pawel.baran@burohappold.com>
2018-11-26 00:27:42 +0100

    Common namespace related methods added

---
+++ Common_Engine/Query/Area.cs
@@ -0,0 +1,41 @@
+﻿using BH.Engine.Geometry;
+using BH.oM.Common;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+
+namespace BH.Engine.Common
+{
+    public static partial class Query
+    {
+        /******************************************/
+        /****            IElement2D            ****/
+        /******************************************/
+
+        public static double Area(this IElement2D element2D)
+        {
+            //TODO: make this work for PolyCurves (Booleans needed)
+
+            double result = element2D.IGetOutlineCurve().Area();
+
+            List<Polyline> openings = new List<Polyline>();
+            foreach (PolyCurve o in element2D.IGetInternalOutlineCurves())
+            {
+                Polyline p = o.ToPolyline();
+                if (p == null)
+                    throw new NotImplementedException();
+
+                openings.Add(p);
+            }
+
+            foreach (Polyline p in openings.BooleanUnion())
+            {
+                result -= p.Area();
+            }
+
+            return result;
+        }
+
+        /******************************************/
+    }
+}
---
+++ Common_Engine/Query/ElementCurves.cs
@@ -0,0 +1,71 @@
+﻿using BH.Engine.Geometry;
+using BH.oM.Base;
+using BH.oM.Common;
+using BH.oM.Geometry;
+using System.Collections.Generic;
+
+namespace BH.Engine.Common
+{
+    public static partial class Query
+    {
+        /******************************************/
+        /****            IElement1D            ****/
+        /******************************************/
+
+        public static List<ICurve> ElementCurves(this IElement1D element1D, bool recursive = true)
+        {
+            return new List<ICurve> { element1D.IGetGeometry() };
+        }
+
+
+        /******************************************/
+        /****            IElement2D            ****/
+        /******************************************/
+
+        public static List<ICurve> ElementCurves(this IElement2D element2D, bool recursive)
+        {
+            List<ICurve> result = new List<ICurve>();
+
+            PolyCurve outline = element2D.IGetOutlineCurve();
+            foreach (ICurve curve in outline.Curves)
+            {
+                if (recursive)
+                    result.AddRange(curve.ISubParts());
+                else
+                    result.Add(curve);
+            }
+
+            foreach (IElement2D e in element2D.IGetInternal2DElements())
+            {
+                result.AddRange(e.ElementCurves(recursive));
+            }
+
+            return result;
+        }
+
+
+        /******************************************/
+        /**** Public Methods - Interfaces      ****/
+        /******************************************/
+
+        public static List<ICurve> IElementCurves(this BHoMObject element, bool recursive = true)
+        {
+            return ElementCurves(element as dynamic, recursive);
+        }
+
+
+        /******************************************/
+
+        public static List<ICurve> IElementCurves(this List<BHoMObject> elements, bool recursive = true)
+        {
+            List<ICurve> result = new List<ICurve>();
+            foreach (BHoMObject element in elements)
+            {
+                result.AddRange(element.IElementCurves(recursive));
+            }
+            return result;
+        }
+
+        /******************************************/
+    }
+}
