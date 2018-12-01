e36f5896a3519ff3e5ea4b9c5b6678f1fb2e5a18
Pawel Baran <pawel.baran@burohappold.com>
2018-12-01 13:05:05 +0100

    Added Get/Set for elements0D, code refactor & cleanup

---
+++ Common_Engine/Create/NewElement0D.cs
@@ -0,0 +1,20 @@
+﻿using BH.oM.Common;
+using BH.oM.Geometry;
+
+namespace BH.Engine.Common
+{
+    public static partial class Create
+    {
+        /******************************************/
+        /****            IElement1D            ****/
+        /******************************************/
+
+        public static IElement0D INewElement0D(this IElement1D element1D, Point point)
+        {
+            IElement0D newElement0D = Reflection.Compute.RunExtentionMethod(element1D, "NewElement0D") as IElement0D;
+            return newElement0D.ISetGeometry(point);
+        }
+
+        /******************************************/
+    }
+}
---
+++ Structure_Engine/Modify/Set0DElements.cs
@@ -0,0 +1,28 @@
+﻿using BH.oM.Structure.Elements;
+using System.Collections.Generic;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Modify
+    {
+        /******************************************/
+        /****            IElement1D            ****/
+        /******************************************/
+
+        public static Bar Set0DElements(this Bar bar, List<Node> new0DElements)
+        {
+            if (new0DElements.Count != 2)
+            {
+                Reflection.Compute.RecordError("A bar is defined by 2 nodes.");
+                return null;
+            }
+
+            Bar clone = bar.GetShallowClone() as Bar;
+            clone.StartNode = new0DElements[0];
+            clone.EndNode = new0DElements[1];
+            return clone;
+        }
+
+        /******************************************/
+    }
+}
