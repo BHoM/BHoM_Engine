3fb350f5b435f4738c1beb1b48bb79a3047d26da
Pawel Baran <pawel.baran@burohappold.com>
2018-12-02 00:24:49 +0100

    Final bugfixes

--- Common_Engine/Create/NewElement0D.cs
+++ Common_Engine/Create/NewElement0D.cs
@@ -11,8 +11,7 @@ namespace BH.Engine.Common
 
         public static IElement0D INewElement0D(this IElement1D element1D, Point point)
         {
-            IElement0D newElement0D = Reflection.Compute.RunExtentionMethod(element1D, "NewElement0D") as IElement0D;
-            return newElement0D.ISetGeometry(point);
+            return Reflection.Compute.RunExtentionMethod(element1D, "NewElement0D", new object[] { point }) as IElement0D;
         }
 
         /******************************************/
