4f459ddd1bca6b5b2d006312ae34dab285562c55
Arnaud Declercq <arnaud.declercq@burohappold.com>
2018-12-15 18:24:58 +0800

    Provide a way to create the new CrudConfig object

---
+++ BHoM_Engine/Create/CrudConfig.cs
@@ -0,0 +1,27 @@
+﻿using BH.oM.Base;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System;
+using System.Reflection;
+using BH.oM.Base.CRUD;
+
+namespace BH.Engine.Base
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static CrudConfig GenericPushConfig(PushActionType actionType = PushActionType.Replace) 
+        {
+            return new CrudConfig
+            {
+                ActionType = actionType
+            };
+        }
+
+        /***************************************************/
+    }
+}
