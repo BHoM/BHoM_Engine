8508e9b91256d38e8c5adba88fbc6d6e037301bd
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2018-12-17 16:41:57 +0000

    Fix #741 and #740 - tilt protection, azimuth protection, and opening removal

--- Environment_Engine/Query/Tilt.cs
+++ Environment_Engine/Query/Tilt.cs
@@ -29,7 +29,7 @@ namespace BH.Engine.Environment
 
             List<BHG.Point> pts = BH.Engine.Geometry.Query.DiscontinuityPoints(pline);
 
-            if (pts.Count < 3) return -1; //Error protection on pts having less than 3 elements to create a plane
+            if (pts.Count < 3 || !BH.Engine.Geometry.Query.IsClosed(pline)) return -1; //Error protection on pts having less than 3 elements to create a plane or pLine not being closed
 
             BHG.Plane plane = BH.Engine.Geometry.Create.Plane(pts[0], pts[1], pts[2]);
 
