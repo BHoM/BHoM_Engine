db322d794627d81f5cbf5bb4719c090c13da5c89
Pawel Baran <pawel.baran@burohappold.com>
2018-12-18 15:42:42 +0100

    Nurbs Create methods switched to NotImplemented

--- Geometry_Engine/Create/NurbCurve.cs
+++ Geometry_Engine/Create/NurbCurve.cs
@@ -1,4 +1,5 @@
 ﻿using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,44 +12,26 @@ namespace BH.Engine.Geometry
         /**** Public Methods                            ****/
         /***************************************************/
 
+        [NotImplemented]
         public static NurbsCurve NurbsCurve(IEnumerable<Point> controlPoints, int degree = 3)
         {
-            int n = controlPoints.Count();
-            int d = degree - 1;
-
-            return new NurbsCurve
-            {
-                ControlPoints = controlPoints.ToList(),
-                Weights = Enumerable.Repeat(1.0, n).ToList(),
-                Knots = Enumerable.Repeat(0, d).Concat(Enumerable.Range(0, n - d).Concat(Enumerable.Repeat(n - d - 1, d))).Select(x => (double)x).ToList()
-            };
+            throw new NotImplementedException();
         }
 
         /***************************************************/
 
+        [NotImplemented]
         public static NurbsCurve NurbsCurve(IEnumerable<Point> controlPoints, IEnumerable<double> weights, int degree = 3)
         {
-            int n = controlPoints.Count();
-            int d = degree - 1;
-
-            return new NurbsCurve
-            {
-                ControlPoints = controlPoints.ToList(),
-                Weights = weights.ToList(),
-                Knots = Enumerable.Repeat(0, d).Concat(Enumerable.Range(0, n - d).Concat(Enumerable.Repeat(n - d - 1, d))).Select(x => (double)x).ToList()
-            };
+            throw new NotImplementedException();
         }
 
         /***************************************************/
 
+        [NotImplemented]
         public static NurbsCurve NurbsCurve(IEnumerable<Point> controlPoints, IEnumerable<double> weights, IEnumerable<double> knots)
         {
-            return new NurbsCurve
-            {
-                ControlPoints = controlPoints.ToList(),
-                Weights = weights.ToList(),
-                Knots = knots.ToList()
-            };
+            throw new NotImplementedException();
         }
 
 
@@ -56,43 +39,34 @@ namespace BH.Engine.Geometry
         /**** Random Geometry                           ****/
         /***************************************************/
 
+        [NotImplemented]
         public static NurbsCurve RandomNurbsCurve(int seed = -1, BoundingBox box = null, int minNbCPs = 5, int maxNbCPs = 20)
         {
-            if (seed == -1)
-                seed = m_Random.Next();
-            Random rnd = new Random(seed);
-            return RandomNurbsCurve(rnd, box, minNbCPs, maxNbCPs);
+            throw new NotImplementedException();
         }
 
         /***************************************************/
 
+        [NotImplemented]
         public static NurbsCurve RandomNurbsCurve(Random rnd, BoundingBox box = null, int minNbCPs = 5, int maxNbCPs = 20)
         {
-            List<Point> points = new List<Point>();
-            for (int i = 0; i < rnd.Next(minNbCPs, maxNbCPs + 1); i++)
-                points.Add(RandomPoint(rnd, box));
-            return NurbsCurve(points);
+            throw new NotImplementedException();
         }
 
         /***************************************************/
 
+        [NotImplemented]
         public static NurbsCurve RandomNurbsCurve(Point from, int seed = -1, BoundingBox box = null, int minNbCPs = 5, int maxNbCPs = 20)
         {
-            if (seed == -1)
-                seed = m_Random.Next();
-            Random rnd = new Random(seed);
-            return RandomNurbsCurve(from, rnd, box, minNbCPs, maxNbCPs);
+            throw new NotImplementedException();
         }
 
         /***************************************************/
 
+        [NotImplemented]
         public static NurbsCurve RandomNurbsCurve(Point from, Random rnd, BoundingBox box = null, int minNbCPs = 5, int maxNbCPs = 20)
         {
-            List<Point> points = new List<Point>();
-            points.Add(from);
-            for (int i = 0; i < rnd.Next(minNbCPs, maxNbCPs + 1)-1; i++)
-                points.Add(RandomPoint(rnd, box));
-            return NurbsCurve(points);
+            throw new NotImplementedException();
         }
 
         /***************************************************/
