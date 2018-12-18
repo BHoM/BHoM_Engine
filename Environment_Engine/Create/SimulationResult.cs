c0293616f8a5b40278863c7c7358d1fa61d181ef
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2018-12-18 12:01:18 +0000

    Fix #745 - adding result filter methods

---
+++ Environment_Engine/Create/SimulationResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Environment.Elements;
+using BH.oM.Environment.Results;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static SimulationResult SimulationResult(SimulationResultType type, List<ProfileResult> results)
+        {
+            return new oM.Environment.Results.SimulationResult
+            {
+                SimulationResultType = type,
+                SimulationResults = results,
+            };
+        }
+
+        public static SimulationResult SimulationResult(SimulationResultType type, ProfileResult result)
+        {
+            return SimulationResult(type, new List<ProfileResult> { result });
+        }
+
+        /***************************************************/
+    }
+}
