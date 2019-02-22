809c60463b1bb41a158ce2c128895573c36bc74a
Pawel Baran <pawel.baran@burohappold.com>
2019-02-22 15:23:35 +0100

    Partitioning of the DBSCAN-related code

--- DataStructure_Engine/Compute/ClusterDBSCAN.cs
+++ DataStructure_Engine/Compute/ClusterDBSCAN.cs
@@ -19,7 +19,8 @@
  * You should have received a copy of the GNU Lesser General Public License     
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
- 
+
+using BH.oM.DataStructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,98 +35,76 @@ namespace BH.Engine.DataStructure
 
         public static List<List<T>> ClusterDBSCAN<T>(this List<T> items, Func<T, T, bool> metricFunction, int minCount = 1)
         {
-            DBSCAN<T> DBSCAN = new DBSCAN<T>(metricFunction);
-            return DBSCAN.ComputeClusters(items, minCount);
-        }
-
-
-        /***************************************************/
-        /**** Private Helper Classes                    ****/
-        /***************************************************/
-
-        private class DBScanObject<T>
-        {
-            internal bool IsVisited;
-            internal T ClusterItem;
-            internal int ClusterId;
-
-            internal DBScanObject(T obj)
-            {
-                ClusterItem = obj;
-                IsVisited = false;
-                ClusterId = 0;
-            }
+            DBSCANAlgorithm<T> DBSCAN = Create.DBSCANAlgorithm<T>(metricFunction);
+            return DBSCAN.ComputeClustersDBSCAN(items, minCount);
         }
 
         /***************************************************/
 
-        private class DBSCAN<T>
+        public static List<List<T>> ComputeClustersDBSCAN<T>(this DBSCANAlgorithm<T> dbscan, List<T> allItems, int minCount)
         {
-            private readonly Func<T, T, bool> _metricFunction;
-
-            internal DBSCAN(Func<T, T, bool> metricFunction)
+            DBSCANObject<T>[] DBSCANItems = allItems.Select(x => Create.DBSCANObject<T>(x)).ToArray();
+            int c = 0;
+            for (int i = 0; i < DBSCANItems.Length; i++)
             {
-                this._metricFunction = metricFunction;
-            }
+                DBSCANObject<T> p = DBSCANItems[i];
+                if (p.IsVisited)
+                    continue;
+                p.IsVisited = true;
 
-            internal List<List<T>> ComputeClusters(List<T> allItems, int minCount)
-            {
-                DBScanObject<T>[] DBSCANItems = allItems.Select(x => new DBScanObject<T>(x)).ToArray();
-                int c = 0;
-                for (int i = 0; i < DBSCANItems.Length; i++)
+                DBSCANObject<T>[] neighbourItems = null;
+                dbscan.RegionQuery(DBSCANItems, p.ClusterItem, out neighbourItems);
+
+                if (neighbourItems.Length < minCount)
+                    p.ClusterId = -1;
+                else
                 {
-                    DBScanObject<T> p = DBSCANItems[i];
-                    if (p.IsVisited)
-                        continue;
-                    p.IsVisited = true;
-
-                    DBScanObject<T>[] neighbourItems = null;
-                    RegionQuery(DBSCANItems, p.ClusterItem, out neighbourItems);
-
-                    if (neighbourItems.Length < minCount)
-                        p.ClusterId = -1;
-                    else
-                    {
-                        c++;
-                        ExpandCluster(DBSCANItems, p, neighbourItems, c, minCount);
-                    }
-                }
-
-                List<List<T>> clusters = new List<List<T>>(
-                    DBSCANItems
-                        .Where(x => x.ClusterId > 0)
-                        .GroupBy(x => x.ClusterId)
-                        .Select(x => x.Select(y => y.ClusterItem).ToList())
-                    );
-
-                return clusters;
-            }
-
-            private void ExpandCluster(DBScanObject<T>[] allItems, DBScanObject<T> item, DBScanObject<T>[] neighbourItems, int c, int minCount)
-            {
-                item.ClusterId = c;
-                for (int i = 0; i < neighbourItems.Length; i++)
-                {
-                    DBScanObject<T> neighbourItem = neighbourItems[i];
-
-                    if (!neighbourItem.IsVisited)
-                    {
-                        neighbourItem.IsVisited = true;
-                        DBScanObject<T>[] neighbourItems2 = null;
-                        RegionQuery(allItems, neighbourItem.ClusterItem, out neighbourItems2);
-                        if (neighbourItems2.Length >= minCount)
-                            neighbourItems = neighbourItems.Union(neighbourItems2).ToArray();
-                    }
-
-                    if (neighbourItem.ClusterId == 0)
-                        neighbourItem.ClusterId = c;
+                    c++;
+                    dbscan.ExpandCluster(DBSCANItems, p, neighbourItems, c, minCount);
                 }
             }
 
-            private void RegionQuery(DBScanObject<T>[] allItems, T queryObject, out DBScanObject<T>[] neighbourItems)
+            List<List<T>> clusters = new List<List<T>>(
+                DBSCANItems
+                    .Where(x => x.ClusterId > 0)
+                    .GroupBy(x => x.ClusterId)
+                    .Select(x => x.Select(y => y.ClusterItem).ToList())
+                );
+
+            return clusters;
+        }
+
+
+        /***************************************************/
+        /**** Private methods                           ****/
+        /***************************************************/
+
+        private static void ExpandCluster<T>(this DBSCANAlgorithm<T> dbscan, DBSCANObject<T>[] allItems, DBSCANObject<T> item, DBSCANObject<T>[] neighbourItems, int c, int minCount)
+        {
+            item.ClusterId = c;
+            for (int i = 0; i < neighbourItems.Length; i++)
             {
-                neighbourItems = allItems.Where(x => this._metricFunction(queryObject, x.ClusterItem)).ToArray();
+                DBSCANObject<T> neighbourItem = neighbourItems[i];
+
+                if (!neighbourItem.IsVisited)
+                {
+                    neighbourItem.IsVisited = true;
+                    DBSCANObject<T>[] neighbourItems2 = null;
+                    dbscan.RegionQuery(allItems, neighbourItem.ClusterItem, out neighbourItems2);
+                    if (neighbourItems2.Length >= minCount)
+                        neighbourItems = neighbourItems.Union(neighbourItems2).ToArray();
+                }
+
+                if (neighbourItem.ClusterId == 0)
+                    neighbourItem.ClusterId = c;
             }
+        }
+
+        /***************************************************/
+
+        private static void RegionQuery<T>(this DBSCANAlgorithm<T> dbscan, DBSCANObject<T>[] allItems, T queryObject, out DBSCANObject<T>[] neighbourItems)
+        {
+            neighbourItems = allItems.Where(x => dbscan.MetricFunction(queryObject, x.ClusterItem)).ToArray();
         }
 
         /***************************************************/
