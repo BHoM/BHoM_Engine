f339bfd0c6858ccec2c5cb80d389276f9210667e
Pawel Baran <pawel.baran@burohappold.com>
2019-03-10 15:39:19 +0100

    CurveIntersections and IsSelfIntersecting

--- Geometry_Engine/Query/CurvePlanarIntersections.cs
+++ Geometry_Engine/Query/CurvePlanarIntersections.cs
@@ -21,6 +21,7 @@
  */
 
 using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,131 +34,74 @@ namespace BH.Engine.Geometry
         /**** Public Methods                            ****/
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Arc curve1, Arc curve2, double tolerance = Tolerance.Distance)
         {
-            Circle circle1 = new Circle { Centre = curve1.CoordinateSystem.Origin, Normal = curve1.CoordinateSystem.Z, Radius = curve1.Radius };
-            Circle circle2 = new Circle { Centre = curve2.CoordinateSystem.Origin, Normal = curve2.CoordinateSystem.Z, Radius = curve2.Radius };
-
-            List<Point> iPts = circle1.CurvePlanarIntersections(circle2, tolerance);
-
-            Point midPoint1 = curve1.PointAtParameter(0.5);
-            Point midPoint2 = curve2.PointAtParameter(0.5);
-            double dist1 = midPoint1.Distance(curve1.StartPoint());
-            double dist2 = midPoint2.Distance(curve2.StartPoint());
-
-            for (int i = iPts.Count - 1; i >= 0; i--)
-            {
-                if (midPoint1.Distance(iPts[i]) - dist1 > tolerance || midPoint2.Distance(iPts[i]) - dist2 > tolerance)
-                    iPts.RemoveAt(i);
-            }
-
-            return iPts;
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Arc curve1, Circle curve2, double tolerance = Tolerance.Distance)
         {
-            Circle circle1 = new Circle { Centre = curve1.CoordinateSystem.Origin, Normal = curve1.CoordinateSystem.Z, Radius = curve1.Radius };
-
-            List<Point> iPts = circle1.CurvePlanarIntersections(curve2, tolerance);
-
-            Point midPoint1 = curve1.PointAtParameter(0.5);
-            double dist = midPoint1.Distance(curve1.StartPoint());
-
-            for (int i = iPts.Count - 1; i >= 0; i--)
-            {
-                if (midPoint1.Distance(iPts[i]) - dist > tolerance)
-                    iPts.RemoveAt(i);
-            }
-
-            return iPts;
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Circle curve1, Arc curve2, double tolerance = Tolerance.Distance)
         {
-            return curve2.CurvePlanarIntersections(curve1, tolerance);
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Circle curve1, Circle curve2, double tolerance = Tolerance.Distance)
         {
-            List<Point> iPts = new List<Point>();
-            Point c1 = curve1.Centre;
-            Point c2 = curve2.Centre;
-            double r1 = curve1.Radius;
-            double r2 = curve2.Radius;
-            Vector n1 = curve1.Normal;
-            Vector n2 = curve2.Normal;
-            Plane p1 = new Plane { Origin = c1, Normal = n1 };
-            Plane p2 = new Plane { Origin = c2, Normal = n2 };
-
-            if (!p1.IsCoplanar(p2))
-                return iPts;
-
-            double sqrDist = c1.SquareDistance(c2);
-            double dist = Math.Sqrt(sqrDist);
-
-            if (dist <= tolerance)
-                return iPts;
-
-            double sumRadii = r1 + r2;
-            double difRadii = Math.Abs(r1 - r2);
-            Vector dir = (c2 - c1).Normalise();
-
-            if (Math.Abs(dist - sumRadii) <= tolerance)
-                iPts.Add(c1 + dir * r1);
-            else if (Math.Abs(dist - difRadii) <= tolerance)
-                iPts.Add(c1 - dir * r1);
-            else if (dist - sumRadii < tolerance && dist - difRadii >= tolerance)
-            {
-                double a = (r1 * r1 - r2 * r2 + sqrDist) / (2 * dist);
-                Point midPt = (c1 + dir * a);
-                Vector perp = dir.Rotate(Math.PI * 0.5, n1);
-                double shift = Math.Sqrt(r1 * r1 - midPt.SquareDistance(c1));
-                iPts.Add(midPt + perp * shift);
-                iPts.Add(midPt - perp * shift);
-            }
-
-            return iPts;
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Line curve1, Arc curve2, double tolerance = Tolerance.Distance)
         {
-            return curve2.LineIntersections(curve1, false, tolerance);
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Arc curve1, Line curve2, double tolerance = Tolerance.Distance)
         {
-            return curve1.LineIntersections(curve2, false, tolerance);
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Circle curve1, Line curve2, double tolerance = Tolerance.Distance)
         {
-            return curve1.LineIntersections(curve2, false, tolerance);
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Line curve1, Circle curve2, double tolerance = Tolerance.Distance)
         {
-            return curve2.LineIntersections(curve1, false, tolerance);
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> CurvePlanarIntersections(this Line curve1, Line curve2, double tolerance = Tolerance.Distance)
         {
-            return curve1.LineIntersections(curve2, false, tolerance);
+            return curve1.CurveIntersections(curve2, tolerance);
         }
 
 
@@ -165,17 +109,10 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Interfaces               ****/
         /***************************************************/
 
+        [DeprecatedAttribute("CurveIntersections")]
         public static List<Point> ICurvePlanarIntersections(this ICurve curve1, ICurve curve2, double tolerance = Tolerance.Distance)
         {
-            List<Point> result = new List<Point>();
-            foreach (ICurve c1 in curve1.ISubParts())
-            {
-                foreach (ICurve c2 in curve2.ISubParts())
-                {
-                    result.AddRange(CurvePlanarIntersections(c1 as dynamic, c2 as dynamic, tolerance));
-                }
-            }
-            return result;
+            return ICurveIntersections(curve1, curve2, tolerance);
         }
 
         /***************************************************/
---
+++ Geometry_Engine/Query/IsSelfIntersecting.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods - Curves                   ****/
+        /***************************************************/
+
+        public static bool IsSelfIntersecting(this Line curve, double tolerance = Tolerance.Distance)
+        {
+            return false;
+        }
+
+        /***************************************************/
+
+        public static bool IsSelfIntersecting(this Arc curve, double tolerance = Tolerance.Distance)
+        {
+            return Math.Abs(curve.StartAngle - curve.EndAngle) > 2 * Math.PI;
+        }
+
+        /***************************************************/
+
+        public static bool IsSelfIntersecting(this Circle curve, double tolerance = Tolerance.Distance)
+        {
+            return false;
+        }
+
+        /***************************************************/
+
+        [NotImplemented]
+        public static bool IsSelfIntersecting(this NurbsCurve curve, double tolerance = Tolerance.Distance)
+        {
+            throw new NotImplementedException();
+        }
+
+        /***************************************************/
+
+        public static bool IsSelfIntersecting(this Polyline curve, double tolerance = Tolerance.Distance)
+        {
+            List<Line> curves = curve.SubParts();
+            if (curves.Count < 2)
+                return false;
+
+            bool closed = curve.IsClosed();
+            double sqTolerance = tolerance * tolerance;
+            for (int i = 0; i < curves.Count - 1; i++)
+            {
+                for (int j = i + 1; j < curves.Count; j++)
+                {
+                    foreach (Point intPt in curves[i].LineIntersections(curves[j]))
+                    {
+                        if (j == i + 1 && intPt.SquareDistance(curves[i].End) <= tolerance && intPt.SquareDistance(curves[j].Start) <= tolerance)
+                            continue;
+                        else if (closed && i == 0 && j == curves.Count - 1 && intPt.SquareDistance(curves[i].Start) <= tolerance && intPt.SquareDistance(curves[j].End) <= tolerance)
+                            continue;
+                        else
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /***************************************************/
+
+        public static bool IsSelfIntersecting(this PolyCurve curve, double tolerance = Tolerance.Distance)
+        {
+            List<ICurve> curves = curve.Curves;
+            if (curves.Count == 0)
+                return false;
+
+            if (curves.Count == 1)
+                return curves[0].IIsSelfIntersecting(tolerance);
+
+            bool closed = curve.IsClosed();
+            double sqTolerance = tolerance * tolerance;
+            for (int i = 0; i < curves.Count - 1; i++)
+            {
+                for (int j = i + 1; j < curves.Count; j++)
+                {
+                    foreach (Point intPt in curves[i].ICurveIntersections(curves[j]))
+                    {
+                        if (j == i + 1 && intPt.SquareDistance(curves[i].IEndPoint()) <= tolerance && intPt.SquareDistance(curves[j].IStartPoint()) <= tolerance)
+                            continue;
+                        else if (closed && i == 0 && j == curves.Count - 1 && intPt.SquareDistance(curves[i].IStartPoint()) <= tolerance && intPt.SquareDistance(curves[j].IEndPoint()) <= tolerance)
+                            continue;
+                        else
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+        /***************************************************/
+        /**** Public Methods = Interfaces               ****/
+        /***************************************************/
+
+        public static bool IIsSelfIntersecting(this ICurve curve, double tolerance = Tolerance.Distance)
+        {
+            return IsSelfIntersecting(curve as dynamic, tolerance);
+        }
+
+        /***************************************************/
+    }
+}
