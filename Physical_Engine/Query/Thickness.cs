6495e67b9cbf6b3715264740fa5ca202974a4aff
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-04-19 14:41:57 +0100

    Add construction related queries

---
+++ Physical_Engine/Query/Thickness.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Physical.Properties.Construction;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        [Description("BH.Engine.Physical.Query.Thickness => Returns the thickness of a generic construction")]
+        [Input("construction", "A generic Construction object")]
+        [Output("thickness", "The total thickness of the generic construction as a sum of all its layers")]
+        public static double Thickness(this IConstruction construction)
+        {
+            return Thickness(construction as dynamic);
+        }
+
+        [Description("BH.Engine.Physical.Query.Thickness => Returns the thickness of a construction")]
+        [Input("construction", "A Construction object")]
+        [Output("thickness", "The total thickness of the construction as a sum of all its layers")]
+        public static double Thickness(this Construction construction)
+        {
+            double thickness = 0;
+            foreach (Layer l in construction.Layers)
+                thickness += l.Thickness;
+
+            return thickness;
+        }
+    }
+}
