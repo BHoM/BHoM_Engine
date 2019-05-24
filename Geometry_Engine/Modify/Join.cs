a26f5e11cfc4eb478e7915423aa8d39824c6ae1a
LMarkowski <48125514+LMarkowski@users.noreply.github.com>
2019-05-24 13:08:15 +0200

    Moving Join to Compute

--- Geometry_Engine/Modify/Join.cs
+++ Geometry_Engine/Modify/Join.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -24,6 +24,7 @@ using BH.oM.Geometry;
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using BH.oM.Reflection.Attributes;
 
 namespace BH.Engine.Geometry
 {
@@ -33,158 +34,165 @@ namespace BH.Engine.Geometry
         /****                Join curves                ****/
         /***************************************************/
 
+        [DeprecatedAttribute("2.3", "Moved to Compute", typeof(Compute), "IJoin")]
         public static List<PolyCurve> IJoin(this List<ICurve> curves, double tolerance = Tolerance.Distance)
         {
-            List<PolyCurve> sections = curves.Select(c => new PolyCurve { Curves = new List<ICurve> { c.IClone() } }).ToList();
+            //List<PolyCurve> sections = curves.Select(c => new PolyCurve { Curves = new List<ICurve> { c.IClone() } }).ToList();
 
-            double sqTol = tolerance * tolerance;
-            int counter = 0;
-            while (counter < sections.Count)
-            {
-                for (int j = counter + 1; j < sections.Count; j++)
-                {
-                    if (sections[j].IStartPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
-                    {
-                        sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
-                        sections[counter].Curves.Reverse();
-                        sections[j].Curves.InsertRange(0, sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].IStartPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
-                    {
-                        sections[j].Curves.InsertRange(0, sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].IEndPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
-                    {
-                        sections[j].Curves.AddRange(sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].IEndPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
-                    {
-                        sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
-                        sections[counter].Curves.Reverse();
-                        sections[j].Curves.AddRange(sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                }
-                counter++;
-            }
-            return sections;
+            //double sqTol = tolerance * tolerance;
+            //int counter = 0;
+            //while (counter < sections.Count)
+            //{
+            //    for (int j = counter + 1; j < sections.Count; j++)
+            //    {
+            //        if (sections[j].IStartPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
+            //        {
+            //            sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
+            //            sections[counter].Curves.Reverse();
+            //            sections[j].Curves.InsertRange(0, sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].IStartPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
+            //        {
+            //            sections[j].Curves.InsertRange(0, sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].IEndPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
+            //        {
+            //            sections[j].Curves.AddRange(sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].IEndPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
+            //        {
+            //            sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
+            //            sections[counter].Curves.Reverse();
+            //            sections[j].Curves.AddRange(sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //    }
+            //    counter++;
+            //}
+            //return sections;
+            return BH.Engine.Geometry.Compute.IJoin(curves, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("2.3", "Moved to Compute", typeof(Compute), "Join")]
         public static List<PolyCurve> Join(this List<PolyCurve> curves, double tolerance = Tolerance.Distance)
         {
+            //List<PolyCurve> sections = new List<PolyCurve>();
 
-            List<PolyCurve> sections = new List<PolyCurve>();
+            //foreach(PolyCurve crv in curves)
+            //{
+            //    foreach(ICurve icrv in crv.ISubParts())
+            //    {
+            //        sections.Add(new PolyCurve { Curves = new List<ICurve> { icrv } });
+            //    }
+            //}
 
-            foreach(PolyCurve crv in curves)
-            {
-                foreach(ICurve icrv in crv.ISubParts())
-                {
-                    sections.Add(new PolyCurve { Curves = new List<ICurve> { icrv } });
-                }
-            }
-
-            double sqTol = tolerance * tolerance;
-            int counter = 0;
-            while (counter < sections.Count)
-            {
-                for (int j = counter + 1; j < sections.Count; j++)
-                {
-                    if (sections[j].IStartPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
-                    {
-                        sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
-                        sections[counter].Curves.Reverse();
-                        sections[j].Curves.InsertRange(0, sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].IStartPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
-                    {
-                        sections[j].Curves.InsertRange(0, sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].IEndPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
-                    {
-                        sections[j].Curves.AddRange(sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].IEndPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
-                    {
-                        sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
-                        sections[counter].Curves.Reverse();
-                        sections[j].Curves.AddRange(sections[counter].Curves);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                }
-                counter++;
-            }
-            return sections;
+            //double sqTol = tolerance * tolerance;
+            //int counter = 0;
+            //while (counter < sections.Count)
+            //{
+            //    for (int j = counter + 1; j < sections.Count; j++)
+            //    {
+            //        if (sections[j].IStartPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
+            //        {
+            //            sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
+            //            sections[counter].Curves.Reverse();
+            //            sections[j].Curves.InsertRange(0, sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].IStartPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
+            //        {
+            //            sections[j].Curves.InsertRange(0, sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].IEndPoint().SquareDistance(sections[counter].IStartPoint()) <= sqTol)
+            //        {
+            //            sections[j].Curves.AddRange(sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].IEndPoint().SquareDistance(sections[counter].IEndPoint()) <= sqTol)
+            //        {
+            //            sections[counter].Curves = sections[counter].Curves.Select(c => c.IFlip()).ToList();
+            //            sections[counter].Curves.Reverse();
+            //            sections[j].Curves.AddRange(sections[counter].Curves);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //    }
+            //    counter++;
+            //}
+            //return sections;
+            return BH.Engine.Geometry.Compute.Join(curves, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("2.3", "Moved to Compute", typeof(Compute), "Join")]
         public static List<Polyline> Join(this List<Line> lines, double tolerance = Tolerance.Distance)
         {
-            List<Polyline> sections = lines.Select(l => new Polyline { ControlPoints = l.ControlPoints() }).ToList();
-            return sections.Join(tolerance);
+            //List<Polyline> sections = lines.Select(l => new Polyline { ControlPoints = l.ControlPoints() }).ToList();
+            //return sections.Join(tolerance);
+            return BH.Engine.Geometry.Compute.Join(lines, tolerance);
         }
 
         /***************************************************/
 
+        [DeprecatedAttribute("2.3", "Moved to Compute", typeof(Compute), "Join")]
         public static List<Polyline> Join(this List<Polyline> curves, double tolerance = Tolerance.Distance)
         {
-            List<Polyline> sections = curves.Select(l => new Polyline { ControlPoints = l.ControlPoints.Select(p => p.Clone()).ToList() }).ToList();
+            //List<Polyline> sections = curves.Select(l => new Polyline { ControlPoints = l.ControlPoints.Select(p => p.Clone()).ToList() }).ToList();
 
-            double sqTol = tolerance * tolerance;
-            int counter = 0;
-            while (counter < sections.Count)
-            {
-                for (int j = counter + 1; j < sections.Count; j++)
-                {
-                    if (sections[j].ControlPoints[0].SquareDistance(sections[counter].ControlPoints[0]) <= sqTol)
-                    {
-                        List<Point> aPts = sections[counter].ControlPoints.Skip(1).ToList();
-                        aPts.Reverse();
-                        sections[j].ControlPoints.InsertRange(0, aPts);
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].ControlPoints[0].SquareDistance(sections[counter].ControlPoints.Last()) <= sqTol)
-                    {
-                        List<Point> aPts = sections[counter].ControlPoints;
-                        aPts.AddRange(sections[j].ControlPoints.Skip(1).ToList());
-                        sections[j].ControlPoints = aPts;
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].ControlPoints.Last().SquareDistance(sections[counter].ControlPoints[0]) <= sqTol)
-                    {
-                        sections[j].ControlPoints.AddRange(sections[counter].ControlPoints.Skip(1).ToList()); ;
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                    else if (sections[j].ControlPoints.Last().SquareDistance(sections[counter].ControlPoints.Last()) <= sqTol)
-                    {
-                        sections[counter].ControlPoints.Reverse();
-                        sections[j].ControlPoints.AddRange(sections[counter].ControlPoints.Skip(1).ToList()); ;
-                        sections.RemoveAt(counter--);
-                        break;
-                    }
-                }
-                counter++;
-            }
-            return sections;
+            //double sqTol = tolerance * tolerance;
+            //int counter = 0;
+            //while (counter < sections.Count)
+            //{
+            //    for (int j = counter + 1; j < sections.Count; j++)
+            //    {
+            //        if (sections[j].ControlPoints[0].SquareDistance(sections[counter].ControlPoints[0]) <= sqTol)
+            //        {
+            //            List<Point> aPts = sections[counter].ControlPoints.Skip(1).ToList();
+            //            aPts.Reverse();
+            //            sections[j].ControlPoints.InsertRange(0, aPts);
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].ControlPoints[0].SquareDistance(sections[counter].ControlPoints.Last()) <= sqTol)
+            //        {
+            //            List<Point> aPts = sections[counter].ControlPoints;
+            //            aPts.AddRange(sections[j].ControlPoints.Skip(1).ToList());
+            //            sections[j].ControlPoints = aPts;
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].ControlPoints.Last().SquareDistance(sections[counter].ControlPoints[0]) <= sqTol)
+            //        {
+            //            sections[j].ControlPoints.AddRange(sections[counter].ControlPoints.Skip(1).ToList()); ;
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //        else if (sections[j].ControlPoints.Last().SquareDistance(sections[counter].ControlPoints.Last()) <= sqTol)
+            //        {
+            //            sections[counter].ControlPoints.Reverse();
+            //            sections[j].ControlPoints.AddRange(sections[counter].ControlPoints.Skip(1).ToList()); ;
+            //            sections.RemoveAt(counter--);
+            //            break;
+            //        }
+            //    }
+            //    counter++;
+            //}
+            //return sections;
+            return BH.Engine.Geometry.Compute.Join(curves, tolerance);
         }
 
         /***************************************************/
