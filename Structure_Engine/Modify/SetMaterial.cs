18fd2606dd551b4b3e4785e5a30fbbfebfd1dfd1
PiotrBuda <32908903+PiotrBuda@users.noreply.github.com>
2019-06-04 16:52:33 +0100

    Formatting updated

--- Structure_Engine/Modify/SetMaterial.cs
+++ Structure_Engine/Modify/SetMaterial.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2018, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -33,9 +33,6 @@ namespace BH.Engine.Structure
         /**** Public Methods                            ****/
         /***************************************************/
 
-
-
-
         public static Bar SetMaterial(this Bar bar, IMaterialFragment material)
         {
             Bar clone = bar.GetShallowClone() as Bar;
@@ -49,7 +46,9 @@ namespace BH.Engine.Structure
             clone.SectionProperty = Isectionproperty;
             return clone;
         }
-        
+
+        /***************************************************/
+
     }
 
 }
