d92abc6cce06799cc7e31cc6cd24c2f82cbe5ad5
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2019-06-19 15:08:19 +0100

    Geometry_Engine - replace namespaces

--- Geometry_Engine/Compute/PointClustersDBSCAN.cs
+++ Geometry_Engine/Compute/PointClustersDBSCAN.cs
@@ -20,7 +20,7 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using BH.Engine.DataStructure;
+using BH.Engine.Data;
 using BH.oM.Geometry;
 using System;
 using System.Collections.Generic;
