f7ab0d727bc4a67f3e4bba37491d3989cb4b460d
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-07-02 09:00:32 +0100

    #1046 - points in space fix

--- Environment_Engine/Query/PointInSpace.cs
+++ Environment_Engine/Query/PointInSpace.cs
@@ -20,10 +20,11 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using BH.oM.Environment.Elements;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
+using BH.oM.Environment.Elements;
 using BH.oM.Geometry;
 using BH.Engine.Geometry;
 
@@ -38,46 +39,33 @@ namespace BH.Engine.Environment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Returns a collection of BHoM Geometry Points that are at the centre of each space")]
-        [Input("panelsAsSpaces", "The nested collection of Environment Panels that represent the spaces to get the centre of")]
-        [Output("centrePoints", "A collection of points at the centre of each space")]
-        public static List<Point> SpaceCentres(this List<List<Panel>> panelsAsSpaces)
+        [Description("Returns a collection of BHoM Geometry Points that are in each space")]
+        [Input("panelsAsSpaces", "The nested collection of Environment Panels that represent the spaces to points in")]
+        [Output("spacePoints", "A collection of points in each space")]
+        public static List<Point> PointsInSpaces(this List<List<Panel>> panelsAsSpaces)
         {
-            List<Point> centrePts = new List<Point>();
+            List<Point> spacePnts = new List<Point>();
             foreach (List<Panel> space in panelsAsSpaces)
-                centrePts.Add(space.SpaceCentre());
+                spacePnts.Add(space.PointInSpace());
 
-            return centrePts;
+            return spacePnts;
         }
 
-        [Description("Returns a BHoM Geometry Points that is at the centre of the provided space")]
-        [Input("panelsAsSpace", "The collection of Environment Panels that represent a single space to get the centre of")]
-        [Output("centrePoint", "A point at the centre of the space")]
-        public static Point SpaceCentre(this List<Panel> panelsAsSpace)
+        [Description("Returns a BHoM Geometry Point that is in the provided space")]
+        [Input("panelsAsSpace", "The collection of Environment Panels that represent a single space to get the point in")]
+        [Output("spacePoint", "A point in the space")]
+        public static Point PointInSpace(this List<Panel> panelsAsSpace)
         {
-            //Calculate the centre point of the space comprised of the building elements
-            //Done using the centre of mass of the vertices
+            Polyline floor = panelsAsSpace.FloorGeometry();
+            Point spacePnt = floor.PointInRegion();
 
-            List<Point> vertexPoints = new List<Point>();
-            foreach (Panel be in panelsAsSpace)
-                vertexPoints.AddRange(be.Polyline().IControlPoints());
+            double height = 0.0;
+            foreach (Panel p in panelsAsSpace)
+                height = Math.Max(height, p.Height());
 
-            double centreX = 0;
-            double centreY = 0;
-            double centreZ = 0;
+            spacePnt.Z += height / 2;
 
-            foreach(Point p in vertexPoints)
-            {
-                centreX += p.X;
-                centreY += p.Y;
-                centreZ += p.Z;
-            }
-
-            centreX /= vertexPoints.Count;
-            centreY /= vertexPoints.Count;
-            centreZ /= vertexPoints.Count;
-
-            return BH.Engine.Geometry.Create.Point(centreX, centreY, centreZ);
+            return spacePnt;
         }
     }
 }
