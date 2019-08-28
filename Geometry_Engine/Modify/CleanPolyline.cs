60a8662dc16845372552b7dbc9eced775432bb44
michaldengusiak <michal.dengusiak@burohappold.com>
2019-08-28 14:05:37 +0100

    fix #1151

--- Geometry_Engine/Modify/CleanPolyline.cs
+++ Geometry_Engine/Modify/CleanPolyline.cs
@@ -36,11 +36,12 @@ namespace BH.Engine.Geometry
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Returns a polyline defined by the points which result in deviations from a straight line only. For example, if a polyline has 3 points in a straight line, the middle point is removed as part of this cleaning process. This is designed for closed polylines only")]
+        [Description("Returns a polyline defined by the points which result in deviations from a straight line only. For example, if a polyline has 3 points in a straight line, the middle point is removed as part of this cleaning process if distance betwen point is less than distanceTolerance. This is designed for closed polylines only")]
         [Input("polyline", "The polyline you wish to clean by removing unnecessary points")]
-        [Input("tolerance", "The tolerance of the angle that defines a straight line. Default is set to the value defined by BH.oM.Geometry.Tolerance.Angle")]
+        [Input("angleTolerance", "The tolerance of the angle that defines a straight line. Default is set to the value defined by BH.oM.Geometry.Tolerance.Angle")]
+        [Input("distanceTolerance", "The distance between two point that should be removed. Default is set to the value 0.02")]
         [Output("polyline", "The cleaned polyline")]
-        public static Polyline CleanPolyline(this Polyline polyline, double angleTolerance = Tolerance.Angle, double distanceTolerance = Tolerance.Distance)
+        public static Polyline CleanPolyline(this Polyline polyline, double angleTolerance = Tolerance.Angle, double distanceTolerance = 0.02)
         {
             //This method is for closed polylines only at the moment
             if(!polyline.IsClosed())
