0dfc46d69d2240b47a28717c3763e3b199217210
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-09-06 09:55:30 +0100

    #1162 add DateTime query function for SpaceTime object

---
+++ Environment_Engine/Query/DateTime.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using BH.oM.Environment;
+
+using BH.Engine.Geometry;
+using BH.oM.Geometry;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+using BH.oM.Environment.Climate;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Returns the azimuth of a given environmental object")]
+        [Input("environmentObject", "Any object implementing the IEnvironmentObject interface that can have its azimuth queried")]
+        [Input("referenceVector", "The reference vector for querying the azimuth from the object")]
+        [Output("azimuth", "The azimuth of the Environment Object")]
+        public static DateTime DateTime(this SpaceTime spaceTime)
+        {
+            return new DateTime(spaceTime.Year, spaceTime.Month, spaceTime.Day, spaceTime.Hour, spaceTime.Minute, spaceTime.Second, spaceTime.Millisecond);
+        }
+    }
+
+}
