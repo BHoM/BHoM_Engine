3349eefa923e00ee5a75dbcd539dcb5efc979b79
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2019-09-06 10:02:50 +0100

    #1162 create methods for space time and location objects

--- Environment_Engine/Create/Location.cs
+++ Environment_Engine/Create/Location.cs
@@ -22,33 +22,41 @@
 
 using System;
 using System.Collections.Generic;
-
 using System.Linq;
-using BH.oM.Environment;
+using System.Text;
+using System.Threading.Tasks;
 
-using BH.Engine.Geometry;
+using BH.oM.Environment.Climate;
 using BH.oM.Geometry;
 
 using BH.oM.Reflection.Attributes;
 using System.ComponentModel;
 
-using BH.oM.Environment.Climate;
-
 namespace BH.Engine.Environment
 {
-    public static partial class Query
+    public static partial class Create
     {
         /***************************************************/
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Returns the date time object from a Space Time")]
-        [Input("spaceTime", "A space time object defining a point in space and time")]
-        [Output("dateTime", "A C# DateTime object with the values from the SpaceTime object")]
-        public static DateTime DateTime(this SpaceTime spaceTime)
+        [Description("Returns an Environment Location object")]
+        [Input("latitude", "The latitude for the location, default 0")]
+        [Input("longitude", "The longitude for the location, default 0")]
+        [Input("elevation", "The elevation at the location, default 0")]
+        [Input("utcOffset", "The offset from UTC for the location (positive or negative), default 0")]
+        [Input("name", "The name of the location, default empty string")]
+        [Output("location", "An Environment location object - used for defining locations in space for climate analysis")]
+        public static Location Location(double latitude = 0, double longitude = 0, double elevation = 0, double utcOffset = 0, string name = "")
         {
-            return new DateTime(spaceTime.Year, spaceTime.Month, spaceTime.Day, spaceTime.Hour, spaceTime.Minute, spaceTime.Second, spaceTime.Millisecond);
+            return new Location
+            {
+                Name = name,
+                Latitude = latitude,
+                Longitude = longitude,
+                Elevation = elevation,
+                UtcOffset = utcOffset,
+            };
         }
     }
-
 }
