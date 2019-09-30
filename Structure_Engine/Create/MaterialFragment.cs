a15f25767d2e5d476baa0cd10f709a878b80d1d4
Josef Taylor <josef.taylor@burohappold.com>
2019-09-30 16:13:00 -0400

    Update carbon numbers to slightly more realistic

--- Structure_Engine/Create/MaterialFragment.cs
+++ Structure_Engine/Create/MaterialFragment.cs
@@ -45,7 +45,7 @@ namespace BH.Engine.Structure
         [Input("dampingRatio", "Dynamic damping ratio of the material. Will be stored on the material fragment")]
         [Input("embodiedCarbon", "Embodied carbon for the material. Will be stored on the material fragment")]
         [Output("Material", "The created material with a aluminium fragment")]
-        public static Aluminium Aluminium(string name, double E = 70000000000, double v = 0.34, double tC = 0.000023, double density = 2710, double dampingRatio = 0, double embodiedCarbon = 100)
+        public static Aluminium Aluminium(string name, double E = 70000000000, double v = 0.34, double tC = 0.000023, double density = 2710, double dampingRatio = 0, double embodiedCarbon = 7.9)
         {
             return new Aluminium()
             {
@@ -71,7 +71,7 @@ namespace BH.Engine.Structure
         [Input("cylinderStrength", "Cylinder strength of the concrete material. Given in [Pa] or [N/m].Will be stored on the material fragment")]
         [Input("embodiedCarbon", "Embodied carbon for the material. Will be stored on the material fragment")]
         [Output("Material", "The created material with a concrete fragment")]
-        public static Concrete Concrete(string name, double E = 33000000000, double v = 0.2, double tC = 0.00001, double density = 2550, double dampingRatio = 0, double cubeStrength = 0, double cylinderStrength = 0, double embodiedCarbon = 200)
+        public static Concrete Concrete(string name, double E = 33000000000, double v = 0.2, double tC = 0.00001, double density = 2550, double dampingRatio = 0, double cubeStrength = 0, double cylinderStrength = 0, double embodiedCarbon = 0.12)
         {
             return new Concrete()
             {
@@ -99,7 +99,7 @@ namespace BH.Engine.Structure
         [Input("ultimateStress", "Stress level at break for the material. Given in [Pa] or [N/m]. Will be stored on the material fragment")]
         [Input("embodiedCarbon", "Embodied carbon for the material. Will be stored on the material fragment")]
         [Output("Material", "The created material with a steel fragment")]
-        public static Steel Steel(string name, double E = 210000000000, double v = 0.3, double tC = 0.000012, double density = 7850, double dampingRatio = 0, double yieldStress = 0, double ultimateStress = 0, double embodiedCarbon = 300)
+        public static Steel Steel(string name, double E = 210000000000, double v = 0.3, double tC = 0.000012, double density = 7850, double dampingRatio = 0, double yieldStress = 0, double ultimateStress = 0, double embodiedCarbon = 1.3)
         {
             return new Steel()
             {
@@ -126,7 +126,7 @@ namespace BH.Engine.Structure
         [Input("dampingRatio", "Dynamic damping ratio of the material. Will be stored on the material fragment")]
         [Input("embodiedCarbon", "Embodied carbon for the material. Will be stored on the material fragment")]
         [Output("Material", "The created material with a timber fragment")]
-        public static Timber Timber(string name, Vector E, Vector v, Vector G, Vector tC, double density, double dampingRatio, double embodiedCarbon = 0)
+        public static Timber Timber(string name, Vector E, Vector v, Vector G, Vector tC, double density, double dampingRatio, double embodiedCarbon = 0.4)
         {
             return new Timber()
             {
