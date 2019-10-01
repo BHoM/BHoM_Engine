c5ae9a1a3b28f6c2dfc162ecca17cea14691ab2c
Łukasz Markowski <lukasz.markowski@burohappold.com>
2019-10-01 11:07:07 +0200

    Update Extend.cs

--- Geometry_Engine/Modify/Extend.cs
+++ Geometry_Engine/Modify/Extend.cs
@@ -32,10 +32,10 @@ namespace BH.Engine.Geometry
     public static partial class Modify
     {
         /***************************************************/
-        /**** Private Methods - Curves                  ****/
+        /**** Public Methods - Curves                  ****/
         /***************************************************/
 
-        private static Line Extend(this Line curve, double start = 0.0, double end = 0.0)
+        public static Line Extend(this Line curve, double start = 0.0, double end = 0.0)
         {
             Vector dir = curve.Direction();
             return new Line { Start = curve.Start - dir * start, End = curve.End + dir * end };
