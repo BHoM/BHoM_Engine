fa875eb63a8c4a2ff3e1501003c15763f35528cf
Maja Lindroth <maja.lindroth@burohappold.com>
2019-10-10 10:45:08 +0100

    #1176 Added SetOpeningType to Environment_Engine Modify

---
+++ Environment_Engine/Modify/SetOpeningType.cs
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Linq;
+using System.Collections.Generic;
+using BH.oM.Environment.Elements;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+using BH.oM.Environment.Fragments;
+using System;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+
+namespace BH.Engine.Environment
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        [Description("Set the opening type based on the provided type")]
+        [Input("openings", "A collection of Environment Openings to set the type off")]
+        [Input("type", "The opening type to assign to the openings")]
+        [Output("openings", "A collection of Environment Openings with their type set")]
+
+        public static List<Opening> SetOpeningType (this List<Opening> openings, OpeningType type)
+        {
+            foreach (Opening o in openings)
+                o.Type = type;
+
+            return openings;
+                
+        }
+    }
+    
+}
