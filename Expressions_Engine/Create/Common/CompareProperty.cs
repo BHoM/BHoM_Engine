d287fa5ab3406700b3abe2fded6decb23bd64394
Andrew Wakeman <andrew.wakeman@burohappold.com>
2019-10-14 08:39:48 +0100

    Add basic property comparison wrapper

---
+++ Expressions_Engine/Create/Common/CompareProperty.cs
@@ -0,0 +1,26 @@
+﻿using BH.oM.Reflection.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Expressions
+{
+    public static partial class Create
+    {
+        public static LambdaExpression CompareProperty(string property, LambdaExpression comparitor)
+        {
+            ParameterExpression a = Expression.Parameter(typeof(object));
+            ParameterExpression b = Expression.Parameter(typeof(object));
+
+            Expression prop_a = GetProperty(a, property);
+            Expression prop_b = GetProperty(b, property);
+
+            return Expression.Lambda(Expression.Invoke(comparitor, prop_a, prop_b), a, b);
+        }
+    }
+}
---
+++ Expressions_Engine/Create/GetProperty.cs
@@ -0,0 +1,39 @@
+﻿using BH.oM.Reflection.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Expressions
+{
+    public static partial class Create
+    {
+        [Input("obj","The object to get the property from")]
+        [Input("property","The name of the property")]
+        [Output("A MethodCallExpression that represents a call to PropertyValue for the object supplied")]
+        [Description("Creates a MethodCallExpression that represents a call to PropertyValue for the object supplied")]
+        public static MethodCallExpression GetProperty(Expression obj, Expression property)
+        {
+            return Call(typeof(Reflection.Query), "PropertyValue", new List<Expression> { obj, property });
+        }
+
+        [Input("obj","The object to get the property from")]
+        [Input("property","The name of the property")]
+        [Output("A MethodCallExpression that represents a call to PropertyValue for the object supplied")]
+        [Description("Creates a MethodCallExpression that represents a call to PropertyValue for the object supplied")]
+        public static MethodCallExpression GetProperty(Expression obj, string property)
+        {
+            return GetProperty(obj, Expression.Constant(property));
+        }
+
+        public static LambdaExpression GetProperty(string property)
+        {
+            ParameterExpression param = Expression.Parameter(typeof(object), "obj");
+            return Expression.Lambda(GetProperty(param, Expression.Constant(property)), param);
+        }
+    }
+}
