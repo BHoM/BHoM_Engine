661ad475ad3a4f27c6bb96a6035f0f02950443aa
Isak Naslund <Isak.Naslund@burohappold.com>
2019-10-16 17:18:36 +0200

    Add create method for Dataset

---
+++ Library_Engine/Create/Dataset.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Base;
+using BH.oM.Data.Library;
+
+namespace BH.Engine.Library
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("")]
+        [Input("", "")]
+        [Output("", "")]
+        public static Dataset Dataset(object data, Source source, string name, DateTime? timeOfCreation = null)
+        {
+            DateTime time = timeOfCreation == null ? DateTime.UtcNow : (DateTime)timeOfCreation;
+ 
+            return new Dataset
+            {
+                SourceInformation = source,
+                Data = data,
+                Name = name,
+                TimeOfCreation = time
+            }
+
+            throw new NotImplementedException();
+        }
+
+        /***************************************************/
+    }
+}
