4c47007a9a5f118f9c00dfc95217e895366e518f
Rob May <rob.may@burohappold.com>
2019-11-07 21:19:37 +0000

    Add OrderBy for comment

--- Data_Engine/Query/Linq/OrderBy.cs
+++ Data_Engine/Query/Linq/OrderBy.cs
@@ -19,11 +19,15 @@
  * You should have received a copy of the GNU Lesser General Public License     
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
- 
+
 
 using System.Collections.Generic;
 using System.Linq;
 using BH.Engine.Reflection;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+using System;
+using BHRE = BH.Engine.Reflection;
 
 namespace BH.Engine.Data
 {
@@ -33,11 +37,40 @@ namespace BH.Engine.Data
         /**** Public Methods                            ****/
         /***************************************************/
 
+        [Description("Orders a list of objects by a property of the object - e.g. input 'Name' to sort objects by their names. Note that not all object properties have sortable values")]
+
         public static List<T> OrderBy<T>(this List<T> objects, string propertyName)
         {
-            return objects.OrderBy(x => BH.Engine.Reflection.Query.PropertyValue(x, propertyName)).ToList();
+            if (objects == null || objects.Count == 0) return null;
+            int groupedObjects = objects.GroupBy(x => x.GetType()).Count();
+            System.Type objectType = objects[0].GetType();
+            if (groupedObjects != 1)
+            {
+                BHRE.Compute.RecordError("All objects in the list to be sorted should be of similiar type.");
+                return null;
+            }
+            try
+            {
+                if (!propertyName.Contains("."))
+                {
+                    System.Reflection.PropertyInfo prop = objects[0].GetType().GetProperty(propertyName); 
+                    if (prop != null)
+                        return objects.OrderBy(x => prop.GetValue(x)).ToList();
+                }
+
+                BHRE.Compute.RecordNote("CustomData or nested property is used as the sorting property (using 'Object.Property.Property...') which is slower than a base property.");
+                return objects.OrderBy(x => BHRE.Query.PropertyValue(x, propertyName)).ToList();
+            }
+            catch
+            {
+                BHRE.Compute.RecordWarning("The sorting property does not have a sort function.");
+                return null;
+            }
         }
 
         /***************************************************/
+
+        //Add select linq
+        //Add where linq (filter)
     }
 }
