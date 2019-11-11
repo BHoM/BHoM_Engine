aa743a00c789a718f339b38bfaee6f51a3657870
Jonathan Nillius <jonathan.nillius@burohappold.com>
2019-11-11 09:44:57 +0000

    added isectionprofile and layout fixes

--- Structure_Engine/Query/Description.cs
+++ Structure_Engine/Query/Description.cs
@@ -32,87 +32,132 @@ namespace BH.Engine.Structure
 {
     public static partial class Query
     {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
 
+        public static string Description(this ISectionProfile profile)
+        {
+            return "Box " + profile.Height + "x" + profile.Width + "x" + profile.WebThickness + "x" + profile.FlangeThickness + "x" + profile.RootRadius + "x" + profile.ToeRadius;
+        }
 
         public static string Description(this BoxProfile profile)
         {
             return "Box " + profile.Height + "x" + profile.Width + "x" + profile.Thickness;
         }
 
+        /***************************************************/
+
         public static string Description(this AngleProfile profile)
         {
             return "Angle " + profile.Height + "x" + profile.Width + "x" + profile.WebThickness + "x" + profile.FlangeThickness + "x" + profile.RootRadius + "x" + profile.ToeRadius;
         }
+
+        /***************************************************/
 
         public static string Description(this ChannelProfile profile)
         {
             return "Channel " + profile.Height + "x" + profile.FlangeWidth + "x" + profile.WebThickness + "x" + profile.FlangeThickness + "x" + profile.RootRadius + "x" + profile.ToeRadius;
         }
 
+        /***************************************************/
+
         public static string Description(this CircleProfile profile)
         {
             return "Circle " + profile.Diameter;
         }
+
+        /***************************************************/
 
         public static string Description(this FabricatedBoxProfile profile)
         {
             return "FabBox " + profile.Height + "x" + profile.Width + "x" + profile.WebThickness + "x" + profile.TopFlangeThickness + "x" + profile.BotFlangeThickness;
         }
 
+        /***************************************************/
+
         public static string Description(this GeneralisedFabricatedBoxProfile profile)
         {
             return "GenFabBox " + profile.Height + "x" + profile.Width + "x" + profile.WebThickness + "x" + profile.TopFlangeThickness + "x" + profile.BotFlangeThickness + "x" + profile.TopLeftCorbelWidth + "x" + profile.TopRightCorbelWidth;
         }
+
+        /***************************************************/
 
         public static string Description(this KiteProfile profile)
         {
             return "Kite " + profile.Angle1 + "x" + profile.Width1 + "x" + profile.Thickness;
         }
 
+        /***************************************************/
+
         public static string Description(this FabricatedISectionProfile profile)
         {
             return "FabI " + profile.Height + "x" + profile.TopFlangeWidth + "x" + profile.BotFlangeWidth + "x" + profile.WebThickness + "x" + profile.TopFlangeThickness + "x" + profile.BotFlangeThickness + "x" + profile.WeldSize;
         }
+
+        /***************************************************/
 
         public static string Description(this FreeFormProfile profile)
         {
             return "FreeForm";
         }
 
+        /***************************************************/
+
         public static string Description(this RectangleProfile profile)
         {
             return "Rectangle " + profile.Height + "x" + profile.Width + "x" + profile.CornerRadius;
         }
+
+        /***************************************************/
 
         public static string Description(this TSectionProfile profile)
         {
             return "T " + profile.Height + "x" + profile.Width + "x" + profile.WebThickness + "x" + profile.FlangeThickness + "x" + profile.RootRadius + "x" + profile.ToeRadius;
         }
 
+        /***************************************************/
+
         public static string Description(this GeneralisedTSectionProfile profile)
         {
             return "GenT " + profile.Height + "x" + profile.WebThickness + "x" + profile.LeftOutstandWidth + "x" + profile.LeftOutstandThickness + "x" + profile.RightOutstandWidth + "x" + profile.RightOutstandThickness;
         }
+
+        /***************************************************/
 
         public static string IDescription(this IProfile profile)
         {
             return Description(profile as dynamic);
         }
 
+        /***************************************************/
+
         public static string Description(this SteelSection section)
         {
             return "Steel " + section.SectionProfile.IDescription() + "-" + section.Material.Name;
         }
+
+        /***************************************************/
 
         public static string Description(this ConcreteSection section)
         {
             return "Concrete " + section.SectionProfile.IDescription() + "-" + section.Material.Name;
         }
 
-        public static string Description(this ISectionProperty profile)
+        /***************************************************/
+
+        public static string IDescription(this ISectionProperty section)
         {
-            return Description(profile as dynamic);
+            return Description(section as dynamic);
         }
 
+        /***************************************************/
+
+        public static string Description()
+        {
+            return "No description found";
+        }
+
+        /***************************************************/
     }
 }
