4f7b17532ccc585b550d4468a2a19f090bbb3480
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-01-17 12:41:51 +0000

    #1414 compliance update following PR review

--- Geometry_Engine/Modify/RoundPoint.cs
+++ Geometry_Engine/Modify/RoundPoint.cs
@@ -38,7 +38,7 @@ namespace BH.Engine.Geometry
         [Input("point", "The BHoM Geometry Point to modify")]
         [Input("decimalPlaces", "The number of decimal places to round to, default 6")]
         [Output("point", "The modified BHoM Geometry Point")]
-        public static Point RoundedPoint(this Point point, int decimalPlaces = 6)
+        public static Point RoundPoint(this Point point, int decimalPlaces = 6)
         {
             return new Point
             {
