d2b994dde450479357603d224368dd2eae8d2ce6
Isak Naslund <Isak.Naslund@burohappold.com>
2020-01-17 16:06:36 +0100

    Instansiating new FragmentSet in ShallowClone method

--- BHoM_Engine/Query/ShallowClone.cs
+++ BHoM_Engine/Query/ShallowClone.cs
@@ -63,6 +63,11 @@ namespace BH.Engine.Base
             else
                 clone.Tags = new HashSet<string>();
 
+            if (bhomObject.Fragments != null)
+                clone.Fragments = new FragmentSet(bhomObject.Fragments);
+            else
+                clone.Fragments = new FragmentSet();
+
             if (newGuid)
                 clone.BHoM_Guid = Guid.NewGuid();
 
