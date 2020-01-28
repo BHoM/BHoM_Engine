2c63d3b88a1412f190afd3ea4aa6f7f8455fad52
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-01-28 09:41:55 +0000

    Update CloneTest file for BHoM_Engine only

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -8,6 +8,10 @@ $repo = "BHoM"
 # **** Defining Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
 
+# **** Cloning Repo ****
+Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
+git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+
 # **** Restore NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
@@ -17,10 +21,6 @@ $repo = "BHoM_Engine"
 
 # **** Defining Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
-
-# **** Cloning Repo ****
-Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
-git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
 
 # **** Restore NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
