bc13c36dc88750103f92a8e9893d8a26664dbd87
Erik Narhi <Erik.Narhi@burohappold.com>
2020-01-28 13:03:02 -0800

    Nested objects with headers supported using iterative approach

--- Serialiser_Engine/Convert/Json.cs
+++ Serialiser_Engine/Convert/Json.cs
@@ -77,16 +77,25 @@ namespace BH.Engine.Serialiser
             else if (json.StartsWith("["))
             {
                 Reflection.Compute.RecordNote("The string provided represents a top-level Json Array instead of a Json object.\nDeserializing to a CustomObject.");
+
                 BsonArray array = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonArray>(json);
 
-                return new BH.oM.Base.CustomObject
+                List<object> objects = new List<object>();
+
+                for (int i=0; i<array.Count; i++)
+                {
+                    object obj = FromJson(array[i].ToString());
+                    objects.Add(obj);
+                }
+                return new BH.oM.Base.CustomObject()
                 {
                     CustomData = new Dictionary<string, object>()
                     {
-                        { "Objects", array.Select(b => Convert.FromBson(b.AsBsonDocument)).ToList() }
+                        {"Objects", objects}
                     }
                 };
             }
+
             else
             {
                 // Could we do something when a string is not a valid json?
