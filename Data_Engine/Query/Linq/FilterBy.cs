88b54d48e5e05c772bf783c6d1c4b4d1fe70e547
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-02-01 20:59:17 +0000

    Update OrderBy and FilterBy per comments/refactoring

--- Data_Engine/Query/Linq/FilterBy.cs
+++ Data_Engine/Query/Linq/FilterBy.cs
@@ -27,7 +27,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using BHRE = BH.Engine.Reflection;
+using BH.Engine.Reflection;
 
 namespace BH.Engine.Data
 {
@@ -43,6 +43,7 @@ namespace BH.Engine.Data
         [Input("value", "The value of the selected property to filter the objects by")]
         [Input("ignoreStringCase", "Ignore upper/lower case letters if the property filtered by is a text/string.")]
         [Input("exactStringMatch", "If true checks if the property filtered by contains the text value (filtering must be a string/text)")]
+        [Output("filteredObjects", "The collection of objects that match the given filter conditions")]
         public static List<T> FilterBy<T>(this List<T> objects, string propertyName = null, object value = null, bool ignoreStringCase = false, bool exactStringMatch = true)
         {
             if (objects == null || objects.Count == 0)
@@ -70,86 +71,27 @@ namespace BH.Engine.Data
                 return new List<T>();
             }
 
-            bool isNested = propertyName.Contains(".");
-            bool isCustomData = false;
-
-            System.Reflection.PropertyInfo prop = firstObject.GetType().GetProperty(propertyName);
-            if (prop == null && firstObject is IBHoMObject)
-            {
-                if ((firstObject as IBHoMObject).CustomData.ContainsKey(propertyName))
-                    isCustomData = true;
-            }
-
             System.Type type = PropertyType(firstObject, propertyName);
 
-            int groupedObjects = objects.GroupBy(x => x.GetType()).Count();
-            if (groupedObjects != 1)
-            {
-                BHRE.Compute.RecordError("All objects in the list to be sorted should be of similiar type.");
-                return null;
-            }
+            List<T> filteredObjects = new List<T>();
 
             if (type == typeof(System.String))
             {
-                string stringValue = value.ToString();
-                if (ignoreStringCase && exactStringMatch)
-                {
-                    if (!isNested)
-                        return objects.Where(x => string.Equals((string)prop.GetValue(x), stringValue, System.StringComparison.CurrentCultureIgnoreCase)).ToList();
-                    else
-                        return objects.Where(x => string.Equals((string)BHRE.Query.PropertyValue(x, propertyName), stringValue, System.StringComparison.CurrentCultureIgnoreCase)).ToList();
-                }
+                if (ignoreStringCase)
+                    filteredObjects.AddRange(objects.Where(x => x.PropertyValue(propertyName).ToString().Equals(value.ToString(), System.StringComparison.CurrentCultureIgnoreCase)).ToList());
+                else
+                    filteredObjects.AddRange(objects.Where(x => x.PropertyValue(propertyName).ToString().Equals(value.ToString())).ToList());
+
                 if (!exactStringMatch)
-                {
-                    if (!isNested)
-                    {
-                        if (ignoreStringCase)
-                            return objects.Where(x => (prop.GetValue(x) as string).ToUpper().Contains(stringValue.ToUpper())).ToList();
-                        else
-                            return objects.Where(x => (prop.GetValue(x) as string).Contains(stringValue)).ToList();
-                    }
-                    else
-                    {
-                        if (ignoreStringCase)
-                            return objects.Where(x => (BHRE.Query.PropertyValue(x, propertyName) as string).ToUpper().Contains(stringValue.ToUpper())).ToList();
-                        else
-                            return objects.Where(x => (BHRE.Query.PropertyValue(x, propertyName) as string).Contains(stringValue)).ToList();
-                    }
-                }
-                if (!isNested)
-                    return objects.Where(x => prop.GetValue(x).Equals(stringValue)).ToList();
-                else
-                    return objects.Where(x => BHRE.Query.PropertyValue(x, propertyName).Equals(stringValue)).ToList();
+                    filteredObjects.AddRange(objects.Where(x => x.PropertyValue(propertyName).ToString().Contains(value.ToString())).ToList());
             }
-
-            if (type == typeof(System.Double))
+            else
             {
-                double doubleValue = 0.0;
-                string searchValue = value.ToString();
-                int rounding = searchValue.Contains('.') ? searchValue.Split('.').Last().Length : 0;
-
-                try
-                { 
-                    double.TryParse(searchValue, out doubleValue); 
-                }
-                catch
-                { 
-                    BHRE.Compute.RecordError("Property value to search for cannot be converted to a number");
-                }
-
-                if (!isNested && !isCustomData)
-                    return objects.Where(x => System.Math.Round((double)prop.GetValue(x), rounding) == doubleValue).ToList();
-                else
-                    return objects.Where(x => System.Math.Round((double)BHRE.Query.PropertyValue(x, propertyName), rounding) == doubleValue).ToList();
+                dynamic val = System.Convert.ChangeType(value, type);
+                filteredObjects = objects.Where(x => (System.Convert.ChangeType(x.PropertyValue(propertyName), type) as dynamic) == val).ToList();
             }
 
-            if (prop != null && !isNested)
-            {
-                dynamic val = System.Convert.ChangeType(value, prop.PropertyType);
-                return objects.Where(x => (System.Convert.ChangeType(prop.GetValue(x), type) as dynamic) == val).ToList();
-            }
-
-            return objects.Where(x => BHRE.Query.PropertyValue(x, propertyName) == value).ToList();
+            return filteredObjects.Distinct().ToList();
         }
 
         /***************************************************/
@@ -168,7 +110,7 @@ namespace BH.Engine.Data
                 string[] props = propName.Split('.');
                 for (int i = 0; i < props.Count() - 1; i++)
                 {
-                    obj = BHRE.Query.PropertyValue(obj, props[i]);
+                    obj = BH.Engine.Reflection.Query.PropertyValue(obj, props[i]);
                     if (obj == null)
                         break;
                 }
@@ -185,12 +127,12 @@ namespace BH.Engine.Data
                 if (bhom.CustomData.ContainsKey(propName))
                 {
                     if (!(bhom is CustomObject))
-                        BHRE.Compute.RecordNote($"{propName} is stored in CustomData");
+                        BH.Engine.Reflection.Compute.RecordNote($"{propName} is stored in CustomData");
                     return bhom.CustomData[propName].GetType();
                 }
                 else
                 {
-                    BHRE.Compute.RecordWarning($"{bhom} does not contain a property: {propName}, or: CustomData[{propName}]");
+                    BH.Engine.Reflection.Compute.RecordWarning($"{bhom} does not contain a property: {propName}, or: CustomData[{propName}]");
                     return null;
                 }
 
@@ -204,13 +146,13 @@ namespace BH.Engine.Data
                 }
                 else
                 {
-                    BHRE.Compute.RecordWarning($"{dic} does not contain the key: {propName}");
+                    BH.Engine.Reflection.Compute.RecordWarning($"{dic} does not contain the key: {propName}");
                     return null;
                 }
             }
             else
             {
-                BHRE.Compute.RecordWarning($"This instance of {obj.GetType()} does not contain the property: {propName}");
+                BH.Engine.Reflection.Compute.RecordWarning($"This instance of {obj.GetType()} does not contain the property: {propName}");
                 return null;
             }
         }
