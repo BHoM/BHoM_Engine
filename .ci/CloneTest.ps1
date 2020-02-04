b021884e2e96d8f69a3460f5ce563d4bc0567357
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-02-04 14:07:35 +0000

    Update CI file

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -24,7 +24,7 @@ Set-Location $ENV:BUILD_SOURCESDIRECTORY\$repo
 If((git rev-parse --verify --quiet ("origin/"+$ENV:SYSTEM_PULLREQUEST_SOURCEBRANCH)).length -gt 0)
 {
 	Write-Output("Changing branch in repo " + $repo + " to " + $ENV:SYSTEM_PULLREQUEST_SOURCEBRANCH)
-	git clone -q --branch=$ENV:SYSTEM_PULLREQUEST_SOURCEBRANCH https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+	git checkout --quiet $ENV:SYSTEM_PULLREQUEST_SOURCEBRANCH
 }
 
 Set-Location $cwd
