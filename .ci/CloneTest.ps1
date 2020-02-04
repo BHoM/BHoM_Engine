362346bfa27e16089ac317d7f14ab15b95a64661
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-02-04 11:15:21 +0000

    Update CI checking file

--- .ci/CloneTest.ps1
+++ .ci/CloneTest.ps1
@@ -2,27 +2,29 @@ Param([string]$repo)
 
 $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
+# *** Test_Toolkit requires BHoM and BHoM_Engine in order to build correctly for compliance tests, so here we make sure we have BHoM and BHoM_Engine ***
+
 # **** Cloning BHoM ****
 $repo = "BHoM"
 
-# **** Defining Paths ****
+# **** Defining BHoM Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
 
 # **** Cloning Repo ****
-Write-Output ("Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
-git clone -q --branch=master https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
+Write-Output ("BHoM Cloning " + $repo + " to " + $ENV:BUILD_SOURCESDIRECTORY + "\" + $repo)
+git clone -q --branch=$ENV:SYSTEM_PULLREQUEST_SOURCEBRANCH https://github.com/BHoM/$repo.git  $ENV:BUILD_SOURCESDIRECTORY\$repo
 
-# **** Restore NuGet ****
+# **** Restore BHoM NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
 
-# **** Cloning BHoM_Engine ****
+# **** Preparing BHoM_Engine ****
 $repo = "BHoM_Engine"
 
-# **** Defining Paths ****
+# **** Defining BHoM_Engine Paths ****
 $slnPath = "$ENV:BUILD_SOURCESDIRECTORY\$repo\$repo.sln"
 
-# **** Restore NuGet ****
+# **** Restore BHoM_Engine NuGet ****
 Write-Output ("Restoring NuGet packages for " + $repo)
 & NuGet.exe restore $slnPath
 
