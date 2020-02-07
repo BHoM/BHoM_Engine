02daf09edbb4a1018153e7df37023cb793da1aa3
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-02-07 14:56:59 +0000

    Updated descriptions for compliance

--- Diffing_Engine/Compute/DiffingHash.cs
+++ Diffing_Engine/Compute/DiffingHash.cs
@@ -43,7 +43,7 @@ namespace BH.Engine.Diffing
         ///***************************************************/
 
         [Description("Computes the hash code required for the Diffing.")]
-        [Input("objects", "Objects the hash code should be calculated for")]
+        [Input("obj", "Objects the hash code should be calculated for")]
         [Input("diffConfig", "Sets configs for the hash calculation, such as properties to be ignored.")]
         public static string DiffingHash(this IObject obj, DiffConfig diffConfig)
         {
