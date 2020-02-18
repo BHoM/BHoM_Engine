f3838d8baa56b1816eff94059ba3d062b56a3e4e
Isak Naslund <Isak.Naslund@burohappold.com>
2020-02-18 19:48:54 +0100

    Update DistributedPoints.cs

--- Structure_Engine/Query/DistributedPoints.cs
+++ Structure_Engine/Query/DistributedPoints.cs
@@ -40,8 +40,9 @@ namespace BH.Engine.Structure
         [Description("Gets a list of evenly distributed points along a bar from a given number of dividsions")]
         [Input("bar", "The bar to get division points for")]
         [Input("divisions", "Number of segments to divide the bar into. The number of points returned will be divisions +1.")]
-        [Input("startLength", "Optional offset from bar start node to start of divisions.")]
-        [Input("startLength", "Optional offset from bar end node to start of divisions.")]
+        [Input("startLength", "Optional offset from bar start node to start of divisions.", typeof(Length))]
+        [Input("startLength", "Optional offset from bar end node to start of divisions.", typeof(Length))]
+        [Output("points", "List of evenly distibuted points along the bar.")]
         public static List<Point> DistributedPoints(this Bar bar, int divisions, double startLength = 0, double endLength = 0)
         {
             if (divisions < 1)
