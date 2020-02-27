e79a77d01e45b66bdd9a9ea8c29fe000939a907e
kThorsager <Kalle.Thorsager@BuroHappold.com>
2020-02-27 11:10:00 +0000

    Migrate into Spatial

---
+++ Spatial_Engine/Query/Bounds.cs
@@ -0,0 +1,102 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Geometry;
+using BH.oM.Base;
+using BH.oM.Dimensional;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Engine.Spatial
+{
+    public static partial class Query
+    {
+        /******************************************/
+        /****            IElement0D            ****/
+        /******************************************/
+
+        public static BoundingBox Bounds(this IElement0D element0D)
+        {
+            return Geometry.Query.Bounds(element0D.IGeometry());
+        }
+
+
+        /******************************************/
+        /****            IElement1D            ****/
+        /******************************************/
+
+        public static BoundingBox Bounds(this IElement1D element1D)
+        {
+            return Geometry.Query.IBounds(element1D.IGeometry());
+        }
+
+
+        /******************************************/
+        /****            IElement2D            ****/
+        /******************************************/
+
+        public static BoundingBox Bounds(this IElement2D element2D)
+        {
+            List<ICurve> elementCurves = element2D.ElementCurves(true);
+
+            if (elementCurves.Count == 0)
+                return null;
+
+            BoundingBox box = Geometry.Query.IBounds(elementCurves[0]);
+            for (int i = 1; i < elementCurves.Count; i++)
+                box += Geometry.Query.IBounds(elementCurves[i]);
+
+            return box;
+        }
+
+
+        /******************************************/
+        /****        Interface methods         ****/
+        /******************************************/
+
+        public static BoundingBox IBounds(this IElement element)
+        {
+            return Bounds(element as dynamic);
+        }
+
+        /******************************************/
+
+        public static BoundingBox IBounds(this IEnumerable<IElement> elements)
+        {
+            if (elements.Count() == 0)
+                return null;
+
+            BoundingBox box = elements.First().IBounds();
+            foreach (IElement element in elements.Skip(1))
+            {
+                box += element.IBounds();
+            }
+
+            return box;
+        }
+
+        /******************************************/
+    }
+}
+
---
+++ Spatial_Engine/Query/ElementVertices.cs
@@ -0,0 +1,111 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Geometry;
+using BH.oM.Base;
+using BH.oM.Dimensional;
+using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Engine.Spatial
+{
+    public static partial class Query
+    {
+        /******************************************/
+        /****            IElement1D            ****/
+        /******************************************/
+
+        public static List<Point> ElementVertices(this IElement1D element1D)
+        {
+            ICurve curve = element1D.IGeometry();
+            List<Point> vertices = curve.IDiscontinuityPoints();
+
+            if (curve.IIsClosed())
+                vertices.RemoveAt(vertices.Count - 1);
+
+            return vertices;
+        }
+
+
+        /******************************************/
+        /****            IElement2D            ****/
+        /******************************************/
+
+        public static List<Point> ElementVertices(this IElement2D element2D)
+        {
+            List<Point> result = new List<Point>();
+            result.AddRange(element2D.IOutlineCurve().ElementVertices());
+            foreach (IElement2D e in element2D.IInternalElements2D())
+            {
+                result.AddRange(e.ElementVertices());
+            }
+
+            return result;
+        }
+
+
+        /******************************************/
+        /**** Public Methods - Interfaces      ****/
+        /******************************************/
+
+        public static List<Point> IElementVertices(this IElement element)
+        {
+            return ElementVertices(element as dynamic);
+        }
+
+        /******************************************/
+
+        public static List<Point> IElementVertices(this IEnumerable<IElement> elements)
+        {
+            List<Point> result = new List<Point>();
+            foreach(IElement element in elements)
+            {
+                result.AddRange(element.IElementVertices());
+            }
+            return result;
+        }
+
+
+        /******************************************/
+        /****        Deprecated methods        ****/
+        /******************************************/
+
+        [DeprecatedAttribute("2.3", "Input type changed from BHoMObject to IElement", null, "IElementVertices")]
+        public static List<Point> IElementVertices(this BHoMObject element)
+        {
+            return IElementVertices(element as IElement);
+        }
+
+        /******************************************/
+
+        [DeprecatedAttribute("2.3", "Input type changed from List<BHoMObject> to IEnumerable<IElement>", null, "IElementVertices")]
+        public static List<Point> IElementVertices(this List<BHoMObject> elements)
+        {
+            return IElementVertices(elements.Cast<IElement>());
+        }
+
+        /******************************************/
+    }
+}
+
