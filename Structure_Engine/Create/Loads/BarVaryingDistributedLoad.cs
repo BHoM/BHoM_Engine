df3d976776856cc860f46738de0ebc49eabad9c0
Isak Naslund <Isak.Naslund@burohappold.com>
2020-03-03 09:08:31 +0100

    Splitting loads into separate files

---
+++ Structure_Engine/Create/Loads/BarVaryingDistributedLoad.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Geometry;
+using BH.oM.Structure.Loads;
+using BH.oM.Base;
+using BH.oM.Structure.Elements;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static BarVaryingDistributedLoad BarVaryingDistributedLoad(Loadcase loadcase, BHoMGroup<Bar> group, double distFromA = 0, Vector forceA = null, Vector momentA = null, double distFromB = 0, Vector forceB = null, Vector momentB = null, LoadAxis axis = LoadAxis.Global, bool projected = false, string name = "")
+        {
+            if ((forceA == null || forceB == null) && (momentA == null || momentB == null))
+                throw new ArgumentException("Bar varying load requires either the force at A and B OR the moment at A and B to be defined");
+
+            return new BarVaryingDistributedLoad
+            {
+                Loadcase = loadcase,
+                Objects = group,
+                DistanceFromA = distFromA,
+                DistanceFromB = distFromB,
+                ForceA = forceA == null ? new Vector() : forceA,
+                ForceB = forceB == null ? new Vector() : forceB,
+                MomentA = momentA == null ? new Vector() : momentA,
+                MomentB = momentB == null ? new Vector() : momentB,
+                Projected = projected,
+                Axis = axis,
+                Name = name
+            };
+
+        }
+
+        /***************************************************/
+
+        public static BarVaryingDistributedLoad BarVaryingDistributedLoad(Loadcase loadcase, IEnumerable<Bar> objects, double distFromA = 0, Vector forceA = null, Vector momentA = null, double distFromB = 0, Vector forceB = null, Vector momentB = null, LoadAxis axis = LoadAxis.Global, bool projected = false, string name = "")
+        {
+            return BarVaryingDistributedLoad(loadcase, new BHoMGroup<Bar>() { Elements = objects.ToList() }, distFromA, forceA, momentA, distFromB, forceB, momentB, axis, projected, name);
+        }
+
+        /***************************************************/
+
+    }
+}
+
---
+++ Structure_Engine/Create/Loads/PointLoad.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Geometry;
+using BH.oM.Structure.Loads;
+using BH.oM.Base;
+using BH.oM.Structure.Elements;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static PointLoad PointLoad(Loadcase loadcase, BHoMGroup<Node> group, Vector force = null, Vector moment = null, LoadAxis axis = LoadAxis.Global, string name = "")
+        {
+            if (force == null && moment == null)
+                throw new ArgumentException("Point force requires either the force or the moment vector to be defined");
+
+            return new PointLoad
+            {
+                Loadcase = loadcase,
+                Objects = group,
+                Force = force == null ? new Vector() : force,
+                Moment = moment == null ? new Vector() : moment,
+                Axis = axis,
+                Name = name
+            };
+
+        }
+
+        /***************************************************/
+
+        public static PointLoad PointLoad(Loadcase loadcase, IEnumerable<Node> objects, Vector force = null, Vector moment = null, LoadAxis axis = LoadAxis.Global, string name = "")
+        {
+            return PointLoad(loadcase, new BHoMGroup<Node>() { Elements = objects.ToList() }, force, moment, axis, name);
+        }
+
+        /***************************************************/
+        /**** Public Methods - Deprecated               ****/
+        /***************************************************/
+
+        [Deprecated("2.3", "Method and class name updated from PointForce to PointLoad", null, "PointLoad")]
+        public static PointLoad PointForce(Loadcase loadcase, BHoMGroup<Node> group, Vector force = null, Vector moment = null, LoadAxis axis = LoadAxis.Global, string name = "")
+        {
+            return PointLoad(loadcase, group, force, moment, axis, name);
+
+        }
+
+        /***************************************************/
+
+        [Deprecated("2.3", "Method and class name updated from PointForce to PointLoad", null, "PointLoad")]
+        public static PointLoad PointForce(Loadcase loadcase, IEnumerable<Node> objects, Vector force = null, Vector moment = null, LoadAxis axis = LoadAxis.Global, string name = "")
+        {
+            return PointLoad(loadcase, objects, force, moment, axis, name);
+        }
+
+        /***************************************************/
+
+    }
+}
+
