53ad28b64decc7ecc4e8178ee1a3321b5ba46592
kThorsager <Kalle.Thorsager@BuroHappold.com>
2020-03-04 16:35:28 +0000

    The required Extension methods

---
+++ Physical_Engine/Modify/SetInternalElements2D.cs
@@ -0,0 +1,69 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Physical.Materials;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+using BH.oM.Physical.Elements;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+using BH.oM.Dimensional;
+using BH.Engine.Common;
+
+namespace BH.Engine.Physical
+{
+    public static partial class Modify
+    {
+
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        public static IOpening SetInternalElements2D(this IOpening opening, List<IElement2D> internalElements2D)
+        {
+            if (internalElements2D.Count != 0)
+                Reflection.Compute.RecordError("Cannot set internal 2D elements to an opening.");
+
+            return opening.GetShallowClone() as IOpening;
+        }
+
+        /***************************************************/
+
+        public static oM.Physical.Elements.ISurface SetInternalElements2D(this oM.Physical.Elements.ISurface panel, List<IElement2D> internalElements2D)
+        {
+            oM.Physical.Elements.ISurface pp = panel.GetShallowClone() as oM.Physical.Elements.ISurface;
+            pp.Openings = new List<IOpening>(internalElements2D.Cast<IOpening>().ToList());
+            return pp;
+        }
+
+        /***************************************************/
+
+    }
+}
+
---
+++ Physical_Engine/Query/OutlineElements1D.cs
@@ -0,0 +1,72 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Physical.Elements;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+using BH.oM.Dimensional;
+
+namespace BH.Engine.Physical
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static List<IElement1D> OutlineElements1D(this oM.Physical.Elements.ISurface surface)
+        {
+            PlanarSurface pSurface = surface.Location as PlanarSurface;
+            if (pSurface == null)
+            {
+                Engine.Reflection.Compute.RecordError("Not implemented for non-PlanarSurfaces");
+                return null;
+            }
+
+            return pSurface.ExternalBoundary.ISubParts().ToList<IElement1D>(); ;
+        }
+
+        /***************************************************/
+
+        public static List<IElement1D> OutlineElements1D(this IOpening surface)
+        {
+            PlanarSurface pSurface = surface.Location as PlanarSurface;
+            if (pSurface == null)
+            {
+                Engine.Reflection.Compute.RecordError("Not implemented for non-PlanarSurfaces");
+                return null;
+            }
+
+            return pSurface.ExternalBoundary.ISubParts().ToList<IElement1D>(); ;
+        }
+
+        /***************************************************/
+
+
+    }
+}
+
