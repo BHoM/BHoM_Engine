97de90d12c293c67d4c60d607cba76c176e3824d
Isak Naslund <Isak.Naslund@burohappold.com>
2020-03-05 16:25:32 +0100

    Update BarVaryingDistributedLoad.cs

--- Structure_Engine/Create/Loads/BarVaryingDistributedLoad.cs
+++ Structure_Engine/Create/Loads/BarVaryingDistributedLoad.cs
@@ -38,17 +38,31 @@ namespace BH.Engine.Structure
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static BarVaryingDistributedLoad BarVaryingDistributedLoad(Loadcase loadcase, BHoMGroup<Bar> group, double distFromA = 0, Vector forceA = null, Vector momentA = null, double distFromB = 0, Vector forceB = null, Vector momentB = null, LoadAxis axis = LoadAxis.Global, bool projected = false, string name = "")
+
+        [Description("Creates a varying distributed load to be applied to Bar elements.")]
+        [InputFromProperty("loadcase")]
+        [InputFromProperty("group", "Objects")]
+        [InputFromProperty("distanceFromA")]
+        [InputFromProperty("distanceFromB")]
+        [InputFromProperty("forceA")]
+        [InputFromProperty("forceB")]
+        [InputFromProperty("momentA")]
+        [InputFromProperty("momentB")]
+        [InputFromProperty("axis")]
+        [InputFromProperty("projected")]
+        [Input("name", "The name of the created load.")]
+        [Output("barVarLoad", "The created BarVaryingDistributedLoad.")]
+        public static BarVaryingDistributedLoad BarVaryingDistributedLoad(Loadcase loadcase, BHoMGroup<Bar> group, double distanceFromA = 0, Vector forceA = null, Vector momentA = null, double distanceFromB = 0, Vector forceB = null, Vector momentB = null, LoadAxis axis = LoadAxis.Global, bool projected = false, string name = "")
         {
             if ((forceA == null || forceB == null) && (momentA == null || momentB == null))
-                throw new ArgumentException("Bar varying load requires either the force at A and B OR the moment at A and B to be defined");
+                throw new ArgumentException("Bar varying load requires either the force at A and B AND/OR the moment at A and B to be defined");
 
             return new BarVaryingDistributedLoad
             {
                 Loadcase = loadcase,
                 Objects = group,
-                DistanceFromA = distFromA,
-                DistanceFromB = distFromB,
+                DistanceFromA = distanceFromA,
+                DistanceFromB = distanceFromB,
                 ForceA = forceA == null ? new Vector() : forceA,
                 ForceB = forceB == null ? new Vector() : forceB,
                 MomentA = momentA == null ? new Vector() : momentA,
@@ -62,6 +76,19 @@ namespace BH.Engine.Structure
 
         /***************************************************/
 
+        [Description("Creates a varying distributed load to be applied to Bar elements.")]
+        [InputFromProperty("loadcase")]
+        [Input("objects", "The collection of Bars the load should be applied to.")]
+        [InputFromProperty("distanceFromA")]
+        [InputFromProperty("distanceFromB")]
+        [InputFromProperty("forceA")]
+        [InputFromProperty("forceB")]
+        [InputFromProperty("momentA")]
+        [InputFromProperty("momentB")]
+        [InputFromProperty("axis")]
+        [InputFromProperty("projected")]
+        [Input("name", "The name of the created load.")]
+        [Output("barVarLoad", "The created BarVaryingDistributedLoad.")]
         public static BarVaryingDistributedLoad BarVaryingDistributedLoad(Loadcase loadcase, IEnumerable<Bar> objects, double distFromA = 0, Vector forceA = null, Vector momentA = null, double distFromB = 0, Vector forceB = null, Vector momentB = null, LoadAxis axis = LoadAxis.Global, bool projected = false, string name = "")
         {
             return BarVaryingDistributedLoad(loadcase, new BHoMGroup<Bar>() { Elements = objects.ToList() }, distFromA, forceA, momentA, distFromB, forceB, momentB, axis, projected, name);
