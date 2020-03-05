b728687625750eb17e1ffc9561d6cb65875f4f58
rolyh <roly.hudson@gmail.com>
2020-03-05 13:44:27 +0000

    Adding graph processing methods

---
+++ Data_Engine/Query/AdjacencyDictionary.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Data.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Engine.Data
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        public static Dictionary<GraphNode<T>, List<GraphNode<T>>> AdjacencyDictionary<T>(this Graph<T> graph)
+        {
+            //get graph representation as adjacency list
+            //adjacency list is a collection of unordered lists used to represent a finite graph
+            Dictionary<GraphNode<T>, List<GraphNode<T>>> adjacency = new Dictionary<GraphNode<T>, List<GraphNode<T>>>();
+            foreach (GraphNode<T> node in graph.Nodes)
+            {
+                List<GraphNode<T>> neighbours = new List<GraphNode<T>>();
+                //neigbours in both directions
+                List<GraphNode<T>> neighboursOut = graph.Links.Where(x => x.StartNode == node).Select(x => x.EndNode).ToList();
+                List<GraphNode<T>> neighboursIn = graph.Links.Where(x => x.EndNode == node).Select(x => x.StartNode).ToList();
+
+                neighbours.AddRange(neighboursOut);
+                neighbours.AddRange(neighboursIn);
+                adjacency.Add(node, neighbours);
+            }
+            return adjacency;
+        }
+    }
+}
---
+++ Data_Engine/Query/DepthDictionary.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Data.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Engine.Data
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        public static Dictionary<GraphNode<T>, int> DepthDictionary<T>(Dictionary<GraphNode<T>, List<GraphNode<T>>> adjacency, GraphNode<T> x)
+        {//https://www.geeksforgeeks.org/level-node-tree-source-node-using-bfs/
+         // dictionary to store level of each node  
+            Dictionary<GraphNode<T>, int> level = new Dictionary<GraphNode<T>, int>();
+            Dictionary<GraphNode<T>, bool> marked = new Dictionary<GraphNode<T>, bool>();
+            // create a queue  
+            Queue<GraphNode<T>> que = new Queue<GraphNode<T>>();
+            // enqueue element x  
+            que.Enqueue(x);
+            // initialize level of source node to 0  
+            level[x] = 0;
+            // marked it as visited  
+            marked[x] = true;
+            // do until queue is empty  
+            while (que.Count > 0)
+            {
+                // dequeue element  
+                x = que.Dequeue();
+                // traverse neighbors of node x  
+                foreach (GraphNode<T> b in adjacency[x])
+                {
+                    // b is neighbor of node x  
+                    // if b is not marked already  
+                    if (!marked.ContainsKey(b))
+                    {
+                        // enqueue b in queue  
+                        que.Enqueue(b);
+                        // level of b is level of x + 1  
+                        level[b] = level[x] + 1;
+                        // mark b  
+                        marked[b] = true;
+                    }
+                }
+            }
+
+            return level;
+        }
+    }
+}
