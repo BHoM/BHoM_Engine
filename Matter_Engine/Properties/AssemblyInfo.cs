5668e1913417e9d7bb5f50e236082882841fde36
kThorsager <Kalle.Thorsager@BuroHappold.com>
2020-03-06 10:56:40 +0000

    Update AssemblyInfo.cs

--- Matter_Engine/Properties/AssemblyInfo.cs
+++ Matter_Engine/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Matter_Engine_Engine")]
+[assembly: AssemblyTitle("Matter_Engine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Buro Happold")]
-[assembly: AssemblyProduct("Matter_Engine_Engine")]
+[assembly: AssemblyProduct("Matter_Engine")]
 [assembly: AssemblyCopyright("Copyright © Buro Happold 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
