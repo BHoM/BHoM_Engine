632a0dc284d1fad9edbff1cfe2ecd7de9bc920f9
kThorsager <Kalle.Thorsager@BuroHappold.com>
2020-03-10 09:19:52 +0000

    MatchMaterialFragment

---
+++ Matter_Engine/Query/MatchMaterialFragment.cs
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Physical;
+using BH.oM.Data.Collections;
+using BH.oM.Dimensional;
+using BH.oM.Geometry;
+using BH.oM.Physical.Materials;
+using BH.oM.Quantities.Attributes;
+using BH.oM.Reflection.Attributes;
+using BH.Engine.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace BH.Engine.Matter
+{
+    public static partial class Query
+    {
+        /******************************************/
+        
+        public static IMaterialProperties Match(this IMaterialProperties material, IEnumerable<IMaterialProperties> others)
+        {
+            IMaterialProperties excactName = others.Where(x => x.Name == material.Name).FirstOrDefault();
+            material.GetType().Name.Contains(others.ElementAt(0).Name);
+
+            List<IMaterialProperties> partialNameMatch = others.Where(x => x.Name.Contains(material.Name) || material.Name.Contains(x.Name)).ToList();
+            
+
+            return null;
+        }
+
+        /******************************************/
+        
+    }
+}
+
