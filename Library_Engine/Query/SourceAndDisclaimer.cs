cf16656ca3bd28d5972ce0e7fbea74dfe58b1847
Isak Naslund <Isak.Naslund@burohappold.com>
2020-03-10 17:03:27 +0100

    Adding methods for extracting source as text from a library

---
+++ Library_Engine/Query/SourceAndDisclaimer.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Data.Library;
+using BH.oM.Base;
+
+namespace BH.Engine.Library
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Extracts the source information, if available, from a library and returns a compiled version of the source information and a general disclaimer the dataset.")]
+        [Input("libraryName", "The name of the Dataset(s) to extract source information from")]
+        [Output("source", "Text representation of the source and general disclaimer of the data.")]
+        public static string SourceAndDisclaimer(string libraryName)
+        {
+            List<Source> sources = Source(libraryName).Where(x => x != null).ToList();
+
+            string desc;
+            if (sources.Count == 1)
+            {
+                desc = "Dataset based on the following source:" + Environment.NewLine;
+                desc += sources[0].ToText();
+            }
+            else if (sources.Count > 1)
+            {
+                desc = "Multiple sources found for the library. Datasets based on the following sources:" + Environment.NewLine;
+
+                for (int i = 0; i < sources.Count; i++)
+                {
+                    desc += Environment.NewLine;
+                    desc += "Source " + i + ":" + Environment.NewLine;
+                    desc += sources[i].ToText();
+                }
+            }
+            else
+            {
+                desc = "No available source information available for this dataset." + Environment.NewLine;
+            }
+            desc += Environment.NewLine;
+            desc += "Although care has been taken to ensure, to the best of our knowledge, that all information contained herein is accurate, the BHoM contributors assume no responsibility for any errors in or misinterpretations of such information or any loss or damage arising from or related to its use.";
+
+            return desc;
+        }
+
+        /***************************************************/
+    }
+}
