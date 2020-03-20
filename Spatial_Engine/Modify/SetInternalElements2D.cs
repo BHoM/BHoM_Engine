27c521f6b67011ff484a662d50659982f898dbb9
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-03-20 16:48:39 +0000

    Update SetInternalElements2D.cs

--- Spatial_Engine/Modify/SetInternalElements2D.cs
+++ Spatial_Engine/Modify/SetInternalElements2D.cs
@@ -22,7 +22,9 @@
 
 using BH.oM.Dimensional;
 using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace BH.Engine.Spatial
 {
@@ -32,6 +34,10 @@ namespace BH.Engine.Spatial
         /****            IElement2D            ****/
         /******************************************/
 
+        [Description("Exchanges the internal IElement2Ds for the provided internal IElement2Ds. The internal IElement2Ds location is used and must align with the IElement2Ds geometry.")]
+        [Input("element2D", "The IElement2D to exchange the internal IElement2D's of. This includes their location.")]
+        [Input("newElements2D", "The IElement2Ds to set to the IElement2D. Must be of the correct type.")]
+        [Output("element2D", "The modified IElement2D which has unchanged properties and exchanged internal IElement2Ds.")]
         public static IElement2D ISetInternalElements2D(this IElement2D element2D, List<IElement2D> newElements2D)
         {
             return Reflection.Compute.RunExtensionMethod(element2D, "SetInternalElements2D", new object[] { newElements2D }) as IElement2D;
