b1f76cf29b1284225c0659c63f474d1d31909325
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-03-20 16:48:41 +0000

    Update SetOutlineElements1D.cs

--- Spatial_Engine/Modify/SetOutlineElements1D.cs
+++ Spatial_Engine/Modify/SetOutlineElements1D.cs
@@ -22,7 +22,9 @@
 
 using BH.oM.Dimensional;
 using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace BH.Engine.Spatial
 {
@@ -32,6 +34,10 @@ namespace BH.Engine.Spatial
         /****          Public methods          ****/
         /******************************************/
 
+        [Description("Sets the outline IElement1Ds of the IElement2D. This will maintain the IElement2D properties but exchange the outlines IElement1Ds.")]
+        [Input("element2D", "The IElement2D to exchange the outline IElement1D's of. This includes their location.")]
+        [Input("newOutline", "The outline IElement1Ds to set to the IElement2D. Must be of the correct type and planar.")]
+        [Output("element2D", "The modified IElement2D which has unchanged properties and exchanged outline IElement1Ds.")]
         public static IElement2D ISetOutlineElements1D(this IElement2D element2D, List<IElement1D> newOutline)
         {
             return Reflection.Compute.RunExtensionMethod(element2D, "SetOutlineElements1D", new object[] { newOutline }) as IElement2D;
