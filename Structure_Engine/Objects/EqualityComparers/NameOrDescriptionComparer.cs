0b44b4a90e9884ede945d1bcdcbc878fd2735859
Isak Naslund <Isak.Naslund@burohappold.com>
2020-04-09 17:24:44 +0200

    Adding NameOrDescriptionComparer for all structural IProperties

---
+++ Structure_Engine/Objects/EqualityComparers/NameOrDescriptionComparer.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using BH.oM.Structure;
+
+namespace BH.Engine.Structure
+{
+    public class NameOrDescriptionComparer : IEqualityComparer<IProperty>
+    {
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
+
+        public NameOrDescriptionComparer()
+        {
+
+        }
+
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public bool Equals(IProperty prop1, IProperty prop2)
+        {
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(prop1, prop2)) return true;
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(prop1, null) || Object.ReferenceEquals(prop2, null))
+                return false;
+
+            //Return true if name or description string for both objects are the same
+            return prop1.DescriptionOrName() == prop2.DescriptionOrName();
+        }
+
+        /***************************************************/
+
+        public int GetHashCode(IProperty prop)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(prop, null)) return 0;
+
+            return prop.DescriptionOrName().GetHashCode();
+
+        }
+
+        /***************************************************/
+    }
+}
+
