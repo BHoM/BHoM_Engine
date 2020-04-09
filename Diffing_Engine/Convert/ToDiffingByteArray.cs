6ee9700900e3bae129ad7df988a5fae589a5171d
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-04-09 12:40:55 +0100

    Removed unused engine method

--- Diffing_Engine/Convert/ToDiffingByteArray.cs
+++ Diffing_Engine/Convert/ToDiffingByteArray.cs
@@ -45,19 +45,6 @@ namespace BH.Engine.Diffing
         ///**** Public Methods                            ****/
         ///***************************************************/
 
-        public static byte[] ToDiffingByteArray(this object obj, PropertyInfo[] fieldsToIgnore = null)
-        {
-            List<PropertyInfo> propList = fieldsToIgnore?.ToList();
-
-            if (propList == null || propList.Count == 0)
-                return BsonExtensionMethods.ToBson(obj.ToBsonDocument()); // .ToBsonDocument() for consistency with other cases
-
-            List<string> propNames = new List<string>();
-            propList.ForEach(prop => propNames.Add(prop.Name));
-
-            return ToDiffingByteArray(obj, propNames);
-        }
-
         public static byte[] ToDiffingByteArray(this object obj, List<string> fieldsToIgnore)
         {
             if (fieldsToIgnore == null || fieldsToIgnore.Count == 0)
