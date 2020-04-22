b2201ec3a20189db8d8b2e9bad19489dbfa7d935
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-04-22 08:44:31 +0100

    Compliance fix

--- Structure_Engine/Query/Area.cs
+++ Structure_Engine/Query/Area.cs
@@ -73,7 +73,7 @@ namespace BH.Engine.Structure
         /***************************************************/
 
         [Description("Calculates the area of a Surface based on the area of the geometrical surface stored in Extents.")]
-        [Input("mesh", "The Surface to calculate the area for.")]
+        [Input("surface", "The Surface to calculate the area for.")]
         [Output("area", "The area of the Surface.", typeof(Area))]
         public static double Area(this Surface surface)
         {
