4007ea4f42a57a8786e89f7ceb9bae5ec30ddcaa
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-04-28 15:45:50 +0100

    Structure_Engine: Implement HasMergeablePropertiesWith

---
+++ Structure_Engine/Query/HasMergeablePropertiesWith.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+using BH.oM.Structure.Elements;
+
+namespace BH.Engine.Structure
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Evaluates if the two Edges Supports and Releases are equal with the Support and Release comparers.")]
+        [Input("element", "An Structure Edge to compare the properties of with an other Structure Edge")]
+        [Input("other", "The Structure Edge to compare with the other Structure Edge.")]
+        [Output("equal", "True if the Objects non-geometrical property is equal to the point that they could be merged into one object")]
+        public static bool HasMergeablePropertiesWith(Edge element, Edge other)
+        {
+            return new Constraint4DOFComparer().Equals(element.Release, other.Release) &&
+                   new Constraint6DOFComparer().Equals(element.Support, other.Support);
+        }
+
+        /***************************************************/
+
+    }
+}
