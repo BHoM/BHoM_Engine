9434865453e2c991fb8769e17069cbfc50fe1bef
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-05-01 11:32:38 +0100

    Add OpeningsByType Query method #1726

---
+++ Environment_Engine/Query/OpeningsByType.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Environment.Elements;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Returns a collection of Environment Openings that match a given Opening Type")]
+        [Input("openings", "A collection of Environment Openings to filter")]
+        [Input("type", "An Opening Type to filter by from the Opening Type enum")]
+        [Output("openings", "A collection of Environment Opening that match the given type")]
+        public static List<Opening> OpeningsByType(this List<Opening> openings, OpeningType type)
+        {
+            return openings.Where(x => x.Type == type).ToList();
+        }
+    }
+}
