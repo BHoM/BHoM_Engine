06334cd333ec7431006e64038e17208750a2b6a8
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-05-04 13:37:13 +0100

    Add area method for IProfile

--- Spatial_Engine/Query/Area.cs
+++ Spatial_Engine/Query/Area.cs
@@ -23,11 +23,13 @@
 using BH.Engine.Geometry;
 using BH.oM.Dimensional;
 using BH.oM.Geometry;
+using BH.oM.Geometry.ShapeProfiles;
 using BH.oM.Quantities.Attributes;
 using BH.oM.Reflection.Attributes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace BH.Engine.Spatial
 {
@@ -50,6 +52,47 @@ namespace BH.Engine.Spatial
                 result -= o.Area();
 
             return result;
+        }
+
+
+        /******************************************/
+        /****            IProfile              ****/
+        /******************************************/
+
+        [Description("Gets the area for a IProfile where the curves are defining a region's border and eachother step inward defines a hole's border.")]
+        [Input("profile", "The IProfile to evaluate the area of. TaperedProfiles average area is returned.")]
+        [Output("area", "The area for eached closed curve, subtracting eachother closed curve's area stepping inwards.", typeof(Area))]
+        public static double Area(this IProfile profile)
+        {
+            if (profile is TaperedProfile)
+            {
+                Engine.Reflection.Compute.RecordWarning("TaperedProfiles don't have one area. Linear relation between the profiles in the TaperedProfile has been assumed and it's average area returned.");
+                TaperedProfile taperedProfile = profile as TaperedProfile;
+                double sum = 0;
+                for (int i = 0; i < taperedProfile.Profiles.Count - 1; i++)
+                {
+                    double temArea = (taperedProfile.Profiles.ElementAt(i).Value.Area() + taperedProfile.Profiles.ElementAt(i + 1).Value.Area()) / 2;
+                    sum += temArea * System.Convert.ToDouble(taperedProfile.Profiles.ElementAt(i + 1).Key - taperedProfile.Profiles.ElementAt(i).Key);
+                }
+                return sum;
+            }
+
+            List<PolyCurve> curvesZ = Engine.Geometry.Compute.IJoin(profile.Edges.ToList());
+
+            int[] depth = new int[curvesZ.Count];
+            if (curvesZ.Count > 1)
+            {
+                // find which is in which
+                for (int i = 0; i < curvesZ.Count; i++)
+                    for (int j = 0; j < curvesZ.Count; j++)
+                        if (i != j)
+                            if (curvesZ[i].IsContaining(new List<Point>() { curvesZ[j].IStartPoint() }))
+                                depth[j]++;
+            }
+
+            // Using region integration as the Curves are defined on the XY-Plane.
+            depth = depth.Select(x => x % 2 == 0 ? 1 : -1).ToArray();   // positive area as 1 and negative area as -1
+            return curvesZ.Select((x, i) => Math.Abs(x.IIntegrateRegion(0)) * depth[i]).Sum();
         }
 
         /******************************************/
