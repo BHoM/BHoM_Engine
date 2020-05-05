3e5e1509f97f38535709261c26583992e95d658f
Isak Naslund <Isak.Naslund@burohappold.com>
2020-05-05 14:27:52 +0200

    Add method for extracting reinforcing bars from a Bar

--- Structure_Engine/Query/ReinforcingBars.cs
+++ Structure_Engine/Query/ReinforcingBars.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -25,9 +25,19 @@ using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
 using BH.oM.Reflection.Attributes;
+using BH.oM.Structure.SectionProperties;
+using BH.oM.Structure.SectionProperties.Reinforcement;
+using BH.oM.Structure.Elements;
+using BH.oM.Physical.Reinforcement;
+using BH.oM.Physical.Materials;
+using BH.oM.Spatial.Layouts;
+using BH.oM.Geometry;
+using BH.Engine.Spatial;
+using BH.Engine.Geometry;
 using BH.oM.Base;
 
-namespace BH.Engine.Structure.Query //`Query` is a partial class. Remove any reference to `Query` from namespace.
+
+namespace BH.Engine.Structure
 {
     public static partial class Query
     {
@@ -35,12 +45,61 @@ namespace BH.Engine.Structure.Query //`Query` is a partial class. Remove any ref
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("")]
-        [Input("", "")]
-        [Output("", "")]
-        public static void ReinforcingBars()
+        [Description("Extract all ReinforcingBars from the structural Bar. Only extract reinforcement for bars owning a ConcreteSection, for other sectiontypes, an empty list will be returned.")]
+        [Input("bar", "THe structural bar to extract Reinforcement from.")]
+        [Output("rebars", "All ReinforcingBar on the provided Bar.")]
+        public static List<IReinforcingBar> ReinforcingBars(this Bar bar)
         {
-            throw new NotImplementedException();
+            ConcreteSection section = bar.SectionProperty as ConcreteSection;
+            if (section == null)
+                return new List<IReinforcingBar>();
+
+            //Extract Longitudinal reinforcement
+            List<LongitudinalReinforcement> longReif = section.LongitudinalReinforcement();
+
+            //No longitudinal reinforcement available
+            if (longReif.Count == 0)
+                return new List<IReinforcingBar>();
+
+            List<ICurve> outerProfileEdges;
+            List<ICurve> innerProfileEdges;
+
+            ExtractInnerAndOuterEdges(section, out outerProfileEdges, out innerProfileEdges);
+
+            //Need at least one external edge curve
+            if (outerProfileEdges.Count == 0)
+                return new List<IReinforcingBar>();
+
+
+            TransformMatrix transformation = bar.BarSectionTranformation();
+            double length = bar.Length();
+
+            //TODO: include stirups for offset distance
+            double stirupOffset = 0;
+
+            List<IReinforcingBar> rebars = new List<IReinforcingBar>();
+
+            foreach (LongitudinalReinforcement reif in longReif)
+            {
+                Material material;
+                if (reif.Material != null)
+                    material = Engine.Physical.Create.Material(reif.Material);
+                else
+                    material = new Material();
+
+                foreach (Line centreLine in reif.ReinforcementLayout(stirupOffset, outerProfileEdges, innerProfileEdges, length, transformation))
+                {
+                    PrimaryReinforcingBar rebar = new PrimaryReinforcingBar
+                    {
+                        CentreCurve = centreLine,
+                        Diameter = reif.Diameter,
+                        Material = material,
+                    };
+                    rebars.Add(rebar);
+                }
+            }
+
+            return rebars;
         }
 
         /***************************************************/
