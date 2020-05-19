3322fb4d0bfe8839e3756ec9339aa24cb74bf996
Isak Naslund <Isak.Naslund@burohappold.com>
2020-05-19 13:16:27 +0200

    Make LayerReinforcement call the basic create method

--- Structure_Engine/Create/SectionProperties/LongitudinalReinforcement/LayerReinforcement.cs
+++ Structure_Engine/Create/SectionProperties/LongitudinalReinforcement/LayerReinforcement.cs
@@ -53,15 +53,7 @@ namespace BH.Engine.Structure
         [Output("reinforcement", "The created Reinforcement to be applied to a ConcreteSection.")]
         public static LongitudinalReinforcement LayerReinforcement(double diameter, int barCount, Vector direction = null, double offset = 0, ReferencePoint referencePoint = ReferencePoint.BottomCenter, double startLocation = 0, double endLocation = 1, IMaterialFragment material = null)
         {
-            CheckEndLocations(ref startLocation, ref endLocation);
-            return new LongitudinalReinforcement
-            {
-                RebarLayout = Spatial.Create.LinearLayout(barCount, direction, offset, referencePoint),
-                Diameter = diameter,
-                Material = material ?? Query.Default(MaterialType.Rebar),
-                StartLocation = startLocation,
-                EndLocation = endLocation,
-            };
+            return LongitudinalReinforcement(Spatial.Create.LinearLayout(barCount, direction, offset, referencePoint), diameter, startLocation, endLocation, material);
         }
 
         /***************************************************/
