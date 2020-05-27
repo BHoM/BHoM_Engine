38b9120e2d76dd06a4e418a5762fa0c1afb5e67c
Isak Naslund <Isak.Naslund@burohappold.com>
2020-05-27 13:26:28 +0200

    Check if property is overridden before registering it to class map for immutable objects

--- Serialiser_Engine/Objects/MemberMapConventions/ImmutableBHoMClassMapConvention.cs
+++ Serialiser_Engine/Objects/MemberMapConventions/ImmutableBHoMClassMapConvention.cs
@@ -80,7 +80,7 @@ namespace BH.Engine.Serialiser.MemberMapConventions
                 
                 foreach (var property in properties)
                 {
-                    if (property.DeclaringType == classType)
+                    if (property.DeclaringType == classType && !IsOverridden(property))
                         classMap.MapMember(property);
                     else if(!property.CanWrite)
                     {
@@ -103,6 +103,16 @@ namespace BH.Engine.Serialiser.MemberMapConventions
                 }
             }
         }
+
+        /***************************************************/
+
+        private bool IsOverridden(PropertyInfo property)
+        {
+            var getMethod = property.GetGetMethod(false);
+            return getMethod.GetBaseDefinition() != getMethod;
+        }
+
+        /***************************************************/
     }
 }
 
