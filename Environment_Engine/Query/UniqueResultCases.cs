b4d4c0cf516bb9363c2dd444b13796b5e2140e39
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-05-28 11:44:23 +0100

    Add UniqueResultCases method #1814

---
+++ Environment_Engine/Query/UniqueResultCases.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Environment.Results.Mesh;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        [Description("Returns a collection of unique Result Cases from a collection of MeshResult objects")]
+        [Input("meshResults", "A collection of Environment Mesh Results to obtain the unique result cases from")]
+        [Output("uniqueResultCases", "The collection of unique result cases from the Mesh Results")]
+        public static List<IComparable> UniqueResultCases(this List<MeshResult> meshResults)
+        {
+            return meshResults.Select(x => x.ResultCase).Distinct().ToList();
+        }
+    }
+}
