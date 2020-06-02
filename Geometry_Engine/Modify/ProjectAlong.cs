d5a6afa50f9f709121e3c8f7aa9a3505ac1f0f95
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-06-02 11:08:29 +0100

    The proper fix for ProjectAlong for Circle
    Known as giving up.
    As opposed to this methods original assumptions about only having to worriy about the new value for the second radius.
    Both radie can change
    and the calculation for the new axies were wrong as well, (visual inspection)
    And all of that sound s like a later problem as this solution works

--- Geometry_Engine/Modify/ProjectAlong.cs
+++ Geometry_Engine/Modify/ProjectAlong.cs
@@ -80,12 +80,8 @@ namespace BH.Engine.Geometry
             if (circle.Normal.IsParallel(plane.Normal) != 0)
                 return new Circle { Centre = circle.Centre.ProjectAlong(plane, vector), Normal = circle.Normal.Clone(), Radius = circle.Radius };
 
-            Vector axis1 = plane.Normal.CrossProduct(circle.Normal);
-            Vector axis2 = axis1.CrossProduct(plane.Normal);
-            double denominator = circle.Normal.DotProduct(plane.Normal);
-            double radius2 = Math.Abs(denominator) < Tolerance.Distance ? double.PositiveInfinity : circle.Radius / denominator;
-            
-            return new Ellipse { Centre = circle.Centre.ProjectAlong(plane, vector), Axis1 = axis1, Axis2 = axis2, Radius1 = circle.Radius, Radius2 = radius2 };
+            TransformMatrix project = Create.ProjectionMatrix(plane, vector);
+            return circle.Transform(project);
         }
 
         /***************************************************/
