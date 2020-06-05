ca4e64445ba98142a22295fea34d68273d934be4
Isak Naslund <Isak.Naslund@burohappold.com>
2020-06-05 11:03:54 +0200

    Aligning with changes for migrating results to Analytical_oM

--- Results_Engine/Query/Group.cs
+++ Results_Engine/Query/Group.cs
@@ -25,7 +25,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BH.oM.Common;
+using BH.oM.Analytical.Results;
 
 namespace BH.Engine.Results
 {
