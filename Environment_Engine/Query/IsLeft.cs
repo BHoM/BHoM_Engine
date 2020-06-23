e19a2ca6b81e4db83ee536604bd350c8b9568458
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-06-23 12:51:55 +0100

    Migrate code from IES_Toolkit

---
+++ Environment_Engine/Query/IsLeft.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Determine whether a point falls on the left side of a line or not. The left side is defined as the left hand side of the line when standing on the start point and looking at the end point")]
+        [Input("line", "The line to determine directionality")]
+        [Input("check", "The point to check against")]
+        [Output("isLeft", "True if the point is on the left hand side of the line. False if it is on the line or on the right hand side")]
+        public static bool IsLeft(Line line, Point check)
+        {
+            return ((line.End.X - line.Start.X) * (check.Y - line.Start.Y) - (line.End.Y - line.Start.Y) * (check.X - line.Start.X)) > 0;
+        }
+    }
+}
---
+++ Environment_Engine/Query/TopRight.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Geometry;
+using BH.oM.Environment.Elements;
+using BH.Engine.Environment;
+using BH.Engine.Geometry;
+
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Gets the top right most point of a panel when looking from within the space to the outside")]
+        [Input("panel", "The Environment Panel to get the top right most point of")]
+        [Input("panelsAsSpace", "A collection of Environment Panels representing a single space")]
+        [Output("TopRightPoint", "The top right most point of the panel")]
+        public static Point TopRight(this Panel panel, List<Panel> panelsAsSpace)
+        {
+            return panel.Polyline().TopRight(panelsAsSpace);
+        }
+
+        [Description("Gets the top right most point of a polyline when looking from within the space to the outside")]
+        [Input("polyline", "The BHoM Geometry Polyline to get the bottom right most point of")]
+        [Input("panelsAsSpace", "A collection of Environment Panels representing a single space")]
+        [Output("topRightPoint", "The top right most point of the panel")]
+        public static Point TopRight(this Polyline polyline, List<Panel> panelsAsSpace)
+        {
+            Vector normal = polyline.Normal();
+            if (!polyline.NormalAwayFromSpace(panelsAsSpace))
+                normal = polyline.Flip().Normal();
+
+            Point centre = polyline.Centroid();
+
+            Line line = new Line
+            {
+                Start = centre,
+                End = centre.Translate(normal),
+            };
+
+            List<Point> pnts = polyline.DiscontinuityPoints();
+
+            bool wasFlat = false;
+            TransformMatrix transform = null;
+
+            if (pnts.Min(x => Math.Round(x.Z, 6)) == pnts.Max(x => Math.Round(x.Z, 6)))
+            {
+                //All the points are on the same Z level - we're looking at a floor/roof
+                transform = BH.Engine.Geometry.Create.RotationMatrix(polyline.Centroid(), new Vector { X = 1, Y = 0, Z = 0 }, 1.5708);
+                polyline = polyline.Transform(transform);
+                pnts = polyline.ControlPoints;
+                line.End = line.Start.Translate(polyline.Normal());
+                wasFlat = true;
+            }
+
+            Point rightMost = null;
+            foreach (Point p in pnts)
+            {
+                if (!IsLeft(line, p) && (rightMost == null || rightMost.Z < p.Z))
+                    rightMost = p;
+            }
+
+            if (wasFlat && rightMost != null)
+                rightMost = rightMost.Transform(transform.Invert());
+
+            return rightMost;
+        }
+    }
+}
