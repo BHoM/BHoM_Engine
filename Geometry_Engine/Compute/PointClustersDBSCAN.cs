041c7660638f8cb7b5b815bf6df56a92f61eb45e
Kalle Thorsager <Kalle.Thorsager@BuroHappold.com>
2020-07-02 12:27:59 +0100

    Update PointClustersDBSCAN to use DomainTree

--- Geometry_Engine/Compute/PointClustersDBSCAN.cs
+++ Geometry_Engine/Compute/PointClustersDBSCAN.cs
@@ -22,6 +22,7 @@
 
 using BH.Engine.Data;
 using BH.oM.Geometry;
+using BH.oM.Data.Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,12 +34,53 @@ namespace BH.Engine.Geometry
         /***************************************************/
         /**** public Methods - Vectors                  ****/
         /***************************************************/
-
+        
         public static List<List<Point>> PointClustersDBSCAN(this List<Point> points, double maxDist, int minPointCount = 1)
         {
-            double maxSqrDist = maxDist * maxDist;
-            Func<Point, Point, bool> metricFunction = (x, y) => x.SquareDistance(y) <= maxSqrDist;
-            return points.ClusterDBSCAN(metricFunction, minPointCount);
+            List<Point> items = new List<Point>(points);
+            DomainTree<int> indexTree = Data.Create.DomainTree(items.Select((x,i) => Data.Create.DomainTreeLeaf(i, x.IBounds().DomainBox())));
+
+            double sqDist = maxDist * maxDist;
+
+            // Distance between boxes is the distance between Points for Points
+            Func<DomainTree<int>, DomainBox, bool> evaluator = (a, b) => a.DomainBox.SquareDistance(b) < sqDist;
+
+            List<List<Point>> result = new List<List<Point>>();
+            List<Point> toEvaluate = new List<Point>();
+
+            int count = -1;
+            for (int i = 0; i < items.Count; i++)
+            {
+                Point pivot = items[i];
+
+                if (pivot == null)
+                    continue;
+
+                result.Add(new List<Point>());
+                count++;
+                toEvaluate.Add(pivot);
+                items[i] = null;    // Set to null as it is not there anymore, but this also retains the indecies for the tree.
+
+                while (toEvaluate.Count > 0)
+                {
+                    // Find all the neighbours for each point in toEvaluate, and add them in toEvaluate
+                    foreach (int index in Data.Query.ItemsInRange<DomainTree<int>, int>(indexTree, x => evaluator(x, toEvaluate[0].IBounds().DomainBox())))
+                    {
+                        Point pt = items[index];
+                        if (pt != null)
+                        {
+                            toEvaluate.Add(pt);
+                            items[index] = null;
+                        }
+                    }
+
+                    // move the checked points from toEvaluate to result
+                    result[count].Add(toEvaluate[0]);
+                    toEvaluate.RemoveAt(0);
+                }
+            }
+
+            return result.Where(list => list.Count >= minPointCount).ToList();
         }
 
         /***************************************************/
