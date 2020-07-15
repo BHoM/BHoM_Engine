9e407bc43bf481dadc7c5d910526371ec49bf5a2
Alessio Lombardi <alessio.lombardi@burohappold.com>
2020-07-15 20:36:04 +0100

    Added Create() method for DiffConfig to remedy the bug in BHoM UI (see description)
    Bug in the auto Create() method ("auto-property initialisers for ByRef values like lists do not populate default values") is resolved.

--- Diffing_Engine/Create/DiffConfig.cs
+++ Diffing_Engine/Create/DiffConfig.cs
@@ -47,7 +47,22 @@ namespace BH.Engine.Diffing
         [Input("storeUnchangedObjects", "If enabled, the Diff stores also the objects that did not change (`Unchanged` property).")]
         public static DiffConfig DiffConfig(bool enablePropertyDiffing, bool storeUnchangedObjects)
         {
-            return new DiffConfig() { EnablePropertyDiffing = enablePropertyDiffing, StoreUnchangedObjects = storeUnchangedObjects };
+            return Create.DiffConfig(enablePropertyDiffing, storeUnchangedObjects, null, null);
+        }
+
+        [Description("Defines configurations for the diffing.")]
+        [Input("enablePropertyDiffing", "Enables the property-level diffing: differences in object properties are stored in the `ModifiedPropsPerObject` dictionary.")]
+        [Input("storeUnchangedObjects", "If enabled, the Diff stores also the objects that did not change (`Unchanged` property).")]
+        public static DiffConfig DiffConfig(bool enablePropertyDiffing = true, bool storeUnchangedObjects = true, List<string> propertiesToConsider = null, List<string> propertiesToIgnore = null, List<string> customDataToIgnore = null)
+        {
+            return new DiffConfig()
+            {
+                EnablePropertyDiffing = enablePropertyDiffing,
+                StoreUnchangedObjects = storeUnchangedObjects,
+                PropertiesToConsider = propertiesToConsider ?? new List<string>(),
+                PropertiesToIgnore = propertiesToIgnore ?? new List<string>() { "BHoM_Guid" },
+                CustomDataToIgnore = customDataToIgnore ?? new List<string>() { "RenderMesh" },
+            };
         }
     }
 }
