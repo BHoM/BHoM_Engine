befc9ade2b0ccd7e3b3bd153a8a2b2a1bf54fb99
Isak Naslund <Isak.Naslund@burohappold.com>
2020-08-12 13:21:17 +0200

    Adding Geometry for panel and Set outlines and internal openings

---
+++ Analytical_Engine/Modify/SetOutlineElements1D.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Dimensional;
+using BH.oM.Geometry;
+using BH.oM.Analytical.Elements;
+using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+using BH.Engine.Base;
+
+
+namespace BH.Engine.Structure
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        [Description("Sets the Outline Element1Ds of an opening, i.e. the Edges of an Opening. Method required for all IElement2Ds.")]
+        [Input("opening", "The Opening to update the Edges of.")]
+        [Input("edges", "A list of IElement1Ds which all should be of a type Edge accepted by the Opening or Geometrical ICurve. \n" +
+                "ICurve will default the outlines properties.")]
+        [Output("opening", "The opening with updated Edges.")]
+        public static IOpening<TEdge> SetOutlineElements1D<TEdge>(this IOpening<TEdge> opening, IEnumerable<IElement1D> edges)
+                where TEdge : class, IEdge, new()
+        {
+            IOpening<TEdge> o = opening.GetShallowClone(true) as IOpening<TEdge>;
+            o.Edges = edges.Select(x => x is ICurve ? new TEdge() { Curve = (x as ICurve) } : x as TEdge).ToList();
+            return o;
+        }
+
+        /***************************************************/
+
+        [Description("Sets the outline Element1Ds of a Panel, i.e. the ExternalEdges of a Panel. Method required for all IElement2Ds.")]
+        [Input("panel", "The Panel to update the ExternalEdges of.")]
+        [Input("edges", "A list of IElement1Ds which all should be of a type Edge accepted by the Panel or Geometrical ICurve. \n" +
+                        "ICurve will default the outlines properties.")]
+        [Output("panel", "The Panel with updated ExternalEdges.")]
+        public static IPanel<TEdge, TOpening> SetOutlineElements1D<TEdge, TOpening>(this IPanel<TEdge, TOpening> panel, IEnumerable<IElement1D> edges)
+            where TEdge : class, IEdge, new()
+            where TOpening : IOpening<TEdge>
+        {
+            IPanel<TEdge, TOpening> pp = panel.GetShallowClone(true) as IPanel<TEdge, TOpening>;
+            pp.ExternalEdges = edges.Select(x => x is ICurve ? new TEdge() { Curve = (x as ICurve) } : x as TEdge).ToList();
+            return pp;
+        }
+
+        /***************************************************/
+    }
+}
+
---
+++ Analytical_Engine/Query/Geometry.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+
+using BH.Engine.Geometry;
+using BH.oM.Geometry;
+using BH.oM.Analytical.Elements;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Base;
+
+namespace BH.Engine.Analytical
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Gets the geometry of a analytical Panel at its centre. Method required for automatic display in UI packages.")]
+        [Input("panel", "Panel to get the planar surface geometry from.")]
+        [Output("surface", "The geometry of the analytical Panel at its centre.")]
+        public static PlanarSurface Geometry<TEdge, TOpening>(this IPanel<TEdge, TOpening> panel)
+            where TEdge : IEdge
+            where TOpening : IOpening<TEdge>
+        {
+            return new PlanarSurface(
+                    Engine.Geometry.Compute.IJoin(panel.ExternalEdges.Select(x => x.Curve).ToList()).FirstOrDefault(),
+                    panel.Openings.SelectMany(x => Engine.Geometry.Compute.IJoin(x.Edges.Select(y => y.Curve).ToList())).Cast<ICurve>().ToList());
+        }
+
+        /***************************************************/
+    }
+}
