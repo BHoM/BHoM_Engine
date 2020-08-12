9f58c8c74d46ffb1f8795cacfe02f080144e3a29
Isak Naslund <Isak.Naslund@burohappold.com>
2020-08-12 13:30:59 +0200

    more geometry methods

--- Analytical_Engine/Query/Geometry.cs
+++ Analytical_Engine/Query/Geometry.cs
@@ -83,5 +83,81 @@ namespace BH.Engine.Analytical
         }
 
         /***************************************************/
+
+        [Description("Gets the geometry of a analytical Surface at its centre. Method required for automatic display in UI packages.")]
+        [Input("surface", "ANalytical Surface to get the geometrical Surface geometry from.")]
+        [Output("surface", "The geometry of the structural Sufarce at its centre.")]
+        public static IGeometry Geometry(this BH.oM.Analytical.Elements.ISurface surface)
+        {
+            return surface.Extents;
+        }
+
+        /***************************************************/
+
+        [Description("Gets the geometry of a analytical IMesh as a geometrical Mesh. A geometrical mesh only supports 3 and 4 nodes faces, while a FEMesh does not have this limitation. For FEMeshFaces with more than 4 nodes or less than 3 this operation is therefore not possible. Method required for automatic display in UI packages.")]
+        [Input("feMesh", "Analytical IMesh to get the mesh geometry from.")]
+        [Output("mesh", "The geometry of the FEMesh as a geometrical Mesh.")]
+        public static Mesh Geometry<TNode, TFace>(this IMesh<TNode, TFace> mesh)
+            where TNode : INode
+            where TFace : IFace
+        {
+            Mesh geoMesh = new Mesh();
+
+            geoMesh.Vertices = mesh.Nodes.Select(x => x.Position).ToList();
+
+            geoMesh.Faces.AddRange(mesh.Faces.Geometry());
+
+            return geoMesh;
+        }
+
+        /***************************************************/
+
+        [Description("Gets the geometry of a collection of IFaces as a geometrical Mesh's Faces. A geometrical mesh face only supports 3 and 4 nodes faces, while a FEMeshFace does not have this limitation. For FEMeshFaces with more than 4 nodes or less than 3 this operation is therefore not possible. Method required for automatic display in UI packages.")]
+        [Input("feFaces", "Analytical IFaces to get the mesh faces geometry from.")]
+        [Output("faces", "The geometry of the FEMeshFaces as geometrical Mesh Faces.")]
+        public static IEnumerable<Face> Geometry<TFace>(this IEnumerable<TFace> feFaces)
+            where TFace : IFace
+        {
+            List<Face> result = new List<Face>();
+            foreach (IFace feFace in feFaces)
+            {
+                Face face = Geometry(feFace);
+                if (face != null)
+                    result.Add(face);
+            }
+            return result;
+        }
+
+        /***************************************************/
+
+        [Description("Gets the geometry of a analytical IFace as a geometrical Mesh's Face. A geometrical mesh face only supports 3 and 4 nodes faces, while a FEMeshFace does not have this limitation. For FEMeshFaces with more than 4 nodes or less than 3 this operation is therefore not possible. Method required for automatic display in UI packages.")]
+        [Input("feFace", "Analytical IFace to get the mesh face geometry from.")]
+        [Output("face", "The geometry of the FEMeshFace as geometrical Mesh Face.")]
+        public static Face Geometry(this IFace iface)
+        {
+
+            if (iface.NodeListIndices.Count < 3)
+            {
+                Reflection.Compute.RecordError("Insuffiecient node indices");
+                return null;
+            }
+            if (iface.NodeListIndices.Count > 4)
+            {
+                Reflection.Compute.RecordError("To high number of node indices. Can only handle triangular and quads");
+                return null;
+            }
+
+            Face face = new Face();
+
+            face.A = iface.NodeListIndices[0];
+            face.B = iface.NodeListIndices[1];
+            face.C = iface.NodeListIndices[2];
+
+            if (iface.NodeListIndices.Count == 4)
+                face.D = iface.NodeListIndices[3];
+
+            return face;
+        }
+
     }
 }
