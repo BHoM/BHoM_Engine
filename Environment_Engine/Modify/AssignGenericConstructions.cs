930292182e412632418c10bbf055003a09cca9ff
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-08-19 16:51:19 +0100

    Update to use singular objects

--- Environment_Engine/Modify/AssignGenericConstructions.cs
+++ Environment_Engine/Modify/AssignGenericConstructions.cs
@@ -17,9 +17,9 @@ namespace BH.Engine.Environment
         [Description("Assign generic constructions to a collection of Environment Panels based on their panel type. This will load a dataset of Generic Constructions and attempt to assign each generic construction to the panel based on the panel type. For example, panel type wall will be assigned the 'generic_wall' construction. WARNING: This will overwrite constructions hosted on objects passed.")]
         [Input("panels", "A collection of Environment Panels to assign constructions to")]
         [Output("panels", "A collection of Environment Panels with the generic constructions assigned")]
-        public static List<Panel> AssignGenericConstructions(this List<Panel> panels)
+        public static Panel AssignGenericConstructions(this Panel panel)
         {
-            List<Panel> cloned = new List<Panel>(panels.Select(x => x.DeepClone<Panel>()).ToList());
+            Panel cloned = panel.DeepClone<Panel>();
 
             List<Construction> constructions = BH.Engine.Library.Query.Library("GenericConstructions").Select(x => x as Construction).ToList();
             if (constructions.Count == 0)
@@ -28,35 +28,30 @@ namespace BH.Engine.Environment
                 return cloned;
             }
 
-            foreach(Panel p in cloned)
-            {
-                if (p.Construction != null)
-                {
-                    BH.Engine.Reflection.Compute.RecordWarning(string.Format("The construction for {0} was not null and will be replaced with a generic construction", p.BHoM_Guid));
+            if (cloned.Construction != null)
+                BH.Engine.Reflection.Compute.RecordWarning(string.Format("The construction for the panel with GUID {0} was not null and will be replaced with a generic construction", cloned.BHoM_Guid));
 
-                }
-                switch (p.Type)
-                {
-                    case PanelType.Floor:
-                    case PanelType.FloorExposed:
-                    case PanelType.FloorInternal:
-                    case PanelType.FloorRaised:
-                    case PanelType.SlabOnGrade:
-                    case PanelType.UndergroundSlab:
-                        p.Construction = constructions.Where(x => x.Name == "generic_floor").FirstOrDefault();
-                        break;
-                    case PanelType.Ceiling:
-                    case PanelType.UndergroundCeiling:
-                    case PanelType.Roof:
-                        p.Construction = constructions.Where(x => x.Name == "generic_ceiling").FirstOrDefault();
-                        break;
-                    case PanelType.WallInternal:
-                        p.Construction = constructions.Where(x => x.Name == "generic_partition").FirstOrDefault();
-                        break;
-                    default:
-                        p.Construction = constructions.Where(x => x.Name == "generic_wall").FirstOrDefault();
-                        break;
-                }
+            switch (cloned.Type)
+            {
+                case PanelType.Floor:
+                case PanelType.FloorExposed:
+                case PanelType.FloorInternal:
+                case PanelType.FloorRaised:
+                case PanelType.SlabOnGrade:
+                case PanelType.UndergroundSlab:
+                    cloned.Construction = constructions.Where(x => x.Name == "generic_floor").FirstOrDefault();
+                    break;
+                case PanelType.Ceiling:
+                case PanelType.UndergroundCeiling:
+                case PanelType.Roof:
+                    cloned.Construction = constructions.Where(x => x.Name == "generic_ceiling").FirstOrDefault();
+                    break;
+                case PanelType.WallInternal:
+                    cloned.Construction = constructions.Where(x => x.Name == "generic_partition").FirstOrDefault();
+                    break;
+                default:
+                    cloned.Construction = constructions.Where(x => x.Name == "generic_wall").FirstOrDefault();
+                    break;
             }
 
             return cloned;
@@ -65,9 +60,9 @@ namespace BH.Engine.Environment
         [Description("Assign generic constructions to a collection of Environment Openings based on their opening type. This will load a dataset of Generic Constructions and attempt to assign each generic construction to the opening based on the opening type. For example, opening type Window will be assigned the 'generic_window' construction. WARNING: This will overwrite constructions hosted on objects passed.")]
         [Input("openings", "A collection of Environment Openings to assign constructions to")]
         [Output("openings", "A collection of Environment Openings with the generic constructions assigned")]
-        public static List<Opening> AssignGenericConstructions(this List<Opening> opening)
+        public static Opening AssignGenericConstructions(this Opening opening)
         {
-            List<Opening> cloned = new List<Opening>(opening.Select(x => x.DeepClone<Opening>()).ToList());
+            Opening cloned = opening.DeepClone<Opening>();
 
             List<Construction> constructions = BH.Engine.Library.Query.Library("GenericConstructions").Select(x => x as Construction).ToList();
             if (constructions.Count == 0)
@@ -76,32 +71,27 @@ namespace BH.Engine.Environment
                 return cloned;
             }
 
-            foreach (Opening p in cloned)
-            {
-                if (p.OpeningConstruction != null)
-                {
-                    BH.Engine.Reflection.Compute.RecordWarning(string.Format("The construction for {0} was not null and will be replaced with a generic construction", p.BHoM_Guid));
+            if (cloned.OpeningConstruction != null)
+                BH.Engine.Reflection.Compute.RecordWarning(string.Format("The construction for the opening with GUID {0} was not null and will be replaced with a generic construction", cloned.BHoM_Guid));
 
-                }
-                switch (p.Type)
-                {
-                    case OpeningType.CurtainWall:
-                    case OpeningType.Door:
-                    case OpeningType.Frame:
-                    case OpeningType.Glazing:
-                    case OpeningType.Hole:
-                    case OpeningType.Rooflight:
-                    case OpeningType.RooflightWithFrame:
-                    case OpeningType.Undefined:
-                    case OpeningType.VehicleDoor:
-                    case OpeningType.Window:
-                    case OpeningType.WindowWithFrame:
-                        p.OpeningConstruction = constructions.Where(x => x.Name == "generic_window").FirstOrDefault();
-                        break;
-                    default:
-                        p.OpeningConstruction = constructions.Where(x => x.Name == "generic_window").FirstOrDefault();
-                        break;
-                }
+            switch (cloned.Type)
+            {
+                case OpeningType.CurtainWall:
+                case OpeningType.Door:
+                case OpeningType.Frame:
+                case OpeningType.Glazing:
+                case OpeningType.Hole:
+                case OpeningType.Rooflight:
+                case OpeningType.RooflightWithFrame:
+                case OpeningType.Undefined:
+                case OpeningType.VehicleDoor:
+                case OpeningType.Window:
+                case OpeningType.WindowWithFrame:
+                    cloned.OpeningConstruction = constructions.Where(x => x.Name == "generic_window").FirstOrDefault();
+                    break;
+                default:
+                    cloned.OpeningConstruction = constructions.Where(x => x.Name == "generic_window").FirstOrDefault();
+                    break;
             }
 
             return cloned;
