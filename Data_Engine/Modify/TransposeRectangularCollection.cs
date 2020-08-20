93ea96cbdb190204ff41383321b2133faa97d526
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-08-20 09:30:12 +0100

    Update AssignGenericConstructions

--- Data_Engine/Modify/TransposeRectangularCollection.cs
+++ Data_Engine/Modify/TransposeRectangularCollection.cs
@@ -40,11 +40,16 @@ namespace BH.Engine.Data
         {
             var longest = lists.Any() ? lists.Max(l => l.Count) : 0;
             List<List<T>> outer = new List<List<T>>(longest);
+
             for (int i = 0; i < longest; i++)
                 outer.Add(new List<T>(lists.Count));
+
             for (int j = 0; j < lists.Count; j++)
+            {
                 for (int i = 0; i < longest; i++)
                     outer[i].Add(lists[j].Count > i ? lists[j][i] : default(T));
+            }
+
             return outer;
         }
     }
