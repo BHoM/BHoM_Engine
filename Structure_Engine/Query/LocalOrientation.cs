f6fe15838c30b237e6df439e0bea9eb63a371f5d
Isak Naslund <Isak.Naslund@burohappold.com>
2020-08-20 17:57:22 +0200

    Adding local orintation methods for FEMesh and FEMeshFace

--- Structure_Engine/Query/LocalOrientation.cs
+++ Structure_Engine/Query/LocalOrientation.cs
@@ -30,6 +30,7 @@ using BH.oM.Geometry;
 using BH.oM.Geometry.CoordinateSystem;
 using BH.Engine.Geometry;
 using BH.Engine.Spatial;
+using BH.oM.Quantities.Attributes;
 
 namespace BH.Engine.Structure
 {
@@ -44,22 +45,54 @@ namespace BH.Engine.Structure
         [Output("orienation", "The local orientation of the Panel as a vector Basis.")]
         public static Basis LocalOrientation(this Panel panel)
         {
-            Vector normal = Engine.Spatial.Query.Normal(panel);
+            return LocalOrientation(Engine.Spatial.Query.Normal(panel), panel.OrientationAngle);
+        }
 
+        /***************************************************/
+
+        [Description("Get the Vector basis system descibring the local axes orientation of the faces of the FEMesh in the global coordinate system where the z-axis is the normal of each face and the x- and y-axes are the directions of the local in-plane axes.")]
+        [Input("mesh", "The FEMesh to extract the local orientations from from.")]
+        [Output("orienations", "The local orientations of the mesh as a vector Basis.")]
+        public static List<Basis> LocalOrientations(this FEMesh mesh)
+        {
+            return mesh.Faces.Select(x => x.LocalOrientation(mesh)).ToList();
+        }
+
+        /***************************************************/
+
+        [Description("Get the Vector basis system descibring the local axes orientation of a face of a FEMesh in the global coordinate system where the z-axis is the normal of each face and the x- and y-axes are the directions of the local in-plane axes.")]
+        [Input("face", "The FEMeshFace to extract the local orientation from from.")]
+        [Input("mesh", "The FEMesh to which the face belongs.")]
+        [Output("orienation", "The local orientation of the FEMeshFace as a vector Basis.")]
+        public static Basis LocalOrientation(this FEMeshFace face, FEMesh mesh)
+        {
+            return LocalOrientation(face.Normal(mesh), face.OrientationAngle);
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        [Description("Get the Vector basis system descibring the local axis orientation from a normal vector and an orientation angle. Used by both Panels and FEMeshes.")]
+        [Input("normal", "The normal vector of the element.")]
+        [Input("orientationAngle", "The orientation angle of the element.", typeof(Angle))]
+        [Output("orienation", "The local orientation of the element as a vector Basis.")]
+        private static Basis LocalOrientation(this Vector normal, double orientationAngle)
+        {
             Vector localX, localY;
 
             if (normal.IsParallel(Vector.XAxis) == 0)
             {
                 //Normal not parallel to global X
                 localX = Vector.XAxis.Project(new Plane { Normal = normal }).Normalise();
-                localX = localX.Rotate(panel.OrientationAngle, normal);
+                localX = localX.Rotate(orientationAngle, normal);
                 localY = normal.CrossProduct(localX);
             }
             else
             {
                 //Normal is parallel to global x
                 localY = Vector.YAxis.Project(new Plane { Normal = normal }).Normalise();
-                localY = localY.Rotate(panel.OrientationAngle, normal);
+                localY = localY.Rotate(orientationAngle, normal);
                 localX = localY.CrossProduct(normal);
             }
 
