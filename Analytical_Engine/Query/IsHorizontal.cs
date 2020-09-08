0d340e93fda5abb08e62b8b3bc7301a5ed71fe7b
StephennipBH <Stephen.nip@Burohappold.com>
2020-09-08 16:52:16 +0100

    Create IsHorizontal

---
+++ Analytical_Engine/Query/IsHorizontal.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model(BHoM)
+ * Copyright(c) 2015 - 2020, the respective contributors.All rights reserved.
+
+*
+* Each contributor holds copyright over their respective contributions.
+
+* The project versioning(Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or
+* (at your option) any later version.
+
+*
+* The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this code.If not, see<https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Analytical.Elements;
+using BH.oM.Geometry;
+using BH.oM.Reflection.Attributes;
+using BH.Engine.Geometry;
+using BH.Engine.Reflection;
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace BH.Engine.Analytical
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Determines whether a panel is horizontal.")]
+        [Input("panel", "The IPanel to check if a panel is horizontal.")]
+        [Output("bool", "True for panels for panels is horizontal.")]
+        public static bool IsHorizontal<TEdge, TOpening>(this IPanel<TEdge, TOpening> panel)
+            where TEdge : IEdge
+            where TOpening : IOpening<TEdge>
+        {
+            PolyCurve polycurve = ExternalPolyCurve(panel);
+            List<Point> points = polycurve.DiscontinuityPoints();
+            List<Vector> vectors = VectorsBetweenPoints(points);
+            Point P1 = new Point { X = 0, Y = 0, Z = 0 };
+            Point P2 = new Point { X = 1, Y = 0, Z = 0 };
+            Line Flat = new Line { Start = P1, End = P2 };
+            foreach (Vector vector in vectors) 
+            
+                if ((Flat.Direction().IsParallel(vector, Tolerance.Angle))== 0)
+                
+                    return true ;
+            foreach (Vector vector in vectors)
+
+                if ((Flat.Direction().IsParallel(vector, Tolerance.Angle)) == 1)
+
+                    return false;
+
+        }
+
+        
+    }
+
+}
+
