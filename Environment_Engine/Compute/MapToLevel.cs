62be2d489134fb5d73cf5c5faae3edeb872766c8
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-09-18 11:49:25 +0100

    Fix spaces mapping to levels
    Fix spelling

--- Environment_Engine/Compute/MapToLevel.cs
+++ Environment_Engine/Compute/MapToLevel.cs
@@ -28,7 +28,7 @@ using System.Threading.Tasks;
 
 using BH.oM.Reflection;
 using BH.oM.Geometry;
-using BH.oM.Environment;
+using BH.oM.Environment.Elements;
 using BH.oM.Architecture.Elements;
 using BH.oM.Geometry.SettingOut;
 
@@ -41,11 +41,78 @@ namespace BH.Engine.Environment
 {
     public static partial class Compute
     {
-        [Description("Organise the BHoM Room object into a data structure based on the level of each room.")]
-        [Input("rooms", "A collection of BHoM Rooms to be organized.")]
+        [Description("Organise the BHoM Space objects into a data structure based on the level of each space.")]
+        [Input("spaces", "A collection of BHoM Spaces to be organised.")]
         [Input("levels", "A collection of BHoM Levels.")]
         [Input("decimals", "Provide decimal location to define the degree of tolerance for data matching.")]
-        [MultiOutput(0, "roomsByLevel", "A collection of BHoM Rooms group by levels.")]
+        [MultiOutput(0, "spacesByLevel", "A collection of BHoM Spaces grouped by levels.")]
+        [MultiOutput(1, "levelsInUse", "A sublist of the BHoM Levels that have Room.")]
+        [MultiOutput(2, "spacesNotMapped", "A collection of BHoM Spaces which did not sit neatly on any of the provided levels")]
+        public static Output<List<List<Space>>, List<oM.Geometry.SettingOut.Level>, List<Space>> MapToLevel(List<Space> spaces, List<oM.Geometry.SettingOut.Level> levels, int decimals = 6)
+        {
+            List<List<Space>> spacesByLevel = new List<List<Space>>();
+            List<oM.Geometry.SettingOut.Level> levelsInUse = new List<oM.Geometry.SettingOut.Level>();
+            List<Space> spacesNotByLevel = new List<Space>();
+            List<oM.Geometry.SettingOut.Level> roundedLevels = new List<oM.Geometry.SettingOut.Level>();
+
+            for (int x = 0; x < levels.Count; x++)
+            {
+                oM.Geometry.SettingOut.Level lvl = new oM.Geometry.SettingOut.Level { Elevation = Math.Round(levels[x].Elevation, decimals), Name = levels[x].Name, Fragments = levels[x].Fragments, CustomData = levels[x].CustomData };
+                roundedLevels.Add(lvl); //Round the levels
+            }
+
+            Dictionary<double, List<Space>> mappedRooms = new Dictionary<double, List<Space>>();
+
+            //Map everything
+            foreach (Space space in spaces)
+            {
+                BoundingBox bbox = space.Perimeter.IBounds();
+                double zLevel = Math.Round(bbox.Min.Z, decimals);
+
+                oM.Geometry.SettingOut.Level roundedLevel = roundedLevels.Where(x => x.Elevation == zLevel).FirstOrDefault();
+                if (roundedLevel == null)
+                {
+                    spacesNotByLevel.Add(space);
+                    continue; //zLevel does not exist in the search levels
+                }
+
+                int levelIndex = roundedLevels.IndexOf(roundedLevel);
+
+                if (levelIndex == -1)
+                {
+                    spacesNotByLevel.Add(space);
+                    continue; //zLevel does not exist in the search levels
+                }
+
+                if (!mappedRooms.ContainsKey(levels[levelIndex].Elevation))
+                    mappedRooms.Add(levels[levelIndex].Elevation, new List<Space>());
+
+                levelsInUse.Add(levels[levelIndex]);
+
+                mappedRooms[levels[levelIndex].Elevation].Add(space);
+            }
+
+            foreach (KeyValuePair<double, List<Space>> kvp in mappedRooms.OrderBy(x => x.Key))
+                spacesByLevel.Add(kvp.Value);
+
+            Output<List<List<Space>>, List<oM.Geometry.SettingOut.Level>, List<Space>> output = new Output<List<List<Space>>, List<oM.Geometry.SettingOut.Level>, List<Space>>
+            {
+                Item1 = spacesByLevel,
+                Item2 = levelsInUse.OrderBy(x => x.Elevation).Distinct().ToList(),
+                Item3 = spacesNotByLevel,
+            };
+
+            if (spacesNotByLevel.Count > 0)
+                BH.Engine.Reflection.Compute.RecordWarning("Some spaces were not able to be mapped to a level. See the spacesNotMapped output to examine which spaces and resolve any issues");
+
+            return output;
+        }
+
+        [Description("Organise the BHoM Room objects into a data structure based on the level of each room.")]
+        [Input("rooms", "A collection of BHoM Rooms to be organised.")]
+        [Input("levels", "A collection of BHoM Levels.")]
+        [Input("decimals", "Provide decimal location to define the degree of tolerance for data matching.")]
+        [MultiOutput(0, "roomsByLevel", "A collection of BHoM Rooms grouped by levels.")]
         [MultiOutput(1, "levelsInUse", "A sublist of the BHoM Levels that have Room.")]
         [MultiOutput(2, "roomsNotMapped", "A collection of BHoM Rooms which did not sit neatly on any of the provided levels")]
         public static Output<List<List<Room>>, List<oM.Geometry.SettingOut.Level>, List<Room>> MapToLevel(List<Room> rooms, List<oM.Geometry.SettingOut.Level> levels, int decimals = 6)
