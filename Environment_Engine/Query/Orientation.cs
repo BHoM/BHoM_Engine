923f546787499e0d210cd161d774fb61e4f0c4a0
Lina Eriksson <lina.eriksson@burohappold.com>
2020-10-08 10:28:27 +0100

    Changes made for azimuth angle and when north is not 90 degrees

--- Environment_Engine/Query/Orientation.cs
+++ Environment_Engine/Query/Orientation.cs
@@ -31,6 +31,7 @@ using BH.oM.Geometry;
 
 using BH.oM.Reflection.Attributes;
 using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BH.Engine.Environment
 {
@@ -42,17 +43,64 @@ namespace BH.Engine.Environment
 
         [Description("Returns the orientation of a given environmental object")]
         [Input("environmentObject", "Any object implementing the IEnvironmentObject interface that can have its orientation queried")]
-        [Output("orientation", "The orientation of the Environment Object")]
-        public static double Orientation(this IEnvironmentObject environmentObject)
+        [Input("northAngle", "The angle in degrees for north. Default is 90.0.")]
+        [Input("azimuthAngle", "Set to true to return the azimuth angle from north instead of the angle between north and the object normal. Default is false.")]
+        public static double NormalOrientation(this IEnvironmentObject environmentObject, double northAngle = 90.0, bool azimuthAngle = false)
         {
+            double northAngleRadians = northAngle * (Math.PI / 180);
+            Vector northVector = BH.Engine.Geometry.Create.Vector((double)Math.Cos(northAngleRadians), (double)Math.Sin(northAngleRadians));
             Polyline pLine = environmentObject.Polyline();
-
             List<Point> pts = pLine.DiscontinuityPoints();
-            Plane plane = BH.Engine.Geometry.Create.Plane(pts[0], pts[1], pts[2]); //Some protection on this needed maybe?
+            Plane plane = BH.Engine.Geometry.Create.Plane(pts[0], pts[1], pts[2]);
+            Vector normal = plane.Normal;
+            Vector up = BH.Engine.Geometry.Create.Vector(0, 0, 1);
+            Vector down = BH.Engine.Geometry.Create.Vector(0, 0, -1);
+            Vector right = BH.Engine.Geometry.Create.Vector(1, 0, 0);
 
-            Vector xyNormal = BH.Engine.Geometry.Create.Vector(0, 1, 0);
 
-            return BH.Engine.Geometry.Query.Angle(plane.Normal, xyNormal) * (180 / Math.PI);
+
+            if (normal == up || normal == down)
+            {
+                BH.Engine.Reflection.Compute.RecordError("This method cannot successfully evaluate the orientation");
+                return (-1);
+            }
+
+            else if (normal.X > 0 && northAngle == 90.0 && azimuthAngle == true)
+            {
+                return (360 - ((BH.Engine.Geometry.Query.Angle(normal, northVector) * (180 / Math.PI))));
+            }
+
+            else
+            {
+                double normalAngle = 0;
+
+                if (normal.Y <= 0)
+                {
+                    normalAngle = (360 - ((BH.Engine.Geometry.Query.Angle(normal, right) * (180 / Math.PI))));
+                }
+                else
+                {
+                    normalAngle = (BH.Engine.Geometry.Query.Angle(normal, right) * (180 / Math.PI));
+                }
+
+                if ((normalAngle - northAngle) >= 180 && northAngle != 90.0 && northAngle <= 180 && azimuthAngle == true)
+                {
+                    return (360 - ((BH.Engine.Geometry.Query.Angle(normal, northVector) * (180 / Math.PI))));
+                }
+
+                else if (normalAngle < northAngle && normalAngle >= 0 && northAngle != 90.0 && northAngle <= 180 && azimuthAngle == true)
+                {
+                    return (360 - ((BH.Engine.Geometry.Query.Angle(normal, northVector) * (180 / Math.PI))));
+                }
+
+                else if (normalAngle < northAngle && normalAngle >= (northAngle - 180) && northAngle != 90.0 && northAngle >= 180 && azimuthAngle == true)
+                {
+                    return (360 - ((BH.Engine.Geometry.Query.Angle(normal, northVector) * (180 / Math.PI))));
+                }
+
+                return (BH.Engine.Geometry.Query.Angle(plane.Normal, northVector) * (180 / Math.PI));
+
+            }
         }
     }
 }
