ae26f088d2e843307d4ecf4c73db3bab7dfe5397
tg359 <tristan.gerrish@burohappold.com>
2020-10-12 13:50:02 +0100

    Refactored to be radians-based

--- Environment_Engine/Query/Cardinality.cs
+++ Environment_Engine/Query/Cardinality.cs
@@ -20,17 +20,12 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using System;
 using System.Collections.Generic;
-
-using System.Linq;
-using BH.oM.Environment;
-
-using BH.Engine.Geometry;
-using BH.oM.Geometry;
 
 using BH.oM.Reflection.Attributes;
 using System.ComponentModel;
+using System;
+using System.Linq;
 
 namespace BH.Engine.Environment
 {
@@ -40,22 +35,15 @@ namespace BH.Engine.Environment
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Returns the cardinal orientation of a given angle, where that angle is related to north at 0°")]
-        [Input("degreesFromNorth", "The angle in degrees from north (at 0°)")]
+        [Description("Returns the cardinal orientation of a given angle, where that angle is related to north at 0 radians")]
+        [Input("angleFromNorth", "The angle to north in radians (+Ve is interpreted as clockwise from north at 0.0 radians)")]
         [Input("directions", "The number of cardinal directions into which angles shall be binned (This value should be one of 4, 8, 16 or 32, and is centred about \"north\")")]
         [Output("cardinality", "The cardinal direction the angle represents")]
-        public static string Cardinality(double degreesFromNorth, int directions = 8)
+        public static string Cardinality(double angleFromNorth, int directions = 8)
         {
-            if (!new List<int>() { 4, 8, 16, 32 }.Contains(directions))
+            if (angleFromNorth > Math.PI * 2 || angleFromNorth < -Math.PI * 2)
             {
-                BH.Engine.Reflection.Compute.RecordError("The number of cardinal directions must be either 4, 8, 16 or 32");
-                return null;
-            }
-
-            if (degreesFromNorth > 360)
-            {
-                BH.Engine.Reflection.Compute.RecordError("The angle provided must be less than or equal to 360");
-                return null;
+                BH.Engine.Reflection.Compute.RecordWarning("The angle entered is beyond the normally expected range for an angle in radians.");
             }
 
             Dictionary<int, List<string>> cardinalDirections = new Dictionary<int, List<string>>()
@@ -66,6 +54,14 @@ namespace BH.Engine.Environment
                 { 32, new List<string>() { "N", "NbE", "NNE", "NEbN", "NE", "NEbE", "ENE", "EbN", "E", "EbS", "ESE", "SEbE", "SE", "SEbS", "SSE", "SbE", "S", "SbW", "SSW", "SWbS", "SW", "SWbW", "WSW", "WbS", "W", "WbN", "WNW", "NWbW", "NW", "NWbN", "NNW", "NbW" } }
             };
 
+            if (!cardinalDirections.Keys.ToList().Contains(directions))
+            {
+                BH.Engine.Reflection.Compute.RecordError(String.Format("The number of cardinal directions must be in [{0}]", string.Join(", ", cardinalDirections.Keys)));
+                return null;
+            }
+
+            angleFromNorth = angleFromNorth * 180 / Math.PI;
+
             int n = 0;
             foreach (string cardinalDirection in cardinalDirections[directions])
             {
@@ -73,20 +69,21 @@ namespace BH.Engine.Environment
                 double halfAngleBetween = angleBetween / 2;
                 if (n == 0)
                 {
-                    if (degreesFromNorth >= 360 - halfAngleBetween || degreesFromNorth < halfAngleBetween)
+                    if (angleFromNorth >= 360 - halfAngleBetween || angleFromNorth < halfAngleBetween)
                     {
                         return cardinalDirection;
                     }
                 }
                 else
                 {
-                    if (degreesFromNorth >= (n * angleBetween) - halfAngleBetween && degreesFromNorth < (n * angleBetween) + halfAngleBetween)
+                    if (angleFromNorth >= (n * angleBetween) - halfAngleBetween && angleFromNorth < (n * angleBetween) + halfAngleBetween)
                     {
                         return cardinalDirection;
                     }
                 }
-                n += 1;
+                n++;
             }
+            BH.Engine.Reflection.Compute.RecordError(String.Format("No cardinal direction found that matches {0:F3} degrees (or {1:F3} radians from north).", angleFromNorth, angleFromNorth * Math.PI / 180));
             return null;
         }
     }
