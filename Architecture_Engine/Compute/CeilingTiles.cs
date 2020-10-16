9d8145beb87e001a5edc1f386077e9a7f6ad8023
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-10-16 14:24:39 +0100

    Initial commit of ceiling tile algorithm pair-programmed with @JohannaOlin1

---
+++ Architecture_Engine/Compute/CeilingTiles.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Architecture.Elements;
+
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+
+namespace BH.Engine.Architecture
+{
+    public static partial class Compute
+    {
+        public static List<CeilingTile> CeilingTiles(Polyline perimeterCurve, List<Line> ceilingTileLines)
+        {
+            List<Point> intersectingPoints = BH.Engine.Geometry.Query.LineIntersections(ceilingTileLines);
+
+            List<Line> splitCurves = ceilingTileLines.SelectMany(x => x.ISplitAtPoints(intersectingPoints)).Cast<Line>().ToList();
+
+            List<Line> perimeterLines = new List<Line>();
+            List<Line> perimeterCurveLines = perimeterCurve.ISubParts().Cast<Line>().ToList();
+
+            foreach(Line l in splitCurves)
+            {
+                foreach(Line l2 in perimeterCurveLines)
+                {
+                    Line intersect = l.BooleanIntersection(l2);
+                    if(intersect != null && intersect.Length() == l.Length())
+                    {
+                        //This line is part of the outer perimeter
+                        perimeterLines.Add(l);
+                    }
+                }
+            }
+
+            foreach (Line l in perimeterLines)
+                splitCurves.Remove(l); //Remove the perimeter from the splits
+
+            List<Line> splitCurves2 = new List<Line>(splitCurves);
+            foreach (Line l in splitCurves2)
+                splitCurves.Add(l); //Refactor this!!!!
+
+
+
+
+
+
+            return new List<CeilingTile>();
+        }
+    }
+}
