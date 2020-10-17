1fc2c2bfd9fa8e6a93948db1cfb441fbce2e8bed
Arnaud Declercq <arnaud.declercq@burohappold.com>
2020-10-17 14:06:06 +0800

    Update Geometry_Engine/Query/PrincipledCurvatureAtParameter.cs
    Co-authored-by: Isak Näslund <isak.naslund@burohappold.com>

--- Geometry_Engine/Query/PrincipledCurvatureAtParameter.cs
+++ Geometry_Engine/Query/PrincipledCurvatureAtParameter.cs
@@ -43,7 +43,7 @@ namespace BH.Engine.Geometry
         [MultiOutput(1, "maxC", "Maximum principled curvature. Curvature of the intersection of the surface and a plane spanned by the normal and maximum principle direction.")]
         [MultiOutput(2, "minK", "Minimum principled direction. Tangent of the curve on the surface with the least curvature.")]
         [MultiOutput(3, "maxK", "Maximum principled direction. Tangent of the curve on the surface with the most curvature.")]
-        public static Output<double, double, Vector,Vector> PrincipledCurvatureAtParameter(this NurbsSurface surface, double u, double v)
+        public static Output<double, double, Vector,Vector> PrincipalCurvatureAtParameter(this NurbsSurface surface, double u, double v)
         {
             // Vector entries for a "Hessian" with regard to u and v
             Vector dU2 = DerivativeAtParameter(surface, u, v, 2, 0);
@@ -133,4 +133,3 @@ namespace BH.Engine.Geometry
 
     }
 }
-
