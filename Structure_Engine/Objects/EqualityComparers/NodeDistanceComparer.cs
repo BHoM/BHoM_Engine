a72e760f22bcf9997aefce9b366a0bdf3ff74ac4
Isak Naslund <Isak.Naslund@burohappold.com>
2020-10-19 18:13:48 +0200

    Update NodeDistanceComparer.cs
    Remove use of GUID and adding mroe null checks

--- Structure_Engine/Objects/EqualityComparers/NodeDistanceComparer.cs
+++ Structure_Engine/Objects/EqualityComparers/NodeDistanceComparer.cs
@@ -59,9 +59,9 @@ namespace BH.Engine.Structure
             if (Object.ReferenceEquals(node1, null) || Object.ReferenceEquals(node2, null))
                 return false;
 
-            //Check if the GUIDs are the same
-            if (node1.BHoM_Guid == node2.BHoM_Guid)
-                return true;
+            //Check whether any of the compared objects nodes are null.
+            if (Object.ReferenceEquals(node1.Position, null) || Object.ReferenceEquals(node2.Position, null))
+                return false;
 
             if ((int)Math.Round(node1.Position.X * m_multiplier) != (int)Math.Round(node2.Position.X * m_multiplier))
                 return false;
@@ -81,6 +81,9 @@ namespace BH.Engine.Structure
         {
             //Check whether the object is null
             if (Object.ReferenceEquals(node, null)) return 0;
+
+            //Check whether the position is null
+            if (Object.ReferenceEquals(node.Position, null)) return 0;
 
             int x = ((int)Math.Round(node.Position.X * m_multiplier)).GetHashCode();
             int y = ((int)Math.Round(node.Position.Y * m_multiplier)).GetHashCode();
