8af8a4c122d5e1061d356d2ee648ab886413162c
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2020-10-20 15:28:14 +0100

    New save point with migration to Geometry Engine and tidy up of the code with comments

---
+++ Geometry_Engine/Objects/LineTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Geometry;
+
+namespace BH.Engine.Geometry
+{
+    public class LineTree
+    {
+        public Line ThisLine { get; set; } = null;
+        public Line Parent { get; set; } = null;
+        public Point UnconnectedPoint { get; set; } = null;
+    }
+}
