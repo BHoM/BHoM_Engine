51726ffe0dd5717c975fb0e4cce20a94736f08de
Isak Larborn <isak.larborn@burohappold.com>
2020-11-13 12:42:30 +0000

    Further improved FEMesh NullCheck and implemented in Normal

--- Structure_Engine/Query/Normal.cs
+++ Structure_Engine/Query/Normal.cs
@@ -55,7 +55,7 @@ namespace BH.Engine.Structure
         [Output("normal", "List of vectors representing the local z-axes of mesh faces. List order corresponds to the order of the faces.")]
         public static List<Vector> Normals(this FEMesh mesh)
         {
-            return mesh.Faces.Select(x => x.Normal(mesh)).ToList();
+            return mesh.NullCheck("Normals", false, false) ? mesh.Faces.Select(x => x.Normal(mesh)).ToList() : null;
         }
 
         /***************************************************/
@@ -66,6 +66,8 @@ namespace BH.Engine.Structure
         [Output("normal", "Vector representing the local z-axis of a mesh face.")]
         public static Vector Normal(this FEMeshFace face, FEMesh mesh)
         {
+            if (!face.NullCheck(mesh, "Normal"))
+                return null;
 
             if (face.NodeListIndices.Count < 3)
             {
