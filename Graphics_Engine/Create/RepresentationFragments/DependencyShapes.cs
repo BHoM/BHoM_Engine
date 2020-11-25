ec9eda79d66cdb2ff7b67b90eafbb0a9664e5aa8
rolyh <roly.hudson@gmail.com>
2020-11-25 18:45:47 +0000

    Graph projection

---
+++ Graphics_Engine/Create/RepresentationFragments/DependencyShapes.cs
@@ -0,0 +1,21 @@
+﻿using BH.Engine.Reflection;
+using BH.oM.Base;
+using BH.oM.Graphics.Components;
+using BH.oM.Graphics.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Graphics
+{
+    public static partial class Create
+    {
+        private static void RepresentationFragment(this DependencyShapes component, object data, ViewConfig viewConfig)
+        {
+            object x = data.PropertyValue(component.X);
+        }
+       
+    }
+}
