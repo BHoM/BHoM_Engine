d66730de2ed9d93a9eabefc435fa4c3413b6c8db
Erik Narhi <Erik.Narhi@burohappold.com>
2020-12-01 19:25:31 -0800

    Adjacency methods added

--- Facade_Engine/Compute/FrameEdgeAdjacencies.cs
+++ Facade_Engine/Compute/FrameEdgeAdjacencies.cs
@@ -27,8 +27,10 @@ using System.Collections.Generic;
 using System.Linq;
 using BH.oM.Analytical.Elements;
 using BH.oM.Facade.Elements;
+using BH.oM.Facade;
 using BH.oM.Facade.SectionProperties;
 using BH.Engine.Geometry;
+using BH.Engine.Spatial;
 using BH.oM.Reflection;
 using BH.oM.Reflection.Attributes;
 using System.ComponentModel;
@@ -43,59 +45,57 @@ namespace BH.Engine.Facade
 
         [Description("Returns adjacent edges and elements at a provided frame edge for a collection of panels")]
         [Input("edge", "Frame edge to find adjacencies at")]
-        [Input("elems", "Facade elements to use to find edge adjacencies (These should be panels and/or openings)")]
-        [MultiOutput(0, "adjFrameEdges", "Adjacent FrameEdges")]
-        [MultiOutput(1, "adjElems", "Adjacents Elements per adjacent FrameEdge")]
-        public static Output<List<FrameEdge>, List<IElement2D>> FrameEdgeAdjacencies(this IEdge edge, List<IElement2D> elems)
+        [Input("elems", "2D elements to use to find edge adjacencies (These should be panels and/or openings)")]
+        [MultiOutput(0, "adjEdges", "Adjacent edges")]
+        [MultiOutput(1, "adjElems", "Adjacent Elements per adjacent edge")]
+        public static Output<List<IElement1D>, List<IElement2D>> FrameEdgeAdjacencies(this IEdge edge, List<IElement2D> elems)
         {
             Point point = edge.Curve.IPointAtParameter(0.5);
-            return FrameEdgeAdjacencies(point, elems);
+            Vector dir = edge.IGeometry().IEndDir();
+            return FrameEdgeAdjacencies(point, elems, dir)
         }
 
         /***************************************************/
 
         [Description("Returns adjacent edges and elements at a provided point for a collection of panels")]
-        [Input("point", "Point to find adjacencies at")]
-        [Input("elems", "Facade elements to use to find edge adjacencies (These should be panels and/or openings)")]
-        [MultiOutput(0, "adjFrameEdges", "Adjacent FrameEdges")]
-        [MultiOutput(1, "adjElems", "Adjacents Elements per adjacent FrameEdge")]
-        public static Output<List<FrameEdge>, List<IElement2D>> FrameEdgeAdjacencies(this Point point, List<IElement2D> elems)
+        [Input("point", "Frame edge to find adjacencies at")]
+        [Input("elems", "2D elements to use to find edge adjacencies (These should be panels and/or openings)")]
+        [Input("refDir", "Reference vector direction to check adjacencies. Edges not parallel to this direction will not be included.")]
+        [MultiOutput(0, "adjEdges", "Adjacent edges")]
+        [MultiOutput(1, "adjElems", "Adjacent Elements per adjacent edge")]
+        public static Output<List<IElement1D>, List<IElement2D>> FrameEdgeAdjacencies(this Point point, List<IElement2D> elems, Vector refDir)
         {
-            List<FrameEdge> adjEdges = new List<FrameEdge>();
+            List<IElement1D> adjEdges = new List<IElement1D>();
             List<IElement2D> adjElems = new List<IElement2D>();
 
             foreach (IElement2D elem in elems)
             {
-                List<FrameEdge> frameEdges = new List<FrameEdge>();
-                if (elem is Panel)
-                {
-                    Panel panel = elem as Panel;
-                    frameEdges = panel.ExternalEdges;
-                }
-                else if (elem is Opening)
-                {
-                    Opening opening = elem as Opening;
-                    frameEdges = opening.Edges;
-                }
+                List<IElement1D> edges = elem.IOutlineElements1D();
 
-                foreach (FrameEdge frameEdge in frameEdges)
+                foreach (IElement1D refEdge in edges)
                 {
-                    double distance = frameEdge.Curve.IClosestPoint(point).Distance(point);
-                    if (distance<Tolerance.Distance)
+                    double distance = point.IDistance(refEdge.IGeometry());
+                    Vector edgeDir = refEdge.IGeometry().IEndDir();
+                    double angleBetween1 = edgeDir.Angle(refDir);
+                    double angleBetween2 = edgeDir.Angle(refDir.Reverse());
+                    double angleBetween = Math.Min(angleBetween1, angleBetween2);
+
+                    if (distance < Tolerance.Distance && angleBetween < Tolerance.Angle)
                     {
-                        adjEdges.Add(frameEdge);
+                        adjEdges.Add(refEdge);
                         adjElems.Add(elem);
                     }
                 }
             }
 
             // Return the adjacencies
-            return new Output<List<FrameEdge>, List<IElement2D>>
+            return new Output<List<IElement1D>, List<IElement2D>>
             {
                 Item1 = adjEdges,
                 Item2 = adjElems,
             };
         }
+
     }
 }
 
---
+++ Facade_Engine/Compute/UniqueAdjacencies.cs
@@ -0,0 +1,123 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Geometry;
+using BH.oM.Dimensional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Analytical.Elements;
+using BH.oM.Facade.Elements;
+using BH.oM.Facade.SectionProperties;
+using BH.Engine.Geometry;
+using BH.Engine.Spatial;
+using BH.oM.Reflection;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Facade
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /****          Public Methods                   ****/
+        /***************************************************/
+
+        [Description("Returns unique adjacent edge and element conditions for a collection of elements, based on their property names")]
+        [Input("elems", "Facade elements to use to find unique edge adjacencies")]
+        [MultiOutput(0, "uniqueAdjacencyNames", "A list of the unique adjacencies existent within the collection of elements")]
+        [MultiOutput(1, "uniqueAdjacencyEdges", "The collection of edges that represents the first found case of the unique adjacency")]
+        [MultiOutput(2, "uniqueAdjacencyElements", "The collection of elements that represents the first found case of the unique adjacency")]
+        public static Output<List<string>, List<List<IElement1D>>, List<List<IElement2D>>> UniqueAdjacencies(List<IElement2D> elems)
+        {
+            List<string> adjacencyIDs = new List<string>();
+            List<List<IElement1D>> adjEdges = new List<List<IElement1D>>();
+            List<List<IElement2D>> adjElems = new List<List<IElement2D>>();
+
+            foreach (IElement2D elem in elems)
+            {
+                List<IElement2D> tempElems = elems;
+                tempElems.Remove(elem);
+                foreach (IEdge edge in elem.IOutlineElements1D())
+                {                  
+                    BH.oM.Reflection.Output<List<IElement1D>, List<IElement2D>> result = edge.FrameEdgeAdjacencies(tempElems);
+                    for (int i = 0; i < result.Item1.Count; i++)
+                    {
+                        List<IElement1D> edgeAdjPair = new List<IElement1D> { edge, result.Item1[i] };
+                        List<IElement2D> elemAdjPair = new List<IElement2D> { elem, result.Item2[i] };
+                        string adjacencyID = AdjacencyID(edgeAdjPair, elemAdjPair);
+                        if (!adjacencyIDs.Contains(adjacencyID))
+                        {
+                            adjacencyIDs.Add(adjacencyID);
+                            adjEdges.Add(edgeAdjPair);
+                            adjElems.Add(result.Item2);
+                        }
+                    }
+                }
+            }
+            // Return the adjacency ids and elemens as multi output
+            return new Output<List<string>, List<List<IElement1D>>, List<List<IElement2D>>>
+            {
+                Item1 = adjacencyIDs,
+                Item2 = adjEdges,
+                Item3 = adjElems,
+            };
+
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        [Description("Creates adjacency ID from adjacency elements.")]
+        [Input("adjacencyEdges", "Adjacency edges.")]
+        [Input("adjacencyElems", "Adjacency elements.")]
+        [Output("adjacencyID", "The generated name of the adjacency.")]
+        public static string AdjacencyID(List<IElement1D> edges, List<IElement2D> elems)
+        {
+            string adjIDFull = "";
+            string separator = "_";
+            List<string> adjIDs = new List<string>();
+            if (edges.Count != elems.Count)
+            {
+                Reflection.Compute.RecordWarning("edge and element list lengths do not match. Each edge should have a corresponding element, please check your inputs.");
+                return null;
+            }
+            else
+            {
+                for (int i = 0; i < edges.Count; i++)
+                {
+                    IElement1D edge = edges[i];
+                    IElement2D elem = elems[i];
+                    string adjID = "Elem:" + elem.IPrimaryPropertyName() + "-" + "Edge:" + edge.IPrimaryPropertyName();
+                    adjIDs.Add(adjID);
+                }
+            }
+            adjIDs.Sort();
+            string.Join(separator, adjIDs);    
+            return adjIDFull;
+        }
+
+
+    }
+}
+
