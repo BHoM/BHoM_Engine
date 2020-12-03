18c9a7b77bad5f9ffab97f6289eda1b522400c76
Erik Narhi <Erik.Narhi@burohappold.com>
2020-12-02 17:44:36 -0800

    Initial width and height methods for area solving

---
+++ Facade_Engine/Query/Height.cs
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using BH.oM.Geometry;
+using BH.oM.Dimensional;
+using BH.oM.Facade.Elements;
+using BH.oM.Facade.SectionProperties;
+using BH.oM.Spatial.ShapeProfiles;
+using BH.oM.Analytical.Elements;
+using BH.oM.Physical.FramingProperties;
+using BH.Engine.Geometry;
+using BH.oM.Reflection;
+using System.Collections.Generic;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Facade
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Returns total height of a frame edge property")]
+        [Input("frameEdgeProp", "FrameEdgeProperty to get total profile width of")]
+        [Output("height", "Height of FrameEdgeProperty")]
+
+        public static double Height(this FrameEdgeProperty frameEdgeProp)
+        {
+            Polyline rectGeo = frameEdgeProp.SimpleGeometry();
+            BoundingBox bounds = rectGeo.Bounds();
+            return bounds.Extents().Y;
+        }
+
+    }
+}
