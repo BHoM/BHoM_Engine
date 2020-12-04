3af5a69112c92107ac35342d82b43554ea623c40
Erik Narhi <Erik.Narhi@burohappold.com>
2020-12-04 15:06:18 -0800

    Adjacency and area additions

--- Facade_Engine/Compute/FacadeAreasByConstruction.cs
+++ Facade_Engine/Compute/FacadeAreasByConstruction.cs
@@ -44,19 +44,19 @@ namespace BH.Engine.Facade
         /****          Public Methods                   ****/
         /***************************************************/
 
-        [Description("Returns total areas of each unique Construction contained in the provided elements, as well as the toal Frame area (Used for a panel containing openings with FameEdgeProperties).")]
+        [Description("Returns total areas of each unique Construction contained in the provided elements (as per Construction name), as well as the total Frame area (Used for a panel containing openings with FameEdgeProperties).")]
         [Input("elems", "elements to find total areas for.")]
         [MultiOutput(0, "areas", "Total area per each unique Construction. These areas account for decreased areas for openings where frame edges occur.")]
         [MultiOutput(1, "constructions", "Construction corresponding to each area value in areas.")]
         [MultiOutput(2, "frameArea", "Total frame area.")]
-        public static Output<List<double>, List <IConstruction>, double> FacadeAreasByConstruction(this List<IFacadeObject> elems)
+        public static Output<List<double>, List <string>, double> FacadeAreasByConstruction(this List<IFacadeObject> elems)
         {
             if (elems.Any(x => x is Panel == false & x is Opening == false))
             {
                 Reflection.Compute.RecordWarning("Some of the provided elements are not Openings or Panels. These elements have been ignored.");
             }
 
-            Dictionary<IConstruction, double> areasDict = new Dictionary<IConstruction, double>();
+            Dictionary<string, double> areasDict = new Dictionary<string, double>();
             double frameArea = 0;
 
             IEnumerable<Panel> panels = elems.OfType<Panel>();
@@ -75,14 +75,14 @@ namespace BH.Engine.Facade
                 {
                     Panel panel = elem as Panel;
                     double area = panel.Area();
-                    if (areasDict.Keys.Contains(panel.Construction))
+                    if (areasDict.Keys.Contains(panel.IPrimaryPropertyName()))
                     {
-                        double prevArea = areasDict[panel.Construction];
-                        areasDict[panel.Construction] = prevArea + area;
+                        double prevArea = areasDict[panel.IPrimaryPropertyName()];
+                        areasDict[panel.IPrimaryPropertyName()] = prevArea + area;
                     }
                     else
                     {
-                        areasDict.Add(panel.Construction, area);
+                        areasDict.Add(panel.IPrimaryPropertyName(), area);
                     }
                 }
                 else if (elem is Opening)
@@ -90,22 +90,22 @@ namespace BH.Engine.Facade
                     Opening opening = elem as Opening;
                     frameArea += opening.ComponentAreas().Item2;
                     double openArea = opening.ComponentAreas().Item1;
-                    if (areasDict.Keys.Contains(opening.OpeningConstruction))
+                    if (areasDict.Keys.Contains(opening.IPrimaryPropertyName()))
                     {
-                        double prevArea = areasDict[opening.OpeningConstruction];
-                        areasDict[opening.OpeningConstruction] = prevArea + openArea;
+                        double prevArea = areasDict[opening.IPrimaryPropertyName()];
+                        areasDict[opening.IPrimaryPropertyName()] = prevArea + openArea;
                     }
                     else
                     {
-                        areasDict.Add(opening.OpeningConstruction, openArea);
+                        areasDict.Add(opening.IPrimaryPropertyName(), openArea);
                     }
                 }
             }
             
-            List<IConstruction> constructions = areasDict.Keys.ToList();
+            List<string> constructions = areasDict.Keys.ToList();
             List<double> areas = areasDict.Values.ToList();
 
-            return new Output<List<double>, List<IConstruction>, double>
+            return new Output<List<double>, List<string>, double>
             {
                 Item1 = areas,
                 Item2 = constructions,
