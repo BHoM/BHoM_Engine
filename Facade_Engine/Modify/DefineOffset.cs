cb25f04a0d252969fb75978f687562c8ed966702
Valerie Green <valerie.green@burohappold.com>
2021-01-25 10:55:52 -0800

    Add DefineOffset method
    Created a new DefineOffset method

---
+++ Facade_Engine/Modify/DefineOffset.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Base;
+using BH.oM.Geometry;
+using BH.Engine.Geometry;
+using BH.Engine.Reflection;
+using BH.oM.Reflection.Attributes;
+using BH.oM.Reflection.Debugging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using BH.oM.Facade.Elements;
+using BH.oM.Facade.Fragments;
+
+namespace BH.Engine.Facade
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Copies a Panel into a new object")]
+        [Input("panel", "A panel to calculate offset for")]
+        [Input("opening", "An opening to calculate offset for")]
+        [Input("useroffset", "A user defined offset (face of panel to face of glazing)")]
+        [Output("offset", "The copied Environment Panel")]
+        public static ConstructionOffset DefineOffset(this Panel panel, Opening opening, double useroffset)
+        {
+            //TODO:
+            //Check for null case
+            if (panel == null || opening == null)
+                return null;
+
+            //Retrieve panel width and opening glazing width
+            double panelwidth = panel.Construction;
+            double glzwidth = opening.OpeningConstruction.PropertyValue("Layers");
+
+            //Calculate and return offset defined panel centerline to glazing (opening) centerline
+            double offset = 0.5 * panelwidth - 0.5 * glzwidth - useroffset;
+            return offset;
+        }
+    }
+}
