85b1a0d3aa5dfbf331aed6795e2204ec6aa543ea
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-02-04 10:31:15 +0000

    Reviewed mechanism

--- Geometry_Engine/Query/IGeometricalRepresentation.cs
+++ Geometry_Engine/Query/IGeometricalRepresentation.cs
@@ -35,34 +35,59 @@ namespace BH.Engine.Geometry
         /***************************************************/
 
         [Description("Returns the geometrical representation of the point, which is a Sphere.")] // in the future, we might want an option to choose between sphere / box.
-        public static IGeometry GeometricalRepresentation(this Point point, object colour = null, GeometricalRepresentationOptions options = null)
+        public static IGeometry GeometricalRepresentation(this Point point, ElementRepresentationOptions options = null)
         {
-            options = options ?? new GeometricalRepresentationOptions();
+            options = options ?? new ElementRepresentationOptions();
 
             double radius = 0.15 * options.Scale;
             Sphere sphere = BH.Engine.Geometry.Create.Sphere(point, radius);
 
-            return new GeometricalRepresentation() { Geometry = sphere, ColourInfo = colour };
+            return new GeometricalRepresentation() { Geometry = sphere, Colour = options.Colour };
         }
 
         /***************************************************/
 
         [Description("Returns the geometrical representation of the curve, which is a Pipe.")] // the pipe radius corresponds to how big the Curve is when represented.
-        public static IGeometry GeometricalRepresentation(this ICurve curve, object colour = null, GeometricalRepresentationOptions options = null)
+        public static IGeometry GeometricalRepresentation(this ICurve curve, object colour = null, ElementRepresentationOptions options = null)
         {
-            options = options ?? new GeometricalRepresentationOptions();
+            options = options ?? new ElementRepresentationOptions();
 
             double radius = 0.01 * options.Scale;
             bool capped = options.Cap1DElements;
 
-            return BH.Engine.Geometry.Create.Pipe(curve, radius, capped);
+            Pipe pipe = BH.Engine.Geometry.Create.Pipe(curve, radius, capped);
+
+            return new GeometricalRepresentation() { Geometry = pipe, Colour = options.Colour };
+        }
+
+        /***************************************************/
+
+        private static IGeometry GeometricalRepresentation(IObject iObj, IGeometricalRepresentationOptions options)
+        {
+            GeometricalRepresentation geomRepr = Reflection.Compute.RunExtensionMethod(iObj, "GeometricalRepresentation", new object[] { options }) as GeometricalRepresentation;
+
+            if (geomRepr != null) // object has a GeometricalRepresentation method.
+                return geomRepr;
+
+            geomRepr = new GeometricalRepresentation();
+
+            IGeometry geom = iObj as IGeometry;
+            if (geom != null) // it's a geometrical object that does not have a GeometricalRepresentation method.
+                geomRepr.Geometry = geom;
+            else // it's a generic object that does not have a GeometricalRepresentation method.
+                geom = BH.Engine.Base.Query.IGeometry(iObj as BHoMObject);
+
+            if (options != null)
+                geomRepr.Colour = options.Colour;
+
+            return geomRepr;
         }
 
         /***************************************************/
         /****              Interface Methods            ****/
         /***************************************************/
 
-        public static IGeometry IGeometricalRepresentation(this IObject iObj, IRepresentationOptions options = null)
+        public static IGeometry IGeometricalRepresentation(this IObject iObj, IGeometricalRepresentationOptions options = null)
         {
             return GeometricalRepresentation(iObj as dynamic, options as dynamic);
         }
