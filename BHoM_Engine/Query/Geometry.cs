c11720d1b3399651fa031a6b7b342d7340e456d2
Alessio Lombardi <alessio.lombardi@burohappold.com>
2021-02-18 10:21:02 +0000

    Removed #2316

--- BHoM_Engine/Query/Geometry.cs
+++ BHoM_Engine/Query/Geometry.cs
@@ -34,7 +34,7 @@ namespace BH.Engine.Base
         /**** Public Methods                            ****/
         /***************************************************/
 
-        public static IGeometry IGeometry(this IObject obj)
+        public static IGeometry IGeometry(this IBHoMObject obj)
         {
             if (obj == null)
                 return null;
