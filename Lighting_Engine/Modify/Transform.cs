2ba138312338ab54a68b520828512804534879dc
Pawel Baran <pawel.baran@burohappold.com>
2021-02-24 22:16:29 +0100

    transform of Luminaire added

---
+++ Lighting_Engine/Modify/Transform.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.Engine.Geometry;
+using BH.oM.Geometry;
+using BH.oM.Lighting.Elements;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Lighting
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Interface Methods - IElements             ****/
+        /***************************************************/
+
+        [Description("Transforms the Luminaire's position and direction by the transform matrix. Only rigid body transformations are supported.")]
+        [Input("luminaire", "Luminaire to transform.")]
+        [Input("transform", "Transform matrix.")]
+        [Output("transformed", "Modified Luminaire with unchanged properties, but transformed position and direction.")]
+        public static Luminaire Transform(this Luminaire luminaire, TransformMatrix transform)
+        {
+            if (!transform.IsRigidTransformation())
+            {
+                BH.Engine.Reflection.Compute.RecordError("Transformation failed: only rigid body transformations are currently supported.");
+                return null;
+            }
+
+            Luminaire result = luminaire.GetShallowClone() as Luminaire;
+            result.Position = result.Position.Transform(transform);
+            result.Direction = result.Direction.Transform(transform);
+            return result;
+        }
+
+        /***************************************************/
+    }
+}
