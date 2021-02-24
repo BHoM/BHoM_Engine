2fed9c4d1fb87360fcf1842357c67fe5b09d463f
Pawel Baran <pawel.baran@burohappold.com>
2021-02-24 22:01:13 +0100

    Rigid body transform added

---
+++ Security_Engine/Modify/Transform.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using BH.oM.Dimensional;
+using BH.oM.Geometry;
+using System.ComponentModel;
+using BH.oM.Reflection.Attributes;
+using BH.Engine.Geometry;
+using BH.oM.Security.Elements;
+using System.Linq;
+
+namespace BH.Engine.Security
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Interface Methods - IElements             ****/
+        /***************************************************/
+
+        [Description("Transforms the CameraDevice's eye position and target position by the transform matrix. Only rigid body transformations are supported.")]
+        [Input("camera", "CameraDevice to transform.")]
+        [Input("transform", "Transform matrix.")]
+        [Output("transformed", "Modified CameraDevice with unchanged properties, but transformed eye position and target position.")]
+        public static CameraDevice Transform(this CameraDevice camera, TransformMatrix transform)
+        {
+            if (!transform.IsRigidTransformation())
+            {
+                BH.Engine.Reflection.Compute.RecordError("Transformation failed: only rigid body transformations are currently supported.");
+                return null;
+            }
+
+            CameraDevice result = camera.GetShallowClone() as CameraDevice;
+            result.EyePosition = result.EyePosition.Transform(transform);
+            result.TargetPosition = result.TargetPosition.Transform(transform);
+            return result;
+        }
+
+        /***************************************************/
+    }
+}
+
+
