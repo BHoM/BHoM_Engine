64daba47b7dceb80d86a63ba63d82f382a691d72
Łukasz Markowski <lukasz.markowski@burohappold.com>
2021-03-26 16:23:33 +0100

    Fixing bug in the code and updating the UT

--- Spatial_Engine/Modify/RoundCoordinates.cs
+++ Spatial_Engine/Modify/RoundCoordinates.cs
@@ -26,6 +26,7 @@ using BH.oM.Dimensional;
 using BH.oM.Geometry;
 using BH.oM.Reflection.Attributes;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 
@@ -80,8 +81,8 @@ namespace BH.Engine.Spatial
             else
             {
                 IElement2D newElement2d = element2d.ISetOutlineElements1D(element2d.IOutlineElements1D().Select(x => x.ISetGeometry(Geometry.Modify.IRoundCoordinates(x.IGeometry(), decimalPlaces))).ToList());
-                
-                newElement2d.ISetInternalElements2D(newElement2d.IInternalElements2D().Select(y => y.ISetOutlineElements1D(y.IOutlineElements1D().Select(x => x.ISetGeometry(Geometry.Modify.IRoundCoordinates(x.IGeometry(), decimalPlaces))).ToList())).ToList());
+
+                newElement2d = newElement2d.ISetInternalElements2D(element2d.IInternalElements2D().Select(y => y.ISetOutlineElements1D(y.IOutlineElements1D().Select(x => x.ISetGeometry(Geometry.Modify.IRoundCoordinates(x.IGeometry(), decimalPlaces))).ToList())).ToList());
 
                 if (newElement2d.IsPlanar())
                     return newElement2d;
