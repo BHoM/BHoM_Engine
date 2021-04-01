1b9dd8a4e92346d933de8236a0416a2c7f6f5bd0
Arnaud Declercq <arnaud.declercq@burohappold.com>
2021-04-01 16:57:14 +0800

    Update Analytical_Engine/Query/Geometry.cs
    Co-authored-by: Pawel Baran <pawel.baran@burohappold.com>

--- Analytical_Engine/Query/Geometry.cs
+++ Analytical_Engine/Query/Geometry.cs
@@ -155,7 +155,7 @@ namespace BH.Engine.Analytical
         [Output("face", "The geometry of the IFace as geometrical Mesh Face.")]
         public static Face Geometry(this IFace face)
         {
-            if (face == null || face.NodeListIndices == null)
+            if (face?.NodeListIndices == null)
                 return null;
 
             if (face.NodeListIndices.Count < 3)
@@ -232,4 +232,3 @@ namespace BH.Engine.Analytical
         
     }
 }
-
