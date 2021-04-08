cde689ecd0f1be56bcfb721d4d868d7ff5a6cdbd
Maryanne Wachter <maryanne.wachter@burohappold.com>
2021-04-08 14:58:16 -0700

    generalized description parsing for both input and output

--- Serialiser_Engine/Convert/Markdown.cs
+++ Serialiser_Engine/Convert/Markdown.cs
@@ -72,140 +72,145 @@ namespace BH.Engine.Serialiser
         [Description("Convert a BHoM method to Markdown for documentation.")]
         public static string MethodToMarkdown(this MethodBase method)
         {
-            var sw = new StringWriter();
+            var methodString = new StringWriter();
 
             string methodName = method.Name;
             string methodDescription = method.Description();
 
-            sw.WriteLine("### {0}", methodName);
-            sw.WriteLine("<details>");
-            sw.WriteLine("<summary>");
-            sw.WriteLine("{0}", methodDescription);
-            sw.WriteLine("</summary>");
-            sw.WriteLine();
+            methodString.WriteLine("### {0}", methodName);
+            methodString.WriteLine("<details>");
+            methodString.WriteLine("<summary>");
+            methodString.WriteLine("{0}", methodDescription);
+            methodString.WriteLine("</summary>");
+            methodString.WriteLine();
 
             Dictionary<string, string> inputParameters = method.InputDescriptions();
-            sw.WriteLine("#### Inputs");
-            sw.WriteLine("___");
+            methodString.WriteLine("#### Inputs");
+            methodString.WriteLine("___");
             foreach (var input in inputParameters)
             {
                 string inputName = input.Key;
                 string inputDescription = input.Value;
-                sw.WriteLine("- **{0}**", inputName);
-                string enumString = null;
-                List<string> enumValues = new List<string>();
-
+                
+                
+                methodString.WriteLine("- **{0}**", inputName);
                 string parsedInputDescription = parseInputs(inputDescription);
-                // Find enums if they exist
-                if (inputDescription.Contains("Enum values:") == true)
-                {
-                    List<string> splitAtEnums = Regex.Split(inputDescription, "Enum values:").ToList();
-                    // some kind of check to make sure there are only 2 elements in the split list...
-                    enumString = splitAtEnums.Last();
-                    enumValues = Regex.Split(enumString, "\r\n-").Where(s => s != string.Empty).ToList();
-                    inputDescription = splitAtEnums.First();
-                }
-                // Find line breaks that are not due to term/definition separation
-                string[] subDescriptions = Regex.Split(inputDescription, @"(?<!:)\r\n");
-                int i = 1; // set initial indentation level for sublist
-                string prependIndent = new string((char)32, 3 * i); // white space for bullet points
-                foreach (var subDescription in subDescriptions)
-                {
-                    // find term/definitions
-                    if (subDescription.Contains(":\r\n") == true)
-                    {
-                        List<string> termDef = Regex.Split(subDescription, @":\r\n").ToList();
-                        string term = termDef[0];
-                        termDef.RemoveAt(0); // remove term from term/definition pair
-                        if (term.StartsWith("This is a BH.") == true)
-                        {
-                            sw.WriteLine(prependIndent + "- _{0}_", term);
-                        }
-                        else
-                        {
-                            sw.WriteLine(prependIndent + "- {0}", term);
-                        }
-                        if (termDef.Count == 1) // there should only be one definition --- no nested defs supported
-                        {
-                            List<string> definitions = Regex.Split(termDef[0], "\n").Where(s => s != string.Empty).ToList();
-                            int j = i + 1; // set indentation for subsublist
-                            prependIndent = new string((char)32, 3 * j);
-                            foreach (var def in definitions) 
-                            {
-                                if (def.StartsWith("This is a BH.") == true)
-                                {
-                                    sw.WriteLine(prependIndent + "- _{0}_", def);
-                                }
-                                else
-                                {
-                                    sw.WriteLine(prependIndent + "- {0}", def);
-                                }
-                                // Check if enum description reached
-                                if ((def.Contains("This enum") == true) && (enumString != null))
-                                {
-                                    sw.WriteLine(prependIndent + "- Enum values:");
-                                    int k = j + 1; // set indentation for subsubsublist
-                                    prependIndent = new string((char)32, 3 * k);
-                                    foreach (var enumValue in enumValues)
-                                    {
-                                        sw.WriteLine(prependIndent + "- {0}", enumValue);
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            BH.Engine.Reflection.Compute.RecordWarning("Method description contains nested definitions, which are currently unhandled.");
-                        }
-                    }
-                    else
-                    {
-                        // find newlines and split by that delimiter into bullet points
-                        List<string> descriptionAttributes = Regex.Split(subDescription, "\n").Where(s => s != string.Empty).ToList();
-                        foreach (var descriptionAttribute in descriptionAttributes)
-                        {
-                            if (descriptionAttribute.StartsWith("This is a BH.") == true)
-                            {
-                                sw.WriteLine(prependIndent + "- _{0}_", descriptionAttribute);
-                            }
-                            else
-                            {
-                                sw.WriteLine(prependIndent + "- {0}", descriptionAttribute);
-                            }
-                        }
-                    }
-                }
+                methodString.Write(parsedInputDescription);
+
+
             }
 
-            sw.WriteLine();
-            sw.WriteLine("#### Outputs");
-            sw.WriteLine("___");
+            methodString.WriteLine();
+            methodString.WriteLine("#### Outputs");
+            methodString.WriteLine("___");
             string outputName = method.OutputName();
             string outputDescription = method.OutputDescription();
-            sw.WriteLine("- **{0}**", outputName);
+            
+            methodString.WriteLine("- **{0}**", outputName);
             string[] subOutputDescriptions = Regex.Split(outputDescription, @"(?<!:)\r\n");
             foreach (var subOutputDescription in subOutputDescriptions)
             {
-                int i = 1; // set initial indentation level for sublist
-                string prependIndent = new string((char)32, 3 * i); // white space for bullet points
-                if (subOutputDescription.StartsWith("This is a BH.") == true)
-                {
-                    sw.WriteLine(prependIndent + "- _{0}_", subOutputDescription);
-                }
-                else
-                {
-                    sw.WriteLine(prependIndent + "- {0}", subOutputDescription);
-                }
+                string parsedOutputDescription = parseInputs(subOutputDescription);
+                methodString.Write(parsedOutputDescription);
             }
-            sw.WriteLine("</details>");
-            return sw.ToString();
+            methodString.WriteLine("</details>");
+            return methodString.ToString();
         }
 
         private static string parseInputs (string inputToParse)
         {
-            return ;
-        }
+            StringWriter sw = new StringWriter();
+            string enumString = null;
+            List<string> enumValues = new List<string>();
 
+            // Find enums if they exist
+            if (inputToParse.Contains("Enum values:") == true)
+            {
+                List<string> splitAtEnums = Regex.Split(inputToParse, "Enum values:").ToList();
+                // some kind of check to make sure there are only 2 elements in the split list...
+                if (splitAtEnums.Count != 2)
+                {
+                    BH.Engine.Reflection.Compute.RecordWarning("Method description contains multiple enums, which are not currently supported.");
+                }
+                else
+                {
+                    enumString = splitAtEnums.Last();
+                    enumValues = Regex.Split(enumString, "\r\n-").Where(s => s != string.Empty).ToList();
+                    inputToParse = splitAtEnums.First();
+                }
+            }
+            // Find line breaks that are not due to term/definition separation
+            string[] subDescriptions = Regex.Split(inputToParse, @"(?<!:)\r\n");
+            int i = 1; // set initial indentation level for sublist
+            string prependIndent = new string((char)32, 3 * i); // white space for bullet points
+            foreach (var subDescription in subDescriptions)
+            {
+                // find term/definitions
+                if (subDescription.Contains(":\r\n") == true)
+                {
+                    List<string> termDef = Regex.Split(subDescription, @":\r\n").ToList();
+                    string term = termDef[0];
+                    termDef.RemoveAt(0); // remove term from term/definition pair
+                    if (term.StartsWith("This is a BH.") == true)
+                    {
+                        sw.WriteLine(prependIndent + "- _{0}_", term);
+                    }
+                    else
+                    {
+                        sw.WriteLine(prependIndent + "- {0}", term);
+                    }
+                    if (termDef.Count == 1) // there should only be one definition --- no nested defs supported
+                    {
+                        List<string> definitions = Regex.Split(termDef[0], "\n").Where(s => s != string.Empty).ToList();
+                        int j = i + 1; // set indentation for subsublist
+                        prependIndent = new string((char)32, 3 * j);
+                        foreach (var def in definitions)
+                        {
+                            if (def.StartsWith("This is a BH.") == true)
+                            {
+                                sw.WriteLine(prependIndent + "- _{0}_", def);
+                            }
+                            else
+                            {
+                                sw.WriteLine(prependIndent + "- {0}", def);
+                            }
+                            // Check if enum description reached
+                            if ((def.Contains("This enum") == true) && (enumString != null))
+                            {
+                                sw.WriteLine(prependIndent + "- Enum values:");
+                                int k = j + 1; // set indentation for subsubsublist
+                                prependIndent = new string((char)32, 3 * k);
+                                foreach (var enumValue in enumValues)
+                                {
+                                    sw.WriteLine(prependIndent + "- {0}", enumValue);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        BH.Engine.Reflection.Compute.RecordWarning("Method description contains nested definitions, which are currently unhandled.");
+                    }
+                }
+                else
+                {
+                    // find newlines and split by that delimiter into bullet points
+                    List<string> descriptionAttributes = Regex.Split(subDescription, "\n").Where(s => s != string.Empty).ToList();
+                    foreach (var descriptionAttribute in descriptionAttributes)
+                    {
+                        if (descriptionAttribute.StartsWith("This is a BH.") == true)
+                        {
+                            sw.WriteLine(prependIndent + "- _{0}_", descriptionAttribute);
+                        }
+                        else
+                        {
+                            sw.WriteLine(prependIndent + "- {0}", descriptionAttribute);
+                        }
+                    }
+                }
+            }
+            return sw.ToString();
+        }
         /*******************************************/
     }
 }
