1d15d46e24bc4308a8788660c0f30e769b97fc44
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2021-06-01 11:10:13 +0100

    Apply suggestions from code review

--- Environment_Engine/Modify/FlipPanels.cs
+++ Environment_Engine/Modify/FlipPanels.cs
@@ -41,7 +41,7 @@ namespace BH.Engine.Environment
         /***************************************************/
 
         [Description("Modifies a collection of panels to ensure that their normal is pointing away from the space that they enclose.\nAny openings on the panels will also be tested to ensure the normal is pointing away from the space. This is for a single space's worth of panels.\nIf wishing to use this on multiple spaces, use the ToSpaces component to split the panels per space, plug that into here and flatten the output.")]
-        [Input("panelsAsSpace", "A collection of Environment Panels to check normal direction for".)] 
+        [Input("panelsAsSpace", "A collection of Environment Panels to check normal direction for.")]
         [Output("panelsAsSpace", "A collection of modified Environment Panels with normal away from space.")]
         public static void FlipPanels(this List<Panel> panelsAsSpace)
         {
