530ea2dcc3e94f004d4618a8ec0a39e9c037019c
Johanna Olin <Johanna.Olin@BuroHappold.com>
2021-06-03 15:11:56 +0100

    Add RegionNameMapping component

--- Environment_Engine/Compute/RegionNameMapping.cs
+++ Environment_Engine/Compute/RegionNameMapping.cs
@@ -12,16 +12,16 @@ namespace BH.Engine.Environment
 {
     public static partial class Compute
     {
-        public static List<IRegion> RegionNameMapping(List<IRegion> regions, List<Point> points, List<string> names)
+        public static List<IRegion> RegionNameMapping(List<IRegion> regions, List<Point> points, List<string> names, double tolerance = Tolerance.Distance)
         {
             List<IRegion> sortedRegions = regions.OrderBy(x => x.Perimeter.IArea()).ToList();
             for (int x = 0; x < points.Count; x++)
             {
-                IRegion selRegion = sortedRegions.Where(y => y.Perimeter.IIsContaining(new List<Point> { points[x] })).FirstOrDefault();
+                IRegion selRegion = regions.Where(y => y.Perimeter.IIsContaining(new List<Point> { points[x] }, true, tolerance)).FirstOrDefault();
                 if (selRegion != null)
                     selRegion.Name = names[x];
             }
-            return sortedRegions;
+            return regions;
         }
     }
 }
