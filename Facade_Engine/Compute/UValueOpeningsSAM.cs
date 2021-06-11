e78ff03c33b49e94253a6e8ff7427f7e71fbeb2b
Erik Narhi <Erik.Narhi@burohappold.com>
2021-06-10 19:28:35 -0700

    Description cleanup and new methods for total U of many openings

---
+++ Facade_Engine/Compute/UValueOpeningsSAM.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Geometry;
+using BH.oM.Dimensional;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BH.oM.Facade.Elements;
+using BH.oM.Base;
+using BH.Engine.Geometry;
+using BH.Engine.Spatial;
+using BH.Engine.Base;
+using BH.oM.Facade.Fragments;
+using BH.oM.Reflection;
+using BH.oM.Reflection.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Facade
+{
+    public static partial class Compute
+    {
+        /***************************************************/
+        /****          Public Methods                   ****/
+        /***************************************************/
+
+        [Description("Returns effective U-Value of a collection of openings calculated using the Single Assessment Method (Using Psi-tj). Requires center of opening U-value as Opening fragment and frame Psi-tj value as list of Edge fragments.")]
+        [Input("openings", "Openings to find U-value for.")]
+        [Output("effectiveUValue", "Effective total U-value of opening caclulated using SAM.")]
+        public static double UValueOpeningsSAM(this List<Opening> openings)
+        {
+            double uValueProduct = 0;
+            double totalArea = 0;
+            foreach (Opening opening in openings)
+            {
+                double area = opening.Area();
+                uValueProduct += opening.UValueOpeningSAM() * area;
+                totalArea += area;
+            }
+            if (totalArea == 0)
+            {
+                BH.Engine.Reflection.Compute.RecordError("Openings have a total calculated area of 0. Ensure Openings are valid with associated edges defining their geometry and try again.");
+                return double.NaN;
+            }
+
+            return uValueProduct / totalArea; ;
+        }
+
+        /***************************************************/
+
+    }
+}
