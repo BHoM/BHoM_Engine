bbba7afe75a7f75080508408d88c526685f04753
Peter Nugent <Peter.Nugent@burohappold.com>
2021-09-27 16:45:28 +0100

    Add IsReinforcementValid method

--- Physical_Engine/Query/IsReinforcementValid.cs
+++ Physical_Engine/Query/IsReinforcementValid.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
  *
@@ -24,37 +24,43 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
-using BH.Engine.Geometry;
-using BH.oM.Geometry.CoordinateSystem;
-using BH.oM.Quantities.Attributes;
+using BH.oM.Geometry;
 using BH.oM.Reflection.Attributes;
 using BH.oM.Physical.Reinforcement;
-
-
-
+using BH.Engine.Geometry;
 
 namespace BH.Engine.Physical
 {
-    public static partial class Create
+    public static partial class Query
     {
         /***************************************************/
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Creates a Reinforcement object storing the ShapeCode, diameter, bend radius and coordinate system.")]
-        [Input("diameter", "The diameter of the reinforcement.")]
-        [Input("coordinateSystem", "The name of the beam, default empty string")]
-        [Input("shapeCode", "The name of the beam, default empty string")]
-        [Input("bendRadius", "The bend radius of the reinforcement. This will be calculated based on the diameter if the provided value " +
-            "is less than the the minimum scheduling radius defined in BS 8666:2020.")]
-        [Output("reinforcement", "The reinforcement object with a compliant shape code in accordance with BS 8666:2020.")]
-        public static Reinforcement Reinforcement(double diameter, Cartesian coordinateSystem, IShapeCode shapeCode, double bendRadius = 0)
+        [Description("Verifies whether the reinforcement is valid by performing null checks, checking the bend radius and shape code compliance.")]
+        [Input("reinforcement", "The reinforcement containing the ShapeCode, reinforcement and bending radius to be verified.")]
+        [Output("bool", "True if the shape code is valid.")]
+        public static bool IsReinforcementValid(this Reinforcement reinforcement)
         {
-            Reinforcement reinforcement = new Reinforcement(){Diameter = diameter, BendRadius = bendRadius, CoordinateSystem = coordinateSystem, ShapeCode = shapeCode};
+            if (reinforcement.IsNull())
+                return false;
+            else if (reinforcement.CoordinateSystem.IsNull())
+                return false;
+            else if (reinforcement.Diameter <= 0)
+            {
+                Reflection.Compute.RecordError("The diameter must be greater than zero.");
+                return false;
+            }
+            else if (reinforcement.BendRadius < reinforcement.Diameter.SchedulingRadius())
+            {
+                reinforcement.BendRadius = reinforcement.Diameter.SchedulingRadius();
+                Reflection.Compute.RecordWarning("The bend radius is less than the minimum scheduling radius and has been assigned the minimum value.");
+            }
+            else if (!reinforcement.ShapeCode.IIsShapeCodeCompliant(reinforcement.Diameter))
+                return false;
 
-            return reinforcement.IsReinforcementValid() ? reinforcement : null;
+            return true;
         }
 
-        /***************************************************/
     }
 }
