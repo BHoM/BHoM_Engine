b89c6b1ec6cf08284819ea6b1405faa88d29cbab
Isak Näslund <isak.naslund@burohappold.com>
2021-09-29 11:03:27 +0200

    Update Structure_Engine/Query/MaterialComposition.cs
    Co-authored-by: Peter Nugent <Peter.Nugent@burohappold.com>

--- Structure_Engine/Query/MaterialComposition.cs
+++ Structure_Engine/Query/MaterialComposition.cs
@@ -150,7 +150,7 @@ namespace BH.Engine.Structure
         /**** Public Methods - Interface                ****/
         /***************************************************/
 
-        [PreviousVersion("4.3", "BH.Engine.Structure.Query.MaterialComposition(BH.oM.Structure.SectionProperties.ISectionProperty)")]
+        [PreviousVersion("5.0", "BH.Engine.Structure.Query.MaterialComposition(BH.oM.Structure.SectionProperties.ISectionProperty)")]
         [Description("Returns a SectionProperty's MaterialComposition.")]
         public static MaterialComposition IMaterialComposition(this ISectionProperty sectionProperty)
         {
