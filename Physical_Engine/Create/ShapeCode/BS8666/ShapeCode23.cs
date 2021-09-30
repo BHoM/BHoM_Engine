48e3f5367fc42fde0f9558648aea1e50e9a48b21
peterjamesnugent <Peter.Nugent@burohappold.com>
2021-09-30 15:52:37 +0100

    Update error messages

--- Physical_Engine/Create/ShapeCode/BS8666/ShapeCode23.cs
+++ Physical_Engine/Create/ShapeCode/BS8666/ShapeCode23.cs
@@ -38,7 +38,7 @@ namespace BH.Engine.Physical
 
         [Description("Creates a ShapeCode object using the parameters provided. Refer to the object descriptions for alignment.")]
         [Output("shapeCode", "A ShapeCode to be used with Reinforcement objects.")]
-        public static ShapeCode23 ShapeCode23(double a, double b, double c, double diameter, bool zBar = false, double bendRadius)
+        public static ShapeCode23 ShapeCode23(double a, double b, double c, double diameter, double bendRadius, bool zBar = false)
         {
             if (a < Tolerance.Distance || b < Tolerance.Distance || c < Tolerance.Distance)
             {
