42bccaef63cfaa766a18e1344d29d7670a84cd60
Isak Naslund <Isak.Naslund@burohappold.com>
2021-10-08 11:34:29 +0200

    fixing query compliance

--- Library_Engine/Query/Confidence.cs
+++ Library_Engine/Query/Confidence.cs
@@ -39,7 +39,7 @@ namespace BH.Engine.Library
         [Description("Extract the level of confidence of the library. If the provided library name returns multiple levels of confidence, the lowest level is returned.")]
         [Input("libraryName", "Name of the library to extract confidence from.")]
         [Output("confidence", "The lowest level of confidence found in any opf the libraries matching the provided library name.")]
-        public static Confidence Confidence(string libraryName)
+        public static Confidence Confidence(this string libraryName)
         {
             if (string.IsNullOrWhiteSpace(libraryName))
                 return oM.Data.Library.Confidence.Undefined;
