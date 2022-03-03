919515b0134b0258ed951c58cd8e5966e7337cdc
Isak Naslund <Isak.Naslund@burohappold.com>
2022-03-03 16:17:14 +0100

    Description tweaks and alignments

--- Results_Engine/Query/DisplayObjectResult.cs
+++ Results_Engine/Query/DisplayObjectResult.cs
@@ -49,13 +49,13 @@ namespace BH.Engine.Results
         [Description("Generates a list of coloured geometry based on provided BHoMObejcts and ObjectResults..")]
         [Input("objects", "BHoMObjects to colour.")]
         [Input("results", "The IObjectResults to colour by.")]
-        [Input("identifier", "The type of IAdapterId fragment to be used to extract the object identification, i.e. which fragment type to look for to find the identifier of the object. If no identifier is provided, the object will be scanned for an IAdapterId to be used.")]
+        [Input("objectIdentifier", "Should either be a string specifying what property on the object that should be used to map the objects to the results, or a type of IAdapterId fragment to be used to extract the object identification, i.e. which fragment type to look for to find the identifier of the object. If no identifier is provided, the object will be scanned an IAdapterId to be used.")]
         [Input("caseFilter", "Which cases to colour by, default is all.")]
         [Input("displayProperty", "THe name of the property on the result to colour by.")]
         [Input("gradientOptions", "How to color the mesh, null defaults to `BlueToRed` with automatic range.")]
         [MultiOutput(0, "results", "A List of Lists of RenderGeometry, where the outer list corresponds to the object and the inner list correspond to the matchis results..")]
         [MultiOutput(1, "gradientOptions", "The gradientOptions that were used to colour the meshes.")]
-        public static Output<List<List<RenderGeometry>>, GradientOptions> DisplayObjectResult(this IEnumerable<IBHoMObject> objects, IEnumerable<IObjectResult> results, string displayProperty, Type identifier = null, List<string> caseFilter = null, GradientOptions gradientOptions = null)
+        public static Output<List<List<RenderGeometry>>, GradientOptions> DisplayObjectResult(this IEnumerable<IBHoMObject> objects, IEnumerable<IObjectResult> results, string displayProperty, object objectIdentifier = null, List<string> caseFilter = null, GradientOptions gradientOptions = null)
         {
             if (objects == null || objects.Count() < 1)
             {
@@ -94,7 +94,7 @@ namespace BH.Engine.Results
             List<IBHoMObject> objectList = objects.ToList();
 
             // Map the Results to Objects
-            List<List<IObjectResult>> mappedResults = objectList.MapResults(results, "ObjectId", identifier, caseFilter);
+            List<List<IObjectResult>> mappedResults = objectList.MapResults(results, "ObjectId", objectIdentifier, caseFilter);
 
             //Extract result value to display
             List<List<double>> resValues = mappedResults.Select(l => l.Select(x => resultPropertySelector(x)).ToList()).ToList();
