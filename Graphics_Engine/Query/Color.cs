230306e21a9da8b9a76d6d955dfe5d42b01385b1
Isak Naslund <Isak.Naslund@burohappold.com>
2022-03-03 15:47:36 +0100

    Adding method for getting colour from Stepped gradient and IGradient

--- Graphics_Engine/Query/Color.cs
+++ Graphics_Engine/Query/Color.cs
@@ -68,13 +68,11 @@ namespace BH.Engine.Graphics
 
         /***************************************************/
 
-        [Description("Gets a color from a gradient at the spacified position scaled between from and to")]
+        [Description("Gets a color from a gradient at the spacified position between 0 and 1")]
         [Input("gradient", "The gradient to Query a Color from")]
-        [Input("val", "The number between 'from' and 'to' to use for interpolation of the markers colors")]
-        [Input("from", "The lower bound of 'val's rescaling to 0 to 1")]
-        [Input("to", "The upper bound of 'val's rescaling to 0 to 1")]
+        [Input("val", "The number between 0 and 1 to use for interpolation of the markers colors")]
         [Output("color", "The Color at the specified position")]
-        public static Color Color(this Gradient gradient, double val, double from, double to)
+        public static Color Color(this SteppedGradient gradient, double val)
         {
             if (gradient == null)
             {
@@ -82,7 +80,49 @@ namespace BH.Engine.Graphics
                 return System.Drawing.Color.Transparent;
             }
 
-            return gradient.Color((val - from) / (to - from));
+            // keep val in the domain
+            val = val > 1 ? 1 : val < 0 ? 0 : val;
+
+            // Find first marker with value above the value
+            return gradient.Markers.LastOrDefault(x => x.Key <= (decimal)val).Value;
+        }
+
+        /***************************************************/
+
+        [Description("Gets a color from a gradient at the spacified position scaled between from and to")]
+        [Input("gradient", "The gradient to Query a Color from")]
+        [Input("val", "The number between 'from' and 'to' to use for interpolation of the markers colors")]
+        [Input("from", "The lower bound of 'val's rescaling to 0 to 1")]
+        [Input("to", "The upper bound of 'val's rescaling to 0 to 1")]
+        [Output("color", "The Color at the specified position")]
+        public static Color Color(this IGradient gradient, double val, double from, double to)
+        {
+            if (gradient == null)
+            {
+                BH.Engine.Base.Compute.RecordError("Cannot query the colour of a null gradient.");
+                return System.Drawing.Color.Transparent;
+            }
+
+            return gradient.IColor((val - from) / (to - from));
+        }
+
+        /***************************************************/
+        /**** Public Methods - Interface                ****/
+        /***************************************************/
+
+        [Description("Gets a color from a gradient at the spacified position between 0 and 1")]
+        [Input("gradient", "The gradient to Query a Color from")]
+        [Input("val", "The number between 0 and 1 to use for interpolation of the markers colors")]
+        [Output("color", "The Color at the specified position")]
+        public static Color IColor(this IGradient gradient, double val)
+        {
+            if (gradient == null)
+            {
+                BH.Engine.Base.Compute.RecordError("Cannot query the colour of a null gradient.");
+                return System.Drawing.Color.Transparent;
+            }
+
+            return Color(gradient as dynamic, val);
         }
 
         /***************************************************/
