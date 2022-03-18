bb0cd550e7caf273e70b78577ef2dc5c2abda5f4
Erik Narhi <Erik.Narhi@burohappold.com>
2022-03-18 13:53:03 -0700

    Mesh results updated per analytical changes and lux requirements

---
+++ Lighting_Engine/Create/MeshLux.cs
@@ -0,0 +1,57 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
+
+using BH.oM.Geometry;
+using BH.oM.Spatial;
+using BH.oM.Lighting.Elements;
+using BH.oM.Lighting.Results;
+
+using BH.Engine.Geometry;
+using BH.Engine.Spatial;
+
+namespace BH.Engine.Lighting
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        public static MeshElementResult MeshLux(double lux, IComparable objectId, IComparable nodeId, IComparable resultCase, double timeStep)
+        {
+            return new MeshLux(objectId, nodeId, resultCase, timeStep, lux);
+        }
+
+
+    }
+}
+
+
+
