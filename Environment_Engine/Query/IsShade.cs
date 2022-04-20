56de77e94de08ab1547cfb32e0dce8d3d3dfda81
Robin Flyman <robin.flyman@burohappold.com>
2022-04-20 12:22:24 +0100

    Created new methods for IsShade

---
+++ Environment_Engine/Query/IsShade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
+
+using BH.oM.Environment.Elements;
+
+namespace BH.Engine.Environment
+{
+    public static partial class Query
+    {
+        [Description("Something smart")]
+        [Input("panelType", "panel again")]
+        [Output("Same with panel", "when does this end??")]
+
+        public static bool IsShade(this PanelType panelType)
+        {
+            List<PanelType> shadeTypes = new List<PanelType>() {
+                PanelType.Shade,
+                PanelType.TranslucentShade,
+            };
+
+            return shadeTypes.Contains(panelType);
+        }
+
+        [Description("something")]
+        [Input("panel", "panelpanel")]
+        [Output("some", "results")]
+
+        public static bool IsShade(this Panel panel)
+        {
+            return panel.Type.IsShade();
+        }
+
+    }
+}
