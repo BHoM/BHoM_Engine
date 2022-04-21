e21a8027faac2b78dd6a479426ac99db1f9de045
Isak Naslund <Isak.Naslund@burohappold.com>
2022-04-21 16:51:59 +0200

    Adding methods to group by properties tagged with identifier attribute

--- Results_Engine/Query/Group.cs
+++ Results_Engine/Query/Group.cs
@@ -26,8 +26,11 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using BH.oM.Analytical.Results;
 using BH.oM.Base.Attributes;
+using BH.Engine.Base;
+using BH.Engine.Reflection;
 
 namespace BH.Engine.Results
 {
@@ -67,6 +70,108 @@ namespace BH.Engine.Results
         }
 
         /***************************************************/
+
+        [Description("Group results by all Scenario type properties of the object.")]
+        [Input("results", "Results to group.")]
+        [Output("groupedResults", "Results grouped as List of Lists.")]
+        public static List<List<T>> GroupByScenario<T>(this IEnumerable<T> results) where T : IResult
+        {
+            if (results == null || !results.Any())
+                return new List<List<T>>();
+
+            return GroupByMultipleSelectors(results, IdentifierFunctions(results.First().GetType(), typeof(ScenarioIdentifierAttribute)));
+        }
+
+        /***************************************************/
+
+        [Description("Group results by Object identifiers of the object.")]
+        [Input("results", "Results to group.")]
+        [Output("groupedResults", "Results grouped as List of Lists.")]
+        public static List<List<T>> GroupByObjectIdentifiers<T>(this IEnumerable<T> results) where T : IResult
+        {
+            if (results == null || !results.Any())
+                return new List<List<T>>();
+
+            return GroupByMultipleSelectors(results, IdentifierFunctions(results.First().GetType(), typeof(ObjectIdentifierAttribute)));
+        }
+
+        /***************************************************/
+
+        [Description("Group results by all identifiers of the object.")]
+        [Input("results", "Results to group.")]
+        [Output("groupedResults", "Results grouped as List of Lists.")]
+        public static List<List<T>> GroupByAllIdentifiers<T>(this IEnumerable<T> results) where T : IResult
+        {
+            if (results == null || !results.Any())
+                return new List<List<T>>();
+
+            return GroupByMultipleSelectors(results, IdentifierFunctions(results.First().GetType(), typeof(IdentifierAttribute)));
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        [Description("Groups by multiple selectors by contiously grouping by each provided selector.")]
+        private static List<List<T>> GroupByMultipleSelectors<T>(this IEnumerable<T> results, IEnumerable<Func<object, object>> funcs)
+        {
+            IEnumerable<IEnumerable<T>> listGroup = new List<IEnumerable<T>>() { results };
+            foreach (var func in funcs)
+                listGroup = listGroup.Select(x => x.GroupBy(y => func(y))).SelectMany(x => x);
+
+            return listGroup.Select(x => x.ToList()).ToList();
+        }
+
+        /***************************************************/
+
+        [Description("Gets a list of compiled selectors corresponding to all properties with the attribute type on the type. Allows the attribute type to be defined on an interface of the object type.")]
+        private static List<Func<object, object>> IdentifierFunctions(Type type, Type attributeType)
+        {
+            Tuple<Type, Type> key = new Tuple<Type, Type>(type, attributeType);
+            List<Func<object, object>> funcs;
+            lock (m_funcLock)   //Lock extraction for thread saftey
+            {
+                if (!m_identifierFunctions.TryGetValue(key, out funcs)) //Try get stored pre-compiled functions
+                {
+                    //Get all properties with the provided attribute and compile to functions
+                    funcs = type.PropertiesWithAttribute(attributeType, true, true).Select(x => x.ToFunc()).ToList();
+                    m_identifierFunctions[key] = funcs; //Store functions for future usage
+                }
+            }
+            return funcs;
+        }
+
+        /***************************************************/
+
+        [Description("Compiles the property info into a Func<object,object>.")]
+        private static Func<object, object> ToFunc(this PropertyInfo prop)
+        {
+            var method = typeof(Query)
+                      .GetMethods(BindingFlags.NonPublic | BindingFlags.Static)
+                      .Single(m => m.Name == nameof(CompileCastProperty) && m.IsGenericMethodDefinition && m.GetParameters().Count() == 1);
+
+            MethodInfo generic = method.MakeGenericMethod(new Type[] { prop.DeclaringType, prop.PropertyType });
+            return (Func<object, object>)generic.Invoke(null, new object[] { prop });
+        }
+
+        /***************************************************/
+
+        [Description("Creates a delagate of type Func<T,P> that matches that of the provided propertyinfo. Returns a Func<obejct,object> that casts the object into a T.")]
+        private static Func<object, object> CompileCastProperty<T, P>(PropertyInfo prop)
+        {
+            Func<T, P> func = (Func<T, P>)Delegate.CreateDelegate(typeof(Func<T, P>), prop.GetGetMethod());
+            return x => func((T)x);
+        }
+
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        private static Dictionary<Tuple<Type, Type>, List<Func<object, object>>> m_identifierFunctions = new Dictionary<Tuple<Type, Type>, List<Func<object, object>>>();
+        private static object m_funcLock = new object();
+
+        /***************************************************/
+
     }
 }
 
