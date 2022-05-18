9a95a460538390326eee31363579fe15b9e768c4
Robin Flyman <robin.flyman@burohappold.com>
2022-05-18 15:26:18 +0100

    Moved Round method from TAS Toolkit to Data_Engine

---
+++ Data_Engine/Modify/Round.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using BH.oM.Base;
+using BH.oM.Base.Attributes;
+
+namespace BH.Engine.Data
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+        [PreviousVersion("5.2", "BH.Engine.Adapters.TAS.Modify.Round(System.Single, System.Int32)")]
+        [PreviousVersion("5.2", "BH.Engine.Adapters.TAS.Modify.Round(System.Double, System.Int32)")]
+
+        [Description("Returns a double number rounded to the specified number of decimal places")]
+        [Input("number", "A double to round")]
+        [Input("decimals", "The number of decimals to round to - default 3 decimal places (e.g. round 3.14159 to 3.141")]
+        [Output("number", "The double number rounded to the specified number of decimal places")]
+        public static double Round(this double number, int decimals = 3)
+        {
+            return Math.Round(number, decimals);
+        }
+
+        /***************************************************/
+    }
+}
