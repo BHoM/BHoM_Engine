04549ab5ebd538e0978ca25ae4cdd0b54c57d292
Isak Naslund <Isak.Naslund@burohappold.com>
2022-05-23 16:50:47 +0200

    Adding IsClosed, IsPlanar and IsSelfIntersecting methods for Polygon and BoundaryCurve
    Also, significant amount of missing descriptions added

--- Geometry_Engine/Query/IsClosed.cs
+++ Geometry_Engine/Query/IsClosed.cs
@@ -25,6 +25,8 @@ using BH.oM.Base.Attributes;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using System.ComponentModel;
+using BH.oM.Quantities.Attributes;
 
 namespace BH.Engine.Geometry
 {
@@ -34,13 +36,21 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Curves                   ****/
         /***************************************************/
 
+        [Description("Checks if the Arc is closed, i.e. forms a closed loop by checking if the end points are within tolerance of each other.")]
+        [Input("arc", "The Arc to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true if the Arc is closed, i.e. a circle.")]
         public static bool IsClosed(this Arc arc, double tolerance = Tolerance.Distance)
         {
-            return (arc.Angle() - Math.PI * 2)*arc.Radius > -tolerance;
+            return (arc.Angle() - Math.PI * 2) * arc.Radius > -tolerance;
         }
 
         /***************************************************/
 
+        [Description("Checks if the Circle is closed, i.e. forms a closed loop. A Circle is by definition always closed, why this method always returns true.")]
+        [Input("circle", "The Circle to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true for all cases.")]
         public static bool IsClosed(this Circle circle, double tolerance = Tolerance.Distance)
         {
             return true;
@@ -48,6 +58,10 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
+        [Description("Checks if the Line is closed, i.e. forms a closed loop. A Line is by definition never closed, why this method always returns false.")]
+        [Input("line", "The Circle to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns false for all cases.")]
         public static bool IsClosed(this Line line, double tolerance = Tolerance.Distance)
         {
             return false;
@@ -55,6 +69,10 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
+        [Description("Checks if the Ellipse is closed, i.e. forms a closed loop. An Ellipse is by definition always closed, why this method always returns true.")]
+        [Input("ellipse", "The Ellipse to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true for all cases.")]
         public static bool IsClosed(this Ellipse ellipse, double tolerance = Tolerance.Distance)
         {
             return true;
@@ -62,6 +80,10 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
+        [Description("Checks if the NurbsCurve is closed, i.e. forms a closed loop, by checking if the end points are within tolerance of each other or if it is a periodic curve.")]
+        [Input("curve", "The NurbsCurve to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true if the NurbsCurve is closed.")]
         public static bool IsClosed(this NurbsCurve curve, double tolerance = Tolerance.Distance)
         {
             if (curve == null || curve.ControlPoints == null || curve.ControlPoints.Count < 2)
@@ -72,6 +94,10 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
+        [Description("Checks if the PolyCurve is closed, i.e. forms a closed loop, by checking if the end points are within tolerance of each other or if it is made up of a single inner Curve that is itself closed.")]
+        [Input("curve", "The PolyCurve to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true if the PolyCurve is closed.")]
         public static bool IsClosed(this PolyCurve curve, double tolerance = Tolerance.Distance)
         {
             if (curve.Curves.Count == 1)
@@ -93,6 +119,10 @@ namespace BH.Engine.Geometry
 
         /***************************************************/
 
+        [Description("Checks if the PolyCurve is closed, i.e. forms a closed loop, by checking if the first and last control points are within tolerance of each other.")]
+        [Input("curve", "The Polyline to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true if the Polyline is closed.")]
         public static bool IsClosed(this Polyline curve, double tolerance = Tolerance.Distance)
         {
             List<Point> pts = curve.ControlPoints;
@@ -102,11 +132,36 @@ namespace BH.Engine.Geometry
             return pts.First().SquareDistance(pts.Last()) < tolerance * tolerance;
         }
 
+        /***************************************************/
+
+        [Description("Checks if the Polygon is closed, i.e. forms a closed loop. A Polygon is always ensured to be closed at creation, why this method always returns true.")]
+        [Input("curve", "The Polygon to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "A Polygon is checked for closedness at creation, why this method always returns true.")]
+        public static bool IsClosed(this Polygon curve, double tolerance = Tolerance.Distance)
+        {
+            return true;
+        }
+
+        /***************************************************/
+
+        [Description("Checks if the BoundaryCurve is closed, i.e. forms a closed loop. A BoundaryCurve is always ensured to be closed at creation, why this method always returns true.")]
+        [Input("curve", "The BoundaryCurve to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "A BoundaryCurve is checked for closedness at creation, why this method always returns true.")]
+        public static bool IsClosed(this BoundaryCurve curve, double tolerance = Tolerance.Distance)
+        {
+            return true;
+        }
 
         /***************************************************/
         /**** Public Methods - Interfaces               ****/
         /***************************************************/
 
+        [Description("Checks if the ICurve is closed, i.e. forms a closed loop.")]
+        [Input("curve", "The ICurve to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true if the ICurve is closed.")]
         public static bool IIsClosed(this ICurve curve, double tolerance = Tolerance.Distance)
         {
             return IsClosed(curve as dynamic, tolerance);
@@ -117,6 +172,10 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Nurbs Surfaces           ****/
         /***************************************************/
 
+        [Description("Checks if the NurbsSurface is closed, i.e. forms a closed loop, in at least one direction, i.e. if one of the edges overlap fully with the edge on the oposite side in the u/v domain.")]
+        [Input("surface", "The NurbsSurface to check for closedness.")]
+        [Input("tolerance", "Distance tolerance for closedness validation.", typeof(Length))]
+        [Output("isClosed", "Returns true if the NurbsSurface is closed in at least one direction.")]
         public static bool IsClosed(this NurbsSurface surface, double tolerance = Tolerance.Distance)
         {
             if (surface.IsPeriodic())
