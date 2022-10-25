51c7c1ad9554d3ac0da6bd11a1aa21f0549ca39f
Isak Naslund <Isak.Naslund@burohappold.com>
2022-10-25 14:25:27 +0200

    Add method for mapping proeprties of a pre-defined material onto another Material matching based on name and/or name of properties

--- Matter_Engine/Modify/MapMaterial.cs
+++ Matter_Engine/Modify/MapMaterial.cs
@@ -38,14 +38,14 @@ namespace BH.Engine.Matter
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Adds a IMaterialFragment to a material based on the mapping defined by the keys and materialFragments. \n" + 
+        [Description("Adds a IMaterialFragment to a material based on the mapping defined by the keys and materialFragments. \n" +
                      "i.e. The materialFragment on index 3 will be added to the Material with the same name as the key at index 3.")]
         [Input("materials", "The Materials to Modify, will be evaluated based on their name.")]
-        [Input("keys", "The key is the name of the Material to be affected. The keys index in this list relates to the index of a materialFragment to add in the other list. \n" + 
+        [Input("keys", "The key is the name of the Material to be affected. The keys index in this list relates to the index of a materialFragment to add in the other list. \n" +
                        "Empty keys means that its related materialFragment will be disgarded.")]
         [Input("materialFragments", "The materialFragments to add to the Materials, the order of which relates to the keys.")]
         [Output("materials", "Materials with modified list of properties. Materials whos names did not appear among the keys are unaffected.")]
-        public static IEnumerable<Material> MapMaterial(IEnumerable<Material> materials, List<string> keys, List<IMaterialProperties> materialFragments)
+        public static IEnumerable<Material> MapMaterial(this IEnumerable<Material> materials, List<string> keys, List<IMaterialProperties> materialFragments)
         {
             if (keys.Count > materialFragments.Count)
             {
@@ -74,21 +74,146 @@ namespace BH.Engine.Matter
                 Engine.Base.Compute.RecordError("Non-empty keys must be distinct.");
                 return null;
             }
-            
+
             // Add the materialFragment to the material and return
-            return materials.Select( (x) =>
+            return materials.Select((x) =>
+           {
+               for (int i = 0; i < culledKeys.Count; i++)
+               {
+                   if (x.Name == culledKeys[i])
+                   {
+                       Material mat = x.DeepClone();
+                       mat.Properties.Add(culledMaterialFragments[i]);
+                       return mat;
+                   }
+               }
+               return x;
+           }).ToList();
+        }
+
+        /***************************************************/
+
+        [Description("Maps a set of materials to a set of provided transdiciplinary materials.\n" +
+                     "First atempts to match the name of the provided materials to the material maps.\n" +
+                     "If no name match is found, atempts to instead find a material with as many matching MaterialProperties (based on type and name) as possible.\n" +
+                     "If a unique match is found based on one of the above matching methods, all Properties from the transdiciplinary material is applied to the material to be matched.")]
+        [Input("materials", "The Materials to Modify, will be evaluated based on their name and properties.")]
+        [Input("materialMaps", "The Material maps to match to. Should generally have unique names. Names of material as well as material properties will be used to map to the materials to be modified.")]
+        [Output("materials", "Materials with modified list of properties. Materials for which no unique match could be found are unaffected.")]
+        public static IEnumerable<Material> MapMaterial(this IEnumerable<Material> materials, IEnumerable<Material> materialMaps)
+        {
+            ILookup<string, Material> nameLookup = materialMaps.ToLookup(x => x.Name);
+
+            Dictionary<Tuple<Type, string>, List<Material>> propertyMaps = new Dictionary<Tuple<Type, string>, List<Material>>();
+
+            foreach (Material mat in materialMaps)
             {
-                for (int i = 0; i < culledKeys.Count; i++)
+                foreach (IMaterialProperties property in mat.Properties)
                 {
-                    if (x.Name == culledKeys[i])
-                    {
-                        Material mat = x.DeepClone();
-                        mat.Properties.Add(culledMaterialFragments[i]);
-                        return mat;
-                    }
+                    Tuple<Type, string> key = new Tuple<Type, string>(property.GetType(), property.Name);
+                    if (propertyMaps.ContainsKey(key))
+                        propertyMaps[key].Add(mat);
+                    else
+                        propertyMaps[key] = new List<Material>() { mat };
                 }
-                return x;
-            }).ToList();
+            }
+
+            List<Material> mappedMaterials = new List<Material>();
+
+            foreach (Material material in materials)
+            {
+                mappedMaterials.Add(material.MapMaterial(nameLookup, propertyMaps));
+            }
+            return mappedMaterials;
+        }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        private static Material MapMaterial(this Material material, ILookup<string, Material> nameLookup, Dictionary<Tuple<Type, string>, List<Material>> propertyMaps)
+        {
+            List<Material> matches = nameLookup[material.Name].ToList();    //Try match by name
+
+            if (matches.Count == 0) //If no name match found, try match by material proeprties - Type and name
+            {
+                foreach (IMaterialProperties property in material.Properties)
+                {
+                    Tuple<Type, string> key = new Tuple<Type, string>(property.GetType(), property.Name);
+
+                    List<Material> propMatches;
+                    if (propertyMaps.TryGetValue(key, out propMatches))
+                        matches.AddRange(propMatches);
+                }
+
+                matches = matches.GroupBy(x => x.Name).Select(x => x.First()).ToList();
+                matches = matches.GroupBy(x => x.MatchScore(material))  //Get and group by score
+                                 .Where(x => x.Key > 0)                 //Only keep materials with score > 0, as negative score indicates at least one mismatch
+                                 .OrderByDescending(x => x.Key)         //Order by score - Higher score first
+                                 .FirstOrDefault()?.ToList() ?? new List<Material>();
+            }
+
+            if (matches.Count == 1) //Exactly one best match. Success!
+            {
+                return material.MergeProperties(matches[0]);
+            }
+            else if (matches.Count == 0)    //No matches, record warning
+            {
+                Base.Compute.RecordWarning($"No map found for material named {material.Name}");
+                return material;
+            }
+            else  //More than one match. Record warning
+            {
+                Base.Compute.RecordWarning($"Material named {material.Name} has ambiguous matches to {string.Join(", ", matches.Select(x => x.Name))}. No mapping preformed.\nEnsure that the transdiciplinaryMaterialMaps have unique names and that your Material has a name matching the transdiciplinary material you want to match.");
+                return material;
+            }
+        }
+
+        /***************************************************/
+
+        [Description("Merges the Properties of the target and source by adding all properties on the source to the target. For duplicate types the Property on the Source is prioritised.")]
+        private static Material MergeProperties(this Material target, Material source)
+        {
+            Material targetClone = target.ShallowClone();
+            targetClone.Properties = new List<IMaterialProperties>(target.Properties);
+            List<Type> targetTypes = target.Properties.Select(x => x.GetType()).ToList();
+            foreach (IMaterialProperties property in source.Properties)
+            {
+                if (targetTypes.Contains(property.GetType()))
+                {
+                    targetClone.Properties.RemoveAll(x => x.GetType() == property.GetType());
+                }
+                targetClone.Properties.Add(property);
+            }
+            return targetClone;
+        }
+
+        /***************************************************/
+
+        [Description("Matches two materials based on how well the properties align based on name. For each aligning property the score is increased by 1. If a single misaligned property is found a score of -1 is returned.")]
+        private static int MatchScore(this Material mat1, Material mat2)
+        {
+            //Method matches two materials based on their properties.
+            //For each proeprty of the same type and same name, the score is increased by 1.
+            //If a property of the same type but a different name is found the score is set to -1, as that means a mismatch of properties.
+            int score = 0;
+            ILookup<Type, IMaterialProperties> matLookup = mat2.Properties.ToLookup(x => x.GetType());
+            foreach (IMaterialProperties prop in mat1.Properties)
+            {
+                string name = prop.Name;
+                Type type = prop.GetType();
+
+                IEnumerable<IMaterialProperties> prop2 = matLookup[type];
+                if (prop2.Any())
+                {
+                    if (prop2.Any(x => x.Name == name))
+                        score++;
+                    else
+                        return -1;
+                }
+
+            }
+            return score;
         }
 
         /***************************************************/
