142a4a12fbf6639ebca4d38be057ee305ddf77fe
Isak Naslund <Isak.Naslund@burohappold.com>
2022-10-28 08:28:59 +0200

    Minor refactoring and updated descriptions and comments

--- Matter_Engine/Query/Density.cs
+++ Matter_Engine/Query/Density.cs
@@ -44,10 +44,10 @@ namespace BH.Engine.Matter
 
         [PreviousVersion("6.0", "BH.Engine.Matter.Query.Density(BH.oM.Physical.Materials.Material, System.Type, System.Double)")]
         [Description("Returns the density of a Material though querying each of the individual IMaterialProperties that implement IDensityProvider." +
-                     "\nIf inconsistent density values are found on multiple different IMaterialProperties, no result will be returned.")]
+                     "\nExtraction process can be controlled via the DensityExtractionOptions allowing for various controls.")]
         [Input("material", "The material to query density from.")]
         [Input("options", "Options controling how the density should be extracted from the Material.")]
-        [Output("density", "The density of the material. Additional info on how the value has been acquired is recorded in the warning", typeof(Density))]
+        [Output("density", "The density of the material.", typeof(Density))]
         public static double Density(this Material material, DensityExtractionOptions options = null)
         {
             if(material == null)
@@ -59,44 +59,48 @@ namespace BH.Engine.Matter
             //Gets all properties on the material able to store density
             List<IDensityProvider> allDensityProviders = material.Properties.OfType<IDensityProvider>().ToList();
 
-            if (allDensityProviders.Count == 0)
+            if (allDensityProviders.Count == 0) //Nothing available -> return
             {
                 Base.Compute.RecordWarning($"Material {material.Name} does not contain any properties able to store density. 0 density returned.");
                 return 0;
             }    
 
-            options = options ?? new DensityExtractionOptions();
+            options = options ?? new DensityExtractionOptions();    //Set up default options
 
+            //Handle type filtering
             Type type = options.Type;
+            List<IDensityProvider> densityProviders;
             if (type == null)
-                type = typeof(IDensityProvider);
-            else if (!typeof(IDensityProvider).IsAssignableFrom(type))
+                densityProviders = allDensityProviders;
+            else if (typeof(IDensityProvider).IsAssignableFrom(type))   //Check type filter valid
+            {
+                //Filter by the provided type
+                densityProviders = allDensityProviders.Where(x => type.IsAssignableFrom(x.GetType())).ToList();
+                if (densityProviders.Count == 0)    //No items found matching type filter
+                {
+                    if (options.AllowFallbackIfNoType)  //If allowing fallback, use all providers rather than filtered out ones
+                    {
+                        Base.Compute.RecordWarning($"No MaterialProperty of type {type.Name}. Falling back to extracting density from other properties with density.");
+                        densityProviders = allDensityProviders;
+                    }
+                    else    //Else raise warning and return
+                    {
+                        Base.Compute.RecordWarning($"No density on any of the properties of {material.Name} of type {type.Name}. To allow falling back to other avilable properties set {nameof(options.AllowFallbackIfNoType)} to true.");
+                        return 0;
+                    }
+                }
+            }
+            else
             {
                 Base.Compute.RecordError($"Type provided for density extraction need to be a type implementing {nameof(IDensityProvider)}. Please provide a valid type or null to allow all types.");
                 return double.NaN;
             }
 
-            //Filter by the provided type
-            List<IDensityProvider> densityProviders = allDensityProviders.Where(x => type.IsAssignableFrom(x.GetType())).ToList();
-
-            if (densityProviders.Count == 0)
-            {
-                if (options.AllowFallbackIfNoType)
-                {
-                    Base.Compute.RecordWarning($"No MaterialProperty of type {type.Name}. Falling back to extracting density from other properties with density.");
-                    densityProviders = allDensityProviders;
-                }
-                else
-                {
-                    Base.Compute.RecordWarning($"No density on any of the properties of {material.Name} of type {type.Name}. To allow falling back to other avilable properties set {nameof(options.AllowFallbackIfNoType)} to true.");
-                    return 0;
-                }
-            }
 
             //Get all density values
             List<double> densities = densityProviders.Select(x => x.Density).ToList();
 
-            if (options.IgnoreZeroValues)
+            if (options.IgnoreZeroValues)   //If true, density values smaller than the 0 tolerance should be ignored
             {
                 if (densities.All(x => x <= options.ZeroTolerance)) //If all densities are below the 0 threshold, do not filter, as that means removing all
                 {
@@ -111,6 +115,7 @@ namespace BH.Engine.Matter
             if (densities.Count == 1)   //Single value - simply return
                 return densities[0];
 
+            //More than single value, extract depending on ExtractionType setting
             switch (options.ExtractionType)
             {
                 case DensityExtractionType.Average:
