4c606737095609925069db7a791eaf8b89ff269d
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-11-08 15:05:36 +0000

    Add documentation to URL file

--- Reflection_Engine/Query/Url.cs
+++ Reflection_Engine/Query/Url.cs
@@ -23,6 +23,7 @@
 using BH.oM.Base.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 
@@ -34,6 +35,9 @@ namespace BH.Engine.Reflection
         /**** Interface Methods                         ****/
         /***************************************************/
 
+        [Description("Obtain the URL to the code of the given object. The URL returned will link directly to the source code for that object if it exists.")]
+        [Input("obj", "An object to obtain the URL for.")]
+        [Output("url", "The URL of the source code for the object. Null if no source code URL could be ascertained.")]
         public static string IUrl(this object obj)
         {
             if (obj == null)
@@ -59,6 +63,9 @@ namespace BH.Engine.Reflection
         /**** Public Methods                            ****/
         /***************************************************/
 
+        [Description("Obtain the URL to the code of the given type. The URL returned will link directly to the source code for that type if it exists.")]
+        [Input("type", "The type to obtain the URL for.")]
+        [Output("url", "The URL of the source code for the type. Null if no source code URL could be ascertained.")]
         public static string Url(this Type type)
         {
             if (type == null)
@@ -68,11 +75,11 @@ namespace BH.Engine.Reflection
             if (ass == null)
                 return null;
 
-            AssemblyUrlAttribute att = ass.GetCustomAttribute<AssemblyUrlAttribute>();
+            AssemblyDescriptionAttribute att = ass.GetCustomAttribute<AssemblyDescriptionAttribute>();
             if (att == null)
                 return null;
 
-            string url = att.Url;
+            string url = att.Description;
             if (url == "")
                 return null;
 
@@ -91,6 +98,9 @@ namespace BH.Engine.Reflection
 
         /***************************************************/
 
+        [Description("Obtain the URL to the code of the given method. The URL returned will link directly to the source code for that method if it exists.")]
+        [Input("method", "The method to obtain the URL for.")]
+        [Output("url", "The URL of the source code for the method. Null if no source code URL could be ascertained.")]
         public static string Url(this MethodBase method)
         {
             if (method == null)
@@ -100,11 +110,11 @@ namespace BH.Engine.Reflection
             if (ass == null)
                 return null;
 
-            AssemblyUrlAttribute att = ass.GetCustomAttribute<AssemblyUrlAttribute>();
+            AssemblyDescriptionAttribute att = ass.GetCustomAttribute<AssemblyDescriptionAttribute>();
             if (att == null)
                 return null;
 
-            string url = att.Url;
+            string url = att.Description;
 
             List<string> path = new List<string>() { url, "blob/main/" };
             path.Add(ass.GetName().Name);
