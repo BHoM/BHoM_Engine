d49ca3a98e9455f5e1ec1a832d5531498ee78ad9
Isak Naslund <Isak.Naslund@burohappold.com>
2022-11-16 09:25:27 +0100

    UpdateParameterAtPoint to use new functions and general cleanup

---
+++ Geometry_Engine/Query/CurvePointsByParameterStep.cs
@@ -0,0 +1,114 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Base.Attributes;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Computes a set number of points on the curve by dividing the curve domain into equal steps. Note that the points wont be equal distance divided for the general case.")]
+        [Input("curve", "The NurbsCurve to divide.")]
+        [Input("steps", "The number of divisions.")]
+        [Output("pts", "Points along the curve.")]
+        public static List<Point> CurvePointsByParameterStep(NurbsCurve curve, int steps)
+        {
+
+            Point[] pts = new Point[steps];
+            bool periodic = curve.IsPeriodic();
+            int degree = curve.Degree();
+
+            List<double> knots = curve.Knots;
+            int n = knots.Count - degree;   //Index of first knot correpsonding to last controlpoint
+            double min = knots[degree - 1];
+            double max = knots[n];
+
+            //Compute stepsize
+            double stepSize = (max - min) / (double)((double)steps - 1.0);
+
+            //Get start point of the curve
+            if (periodic)
+                pts[0] = ComputeCurvePoint(curve.ControlPoints, curve.Weights, BasisFunctions(knots, degree - 1, degree, min), degree - 1, degree);
+            else
+                pts[0] = curve.ControlPoints[0];
+
+            int iter = 1;   //Current point index
+
+            double t = min + stepSize;
+
+            //Iterate along the knot vector and pick up all points corresponding to the current knot span before moving to next
+            for (int k = degree - 1; k < n; k++)
+            {
+                while (knots[k] == knots[k + 1] && knots[k] < 1)
+                {
+                    k++;
+                }
+                while (t < knots[k + 1])    //While t is the knotspan corresponding to k
+                {
+                    double[] basisArray = BasisFunctions(knots, k, degree, t);
+                    pts[iter] = ComputeCurvePoint(curve.ControlPoints, curve.Weights, basisArray, k, degree);
+                    iter++;
+                    t += stepSize;
+                }
+            }
+
+            if (periodic)
+                pts[steps - 1] = ComputeCurvePoint(curve.ControlPoints, curve.Weights, BasisFunctions(knots, n - 1, degree, max), n - 1, degree);
+            else
+                pts[steps - 1] = curve.ControlPoints.Last();
+
+
+            return pts.ToList();
+        }
+
+        /***************************************************/
+        /**** private Methods                           ****/
+        /***************************************************/
+
+        private static Point ComputeCurvePoint(List<Point> pts, List<double> weights, double[] basis, int span, int degree)
+        {
+            Point pt = new Point();
+            double sum = 0;
+            int ptIndexAddtion = span - degree + 1;
+            for (int i = 0; i < basis.Length; i++)
+            {
+                int ptIndex = ptIndexAddtion + i;
+                double basisWeight = basis[i] * weights[ptIndex];
+                pt += pts[ptIndex] * basisWeight;
+                sum += basisWeight;
+            }
+            return pt / sum;
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Geometry_Engine/Query/KnotSpan.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.oM.Base.Attributes;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Gets the span index in the knot vector corresponding to parameter t. Used to evaluate Basis Functions for Nurbs Curves/Surfaces.")]
+        [Input("knots", "The knots to evaluate.")]
+        [Input("degree", "Degree of the Curve/Surface in the direction of the provided knots.")]
+        [Input("t", "The parameter to evaluate.")]
+        [Output("span", "The index of the span of the knotvector in which the parameter t resides.")]
+        public static int KnotSpan(this IReadOnlyList<double> knots, int degree, double t)
+        {
+            int n = knots.Count - degree;
+
+            if (t >= knots[n])  //End span
+                return n - 1;
+
+            //Binary serach
+            int low = degree - 1;
+            int high = n;
+            int mid = (low + high) / 2;
+
+            if (t < knots[low])
+            {
+                Base.Compute.RecordError("Invalid degree in relation to the knotvector and t provided. Cannot compute knot span.");
+                return -1;
+            }
+            while (t < knots[mid] || t >= knots[mid + 1])
+            {
+                if (t < knots[mid])
+                    high = mid;
+                else
+                    low = mid;
+
+                mid = (low + high) / 2;
+            }
+            return mid;
+        }
+
+        /***************************************************/
+    }
+}
