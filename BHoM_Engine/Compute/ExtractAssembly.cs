d5f11a1662d74ecde27d402f961cb724c2881421
adecler <arnaud.declercq@burohappold.com>
2022-11-24 15:44:38 +0000

    Fix serialisation in Blazor web assemblies

--- BHoM_Engine/Compute/ExtractAssembly.cs
+++ BHoM_Engine/Compute/ExtractAssembly.cs
@@ -41,7 +41,7 @@ namespace BH.Engine.Base
         [Input("assembly", "Assembly to be reflected.")]
         public static void ExtractAssembly(Assembly assembly)
         {
-            if (assembly == null || assembly.ReflectionOnly)
+            if (assembly == null || assembly.IsDynamic || assembly.ReflectionOnly)
                 return;
 
             lock (Global.AssemblyReflectionLock)
