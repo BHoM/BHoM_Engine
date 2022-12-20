d210f1641d9577b82564d89f3c0dcf8675fca67e
Pawel Baran <pawel.baran@burohappold.com>
2022-12-20 13:32:42 +0100

    Subtypes query added

---
+++ Reflection_Engine/Query/Subtypes.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Returns all the loaded types that inherit from the specified parent type.")]
+        public static List<Type> Subtypes(this Type parentType, bool includeInterfaces = true)
+        {
+            if (parentType == null)
+            {
+                Base.Compute.RecordWarning("Cannot query the subtypes of a null type.");
+                return new List<Type>();
+            }
+
+            List<Type> implemented = new List<Type>();
+            foreach (Type t in Base.Query.BHoMTypeList())
+            {
+                if ((includeInterfaces || !t.IsInterface) && parentType.IsAssignableFrom(t))
+                    implemented.Add(t);
+            }
+
+            return implemented;
+        }
+
+        /***************************************************/
+    }
+}
+
+
+
