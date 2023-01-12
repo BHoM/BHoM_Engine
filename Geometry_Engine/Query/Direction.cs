0cd3fca833cfc6cabd47b08661b81a088a8d6266
Isak Naslund <Isak.Naslund@burohappold.com>
2023-01-12 15:30:51 +0100

    Remove duplicate method for Normal

--- Geometry_Engine/Query/Direction.cs
+++ Geometry_Engine/Query/Direction.cs
@@ -40,21 +40,6 @@ namespace BH.Engine.Geometry
         }
 
         /***************************************************/
-
-        public static Vector Normal(this Mesh mesh, Face face, double tolerance = Tolerance.Distance)
-        {
-            List<Point> vertices = mesh.Vertices;
-
-            Point p1 = vertices[(face.A)];
-            Point p2 = vertices[(face.B)];
-            Point p3 = vertices[(face.C)];
-            Vector v1 = p2 - p1;
-            Vector v2 = p3 - p1;
-            double sqTol = tolerance * tolerance;
-            return v1.SquareLength() <= sqTol || v2.SquareLength() <= sqTol ? null : Query.CrossProduct(p2 - p1, p3 - p1);
-        }
-
-        /***************************************************/
     }
 }
 
