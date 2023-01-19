161bec45f71dcc3509edac9ddf53aedad05079c0
Isak Naslund <Isak.Naslund@burohappold.com>
2023-01-19 14:15:56 +0100

    Add options for converting points to and from double arrays, allowing for planar input

--- Geometry_Engine/Convert/ToPointAndWeight.cs
+++ Geometry_Engine/Convert/ToPointAndWeight.cs
@@ -43,31 +43,70 @@ namespace BH.Engine.Geometry
         [MultiOutput(1, "weights", "The weights from the controlpoints if rational, otherwise a list of 1's of equal length to the list of control points.")]
         public static Output<List<Point>, List<double>> ToPointAndWeight(this List<double[]> controlPoints, bool isRational)
         {
+            return ToPointAndWeight(controlPoints, isRational, false);
+        }
+
+        /***************************************************/
+
+        [Description("Converts the list of double[] control points to a list of Points and Weights.")]
+        [Input("controlPoints", "The list of control points to convert.")]
+        [Input("isRational", "If true, the controlPoints are assumed to be 4d or 3d (depending on if planar) in honogenous coordinates, if false assumed to be 3d or 2d (depending on if planar) in cartesian coordinates.")]
+        [Input("planar", "If true, the controlpoints are assumed to have only x and y coordinates defined, if false assumed to have x, y and z coordinates defined.")]
+        [MultiOutput(0, "pts", "The points resulting from the conversion from either cartesian or homogenous coordinates, depending on isRational.")]
+        [MultiOutput(1, "weights", "The weights from the controlpoints if rational, otherwise a list of 1's of equal length to the list of control points.")]
+        public static Output<List<Point>, List<double>> ToPointAndWeight(this List<double[]> controlPoints, bool isRational, bool planar)
+        {
             List<Point> points = new List<Point>();
             List<double> weight = new List<double>();
 
             if (isRational)
             {
-                for (int i = 0; i < controlPoints.Count; i++)
+                if (planar)
                 {
-                    double[] ptW = controlPoints[i];
-                    double w = ptW[3];
-                    points.Add(new Point() { X = ptW[0] / w, Y = ptW[1] / w, Z = ptW[2] / w });
-                    weight.Add(w);
+                    for (int i = 0; i < controlPoints.Count; i++)
+                    {
+                        double[] ptW = controlPoints[i];
+                        double w = ptW[2];
+                        points.Add(new Point() { X = ptW[0] / w, Y = ptW[1] / w });
+                        weight.Add(w);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < controlPoints.Count; i++)
+                    {
+                        double[] ptW = controlPoints[i];
+                        double w = ptW[3];
+                        points.Add(new Point() { X = ptW[0] / w, Y = ptW[1] / w, Z = ptW[2] / w });
+                        weight.Add(w);
+                    }
                 }
             }
-            else 
+            else
             {
-                for (int i = 0; i < controlPoints.Count; i++)
+                if (planar)
                 {
-                    double[] pt = controlPoints[i];
-                    points.Add(new Point() { X = pt[0], Y = pt[1], Z = pt[2] });
-                    weight.Add(1.0);    //All weights equal to 1.0 for non-rational
+                    for (int i = 0; i < controlPoints.Count; i++)
+                    {
+                        double[] pt = controlPoints[i];
+                        points.Add(new Point() { X = pt[0], Y = pt[1] });
+                        weight.Add(1.0);    //All weights equal to 1.0 for non-rational
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < controlPoints.Count; i++)
+                    {
+                        double[] pt = controlPoints[i];
+                        points.Add(new Point() { X = pt[0], Y = pt[1], Z = pt[2] });
+                        weight.Add(1.0);    //All weights equal to 1.0 for non-rational
+                    }
                 }
             }
             return new Output<List<Point>, List<double>> { Item1 = points, Item2 = weight };
         }
 
         /***************************************************/
+
     }
 }
