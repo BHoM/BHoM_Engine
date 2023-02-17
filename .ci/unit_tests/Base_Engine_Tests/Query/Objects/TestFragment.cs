8bfb043a2b4d853135da39355f872cc661cddcde
Alessio Lombardi <alessio.lombardi@burohappold.com>
2023-02-17 13:23:21 +0000

    Folder & namespace fixes; report RecordMessage

--- .ci/unit_tests/Base_Engine_Tests/Query/Objects/TestFragment.cs
+++ .ci/unit_tests/Base_Engine_Tests/Query/Objects/TestFragment.cs
@@ -24,37 +24,10 @@ using BH.oM.Base;
 using BH.Engine.Base;
 using System.Collections;
 
-namespace BH.oM.Base
+namespace BH.Tests.Engine.Base.Query.Objects
 {
-    // Must be unpackable
-    public class Container<T> : BHoMObject, IContainer
+    public class TestFragment : IFragment
     {
-        public T? SomeObject { get; set; }
-        public IEnumerable<T> ListOfObjects { get; set; } = new List<T>();
-        public IEnumerable<IEnumerable<T>> ListOfLists { get; set; } = new List<List<T>>();
-    }
-
-    // We want to support this, it should get the values of the dictionary.
-    public class DictionaryContainer<T> : Container<T>
-    {
-        public Dictionary<string, T> Dictionary { get; set; } = new Dictionary<string, T>();
-    }
-
-    // We want to support this, it should get the values of the dictionary and flatten them.
-    public class DictionaryListContainer<T> : Container<T>
-    {
-        public Dictionary<string, IEnumerable<T>> DictionaryOfLists { get; set; } = new Dictionary<string, IEnumerable<T>>();
-    }
-
-    // Not supported.
-    public class ListOfDictionariesContainer<T> : Container<T>
-    {
-        public IEnumerable<Dictionary<string, T>> ListOfDictionaries { get; set; } = new List<Dictionary<string, T>>();
-    }
-
-    // Not supported.
-    public class ListOfListOfListContainer<T> : Container<T>
-    {
-        public IEnumerable<IEnumerable<IEnumerable<T>>> ListOfListOfLists { get; set; } = new List<List<List<T>>>();
+        public object? SomeObject { get; set; }
     }
 }
