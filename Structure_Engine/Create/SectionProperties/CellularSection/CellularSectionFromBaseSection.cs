5b84ce50addc5903e0a40772c0024a141fc7af6a
Isak Naslund <Isak.Naslund@burohappold.com>
2023-02-24 13:17:08 +0100

    Update CellularSectionFromBaseSection.cs

--- Structure_Engine/Create/SectionProperties/CellularSection/CellularSectionFromBaseSection.cs
+++ Structure_Engine/Create/SectionProperties/CellularSection/CellularSectionFromBaseSection.cs
@@ -24,8 +24,10 @@ using BH.Engine.Geometry;
 using BH.oM.Base;
 using BH.oM.Base.Attributes;
 using BH.oM.Geometry;
+using BH.oM.Quantities.Attributes;
 using BH.oM.Spatial.ShapeProfiles;
 using BH.oM.Spatial.ShapeProfiles.CellularOpenings;
+using BH.oM.Structure.MaterialFragments;
 using BH.oM.Structure.SectionProperties;
 using System;
 using System.Collections.Generic;
@@ -40,51 +42,71 @@ namespace BH.Engine.Structure
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("")]
-        [Input("", "")]
-        [Output("", "")]
-        public static CellularSection CellularSectionFromBaseSection(ISectionProfile baseProfile, ICellularOpening opening)
+        [Description("Creates the maximum possible Cellular section from the provided SteelSection and opening.")]
+        [Input("baseSEction", "The prismatic steel section being turned into a cellular section. The Steel section needs to have a profile of type ISectionProfile. Material from steel section will be applied to the created cellular section.")]
+        [Input("opening", "The openings in the finished cellular section. Opening setting will be used to produce as tight cut as possible on the base section, producing the cellular section with maximum possible height.")]
+        [Input("cutThickness", "Additional reduction factor on the final height of the section. Reduction based on thickness of the cutting process when producing the cellular section.", typeof(Length))]
+        [InputFromProperty("name")]
+        [Output("section", "Created cellular section.")]
+        public static CellularSection CellularSectionFromBaseSection(SteelSection baseSection, ICellularOpening opening, double cutThickness = 0, string name = "")
         {
+            ISectionProfile baseProfile = baseSection.SectionProfile as ISectionProfile;
+
+            if (baseProfile == null)
+            {
+                Engine.Base.Compute.RecordError($"Can only create Cellular beams from SteelSections with profiles of type {nameof(ISectionProfile)}");
+                return null;
+            }
             double openingCutHeight = opening.IHeight();
 
             double webHeight = baseProfile.Height - 2 * (baseProfile.FlangeThickness + baseProfile.RootRadius);
 
-            if (webHeight <= openingCutHeight)
-            {
-                Base.Compute.RecordError("Height of web needs to be larger or equal to the height of the opening.");
-            }
-
             double topHeight = (baseProfile.Height - openingCutHeight / 2) / 2;
 
-            TSectionProfile openingProfile = Spatial.Create.TSectionProfile(topHeight, baseProfile.Width, baseProfile.WebThickness, baseProfile.FlangeThickness, baseProfile.RootRadius, baseProfile.ToeRadius);
+            double totalHeight = openingCutHeight / 2 + opening.SpacerHeight + baseProfile.Height - opening.ICutReduction() - cutThickness;
+           
+            VoidedISectionProfile openingProfile = Spatial.Create.VoidedISectionProfile(totalHeight, openingCutHeight, baseProfile.Width, baseProfile.WebThickness, baseProfile.FlangeThickness, baseProfile.RootRadius, baseProfile.ToeRadius);
 
-            double totalHeight = openingCutHeight / 2 + opening.SpacerHeight + baseProfile.Height;
-
+            if (openingProfile == null)
+                return null;
+            
+            
             ISectionProfile solidProfile = Spatial.Create.ISectionProfile(totalHeight, baseProfile.Width, baseProfile.WebThickness, baseProfile.FlangeThickness, baseProfile.RootRadius, baseProfile.ToeRadius);
 
-
-            BoundingBox boundsOpeningProfile = Geometry.Query.Bounds(openingProfile.Edges.Select(x => BH.Engine.Geometry.Query.IBounds(x)).ToList());
-
-            double move = totalHeight / 2 - boundsOpeningProfile.Max.Y;
-            Vector translationVector = new Vector { Y = move };
-            List<ICurve> edges = openingProfile.Edges.Select(x => Engine.Geometry.Modify.ITranslate(x, translationVector)).ToList();
-
-            Plane mirrorPlane = Plane.XZ;
-            edges = edges.Concat(edges.Select(x => x.IMirror(mirrorPlane))).ToList();
-            Dictionary<string, double> constants = Compute.IntegrateSection(edges, Tolerance.MicroDistance);
-
-            constants["J"] = baseProfile.ITorsionalConstant();
-            constants["Iw"] = baseProfile.IWarpingConstant();
-
-            ISectionProfile cutSection = new ISectionProfile(solidProfile.Height, solidProfile.Width, solidProfile.WebThickness, solidProfile.FlangeThickness, solidProfile.RootRadius, solidProfile.ToeRadius, edges);
-
-            return new CellularSection(cutSection, solidProfile, opening, baseProfile, constants["Area"], constants["Rgy"], constants["Rgz"], constants["J"], constants["Iy"], constants["Iz"], constants["Iw"], constants["Wely"],
-                constants["Welz"], constants["Wply"], constants["Wplz"], constants["CentreZ"], constants["CentreY"], constants["Vz"],
-                constants["Vpz"], constants["Vy"], constants["Vpy"], constants["Asy"], constants["Asz"]);
+            return CellularSection(openingProfile, solidProfile, opening, baseProfile, baseSection.Material as Steel, name);
         }
 
         /***************************************************/
         /**** Private Methods                           ****/
+        /***************************************************/
+
+        private static CellularSection CellularSection(VoidedISectionProfile openingProfile, ISectionProfile solidProfile, ICellularOpening opening, ISectionProfile baseProfile, Steel material, string name = "")
+        {
+            Dictionary<string, double> constantsOpen = Compute.IntegrateSection(openingProfile.Edges.ToList(), Tolerance.MicroDistance, computeShearArea : false);
+            Dictionary<string, double> constantsClosed = Compute.IntegrateSection(solidProfile.Edges.ToList(), Tolerance.MicroDistance, computeShearArea: false);
+
+            constantsOpen["J"] = openingProfile.ITorsionalConstant();
+            constantsOpen["Iw"] = openingProfile.IWarpingConstant();
+            constantsOpen["Asz"] = openingProfile.WebThickness * (openingProfile.Height - openingProfile.OpeningHeight - 2 * openingProfile.FlangeThickness);
+            constantsOpen["Asy"] = 2 * openingProfile.Width * openingProfile.FlangeThickness;
+
+            Dictionary<string, double> constants = constantsOpen;
+
+            double spacing = opening.Spacing;
+            double widthWebPost = opening.WidthWebPost;
+
+            //Major axis constants as arimethric means
+            constants["Iy"] = (widthWebPost * constantsClosed["Iy"] + (spacing - widthWebPost) * constantsOpen["Iy"]) / spacing;
+            constants["Rgy"] = (widthWebPost * constantsClosed["Rgy"] + (spacing - widthWebPost) * constantsOpen["Rgy"]) / spacing;
+
+
+            CellularSection section = new CellularSection(openingProfile, solidProfile, opening, baseProfile, constants["Area"], constants["Rgy"], constants["Rgz"], constants["J"], constants["Iy"], constants["Iz"], constants["Iw"], constants["Wely"],
+                constants["Welz"], constants["Wply"], constants["Wplz"], constants["CentreZ"], constants["CentreY"], constants["Vz"],
+                constants["Vpz"], constants["Vy"], constants["Vpy"], constants["Asy"], constants["Asz"]);
+
+            return PostProcessSectionCreate(section, name, material, MaterialType.Steel);
+        }
+
         /***************************************************/
 
         private static double IHeight(this ICellularOpening opening)
@@ -111,6 +133,36 @@ namespace BH.Engine.Structure
         private static double Height(this SinusoidalOpening opening)
         {
             return opening.Height;
+        }
+
+        /***************************************************/
+
+        private static double ICutReduction(this ICellularOpening opening)
+        {
+            return CutReduction(opening as dynamic);
+        }
+
+        /***************************************************/
+
+        private static double CutReduction(this HexagonalOpening opening)
+        {
+            return 0;
+        }
+
+        /***************************************************/
+
+        private static double CutReduction(this CircularOpening opening)
+        {
+            double r = opening.Diameter / 2;
+            double w = opening.WidthWebPost / 2;
+            return r - Math.Sqrt(r * r - w * w);
+        }
+
+        /***************************************************/
+
+        private static double CutReduction(this SinusoidalOpening opening)
+        {
+            return 0;
         }
 
         /***************************************************/
