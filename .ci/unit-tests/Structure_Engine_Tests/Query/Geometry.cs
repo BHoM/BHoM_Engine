87f2fb881f3567ffa4a82f1b863cdd6d41e421ca
Alessio Lombardi <alessio.lombardi@burohappold.com>
2023-04-19 10:35:20 +0100

    Create Geometry.cs

---
+++ .ci/unit-tests/Structure_Engine_Tests/Query/Geometry.cs
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.Engine.Base;
+using NUnit.Framework;
+using AutoBogus;
+using Shouldly;
+using BH.oM.Structure.Elements;
+using BH.oM.Test.NUnit;
+using BH.oM.Structure.SectionProperties;
+
+namespace BH.Tests.Engine.Base.Query
+{
+    public class Geometry : NUnitTest
+    {
+        [Test]
+        public static void CrossSectionGeometry3D()
+        {
+            ConcreteSection concreteSection = (ConcreteSection)BH.Engine.Base.Create.RandomObject(typeof(ConcreteSection));
+            var geom = BH.Engine.Structure.Query.Geometry(concreteSection);
+            geom.ShouldNotBeNull();
+        }
+
+        [Test]
+        public static void CrossSectionIGeometryExtensionMethodNotNull()
+        {
+            ConcreteSection concreteSection = (ConcreteSection)BH.Engine.Base.Create.RandomObject(typeof(ConcreteSection));
+            var geom = BH.Engine.Base.Query.IGeometry(concreteSection);
+            geom.ShouldNotBeNull();
+        }
+    }
+}
