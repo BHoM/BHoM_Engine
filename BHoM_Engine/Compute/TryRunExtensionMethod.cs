08a688346e3dcfbb35f2990f46735263ac12ddaf
Isak Naslund <Isak.Naslund@burohappold.com>
2023-04-19 12:20:32 +0200

    Update TryRunExtensionMethod to be the method responsible for performing the action of running the compiled methods
    Also, in line with change to ExtensionMethodToCall, update to allow for different number of parameters
    This also changes to cache the functions based on similar string as used for ExtensionMethodToCall, rather than method info, as the same method info can be used for different functions, depending on the number of provided parameters, if the method info contains optional arguments (arguments with default values)

--- BHoM_Engine/Compute/TryRunExtensionMethod.cs
+++ BHoM_Engine/Compute/TryRunExtensionMethod.cs
@@ -32,6 +32,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using BH.oM.Base.Attributes;
+using System.Collections.Concurrent;
 
 namespace BH.Engine.Base
 {
@@ -50,14 +51,7 @@ namespace BH.Engine.Base
         [Output("True if a method was found and an invocation was attempted. False otherwise.")]
         public static bool TryRunExtensionMethod(this object obj, string methodName, out object result)
         {
-            result = null;
-
-            System.Reflection.MethodInfo mi = Query.ExtensionMethodToCall(obj, methodName);
-
-            if (mi == null) return false;
-
-            result = Compute.RunExtensionMethod(obj, mi);
-            return true;
+            return TryRunExtensionMethod(methodName, new object[] { obj }, out result);
         }
 
         /***************************************************/
@@ -72,15 +66,138 @@ namespace BH.Engine.Base
         [Output("True if a method was found and an invocation was attempted. False otherwise.")]
         public static bool TryRunExtensionMethod(this object obj, string methodName, object[] parameters, out object result)
         {
-            result = null;
-
-            System.Reflection.MethodInfo mi = Query.ExtensionMethodToCall(obj, methodName, parameters);
-
-            if (mi == null) return false;
-
-            result = Compute.RunExtensionMethod(obj, mi, parameters);
-            return true;
+            return TryRunExtensionMethod(methodName, new object[] { obj }.Concat(parameters).ToArray(), out result);
         }
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+        [Description("Runs the requested method and returns the result. For performance reasons compiles the method to a function the first time it is run, then stores it for subsequent calls.")]
+        private static bool TryRunExtensionMethod(string methodName, object[] parameters, out object result)
+        {
+            if (parameters == null || parameters.Length == 0 || parameters.Any(x => x == null) || string.IsNullOrWhiteSpace(methodName))
+            {
+                result = null;
+                return false;
+            }
+
+            //Construct key used to store/extract method
+            string key = methodName + string.Join("", parameters.Select(x => x.GetType().ToString()));
+            
+            // If the method has been called before, use the previously compiled function
+            Func<object[], object> func;
+            if (FunctionPreviouslyCompiled(key))
+                func = GetStoredCompiledFunction(key);
+            else
+            {
+                MethodInfo method = Query.ExtensionMethodToCall(methodName, parameters);
+                if (method == null)
+                    func = null;
+                else
+                {
+                    //COmpiles the method to a function for quicker execution
+                    Func<object[], object> methodFunc = method.ToFunc();
+
+                    //ExtensionMethodToCall allows for returning methods that har more arguments than the provided parameters, if those have default value.
+                    //Check if the provided parameter count matches the parameters of the method
+                    ParameterInfo[] parameterInfo = method.GetParameters();
+                    if (parameterInfo.Length != parameters.Length)
+                    {
+                        //If count is different, loop through all additional arguments and find the default value
+                        List<object> defaultArgs = new List<object>();
+                        for (int i = parameters.Length; i < parameterInfo.Length; i++)
+                        {
+                            //Check that the parameter has a default value.
+                            //This should always be true, based on the logic of the ExtensionMethodToCall but additional check here for saftey
+                            if (parameterInfo[i].HasDefaultValue)   
+                            {
+                                defaultArgs.Add(parameterInfo[i].DefaultValue); //Add the argument to be used when calling the function
+                            }
+                            else
+                            {
+                                //Should never happen, but added as a guard against crash
+                                func = null;
+                                break;
+                            }
+                        }
+                        //Make a new function to store that adds the default arguments to the end of the array of parameters and then calls the method function
+                        //This function will be stored rather than the methodFunction
+                        func = x => methodFunc(x.Concat(defaultArgs).ToArray());
+                    }
+                    else
+                        func = methodFunc;  //Provided parameter count matches the parameters of the method -> set func to the compiled function of the method
+                }
+                    
+                StoreCompiledFunction(key, func);
+            }
+
+            // Try calling the method
+            try
+            {
+                if (func == null)
+                {
+                    result = null;
+                    return false;
+                }
+                else
+                {
+                    result = func(parameters);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                BH.Engine.Base.Compute.RecordError($"Failed to run {methodName} extension method.\nError: {e.Message}");
+                result = null;
+                return false;
+            }
+        }
+
+        /***************************************************/
+
+        [Description("Checks if an entry with the provided key has already been extracted. Put in its own method to simplify the use of locks to provide thread safety.")]
+        private static bool FunctionPreviouslyCompiled(string methodKey)
+        {
+            lock (m_RunExtensionMethodLock)
+            {
+                return m_CompiledFunctions.ContainsKey(methodKey);
+            }
+        }
+
+        /***************************************************/
+
+        [Description("Gets a previously compiled function from the stored functions. Put in its own method to simplify the use of locks to provide thread safety.")]
+        private static Func<object[], object> GetStoredCompiledFunction(string methodKey)
+        {
+            lock (m_RunExtensionMethodLock)
+            {
+                return m_CompiledFunctions[methodKey];
+            }
+        }
+
+        /***************************************************/
+
+        [Description("Stores a compiled function. Put in its own method to simplify the use of locks to provide thread safety.")]
+        private static void StoreCompiledFunction(string methodKey, Func<object[], object> method)
+        {
+            lock (m_RunExtensionMethodLock)
+            {
+                m_CompiledFunctions[methodKey] = method;
+            }
+        }
+
+
+        /***************************************************/
+        /**** Private fields                            ****/
+        /***************************************************/
+
+        private static ConcurrentDictionary<string, Func<object[], object>> m_CompiledFunctions = new ConcurrentDictionary<string, Func<object[], object>>();
+        //private static readonly object m_RunExtensionMethodLock = new object();
+
+        /***************************************************/
+
+
     }
 }
 
