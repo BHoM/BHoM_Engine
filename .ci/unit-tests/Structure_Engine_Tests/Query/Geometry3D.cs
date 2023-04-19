bc6e7b31348883a733b1179d14606e69fdb43b2d
Alessio Lombardi <alessio.lombardi@burohappold.com>
2023-04-19 09:19:23 +0100

    Adding unit tests demonstrating the issue

---
+++ .ci/unit-tests/Structure_Engine_Tests/Query/Geometry3D.cs
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using BH.Engine.Base;
+using NUnit.Framework;
+using AutoBogus;
+using Shouldly;
+using BH.oM.Structure.Elements;
+using BH.Engine.Base;
+
+namespace BH.Tests.Engine.Base.Query
+{
+    public class Geometry3D
+    {
+        [Test]
+        public static void BarGeometry3DNotNull()
+        {
+            Bar bar = (Bar)BH.Engine.Base.Create.RandomObject(typeof(Bar));
+            var geom = BH.Engine.Structure.Query.Geometry3D(bar);
+            geom.ShouldNotBeNull();
+        }
+
+        [Test]
+        public static void BarIGeometry3DExtensionMethodNotNull()
+        {
+            Bar bar = (Bar)BH.Engine.Base.Create.RandomObject(typeof(Bar));
+            var geom = bar.IGeometry3D();
+            geom.ShouldNotBeNull();
+        }
+    }
+}
