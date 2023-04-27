460c9232ba4e15904c1c1059f57aa914f78258a8
Isak Naslund <Isak.Naslund@burohappold.com>
2023-04-27 16:28:57 +0200

    Add test if all types implement IObject

---
+++ .ci/unit-tests/Serialiser_Engine_Tests/AllTypesIObject.cs
@@ -0,0 +1,41 @@
+﻿using BH.oM.Base;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Tests.Engine.Base.Query
+{
+    public static class AllTypesIObject
+    {
+        [Test]
+        public static void CheckAllTypes()
+        {
+            BH.Engine.Base.Compute.LoadAllAssemblies();
+            string regexFilter = $"^Revit_.*_20\\d\\d$";
+            BH.Engine.Base.Compute.LoadAllAssemblies("", regexFilter);
+
+            List<Type> allTypes = BH.Engine.Base.Query.AllTypeList().Where(x => x.Namespace.StartsWith("BH.oM") || x.Namespace.StartsWith("BH.Revit.oM")).Where(x => !x.IsEnum).ToList();
+
+            List<Type> nonIObjects = allTypes.Where(x => !typeof(IObject).IsAssignableFrom(x)).ToList();
+
+            List<string> interfaceTypes = nonIObjects.Where(x => x.IsInterface).Select(x => x.FullName).Distinct().ToList();
+            List<string> classTypes = nonIObjects.Where(x => !x.IsInterface).Select(x => x.FullName).Distinct().ToList();
+
+            Console.WriteLine("Failing interface types:");
+            foreach (string type in interfaceTypes) 
+            {
+                Console.WriteLine(type);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Failing class types:");
+            foreach (string type in classTypes)
+            {
+                Console.WriteLine(type);
+            }
+        }
+    }
+}
