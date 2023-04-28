f48698073bff3d538ed34bc3977ad4cdd0788277
Isak Naslund <Isak.Naslund@burohappold.com>
2023-04-28 08:11:49 +0200

    Slight restructuring of versioning tests and better access to log dump folder

---
+++ .ci/unit-tests/Serialiser_Engine_Tests/Helpers/TemporaryLogFolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Tests.Engine.Serialiser
+{
+    public static partial class Helpers
+    {
+        public static string TemporaryLogFolder()
+        {
+            return "C:\\Temp\\SerialiserTests";
+        }
+    }
+}
