c044af76845bb769e44725afba60d2f2bf743738
Isak Naslund <Isak.Naslund@burohappold.com>
2023-05-08 15:29:39 +0200

    Add AssignbTemplate methodology for General takeoff obejct

--- Matter_Engine/Modify/AssignTemplate.cs
+++ Matter_Engine/Modify/AssignTemplate.cs
@@ -125,6 +125,55 @@ namespace BH.Engine.Matter
         }
 
         /***************************************************/
+
+        [Description("Maps a set of materials in the MaterialCompositions to a set of provided transdiciplinary materials.\n" +
+             "First atempts to match the name of the provided materials to the transdiciplinary material maps.\n" +
+             "If no name match is found, atempts to instead find a material with as many matching MaterialProperties (based on type and name) as possible.\n" +
+             "If a unique match is found based on one of the above matching methods, all Properties from the transdiciplinary material is applied to the material to be matched.")]
+        [Input("volumetricMaterialTakeoff", "The GeneralMaterialTakeoff to Modify. Materials in the GeneralMaterialTakeoff will be evaluated based on the name and properties.")]
+        [Input("templateMaterials", "The template materials to match to and assign properties from onto the model materials. Should generally have unique names. Names of material as well as material properties will be used to map to the materials to be modified.")]
+        [Input("prioritiseTemplate", "Controls if main material or map material should be prioritised when conflicting information is found on both in terms of Density and/or Properties. If true, map is prioritised, if false, main material is prioritised.")]
+        [Input("uniquePerNamespace", "If true, the method is checking for similarity of MaterialProperties on the materials and found matching material map based on namespace. If false, this check is instead done on exact type.")]
+        [Output("volumetricMaterialTakeoffs", "MaterialComposition with Materials with modified list of properties. Materials for which no unique match could be found are unaffected.")]
+        public static GeneralMaterialTakeoff AssignTemplate(this GeneralMaterialTakeoff generalMaterialTakeoff, IEnumerable<Material> templateMaterials, bool prioritiseTemplate = true, bool uniquePerNamespace = true)
+        {
+            if (generalMaterialTakeoff == null)
+                return null;
+
+            if (templateMaterials == null || !templateMaterials.Any())
+            {
+                Base.Compute.RecordWarning($"No {nameof(templateMaterials)} provied. Unmapped {nameof(GeneralMaterialTakeoff)}s returned.");
+                return generalMaterialTakeoff;
+            }
+
+            List<Material> materials = generalMaterialTakeoff.MaterialTakeoffItems.Select(x => x.Material).AssignTemplate(templateMaterials, prioritiseTemplate, uniquePerNamespace).ToList();
+
+            List<TakeoffItem> takeoffItems = new List<TakeoffItem>();
+
+            for (int i = 0; i < materials.Count; i++)
+            { 
+                TakeoffItem unMapped = generalMaterialTakeoff.MaterialTakeoffItems[i];
+                TakeoffItem item = new TakeoffItem
+                {
+                    Material = materials[i],
+                    Volume = unMapped.Volume,
+                    Mass = unMapped.Mass,
+                    Area = unMapped.Area,
+                    Length = unMapped.Length,
+                    ElectricCurrent = unMapped.ElectricCurrent,
+                    Energy = unMapped.Energy,
+                    NumberItem = unMapped.NumberItem,
+                    Power = unMapped.Power,
+                    VolumetricFlowRate = unMapped.VolumetricFlowRate,
+                };
+                takeoffItems.Add(item);
+            }
+
+
+            return new GeneralMaterialTakeoff { MaterialTakeoffItems = takeoffItems };
+        }
+
+        /***************************************************/
     }
 }
 
