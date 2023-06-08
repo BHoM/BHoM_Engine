89a84070b8e4f1af1b328d95ef2222385aa6631f
Desai Wang <desai.wang@burohappold.com>
2023-06-08 17:08:57 -0400

    Updated URL to BHoM.xyz

--- Geometry_Engine/Query/IsVertical.cs
+++ Geometry_Engine/Query/IsVertical.cs
@@ -34,7 +34,7 @@ namespace BH.Engine.Geometry
         /***************************************************/
 
         [Description("Evaluates whether a line is vertical from its start- and endpoints projected 2d-distance")]
-        [DocumentationURL("https://github.com/BHoM/documentation/wiki/BHoM-Structural-Conventions", DocumentationType.Documentation)]
+        [DocumentationURL("https://bhom.xyz/documentation/Conventions/BHoM-Structural-Conventions/", DocumentationType.Documentation)]
         [Input("line", "The line to determine the verticality of")]
         [Output("isVertical", "If it is vertical, as defined in the wiki.")]
         public static bool IsVertical(this Line line)
