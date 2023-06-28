d0e7e817214695f6f236f212a8beac3e053fc305
Desai Wang <desai.wang@burohappold.com>
2023-06-28 09:47:49 -0400

    Update Serialiser_Engine/Query/Json.cs
    Co-authored-by: Fraser Greenroyd <fraser.greenroyd@burohappold.com>

--- Serialiser_Engine/Query/Json.cs
+++ Serialiser_Engine/Query/Json.cs
@@ -39,7 +39,6 @@ namespace BH.Engine.Serialiser
         public static bool IsValidJson(this string str)
         {
             //JSON validation based on code from https://github.com/prototypejs/prototype/blob/560bb59414fc9343ce85429b91b1e1b82fdc6812/src/prototype/lang/string.js#L699
-
             if (string.IsNullOrWhiteSpace(str))
                 return false;
             else if (str.HasTrailingCommas()) return false;
