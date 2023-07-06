8472e8a827d88dd0ebb5c2ee186f8279166d8d0a
Viet Le <vietcuong.le@burohappold.com>
2023-07-06 16:06:26 +0100

    Recreate the PR

---
+++ BHoM_Engine/Modify/SplitAndRemoveAtIndexes.cs
@@ -0,0 +1,58 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using BH.oM.Base.Attributes;
+
+namespace BH.Engine.Base
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        [Description("Removes list items at given indexes, then returns the remaining objects as sublists of consecutive items.")]
+        [Input("items", "A list of items to split at one or more indexes.")]
+        [Input("indexes", "Indexes of items to remove.")]
+        [Output("lists", "Sublists of consecutive items that remain after items at input indexes have been removed.")]
+        public static List<List<T>> SplitAndRemoveAtIndexes<T>(this List<T> items, List<int> indexes)
+        {
+            int startIndex = 0;
+            indexes.Add(items.Count);
+            List<List<T>> result = new List<List<T>>();
+
+            foreach (int i in indexes)
+            {
+                List<T> subList = items.Skip(startIndex).Take(i - startIndex).ToList();
+                result.Add(subList);
+                startIndex = i + 1;
+            }
+
+            return result;
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Geometry_Engine/Convert/ToXY.cs
@@ -0,0 +1,45 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base.Attributes;
+using BH.oM.Geometry;
+using System.ComponentModel;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        [Description("Returns the projection of a point on the XY plane. This shorthand method should run quicker than BH.Engine.Geometry.Project(this Point, Plane) in performance-sensitive applications.")]
+        [Input("pnt", "A point to project onto the XY plane.")]
+        [Output("pntOnXY", "Projection of the input point on the XY plane.")]
+        public static Point ToXY(this Point pnt)
+        {
+            return new Point { X = pnt.X, Y = pnt.Y, Z = 0 };
+        }
+
+        /***************************************************/
+    }
+}
