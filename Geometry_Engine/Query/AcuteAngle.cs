1dc948a2e546d90f33a6b84478085cad0fe35659
Viet Le <vietcuong.le@burohappold.com>
2023-07-06 16:06:26 +0100

    Recreate the PR

---
+++ Geometry_Engine/Query/AcuteAngle.cs
@@ -0,0 +1,50 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base.Attributes;
+using BH.oM.Geometry;
+using BH.oM.Quantities.Attributes;
+using System;
+using System.ComponentModel;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        [Description("Get the smallest possible angle between 2 vectors regardless of their directions.")]
+        [Input("vector1", "The first vector.")]
+        [Input("vector2", "The second vector.")]
+        [Output("angle", "The smallest possible angle between 2 vectors regardless of their directions.", typeof(Angle))]
+        public static double AcuteAngle(this Vector vector1, Vector vector2)
+        {
+            return Math.Acos(
+                Math.Abs(vector1.DotProduct(vector2))
+                / (vector1.Length() * vector2.Length()));
+        }
+
+        /***************************************************/
+    }
+}
---
+++ Reflection_Engine/Modify/CopyPropertiesByName.cs
@@ -0,0 +1,75 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Reflection;
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
+
+namespace BH.Engine.Reflection
+{
+    public static partial class Modify
+    {
+        /***************************************************/
+        /****               Public Methods              ****/
+        /***************************************************/
+
+        [Description("Copy values from parameters of a source object to parameters of the same names in the target object.")]
+        [Input("sourceObj", "The source object containing parameter values to copy from.")]
+        [Input("targetObj", "The target object containing parameter values to copy to.")]
+        public static void CopyPropertiesByName(this object sourceObj, object targetObj)
+        {
+            if (sourceObj == null || targetObj == null)
+            {
+                Base.Compute.RecordError("Can't copy properties because at least one input object is null.");
+                return;
+            }
+
+            Type targetType = targetObj.GetType();
+            Type sourceType = sourceObj.GetType();
+
+            foreach (PropertyInfo sourcePropInfo in sourceType.GetProperties())
+            {
+                if (!sourcePropInfo.CanRead)
+                    continue;
+
+                PropertyInfo targetPropInfo = targetType.GetProperty(sourcePropInfo.Name);
+                if (targetPropInfo == null)
+                    continue;
+
+                if (!targetPropInfo.CanWrite)
+                    continue;
+
+                var setMethod = targetPropInfo.GetSetMethod(true);
+                if (setMethod != null && setMethod.IsPrivate)
+                    continue;
+
+                if (!targetPropInfo.PropertyType.IsAssignableFrom(sourcePropInfo.PropertyType))
+                    continue;
+
+                targetPropInfo.SetValue(targetObj, sourcePropInfo.GetValue(sourceObj, null), null);
+            }
+        }
+
+        /***************************************************/
+    }
+}
