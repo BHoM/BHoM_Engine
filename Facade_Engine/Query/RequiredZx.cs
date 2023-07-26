6f6bfb58878c417438772b1c8a65762ae988d164
Pawel Baran <pawel.baran@burohappold.com>
2023-07-26 23:01:44 +0200

    RequiredZx method added

---
+++ Facade_Engine/Query/RequiredZx.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using BH.oM.Geometry;
+using BH.oM.Dimensional;
+using BH.oM.Facade.Elements;
+using BH.oM.Facade.SectionProperties;
+using BH.oM.Spatial.ShapeProfiles;
+using BH.oM.Analytical.Elements;
+using BH.oM.Physical.FramingProperties;
+using BH.Engine.Geometry;
+
+using System.Collections.Generic;
+using BH.oM.Base.Attributes;
+using System.ComponentModel;
+using BH.oM.Facade.Enums;
+
+namespace BH.Engine.Facade
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static double RequiredZx(SupportType supportType, double windLoad, double tributaryWidth, double length, double yieldStress, BuildingCode buildingCode)
+        {
+            if (tributaryWidth <= 0)
+            {
+                BH.Engine.Base.Compute.RecordError("Tributary width needs to be a positive number.");
+                return double.NaN;
+            }
+
+            if (length <= 0)
+            {
+                BH.Engine.Base.Compute.RecordError("Length needs to be a positive number.");
+                return double.NaN;
+            }
+
+            if (yieldStress <= 0)
+            {
+                BH.Engine.Base.Compute.RecordError("Yield stress needs to be a positive number.");
+                return double.NaN;
+            }
+
+            double linearLoad = tributaryWidth * windLoad;
+
+            double linearLoadWithFactor;
+            if (buildingCode == BuildingCode.BSEN19902002)
+                linearLoadWithFactor = linearLoad * 1.5;
+            else if (buildingCode== BuildingCode.ASCE705)
+                linearLoadWithFactor = linearLoad * 1.65;
+            else
+                linearLoadWithFactor = 0.6 * linearLoad * 1.65;
+
+            if (supportType == SupportType.PinPin)
+            {
+                double mMax = linearLoadWithFactor * Math.Pow(length, 2) / 8;
+                return mMax / yieldStress;
+            }
+            else
+            {
+                BH.Engine.Base.Compute.RecordError("Only pin-pin support type is supported in required Zx calculation.");
+                return double.NaN;
+            }
+        }
+    }
+}
+
+
