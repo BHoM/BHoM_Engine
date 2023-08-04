1799e9578ef804d8715b697e65c8c58cbb4f4a25
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-08-04 12:26:21 +0100

    Documentation updates

--- Settings_Engine/Query/GetSettings.cs
+++ Settings_Engine/Query/GetSettings.cs
@@ -11,18 +11,18 @@ namespace BH.Engine.Settings
     public static partial class Query
     {
         [Description("Obtain settings of the specified type if they exist in memory.")]
+        [Input("type", "The type of settings you want to obtain.")]
         [Output("settings", "The requested settings if they exist in memory. If they don't exist, a default is returned instead.")]
-        public static T GetSettings<T>()
+        public static object GetSettings(Type type)
         {
-            Type type = typeof(T);
             ISettings settings = null;
-            if(!Global.BHoMSettings.TryGetValue(type, out settings))
+            if (!Global.BHoMSettings.TryGetValue(type, out settings))
             {
-                BH.Engine.Base.Compute.RecordError($"Could not find settings of type {type} loaded in memory.");
-                return default(T);
+                BH.Engine.Base.Compute.RecordWarning($"Could not find settings of type {type} loaded in memory. Returning a default instance of the settings object instead.");
+                return Activator.CreateInstance(type);
             }
 
-            return (T)settings;
+            return settings;
         }
     }
 }
