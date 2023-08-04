278b7493208ba0a492b46b979911d20259a18990
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-08-04 11:16:38 +0100

    Getting settings of requested type

---
+++ Settings_Engine/Query/GetSettings.cs
@@ -0,0 +1,28 @@
+﻿using BH.oM.Base;
+using BH.oM.Base.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Management;
+using System.Text;
+
+namespace BH.Engine.Settings
+{
+    public static partial class Query
+    {
+        [Description("Obtain settings of the specified type if they exist in memory.")]
+        [Output("settings", "The requested settings if they exist in memory. If they don't exist, a default is returned instead.")]
+        public static T GetSettings<T>()
+        {
+            Type type = typeof(T);
+            ISettings settings = null;
+            if(!Global.BHoMSettings.TryGetValue(type, out settings))
+            {
+                BH.Engine.Base.Compute.RecordError($"Could not find settings of type {type} loaded in memory.");
+                return default(T);
+            }
+
+            return (T)settings;
+        }
+    }
+}
