ed3039e498aa0a94136a9c20402bdbe9d3e62e58
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-09-01 12:59:59 +0100

    Add optional panel type inputs to SetRoofPanels method as part of #2795

--- Environment_Engine/Modify/SetRoofPanels.cs
+++ Environment_Engine/Modify/SetRoofPanels.cs
@@ -42,8 +42,12 @@ namespace BH.Engine.Environment
 
         [Description("Modifies a collection of Panels and sets their type to be roof or ceiling if they are the highest panel in the space. If the panel has one connected space then it is deemed to be a roof panel, otherwise it is a ceiling panel")]
         [Input("panelsAsSpace", "A collection of Environment Panels that represent a closed space")]
+        [Input("roofType", "The panel type to define external roof panels. Defaults to 'roof'.")]
+        [Input("ceilingType", "The panel type to define internal ceiling panels, defined as panels on the horizontal tilt with 2 connected spaces. Defaults to 'ceiling'.")]
+        [Input("internalFloorType", "The panel type to define internal floors. Defaults to 'FloorInternal'.")]
         [Output("panelsAsSpace", "BHoM Environment panels representing a closed space where the roof or ceiling panels have had their type set")]
-        public static List<Panel> SetRoofPanels(this List<Panel> panelsAsSpace)
+        [PreviousVersion("6.3", "BH.Engine.Environment.Modify.SetRoofPanels(System.Collections.Generic.List<BH.oM.Environment.Elements.Panel>)")]
+        public static List<Panel> SetRoofPanels(this List<Panel> panelsAsSpace, PanelType roofType = PanelType.Roof, PanelType ceilingType = PanelType.Ceiling, PanelType internalFloorType = PanelType.FloorInternal)
         {
             List<Panel> clones = new List<Panel>(panelsAsSpace.Select(x => x.DeepClone<Panel>()).ToList());
 
@@ -64,15 +68,15 @@ namespace BH.Engine.Environment
             foreach (Panel panel in roofPanels)
             {
                 if (panel.ConnectedSpaces.Where(x => x != "-1").ToList().Count == 1)
-                    panel.Type = PanelType.Roof;
+                    panel.Type = roofType;
                 else if (panel.ConnectedSpaces.Where(x => x != "-1").ToList().Count == 2)
-                    panel.Type = PanelType.Ceiling;
+                    panel.Type = ceilingType;
             }
 
             foreach (Panel panel in roofPanels)
             {
                 if (panel.Type == PanelType.Ceiling && panel.MaximumLevel() != maxZ)
-                    panel.Type = PanelType.FloorInternal;
+                    panel.Type = internalFloorType;
             }
 
             return clones;
