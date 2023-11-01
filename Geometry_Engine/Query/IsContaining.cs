391eb1fe9ede061254ef896c30f8b4ddd0639c87
Erik Narhi <Erik.Narhi@burohappold.com>
2023-11-01 11:03:51 -0700

    IsContaining method call cleanup for point and pointLists

--- Geometry_Engine/Query/IsContaining.cs
+++ Geometry_Engine/Query/IsContaining.cs
@@ -713,32 +713,30 @@ namespace BH.Engine.Geometry
         /**** Public Methods - Closed Volume Polylines  ****/
         /***************************************************/
 
-        [Description("Checks if each of the provided points lies within a collection of polylines describing a closed volume using their primitive planes and bounds.")]
+        [Description("Checks if a point lies within a collection of polylines describing a closed volume using their primitive planes and bounds.")]
+        [Input("closedVolume", "A collection of Polylines outlining each of the faces of the closed volume to check with.")]
+        [Input("point", "The point to check to see if it is contained within the bounds of the panels.")]
+        [Input("acceptOnEdges", "Decide whether to allow the point to sit on the edge of the panel, default false.")]
+        [Input("tolerance", "Distance tolerance to use to determine intersections.")]
+        [Output("isContaining", "True if the point is contained within the bounds of the panels, false if it is not for each point provided.")]
+        public static bool IsContaining(this List<Polyline> closedVolume, Point point, bool acceptOnEdges = false, double tolerance = BH.oM.Geometry.Tolerance.Distance)
+        {
+            List<Point> pointLists = new List<Point>() { point };
+            return IsContaining(closedVolume, pointLists, acceptOnEdges, tolerance).First();
+        }
+
+        /***************************************************/
+
+        [Description("Checks if a list of points lies within a collection of polylines describing a closed volume using their primitive planes and bounds.")]
         [Input("closedVolume", "A collection of Polylines outlining each of the faces of the closed volume to check with.")]
         [Input("points", "The points to check to see if it is contained within the bounds of the panels.")]
         [Input("acceptOnEdges", "Decide whether to allow the point to sit on the edge of the panel, default false.")]
         [Input("tolerance", "Distance tolerance to use to determine intersections.")]
         [Output("isContaining", "True if the point is contained within the bounds of the panels, false if it is not for each point provided.")]
         public static List<bool> IsContaining(this List<Polyline> closedVolume, List<Point> points, bool acceptOnEdges = false, double tolerance = BH.oM.Geometry.Tolerance.Distance)
-        {
-            if (closedVolume == null)
-            {
-                BH.Engine.Base.Compute.RecordError("Cannot query if a collection of panels contains a point if the panels are null.");
-                return new List<bool>() { false };
-            }
-
-            List<Point> ctrPoints = new List<Point>();
-            List<Plane> planes = new List<Plane>();
-            foreach (Polyline be in closedVolume)
-            {
-                List<Point> srfPts = be.IControlPoints();
-                planes.Add(srfPts.FitPlane(tolerance));
-                ctrPoints.AddRange(srfPts);
-            }
-
-            BoundingBox boundingBox = BH.Engine.Geometry.Query.Bounds(ctrPoints);
-
-            return points.Select(point => IsContaining(closedVolume, planes, boundingBox, point, acceptOnEdges)).ToList();
+        {           
+            List<List<Point>> pointLists = points.Select(x => new List<Point>() { x }).ToList();
+            return IsContaining(closedVolume, pointLists, acceptOnEdges, false, tolerance);
         }
 
         /***************************************************/
