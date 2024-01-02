75fd803e6d07275b41ef75869b438150ee6e5eb7
BHoMBot <BHoMBot@burohappold.com>
2024-01-02 14:20:53 +0000

    Uptick copyright headers to 2024

--- BHoM_Engine/Modify/PropertyNamesToFullNames.cs
+++ BHoM_Engine/Modify/PropertyNamesToFullNames.cs
@@ -1,6 +1,6 @@
 /*
  * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
  *
  * Each contributor holds copyright over their respective contributions.
  * The project versioning (Git) records all such contribution source information.
@@ -223,5 +223,6 @@ namespace BH.Engine.Base
         private static Dictionary<string, HashSet<NamedNumericTolerance>> m_cachedPropertyNumericTolerances = new Dictionary<string, HashSet<NamedNumericTolerance>>();
     }
 }
+
 
 
