2cc8dee8b324747ac618cee8bfb561bf9bc01e6e
Alessio Lombardi <alessio.lombardi@burohappold.com>
2024-01-19 18:50:51 +0000

    First stub of IsValidComparisonConfig

---
+++ BHoM_Engine/Query/IsValidComparisonConfig.cs
@@ -0,0 +1,36 @@
+﻿using BH.oM.Base;
+using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BH.Engine.Base
+{
+    public static partial class Query
+    {
+        public static bool IsValidComparisonConfig(BaseComparisonConfig cc)
+        {
+            if (cc == null) { return false; }
+
+            bool isValid = true;
+
+            if (cc.UseGeometryHash) 
+            {
+                if (cc.TypeExceptions.Where(t => typeof(IGeometry).IsAssignableFrom(t)).Any())
+                    return InvalidComparisonConfig(nameof(cc.TypeExceptions));
+
+                if (cc.PropertyExceptions.Where(p => p.Contains("Geometry")).Any())
+                    return InvalidComparisonConfig(nameof(cc.TypeExceptions));
+            }
+        }
+
+        private static bool InvalidComparisonConfig(string invalidProp)
+        {
+            BH.Engine.Base.Compute.RecordError($"The comparison config is not valid because it has {nameof(BaseComparisonConfig.UseGeometryHash)} set to true while specifying also a {invalidProp} that applies to geometric types." +
+                $"If you want to specify this {invalidProp}, you need to toggle off {nameof(BaseComparisonConfig.UseGeometryHash)}.");
+
+            return false;
+        }
+    }
+}
