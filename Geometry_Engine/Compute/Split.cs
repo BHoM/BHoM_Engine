059d1ebde21f18cb89fb6ea829ca175c2335e4b1
Pawel Baran <pawel.baran@burohappold.com>
2024-01-23 15:23:08 +0100

    code made pc

--- Geometry_Engine/Compute/Split.cs
+++ Geometry_Engine/Compute/Split.cs
@@ -94,17 +94,17 @@ namespace BH.Engine.Geometry
                 }).ToList();
 
                 LineTree last = null; //The last line in the path, once we find it then we can traverse up the tree to the start
-                List<LineTree> master = new List<LineTree>(); //As we're only generating the LineTree on each iteration, we need to keep track of what we've created this iteration for traversing. We're generating the LineTrees on each iteration between the children is dependent on the directionality of the start, so cannot be preprocessed (until someone refactors this to enable such a thing!)
+                List<LineTree> main = new List<LineTree>(); //As we're only generating the LineTree on each iteration, we need to keep track of what we've created this iteration for traversing. We're generating the LineTrees on each iteration between the children is dependent on the directionality of the start, so cannot be preprocessed (until someone refactors this to enable such a thing!)
 
                 while (children.Count > 0 && last == null) //while(last == null) risks an infinite loop if we never find the last node, we should eventually run out of children though
                 {
-                    master.AddRange(children);
+                    main.AddRange(children);
                     List<LineTree> grandchildren = new List<LineTree>();
 
                     foreach (LineTree lt in children)
                     {
                         //If the search line already has a LineTree object in `master` then it means we've already looked at it in some capacity, and didn't find what we're looking for - so this is to limit the search space to narrow in on the solution
-                        List<LineTree> ltChildren = searchLines.Where(x => master.FirstOrDefault(y => y.ThisLine == x) == null && (x.Start.SquareDistance(lt.UnconnectedPoint) <= sqTol || x.End.SquareDistance(lt.UnconnectedPoint) <= sqTol)).Select(x =>
+                        List<LineTree> ltChildren = searchLines.Where(x => main.FirstOrDefault(y => y.ThisLine == x) == null && (x.Start.SquareDistance(lt.UnconnectedPoint) <= sqTol || x.End.SquareDistance(lt.UnconnectedPoint) <= sqTol)).Select(x =>
                         {
                             Point uPt = (x.Start.SquareDistance(lt.UnconnectedPoint) <= sqTol ? x.End : x.Start);
                             return new LineTree
@@ -141,7 +141,7 @@ namespace BH.Engine.Geometry
 
                 while (last.Parent != start)
                 {
-                    last = master.FirstOrDefault(x => x.ThisLine == last.Parent);
+                    last = main.FirstOrDefault(x => x.ThisLine == last.Parent);
                     outlines.Add(last.ThisLine);
                 }
                 outlines.Add(start); //Close the region
