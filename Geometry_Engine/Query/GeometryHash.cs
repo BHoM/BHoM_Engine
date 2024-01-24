9a44a7208af8543e082aabb701c03e7938196937
Alessio Lombardi <alessio.lombardi@burohappold.com>
2024-01-24 16:46:48 +0000

    GeometryHash: simplified implementation

--- Geometry_Engine/Query/GeometryHash.cs
+++ Geometry_Engine/Query/GeometryHash.cs
@@ -28,9 +28,12 @@ using BH.oM.Geometry.CoordinateSystem;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlTypes;
 using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.Xml;
+using System.Text;
 
 namespace BH.Engine.Geometry
 {
@@ -69,46 +72,25 @@ namespace BH.Engine.Geometry
                 return null;
 
             double[] hashArray = IHashArray(igeometry);
+            byte[] byteArray = GetBytes(hashArray);
 
-            double infinity = 1e12; // value compatible with the aggregation.
+            HashAlgorithm SHA256Algorithm = SHA256.Create();
+            byte[] byteHash = SHA256Algorithm.ComputeHash(byteArray);
 
-            long result = 0;
-            int j = 1;
-            for (int i = 0; i < hashArray.Length; i++)
-            {
-                double num = hashArray[i];
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteHash)
+                sb.Append(b.ToString("X2"));
 
-                if (double.IsNaN(num))
-                    num = (1 + j) * Tolerance.Distance; 
-                else if (double.IsPositiveInfinity(num))
-                    num = infinity;
-                else if (double.IsNegativeInfinity(num))
-                    num = -infinity;
-
-                result += (long)((num * (1 / Tolerance.Distance)) * (j * m_aggregationMultiplier) * (1 / m_aggregationMultiplier));
-
-                if (m_aggregationMultiplierWrapAfter < 0)
-                    j++;
-                else
-                    j = (j < m_aggregationMultiplierWrapAfter) ? (j + 1) : 1;
-            }
-
-            return result.ToString();
+            return sb.ToString();
         }
 
         /***************************************************/
-        /****  Private fields                           ****/
+        /****              Private Methods              ****/
         /***************************************************/
 
-        [Description("The hash aggregation (array summing) requires that individual numbers get multiplied for different multipliers before being summed," +
-            "in order to avoid incurring in the same aggregated hash for e.g. two points like (1,0,0) and (0,1,0).\n" +
-            "This is the multiplier used when summing the Hash Arrays to compose a single numerical hash;" +
-            "it is increased by a factor of 1 for subsequent hash array numbers.")]
-        private const double m_aggregationMultiplier = 1e-18; // this brings it just over the `long` digits limit
-
-        [Description("During the aggregation process, m_aggregationMultiplier (see its description) increases by a factor of 1 for subsequent hash array numbers, " +
-            "until m_aggregationMultiplierWrapAfter is reached, after which the factor re-starts from 0." +
-            "Defaults to -1; values < 0 disable wrapping, i.e. all aggregated numbers get multiplied by a different multiplier.")]
-        private const int m_aggregationMultiplierWrapAfter = -1;
+        private static byte[] GetBytes(this double[] values)
+        {
+            return values.SelectMany(value => BitConverter.GetBytes(value)).ToArray();
+        }
     }
 }
