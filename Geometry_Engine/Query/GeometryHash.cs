c4746f08423606da6483c80783e97e0f5819689c
Alessio Lombardi <alessio.lombardi@burohappold.com>
2024-01-24 15:20:40 +0000

    Solution with limit near `long`

--- Geometry_Engine/Query/GeometryHash.cs
+++ Geometry_Engine/Query/GeometryHash.cs
@@ -70,20 +70,22 @@ namespace BH.Engine.Geometry
 
             double[] hashArray = IHashArray(igeometry);
 
-            double result = 0;
+            double infinity = 1e12; // value compatible with the aggregation.
+
+            long result = 0;
             int j = 1;
             for (int i = 0; i < hashArray.Length; i++)
             {
                 double num = hashArray[i];
 
                 if (double.IsNaN(num))
-                    num = Tolerance.Distance;
+                    throw new ArgumentException("Cannot calculate the GeometryHash if the input geometries have a non-determined (NaN) value.");
                 else if (double.IsPositiveInfinity(num))
-                    num = double.MaxValue;
+                    num = infinity;
                 else if (double.IsNegativeInfinity(num))
-                    num = double.MinValue;
+                    num = -infinity;
 
-                result += num * (j * m_aggregationMultiplier);
+                result += (long)((num * (1 / Tolerance.Distance)) * (j * m_aggregationMultiplier) * (1 / m_aggregationMultiplier));
 
                 if (m_aggregationMultiplierWrapAfter < 0)
                     j++;
@@ -102,7 +104,7 @@ namespace BH.Engine.Geometry
             "in order to avoid incurring in the same aggregated hash for e.g. two points like (1,0,0) and (0,1,0).\n" +
             "This is the multiplier used when summing the Hash Arrays to compose a single numerical hash;" +
             "it is increased by a factor of 1 for subsequent hash array numbers.")]
-        private const double m_aggregationMultiplier = Tolerance.Distance * 1e-200;
+        private const double m_aggregationMultiplier = 1e-18; // this brings it just over the `long` digits limit
 
         [Description("During the aggregation process, m_aggregationMultiplier (see its description) increases by a factor of 1 for subsequent hash array numbers, " +
             "until m_aggregationMultiplierWrapAfter is reached, after which the factor re-starts from 0." +
