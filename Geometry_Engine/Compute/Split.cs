33b23cc7936ce1b1c8f089cfefd4eb33d035d081
Pawel Baran <pawel.baran@burohappold.com>
2024-02-02 12:18:30 +0100

    OutlinesFromLines removed from the PR

--- Geometry_Engine/Compute/Split.cs
+++ Geometry_Engine/Compute/Split.cs
@@ -30,17 +30,13 @@ namespace BH.Engine.Geometry
 {
     public static partial class Compute
     {
-        /***************************************************/
-        /****              Public Methods               ****/
-        /***************************************************/
-
         [Description("Split an outer region by the cutting lines into a collection of closed contained regions within the outer region.")]
         [Input("outerRegion", "An outer region that will contain the closed regions generated.")]
         [Input("cuttingLines", "The lines to cut the outer region by.")]
         [Input("distanceTolerance", "Tolerance to use for distance measurment operations, default to BH.oM.Geometry.Tolerance.Distance.")]
         [Input("decimalPlaces", "All coordinates of the geometry will be rounded to the number of decimal places specified. Default 6.")]
         [Output("regions", "Closed polygon regions contained within the outer region cut by the cutting lines.")]
-        public static List<Polyline> Split(this Polyline outerRegion, List<Line> cuttingLines, double distanceTolerance = Tolerance.Distance)
+        public static List<Polyline> Split(this Polyline outerRegion, List<Line> cuttingLines, double distanceTolerance = BH.oM.Geometry.Tolerance.Distance, int decimalPlaces = 6)
         {
             //Preproc the cutting lines to take only the parts inside the outer region
             cuttingLines = cuttingLines.BooleanUnion(distanceTolerance);
@@ -53,19 +49,6 @@ namespace BH.Engine.Geometry
             List<Line> splitCurves = cuttingLines.SelectMany(x => x.ISplitAtPoints(intersectingPoints)).Cast<Line>().ToList(); //Split the cutting lines at their points
             List<Line> perimeterLines = outerRegion.SubParts().SelectMany(x => x.SplitAtPoints(cuttingLines.SelectMany(y => y.LineIntersections(x)).ToList())).ToList();
 
-            return LinesToPolygons(perimeterLines, splitCurves, distanceTolerance);
-        }
-
-
-        /***************************************************/
-        /****              Private Methods              ****/
-        /***************************************************/
-
-        private static List<Polyline> LinesToPolygons(List<Line> perimeterLines, List<Line> inner, double distanceTolerance = Tolerance.Distance)
-        {
-            double sqTol = distanceTolerance * distanceTolerance;
-
-            List<Line> splitCurves = inner.ToList();
             splitCurves.AddRange(splitCurves); //Duplicate the inner curves within the list, each line will only be used twice one two adjacent regions
             splitCurves.AddRange(perimeterLines); //Add the perimeters back in as single lines
 
@@ -79,12 +62,12 @@ namespace BH.Engine.Geometry
                 List<Line> searchLines = splitCurves.Distinct().ToList(); //Each iteration will reduce the search space
                 searchLines.Remove(start); //Do not need the current line being evaluated in the search criteria
 
-                Point startPt = start.Start;
-                Point endPt = start.End;
+                Point startPt = start.Start.RoundCoordinates(decimalPlaces);
+                Point endPt = start.End.RoundCoordinates(decimalPlaces);
 
-                List<LineTree> children = searchLines.Where(x => x.Start.SquareDistance(endPt) <= sqTol || x.End.SquareDistance(endPt) <= sqTol).Select(x =>
+                List<LineTree> children = searchLines.Where(x => x.Start.RoundCoordinates(decimalPlaces) == endPt || x.End.RoundCoordinates(decimalPlaces) == endPt).Select(x =>
                 {
-                    Point uPt = (x.Start.SquareDistance(endPt) <= sqTol ? x.End : x.Start);
+                    Point uPt = (x.Start.RoundCoordinates(decimalPlaces) == endPt ? x.End.RoundCoordinates(decimalPlaces) : x.Start.RoundCoordinates(decimalPlaces));
                     return new LineTree
                     {
                         Parent = start,
@@ -94,19 +77,19 @@ namespace BH.Engine.Geometry
                 }).ToList();
 
                 LineTree last = null; //The last line in the path, once we find it then we can traverse up the tree to the start
-                List<LineTree> main = new List<LineTree>(); //As we're only generating the LineTree on each iteration, we need to keep track of what we've created this iteration for traversing. We're generating the LineTrees on each iteration between the children is dependent on the directionality of the start, so cannot be preprocessed (until someone refactors this to enable such a thing!)
+                List<LineTree> master = new List<LineTree>(); //As we're only generating the LineTree on each iteration, we need to keep track of what we've created this iteration for traversing. We're generating the LineTrees on each iteration between the children is dependent on the directionality of the start, so cannot be preprocessed (until someone refactors this to enable such a thing!)
 
                 while (children.Count > 0 && last == null) //while(last == null) risks an infinite loop if we never find the last node, we should eventually run out of children though
                 {
-                    main.AddRange(children);
+                    master.AddRange(children);
                     List<LineTree> grandchildren = new List<LineTree>();
 
                     foreach (LineTree lt in children)
                     {
                         //If the search line already has a LineTree object in `master` then it means we've already looked at it in some capacity, and didn't find what we're looking for - so this is to limit the search space to narrow in on the solution
-                        List<LineTree> ltChildren = searchLines.Where(x => main.FirstOrDefault(y => y.ThisLine == x) == null && (x.Start.SquareDistance(lt.UnconnectedPoint) <= sqTol || x.End.SquareDistance(lt.UnconnectedPoint) <= sqTol)).Select(x =>
+                        List<LineTree> ltChildren = searchLines.Where(x => master.FirstOrDefault(y => y.ThisLine == x) == null && (x.Start.RoundCoordinates(decimalPlaces) == lt.UnconnectedPoint || x.End.RoundCoordinates(decimalPlaces) == lt.UnconnectedPoint)).Select(x =>
                         {
-                            Point uPt = (x.Start.SquareDistance(lt.UnconnectedPoint) <= sqTol ? x.End : x.Start);
+                            Point uPt = (x.Start.RoundCoordinates(decimalPlaces) == lt.UnconnectedPoint ? x.End.RoundCoordinates(decimalPlaces) : x.Start.RoundCoordinates(decimalPlaces));
                             return new LineTree
                             {
                                 Parent = lt.ThisLine,
@@ -118,7 +101,7 @@ namespace BH.Engine.Geometry
                         //Take distinct only
                         ltChildren = ltChildren.GroupBy(p => p.ThisLine).Select(g => g.First()).ToList();
 
-                        LineTree foundStart = ltChildren.FirstOrDefault(x => x.ThisLine.Start.SquareDistance(startPt) <= sqTol || x.ThisLine.End.SquareDistance(startPt) <= sqTol);
+                        LineTree foundStart = ltChildren.FirstOrDefault(x => x.ThisLine.Start.RoundCoordinates(decimalPlaces) == startPt || x.ThisLine.End.RoundCoordinates(decimalPlaces) == startPt);
 
                         if (foundStart != null)
                         {
@@ -141,7 +124,7 @@ namespace BH.Engine.Geometry
 
                 while (last.Parent != start)
                 {
-                    last = main.FirstOrDefault(x => x.ThisLine == last.Parent);
+                    last = master.FirstOrDefault(x => x.ThisLine == last.Parent);
                     outlines.Add(last.ThisLine);
                 }
                 outlines.Add(start); //Close the region
@@ -169,7 +152,9 @@ namespace BH.Engine.Geometry
 
             return cutRegions;
         }
-
-        /***************************************************/
     }
 }
+
+
+
+
