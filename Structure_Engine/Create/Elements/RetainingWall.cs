d7449d8be0422372f47792013b3a121538780715
Kalle Edstroem <kalle.edstroem@burohappold.com>
2024-07-29 15:31:26 +0100

    Add create methods for retaining wall objects.

---
+++ Structure_Engine/Create/Elements/RetainingWall.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using BH.oM.Base.Attributes;
+using BH.oM.Geometry;
+using BH.oM.Spatial.Layouts;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.SurfaceProperties;
+using BH.Engine.Base;
+using BH.Engine.Geometry;
+using BH.Engine.Spatial;
+using System.Drawing.Drawing2D;
+
+
+namespace BH.Engine.Structure
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Creates a structural RetainingWall from a stem, a footing and properties.")]
+        [Input("stem", "The stem of the retaining wall.")]
+        [Input("footing", "The footing of the retaining wall.")]
+        [Input("retainedHeight", "The retained height of soil measured from the bottom of the wall Footing.")]
+        [Input("coverDepth", "The distance from top of Footing to finished floor level on the exposed face.")]
+        [Input("groundWaterDepth", "The distance from the base of the Footing to ground water level.")]
+        [Input("retentionAngle", "A property of the material being retained measured from the horizontal plane.")]
+        [Output("retainingWall", "The created RetainingWall containing the stem and footing.")]
+        public static RetainingWall RetainingWall(Stem stem, Footing footing, double retainedHeight = 0.0, double coverDepth = 0.0, double groundWaterDepth = 0.0, double retentionAngle = 0.0)
+        {
+            if (stem.IsNull())
+                return null;
+
+            if (footing.IsNull())
+                return null;
+
+            return new RetainingWall() { Stem = stem, Footing = footing, RetainedHeight = retainedHeight, CoverDepth = coverDepth, GroundWaterDepth = groundWaterDepth, RetentionAngle = retentionAngle };
+        }
+
+        /***************************************************/
+
+        [Description("Creates a structural RetainingWall from a stem and a footing.")]
+        [Input("stem", "The stem of the retaining wall.")]
+        [Input("footing", "The footing of the retaining wall.")]
+        [Output("retainingWall", "The created RetainingWall containing the stem and footing.")]
+        public static RetainingWall RetainingWall(Stem stem, Footing footing)
+        {
+            if (stem.IsNull())
+                return null;
+
+            if (footing.IsNull())
+                return null;
+
+            return RetainingWall(stem, footing);
+        }
+
+        /***************************************************/
+    }
+
+}
---
+++ Structure_Engine/Create/Elements/Stem.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.ComponentModel;
+using BH.oM.Base.Attributes;
+using BH.oM.Geometry;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.MaterialFragments;
+using BH.oM.Structure.SurfaceProperties;
+using BH.Engine.Base;
+using BH.Engine.Geometry;
+using BH.Engine.Spatial;
+
+
+namespace BH.Engine.Structure
+{
+    public static partial class Create
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Creates a Stem from an outline, thicknesses, normal and material.")]
+        [Input("outline", "The outer edge of the Stem.")]
+        [Input("thicknessTop", "Thickness at the top of the stem.")]
+        [Input("thicknessBottom", "Thickness at the bottom of the stem.")]
+        [Input("normal", "Normal to the surface of the stem denoting the direction of the retained face.")]
+        [Input("material", "Structural material of the property.")]
+        [Output("stem", "The created Stem.")]
+        public static Stem Stem(PolyCurve outline, double thicknessTop = 0, double thicknessBottom = 0, Vector normal = null, IMaterialFragment material = null)
+        {
+            return outline.IsNull() ? null : new Stem() { Outline = outline, ThicknessTop = thicknessTop, ThicknessBottom = thicknessBottom, Normal = normal, Material = material };
+        }
+
+        /***************************************************/
+
+        [Description("Creates a Stem from an outline, one thickness, normal and material.")]
+        [Input("outline", "The outer edge of the Stem.")]
+        [Input("thickness", "Thickness at the top and bottom of the stem.")]
+        [Input("normal", "Normal to the surface of the stem denoting the direction of the retained face.")]
+        [Input("material", "Structural material of the property.")]
+        [Output("stem", "The created Stem with same thickness at top and bottom.")]
+        public static Stem Stem(PolyCurve outline, double thickness = 0, Vector normal = null, IMaterialFragment material = null)
+        {
+            return outline.IsNull() ? null : new Stem() { Outline = outline, ThicknessTop = thickness, ThicknessBottom = thickness, Normal = normal, Material = material };
+        }
+
+        /***************************************************/
+
+    }
+}
