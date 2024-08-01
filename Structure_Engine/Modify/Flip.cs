28bff0acf7ae277bd8bd4b57f7b2357f2b93440a
Kalle Edstroem <kalle.edstroem@burohappold.com>
2024-08-01 14:45:26 +0100

    Update warning to be an error.
    Co-authored-by: Peter Nugent <Peter.Nugent@burohappold.com>

--- Structure_Engine/Modify/Flip.cs
+++ Structure_Engine/Modify/Flip.cs
@@ -77,6 +77,7 @@ namespace BH.Engine.Structure
         {
             if (stem.IsNull())
                 return null;
+			
             if (stem.Normal.IsNull("The Normal of the Stem is null and could not be flipped.","Flip"))
                 return null;
 
