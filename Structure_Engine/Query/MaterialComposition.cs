bfbe03b9e974db4f8c58fe929c2cdab3277a7ac8
Kalle Edstroem <kalle.edstroem@burohappold.com>
2024-08-09 16:30:22 +0100

    Update Null check
    Co-authored-by: Peter Nugent <Peter.Nugent@burohappold.com>

--- Structure_Engine/Query/MaterialComposition.cs
+++ Structure_Engine/Query/MaterialComposition.cs
@@ -436,7 +436,7 @@ namespace BH.Engine.Structure
             };
 
             //Case for when ReinforcementDensity is attached to both higher and lower level objects.
-            if ((!retainingWall.Stem.FindFragment<ReinforcementDensity>().IsNull() || !retainingWall.Footing.FindFragment<ReinforcementDensity>().IsNull()) && !retainingWall.FindFragment<ReinforcementDensity>().IsNull())
+            if ((retainingWall.Stem.FindFragment<ReinforcementDensity>() != null || retainingWall.Footing.FindFragment<ReinforcementDensity>() != null) && retainingWall.FindFragment<ReinforcementDensity>() != null)
             {
                 Base.Compute.RecordWarning("A reinforcement density fragment is found on both the retaining wall object and on at least one of its defining objects. The reinforcementdensity of the lower level parts has been used.");
                 return Matter.Compute.AggregateMaterialComposition(elements);
