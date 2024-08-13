4abedfc45e9398250e005d5a7e27160b4102356b
Peter Nugent <Peter.Nugent@burohappold.com>
2024-08-13 17:07:48 +0100

    Add method that returns a distinct list of suites on a ContaminantSample

---
+++ Ground_Engine/Query/Suites.cs
@@ -0,0 +1,109 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2024, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using BH.oM.Base.Attributes;
+using BH.oM.Data;
+using BH.oM.Geometry;
+using BH.oM.Ground;
+using BH.oM.Quantities.Attributes;
+using BH.Engine.Base;
+using BH.Engine.Data;
+using BH.Engine.Geometry;
+using BH.oM.Data.Requests;
+
+
+namespace BH.Engine.Ground
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Returns the unique number of suites from a Borehole using ContaminantReference.Reference, TestProperties.Name or Top depth.")]
+        [Input("borehole", "The Borehole from which to count the number of suites.")]
+        [Output("n", "The ContaminantReferences from the Borehole provided.")]
+        public static List<string> DistinctSuites(this Borehole borehole)
+        {
+            bool reference = true;
+            bool testName = false;
+            bool depth = false;
+
+            List<string> suites = new List<string>();
+            List<double> depths = new List<double>();
+
+            foreach(ContaminantSample contaminantSample in borehole.ContaminantSamples)
+            {
+                if (reference)
+                {
+                    List<IContaminantProperty> contaminantProperties = contaminantSample.ContaminantProperties;
+                    FilterRequest filterRequest = Data.Create.FilterRequest(typeof(ContaminantReference), "");
+                    ContaminantReference contaminantReference = Data.Compute.FilterData(filterRequest, contaminantProperties).Cast<ContaminantReference>().First();
+                    if (contaminantReference != null && contaminantReference.Reference != "")
+                        suites.Add(contaminantReference.Reference);
+                    else
+                    {
+                        reference = false;
+                        testName = true;
+                    }
+                }    
+                
+                if(testName)
+                {
+                    List<IContaminantProperty> contaminantProperties = contaminantSample.ContaminantProperties;
+                    FilterRequest filterRequest = Data.Create.FilterRequest(typeof(TestProperties), "");
+                    ContaminantReference testProperties = Data.Compute.FilterData(filterRequest, contaminantProperties).Cast<ContaminantReference>().First();
+                    if (testProperties != null && testProperties.Name != "")
+                        suites.Add(testProperties.Name);
+                    else
+                    {
+                        testName = false;
+                        depth = true;
+                    }
+                }    
+                
+                if(depth)
+                {
+                    depths.Add(contaminantSample.Top);
+                }
+                else
+                {
+                    Engine.Base.Compute.RecordError("Neither the ContaminantReference.Reference, TestProperties.Name or Top depth fields contain any data. \n" +
+                        "Therefore, the number of suites cannot be counted.");
+                }
+            }
+
+            if (depth)
+                suites = depths.Distinct().Cast<string>().ToList();
+
+            return suites;
+        }
+    }
+}
+
+
+
