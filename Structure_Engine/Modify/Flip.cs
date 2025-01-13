17ac2a8294621aca2886b029655e4081bdf34028
Peter Nugent <Peter.Nugent@burohappold.com>
2025-01-13 18:27:49 +0000

    Add method for flipping SectionProperty

--- Structure_Engine/Modify/Flip.cs
+++ Structure_Engine/Modify/Flip.cs
@@ -29,6 +29,14 @@ using System.ComponentModel;
 using BH.Engine.Base;
 using BH.oM.Structure.Constraints;
 using System;
+using BH.oM.Structure.SectionProperties;
+using BH.oM.Spatial.ShapeProfiles;
+using BH.oM.Spatial.ShapeProfiles.CellularOpenings;
+using BH.oM.Structure.MaterialFragments;
+using System.Collections.Generic;
+using System.Linq;
+using BH.Engine.Spatial;
+using System.Data;
 
 
 namespace BH.Engine.Structure
@@ -49,38 +57,41 @@ namespace BH.Engine.Structure
 
             Bar flipped = bar.ShallowClone();
 
+            // Flip Nodes
             Node tempNode = flipped.Start;
             flipped.Start = flipped.End;
             flipped.End = tempNode;
 
+            // Flip orientation angle
             if (bar.IsVertical())
-                bar.OrientationAngle = -bar.OrientationAngle + Math.PI;
+                flipped.OrientationAngle = -bar.OrientationAngle + Math.PI;
             else
-                bar.OrientationAngle = -bar.OrientationAngle;
+                flipped.OrientationAngle = -bar.OrientationAngle;
 
             // Flip Offsets
             if (bar.Offset != null)
             {
                 Vector tempV = bar.Offset.Start;
-                bar.Offset.Start = bar.Offset.End;
-                bar.Offset.End = tempV;
+                flipped.Offset.Start = bar.Offset.End;
+                flipped.Offset.End = tempV;
 
                 if (bar.Offset.Start != null)
-                    bar.Offset.Start.X *= -1;
+                    flipped.Offset.Start.X *= -1;
 
                 if (bar.Offset.End != null)
-                    bar.Offset.End.X *= -1;
+                    flipped.Offset.End.X *= -1;
 
                 if (!bar.IsVertical())
                 {
                     if (bar.Offset.Start != null)
-                        bar.Offset.Start.Y *= -1;
+                        flipped.Offset.Start.Y *= -1;
 
                     if (bar.Offset.End != null)
-                        bar.Offset.End.Y *= -1;
+                        flipped.Offset.End.Y *= -1;
                 }
             }
 
+            // Flip releases
             BarRelease flippedRelease = bar.Release?.ShallowClone();
             if (flippedRelease != null)
             {
@@ -88,6 +99,13 @@ namespace BH.Engine.Structure
                 flippedRelease.StartRelease = flippedRelease.EndRelease;
                 flippedRelease.EndRelease = tempRelease;
                 flipped.Release = flippedRelease;
+            }
+
+            // Flip section property
+            if (bar.SectionProperty != null)
+            {
+                ISectionProperty flippedSectionProperty = FlipSection(bar.SectionProperty);
+                flipped.SectionProperty = flippedSectionProperty;
             }
 
             return flipped;
@@ -116,7 +134,7 @@ namespace BH.Engine.Structure
         {
             if (stem.IsNull())
                 return null;
-            if (stem.Normal.IsNull("The Normal of the Stem is null and could not be flipped.","Flip"))
+            if (stem.Normal.IsNull("The Normal of the Stem is null and could not be flipped.", "Flip"))
                 return null;
 
             Stem flipped = stem.ShallowClone();
@@ -124,6 +142,83 @@ namespace BH.Engine.Structure
             flipped.Normal = flipped.Normal * -1;
 
             return flipped;
+        }
+
+        public static ISectionProperty FlipSection(ISectionProperty section)
+        {
+            if (section is IGeometricalSection geometricalSection)
+            {
+                ISectionProperty tempSection = null;
+                IProfile profile = geometricalSection.SectionProfile;
+                IProfile tempProfile = IFlipProfile(profile);
+                IMaterialFragment material = section.Material;
+
+                tempProfile.Name = profile.Name;
+                tempProfile.Fragments = profile.Fragments;
+                tempSection = Create.GenericSectionFromProfile(tempProfile, material, section.Name);
+                return tempSection;
+            }
+            else
+            {
+                Base.Compute.RecordError("The given shape profile is not an IGeometricalSection.");
+                return null;
+            }
+        }
+
+        private static IProfile IFlipProfile(IProfile profile)
+        {
+            if (profile is BoxProfile || profile is ICellularOpening || profile is CircleProfile || profile is FabricatedBoxProfile ||
+                    profile is FabricatedISectionProfile || profile is GeneralisedFabricatedBoxProfile || profile is ISectionProfile || profile is KiteProfile
+                    || profile is RectangleProfile || profile is TaperFlangeISectionProfile || profile is TSectionProfile ||
+                    profile is TubeProfile || profile is VoidedISectionProfile)
+            {
+                return profile;
+            }
+            else
+            {
+                return FlipProfile(profile as dynamic);
+            }
+        }
+        private static IProfile FlipProfile(AngleProfile oldProfile)
+        {
+            return Spatial.Create.AngleProfile(oldProfile.Height, oldProfile.Width, oldProfile.WebThickness, oldProfile.FlangeThickness, oldProfile.RootRadius,
+                oldProfile.ToeRadius, !oldProfile.MirrorAboutLocalZ, oldProfile.MirrorAboutLocalY);
+        }
+
+        private static IProfile FlipProfile(ChannelProfile oldProfile)
+        {
+            return Spatial.Create.ChannelProfile(oldProfile.Height, oldProfile.FlangeWidth, oldProfile.WebThickness, oldProfile.FlangeThickness,
+                oldProfile.RootRadius, oldProfile.ToeRadius, !oldProfile.MirrorAboutLocalZ);
+        }
+        private static IProfile FlipProfile(FreeFormProfile oldProfile)
+        {
+            List<ICurve> curves = oldProfile.Edges.ToList();
+            return Spatial.Create.FreeFormProfile(curves.Select(x => x.IMirror(Geometry.Create.Plane(new Point(), Vector.XAxis))));
+        }
+        private static IProfile FlipProfile(GeneralisedTSectionProfile oldProfile)
+        {
+            return Spatial.Create.GeneralisedTSectionProfile(oldProfile.Height, oldProfile.WebThickness, oldProfile.RightOutstandWidth, oldProfile.RightOutstandThickness,
+                oldProfile.LeftOutstandWidth, oldProfile.LeftOutstandThickness, oldProfile.MirrorAboutLocalY);
+        }
+        private static IProfile FlipProfile(TaperedProfile oldProfile)
+        {
+            List<IProfile> newProfiles = new List<IProfile>();
+            foreach (IProfile profile in oldProfile.Profiles.Values)
+            {
+                newProfiles.Add(IFlipProfile(profile as dynamic));
+            }
+
+            return Spatial.Create.TaperedProfile(oldProfile.Profiles.Keys.ToList(), newProfiles, oldProfile.InterpolationOrder);
+        }
+        private static IProfile FlipProfile(TaperFlangeChannelProfile oldProfile)
+        {
+            return Spatial.Create.TaperFlangeChannelProfile(oldProfile.Height, oldProfile.FlangeWidth, oldProfile.WebThickness, oldProfile.FlangeThickness, oldProfile.FlangeSlope,
+                oldProfile.RootRadius, oldProfile.ToeRadius, !oldProfile.MirrorAboutLocalZ);
+        }
+        private static IProfile FlipProfile(IProfile oldProfile)
+        {
+            Base.Compute.RecordError("The given shape profile does not have a FlipProfile method implemented.");
+            return null;
         }
     }
 }
