27a35b9802b9878497c85008ba54eba5e17b8dd7
IsakNaslundBh <isak.naslund@burohappold.com>
2025-01-24 08:55:30 +0100

    Add local copies of custom comparers for DataTable, Rows and Columns

---
+++ Diffing_Engine/Objects/CustomComparers/DataTableComparer.cs
@@ -0,0 +1,201 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2025, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using KellermanSoftware.CompareNetObjects;
+using KellermanSoftware.CompareNetObjects.TypeComparers;
+
+//Minor modification of file from https://github.com/GregFinzer/Compare-Net-Objects/blob/master/Compare-NET-Objects/TypeComparers/DataTableComparer.cs
+//Reason for inclusion in here is to allow for inclusion of these windows specific comparers in netstandard2.0
+//Link to issue: https://github.com/BHoM/BHoM_Engine/issues/3455
+//Code below under lincence as stated by https://github.com/GregFinzer/Compare-Net-Objects?tab=MS-PL-1-ov-file
+
+namespace BH.Engine.Diffing
+{
+    /// <summary>
+    /// Compare all rows in a data table
+    /// </summary>
+    public class DataTableComparer : BaseTypeComparer
+    {
+        /// <summary>
+        /// Constructor that takes a root comparer
+        /// </summary>
+        /// <param name="rootComparer"></param>
+        public DataTableComparer(RootComparer rootComparer)
+            : base(rootComparer)
+        {
+        }
+
+        /// <summary>
+        /// Returns true if both objects are of type DataTable
+        /// </summary>
+        /// <param name="type1">The type of the first object</param>
+        /// <param name="type2">The type of the second object</param>
+        /// <returns></returns>
+        public override bool IsTypeMatch(Type type1, Type type2)
+        {
+            if (type1 == null || type2 == null)
+                return false;
+
+            return type1 == typeof(DataTable) && type2 == typeof(DataTable);
+        }
+
+        /// <summary>
+        /// Compare two datatables
+        /// </summary>
+        public override void CompareType(CompareParms parms)
+        {
+            DataTable dataTable1 = parms.Object1 as DataTable;
+            DataTable dataTable2 = parms.Object2 as DataTable;
+
+            //This should never happen, null check happens one level up
+            if (dataTable1 == null || dataTable2 == null)
+                return;
+
+            //Only compare specific table names
+            if (parms.Config.MembersToInclude.Count > 0 && !parms.Config.MembersToInclude.Contains(dataTable1.TableName))
+                return;
+
+            //If we should ignore it, skip it
+            if (parms.Config.MembersToInclude.Count == 0 && parms.Config.MembersToIgnore.Contains(dataTable1.TableName))
+                return;
+
+            //There must be the same amount of rows in the datatable
+            if (dataTable1.Rows.Count != dataTable2.Rows.Count)
+            {
+                Difference difference = new Difference
+                {
+                    ParentObject1 = parms.ParentObject1,
+                    ParentObject2 = parms.ParentObject2,
+                    PropertyName = parms.BreadCrumb,
+                    Object1Value = dataTable1.Rows.Count.ToString(CultureInfo.InvariantCulture),
+                    Object2Value = dataTable2.Rows.Count.ToString(CultureInfo.InvariantCulture),
+                    ChildPropertyName = "Rows.Count",
+                    Object1 = parms.Object1,
+                    Object2 = parms.Object2
+                };
+
+                AddDifference(parms.Result, difference);
+
+                if (parms.Result.ExceededDifferences)
+                    return;
+            }
+
+            if (ColumnsDifferent(parms)) return;
+
+            CompareEachRow(parms);
+        }
+
+        private bool ColumnsDifferent(CompareParms parms)
+        {
+            DataTable dataTable1 = parms.Object1 as DataTable;
+            DataTable dataTable2 = parms.Object2 as DataTable;
+
+            if (dataTable1 == null)
+                throw new ArgumentException("parms.Object1");
+
+            if (dataTable2 == null)
+                throw new ArgumentException("parms.Object2");
+
+            if (dataTable1.Columns.Count != dataTable2.Columns.Count)
+            {
+                Difference difference = new Difference
+                {
+                    ParentObject1 = parms.ParentObject1,
+                    ParentObject2 = parms.ParentObject2,
+                    PropertyName = parms.BreadCrumb,
+                    Object1Value = dataTable1.Columns.Count.ToString(CultureInfo.InvariantCulture),
+                    Object2Value = dataTable2.Columns.Count.ToString(CultureInfo.InvariantCulture),
+                    ChildPropertyName = "Columns.Count",
+                    Object1 = parms.Object1,
+                    Object2 = parms.Object2
+                };
+
+                AddDifference(parms.Result, difference);
+
+                if (parms.Result.ExceededDifferences)
+                    return true;
+            }
+
+            foreach (var i in Enumerable.Range(0, dataTable1.Columns.Count))
+            {
+                string currentBreadCrumb = AddBreadCrumb(parms.Config, parms.BreadCrumb, "Columns", string.Empty, i);
+
+                CompareParms childParms = new CompareParms
+                {
+                    Result = parms.Result,
+                    Config = parms.Config,
+                    ParentObject1 = parms.Object1,
+                    ParentObject2 = parms.Object2,
+                    Object1 = dataTable1.Columns[i],
+                    Object2 = dataTable2.Columns[i],
+                    BreadCrumb = currentBreadCrumb
+                };
+
+                RootComparer.Compare(childParms);
+
+                if (parms.Result.ExceededDifferences)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private void CompareEachRow(CompareParms parms)
+        {
+            DataTable dataTable1 = parms.Object1 as DataTable;
+            DataTable dataTable2 = parms.Object2 as DataTable;
+
+            if (dataTable1 == null)
+                throw new ArgumentException("parms.Object1");
+
+            if (dataTable2 == null)
+                throw new ArgumentException("parms.Object2");
+
+            for (int i = 0; i < Math.Min(dataTable1.Rows.Count, dataTable2.Rows.Count); i++)
+            {
+                string currentBreadCrumb = AddBreadCrumb(parms.Config, parms.BreadCrumb, "Rows", string.Empty, i);
+
+                CompareParms childParms = new CompareParms
+                {
+                    Result = parms.Result,
+                    Config = parms.Config,
+                    ParentObject1 = parms.Object1,
+                    ParentObject2 = parms.Object2,
+                    Object1 = dataTable1.Rows[i],
+                    Object2 = dataTable2.Rows[i],
+                    BreadCrumb = currentBreadCrumb
+                };
+
+                RootComparer.Compare(childParms);
+
+                if (parms.Result.ExceededDifferences)
+                    return;
+            }
+        }
+
+
+    }
+}
