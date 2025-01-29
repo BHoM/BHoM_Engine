d4d989f5f7ff749224dcb27611f7ed22ce8d6738
Pawel Baran <pawel.baran@burohappold.com>
2025-01-29 15:12:41 +0100

    new query methods added to verification engine

---
+++ Verification_Engine/Query/IsNumberComparisonType.cs
@@ -0,0 +1,23 @@
+﻿using BH.oM.Verification;
+using System.Collections.Generic;
+
+namespace BH.Engine.Verification
+{
+    public static partial class Query
+    {
+        public static bool IsNumberComparisonType(this ValueComparisonType comparisonType)
+        {
+            return m_NumberComparisonTypes.Contains(comparisonType);
+        }
+
+        private static readonly HashSet<ValueComparisonType> m_NumberComparisonTypes = new HashSet<ValueComparisonType>
+        {
+            ValueComparisonType.EqualTo,
+            ValueComparisonType.NotEqualTo,
+            ValueComparisonType.LessThan,
+            ValueComparisonType.LessThanOrEqualTo,
+            ValueComparisonType.GreaterThan,
+            ValueComparisonType.GreaterThanOrEqualTo
+        };
+    }
+}
---
+++ Verification_Engine/Query/IsTextComparisonType.cs
@@ -0,0 +1,22 @@
+﻿using BH.oM.Verification;
+using System.Collections.Generic;
+
+namespace BH.Engine.Verification
+{
+    public static partial class Query
+    {
+        public static bool IsTextComparisonType(this ValueComparisonType comparisonType)
+        {
+            return m_TextComparisonTypes.Contains(comparisonType);
+        }
+
+        private static readonly HashSet<ValueComparisonType> m_TextComparisonTypes = new HashSet<ValueComparisonType>
+        {
+            ValueComparisonType.EqualTo,
+            ValueComparisonType.NotEqualTo,
+            ValueComparisonType.Contains,
+            ValueComparisonType.StartsWith,
+            ValueComparisonType.EndsWith
+        };
+    }
+}
