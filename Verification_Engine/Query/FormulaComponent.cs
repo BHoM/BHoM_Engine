c4b06aa4b48e6f9b1744d0af5e25e19e4eb60d7c
Pawel Baran <pawel.baran@burohappold.com>
2025-03-21 14:26:40 +0100

    verification mechanism further partitioned to enable more customisation

---
+++ Verification_Engine/Query/FormulaComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BH.Engine.Verification
+{
+    public static partial class Query
+    {
+        public static object IFormulaComponent(this object obj)
+        {
+            if (obj == null)
+                return null;
+
+            return BH.Engine.Base.Compute.RunExtensionMethod(obj, nameof(FormulaComponent));
+        }
+
+        public static object FormulaComponent(this object obj)
+        {
+            return obj;
+        }
+    }
+}
---
+++ Verification_Engine/Query/HasValue.cs
@@ -0,0 +1,37 @@
+﻿using BH.Engine.Base;
+
+namespace BH.Engine.Verification
+{
+    public static partial class Query
+    {
+        public static bool? IHasValue(this object obj)
+        {
+            if (obj == null)
+                return false;
+
+            object result;
+            if (!BH.Engine.Base.Compute.TryRunExtensionMethod(obj, nameof(HasValue), out result))
+            {
+                BH.Engine.Base.Compute.RecordError($"Can't check if object has value because type {obj.GetType().Name} is currently not supported.");
+                return null;
+            }
+
+            return (bool?)result;
+        }
+
+        public static bool? HasValue(this double obj)
+        {
+            return !double.IsNaN(obj);
+        }
+
+        public static bool? HasValue(this string obj)
+        {
+            return !string.IsNullOrEmpty(obj);
+        }
+
+        public static bool? HasValue(this object obj)
+        {
+            return obj != null;
+        }
+    }
+}
