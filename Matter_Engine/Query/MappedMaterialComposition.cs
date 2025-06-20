f5b6ff75803461fb52ebd8ca04b1a1abbf6566bc
Peter Nugent <Peter.Nugent@BuroHappold.com>
2025-06-20 09:22:48 +0100

    Matter_Engine: Fix XML documentation spelling and grammar errors
    - Fix 'transdiciplinary' to 'transdisciplinary' in MappedGeneralMaterialTakeoff.cs
    - Fix 'transdiciplinary' to 'transdisciplinary' in MappedMaterialComposition.cs
    - Fix 'transdiciplinary' to 'transdisciplinary' in MappedVolumetricMaterialTakeoff.cs

--- Matter_Engine/Query/MappedMaterialComposition.cs
+++ Matter_Engine/Query/MappedMaterialComposition.cs
@@ -38,16 +38,16 @@ namespace BH.Engine.Matter
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Maps a set of materials in the MaterialCompositions of the provided elements to a set of provided transdiciplinary materials.\n" +
-                     "First atempts to match the name of the provided materials to the transdiciplinary material maps.\n" +
-                     "If no name match is found, atempts to instead find a material with as many matching MaterialProperties (based on type and name) as possible.\n" +
-                     "If a unique match is found based on one of the above matching methods, all Properties from the transdiciplinary material is applied to the material to be matched.")]
+        [Description("Maps a set of materials in the MaterialCompositions of the provided elements to a set of provided transdisciplinary materials.\n" +
+                     "First attempts to match the name of the provided materials to the transdisciplinary material maps.\n" +
+                     "If no name match is found, attempts to instead find a material with as many matching MaterialProperties (based on type and name) as possible.\n" +
+                     "If a unique match is found based on one of the above matching methods, all Properties from the transdisciplinary material are applied to the material to be matched.")]
         [Input("element", "The elements to fetch MaterialComposition from.")]
         [Input("templateMaterials", "The template materials to match to and assign properties from onto the model materials. Should generally have unique names. Names of material as well as material properties will be used to map to the materials to be modified.")]
-        [Input("checkForTakeoffFragment", "If true and the provided element is a BHoMObject, the incoming item is checked if it has a VolumetricMaterialTakeoff fragment attached, and if so, returns that Material composition corresponding to this fragment. If false, the MaterialComposition returned will be calculated, independant of fragment attached.")]
+        [Input("checkForTakeoffFragment", "If true and the provided element is a BHoMObject, the incoming item is checked if it has a VolumetricMaterialTakeoff fragment attached, and if so, returns that Material composition corresponding to this fragment. If false, the MaterialComposition returned will be calculated, independent of fragment attached.")]
         [Input("prioritiseTemplate", "Controls if main material or map material should be prioritised when conflicting information is found on both in terms of Density and/or Properties. If true, map is prioritised, if false, main material is prioritised.")]
         [Input("uniquePerNamespace", "If true, the method is checking for similarity of MaterialProperties on the materials of the element and found matching material map based on namespace. If false, this check is instead done on exact type.")]
-        [Output("materialComposition", "The material compositions for each element with materials mapped to the provided transdiciplinary materials.")]
+        [Output("materialComposition", "The MaterialComposition for each element with materials mapped to the provided transdisciplinary materials.")]
         public static MaterialComposition MappedMaterialComposition(this IElementM element, IEnumerable<Material> templateMaterials, bool checkForTakeoffFragment = true, bool prioritiseTemplate = true, bool uniquePerNamespace = true)
         {
             if (element == null)
