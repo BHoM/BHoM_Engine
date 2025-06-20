86c863d25be321c630931c6e42d55bf064c15918
Peter Nugent <Peter.Nugent@BuroHappold.com>
2025-06-20 09:22:41 +0100

    Library_Engine: Fix XML documentation spelling and grammar errors
    - Fix 'atempts' to 'attempts' in ValidatePath.cs

--- Library_Engine/Query/ValidatePath.cs
+++ Library_Engine/Query/ValidatePath.cs
@@ -36,7 +36,7 @@ namespace BH.Engine.Library
         /**** Public Methods                            ****/
         /***************************************************/
 
-        [Description("Validates that the provided string is a valid full library path, and atempts to upgrade the path if it is not. Returns the input path if valid, or failing to upgrade. Returns the upgraded path if an upgrade is possible.")]
+        [Description("Validates that the provided string is a valid full library path, and attempts to upgrade the path if it is not. Returns the input path if valid, or failing to upgrade. Returns the upgraded path if an upgrade is possible.")]
         [Input("fullLibraryName", "The full library path to the particular Library to validate. Only full paths supported, not super paths or partial paths to libraries.")]
         [Output("path", "Returns the input path if valid or failing to upgrade. Returns the upgraded path if able to upgrade.")]
         public static string ValidatePath(this string fullLibraryName, string versionFrom = "")
