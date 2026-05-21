3b4ec914c6005c9e6033647d982f82f14ab8e76b
wojciech buda <wojciech.buda@burohappold.com>
2026-05-21 18:09:37 +0200

    IsRectangle and LongestEdgeDirection addded

---
+++ Geometry_Engine/Query/LongestEdgeDirection.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2026, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base.Attributes;
+using BH.oM.Geometry;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace BH.Engine.Geometry
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        [Description("Returns the direction of the longest edge of a polyline.")]
+        [Input("polyline", "Polyline whose edge directions are evaluated.")]
+        [Input("tolerance", "Minimum edge length.")]
+        [Input("angleTolerance", "Angular tolerance used when comparing edge directions for parallelism.")]
+        [Output("direction", "Direction vector of the longest edge.")]
+
+        public static Vector LongestEdgeDirection(this Polyline polyline, double tolerance, double angleTolerance = Tolerance.Angle)
+        {
+            List<Line> edges = polyline.SubParts().Where(x => x != null && x.Length() > tolerance).ToList();
+            Line longest = edges.OrderByDescending(x => x.Length()).First();
+            Vector longestDir = longest.Direction();
+            longestDir.Z = 0;
+
+            Dictionary<Vector, double> dirLen = new Dictionary<Vector, double>();
+            foreach (Line edge in edges)
+            {
+                Vector direction = edge.Direction();
+                direction.Z = 0;
+
+                Vector dir = dirLen.Keys.FirstOrDefault(x => x.IsParallel(direction, angleTolerance) != 0);
+                if (dir != null)
+                    dirLen[dir] += edge.Length();
+                else
+                    dirLen[direction] = edge.Length();
+            }
+
+            return longestDir;
+        }
+    }
+}
